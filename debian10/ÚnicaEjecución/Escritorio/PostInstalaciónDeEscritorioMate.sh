a41bd6c6635ba7e3e24e83f86556d97c81012568
NiPeGun <nipegun@gmail.com>
2019-09-25 09:08:48 +0200

    Update PostInstalaciónDeEscritorioMate.sh

--- debian10/ÚnicaEjecución/Escritorio/PostInstalaciónDeEscritorioMate.sh
+++ debian10/ÚnicaEjecución/Escritorio/PostInstalaciónDeEscritorioMate.sh
@@ -10,8 +10,7 @@ ColorVerde='\033[1;32m'
 FinColor='\033[0m'
 
 apt-get -y install thunderbird unrar
-apt-get -y install libreoffice-l10n-es firefox-esr-l10n-es-es thunderbird-l10n-es-es lightning-l10n-es-es
-apt-get -y install filezilla vlc clamav clamtk audacity obs-studio openshot htop remmina etherape
+apt-get -y install filezilla vlc clamav clamtk audacity obs-studio openshot htop remmina etherape virt-viewer
 apt-get -y install firefox-esr-l10n-es-es libreoffice-l10n-es thunderbird-l10n-es-es lightning-l10n-es-es
 apt-get -y remove xterm reportbug blender imagemagick inkscape gnome-disk-utility
 apt-get -y autoremove
