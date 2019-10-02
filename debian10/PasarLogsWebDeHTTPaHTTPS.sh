70358caf556f48b05d2a688888f17274f65e93c3
NiPeGun <nipegun@gmail.com>
2019-10-02 16:34:46 +0200

    Create PasarLogsWebDeHTTPaHTTPS.sh

---
+++ debian10/PasarLogsWebDeHTTPaHTTPS.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#----------------------------------------------------------
+#  Script de NiPeGun para pasar logs webs de http a https
+#----------------------------------------------------------
+
+CantArgsEsperados=3
+ArgsInsuficientes=65
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+if [ $# -ne $CantArgsEsperados ]
+  then
+    echo ""
+    echo "------------------------------------------------------------------------------"
+    echo -e "${ColorRojo}Mal uso del script.${FinColor} El uso correcto sería:"
+    echo ""
+    echo -e "$0 ${ColorVerde}[DominioyExtensiónAReemplazar]${FinColor}"
+    echo ""
+    echo "Ejemplo:"
+    echo " $0 nipegun.com"
+    echo "------------------------------------------------------------------------------"
+    echo ""
+    exit $ArgsInsuficientes
+  else
+    find /var/www/$1/logs/ -type f -exec sed -i -e "s|http://$1|https://$2|g" {} \;
+fi
+
