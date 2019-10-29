6b983476f8988f17af3f5750894f1ace691fac5c
NiPeGun <nipegun@gmail.com>
2019-10-29 22:13:30 +0100

    Update PostInstalaciónDeEscritorioMate.sh

--- debian10/ÚnicaEjecución/Escritorio/PostInstalaciónDeEscritorioMate.sh
+++ debian10/ÚnicaEjecución/Escritorio/PostInstalaciónDeEscritorioMate.sh
@@ -9,9 +9,25 @@ ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
 FinColor='\033[0m'
 
-apt-get -y install thunderbird unrar
-apt-get -y install filezilla vlc clamav clamtk audacity obs-studio openshot htop remmina etherape virt-viewer
-apt-get -y install firefox-esr-l10n-es-es libreoffice-l10n-es thunderbird-l10n-es-es lightning-l10n-es-es
+apt-get -y install caja-open-terminal
+apt-get -y install firefox-esr-l10n-es-es
+apt-get -y install libreoffice-l10n-es
+apt-get -y install thunderbird
+apt-get -y install thunderbird-l10n-es-es
+apt-get -y install lightning-l10n-es-es
+apt-get -y install filezilla
+apt-get -y install vlc
+apt-get -y install virt-viewer
+apt-get -y install unrar
+apt-get -y install clamav
+apt-get -y install clamtk
+apt-get -y install audacity
+apt-get -y install obs-studio
+apt-get -y install openshot
+apt-get -y install htop
+apt-get -y install remmina
+apt-get -y install etherape
+
 apt-get -y remove xterm reportbug blender imagemagick inkscape gnome-disk-utility
 apt-get -y autoremove
 
