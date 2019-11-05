b1fdbefe5852099dae11e767cbde0ef02c39b238
NiPeGun <nipegun@gmail.com>
2019-11-05 21:48:52 +0100

    Create Fail2Ban-InstalarYConfigurar.sh

---
+++ debian09/ÚnicaEjecución/Fail2Ban-InstalarYConfigurar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#---------------------------------------------------------
+#  Script de NiPeGun para instalar y configurar Fail2Ban
+#---------------------------------------------------------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+echo ""
+echo -e "${ColorVerde}Instalando Fail2Ban...${FinColor}"
+apt-get -y install fail2ban
