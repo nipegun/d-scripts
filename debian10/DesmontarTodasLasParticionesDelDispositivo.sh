fabf27209be3de4d4becc17f20f03f7555b7e03f
NiPeGun <nipegun@gmail.com>
2019-11-27 09:26:15 +0100

    Create DesmontarTodasLasParticionesDelDispositivo.sh

---
+++ debian10/DesmontarTodasLasParticionesDelDispositivo.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#--------------------------------------------------------------------------------------
+#  Script de NiPeGun para desmontar de una sola vez todas las particiones de un disco
+#--------------------------------------------------------------------------------------
+
+CantArgsEsperados=3
+ArgsInsuficientes=65
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+if [ $# -ne $CantArgsEsperados ]
+  then
+    echo ""
+    echo "------------------------------------------------------------------------------"
+    echo -e "${ColorRojo}Mal uso del script.${FinColor} El uso correcto sería:"
+    echo ""
+    echo -e "$0 ${ColorVerde}[Dispositivo]${FinColor}"
+    echo ""
+    echo "Ejemplo:"
+    echo " $0 /dev/sdf"
+    echo "------------------------------------------------------------------------------"
+    echo ""
+    exit $ArgsInsuficientes
+  else
+    echo ""
+    echo "  Desmontando todas las particiones del dispositivo $1"
+    echo ""
+    umount $1?*
+    echo ""
+fi
