b157c94f871dd79a0d8faac07f331ec50403328d
NiPeGun <nipegun@gmail.com>
2020-03-23 15:16:34 +0100

    Create Jitsi-Desinstalar.sh

---
+++ debian10/ÚnicaEjecución/Jitsi-Desinstalar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-------------------------------------------------------------------
+#  Script de NiPeGun para instalar y configurar Jitsi en Debian 10
+#-------------------------------------------------------------------
+
+apt-get -y remove jitsi-meet
+apt-get -y purge jitsi*
+apt-get -y autoremove
+rm /etc/jitsi -R
+rm /etc/prosody -R
+
