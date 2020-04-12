6d250af3f9d33dc35deede1a94948c3501b93c49
NiPeGun <nipegun@gmail.com>
2020-04-12 18:43:52 +0200

    Delete BuscarArchivosConAtributosExtendidos.sh

--- macOS/BuscarArchivosConAtributosExtendidos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Pongo a disposición pública este script bajo el término de "software de dominio público".
-# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
-# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
-# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
-
-find $1 -xattr -exec xattr -v {} \;
-
