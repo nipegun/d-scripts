29bead681d0544fbfc89dddadc2c5c60c5516a3d
NiPeGun <nipegun@gmail.com>
2020-04-16 02:01:36 +0200

    Update CrearAliasParaLosDScripts.sh

--- CrearAliasParaLosDScripts.sh
+++ CrearAliasParaLosDScripts.sh
@@ -5,9 +5,9 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------
-#  Script de NiPeGun para crear los alias de los nipe-scripts para Debian 9
-#----------------------------------------------------------------------------
+#-----------------------------------------------------------
+#  Script de NiPeGun para crear los alias de los d-scripts 
+#-----------------------------------------------------------
 
 ColorVerde="\033[1;32m"
 FinColor="\033[0m"
