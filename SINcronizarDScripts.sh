c014716120943d51991e73a80768e03dfe28025a
NiPeGun <nipegun@gmail.com>
2020-04-16 02:03:48 +0200

    Update SINcronizarDScripts.sh

--- SINcronizarDScripts.sh
+++ SINcronizarDScripts.sh
@@ -5,15 +5,15 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------------------
+#----------------------------------------------------
 #  Script de NiPeGun para sincronizar los d-scripts
-#-------------------------------------------------------
+#----------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
 FinColor='\033[0m'
 
-# Comprobar si hay conexión a Internet antes de sincronizar los nipe-scripts
+# Comprobar si hay conexión a Internet antes de sincronizar los d-scripts
 wget -q --tries=10 --timeout=20 --spider https://github.com
   if [[ $? -eq 0 ]]; then
     echo ""
