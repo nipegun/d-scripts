06eafb44cb236225db8481acb0d44e90962273fa
NiPeGun <nipegun@gmail.com>
2020-04-21 16:24:16 +0200

    Update and rename ÚnicaEjecución/debian08/ServidorPVPGN-InstalarYConfigurar.sh to ÚnicaEjecución/ServidorPvPGN-InstalarYConfigurar.sh

--- ÚnicaEjecución/ServidorPvPGN-InstalarYConfigurar.sh
+++ ÚnicaEjecución/ServidorPvPGN-InstalarYConfigurar.sh
@@ -6,13 +6,11 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 #------------------------------------------------------------------
-#  SCRIPT DE NIPEGUN PARA INSTALAR Y CONFIGURAR EL SERVIDOR PVPGN
+#  Script de NiPeGun para instalar y configurar el servidor PvPGN
 #------------------------------------------------------------------
 
 echo ""
-echo "-----------------------------------"
-echo "  INSTALANDO Y CONFIGURANDO pvpgn"
-echo "-----------------------------------"
+echo "  Instalando y configurando PvPGN..."
 echo ""
 apt-get -y install pvpgn wget
 wget http://downloads.sourceforge.net/project/pvpgn.berlios/pvpgn-support-1.3.tar.gz -P /tmp/
