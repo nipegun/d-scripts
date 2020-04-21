5319df378b9d82e946d950db02285e63ee1b00dd
NiPeGun <nipegun@gmail.com>
2020-04-21 16:41:09 +0200

    Update ServidorGamingCoD4Server-InstalarYConfigurar.sh

--- ÚnicaEjecución/ServidorGamingCoD4Server-InstalarYConfigurar.sh
+++ ÚnicaEjecución/ServidorGamingCoD4Server-InstalarYConfigurar.sh
@@ -1,5 +1,25 @@
+#!/bin/bash
 
-# Dependencias
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#------------------------------------------------------------------------
+#  Script de NiPeGun para instalar el servidor gaming de Call of Duty 4
+#------------------------------------------------------------------------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+echo ""
+echo -e "${ColorVerde}Instalando el servidor cod4server...${FinColor}"
+echo ""
+
+echo ""
+echo "  Instalando dependencias..."
+echo ""
 dpkg --add-architecture i386
 apt-get -y update
 apt-get -y install mailutils
@@ -34,10 +54,7 @@ apt-get -y install steamcmd
 
 
 # Cómo administrarlo
-
-# Todos los comandos de aquí abajo se pueden ver ejecutando ./cod4server
-
-
+#
 #./cod4server start
 #./cod4server stop
 #./cod4server restart
