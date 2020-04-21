455c6ce723ba95036ec63c633015cc9535850883
NiPeGun <nipegun@gmail.com>
2020-04-21 16:33:06 +0200

    Create ServidorGamingCoD4Server-InstalarYConfigurar.sh

---
+++ ÚnicaEjecución/ServidorGamingCoD4Server-InstalarYConfigurar.sh
@@ -0,0 +1,34 @@
+
+# Dependencias
+dpkg --add-architecture i386
+apt-get -y update
+apt-get -y install mailutils
+apt-get -y install postfix
+apt-get -y install curl
+apt-get -y install wget
+apt-get -y install file
+apt-get -y install tar
+apt-get -y install bzip2
+apt-get -y install gzip
+apt-get -y install unzip
+apt-get -y install bsdmainutils
+apt-get -y install python
+apt-get -y install util-linux
+apt-get -y install ca-certificates
+apt-get -y install binutils
+apt-get -y install bc
+apt-get -y install jq
+apt-get -y install tmux
+apt-get -y install lib32gcc1
+apt-get -y install libstdc++6
+apt-get -y install lib32stdc++6
+apt-get -y install steamcmd
+
+# A partir de aquí, ejecución manual
+# adduser cod4server
+# su - cod4server
+# wget -O linuxgsm.sh https://linuxgsm.sh
+# chmod +x linuxgsm.sh
+# bash linuxgsm.sh cod4server
+# ./cod4server install
+
