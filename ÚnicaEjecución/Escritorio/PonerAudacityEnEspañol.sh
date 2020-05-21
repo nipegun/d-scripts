c5a6944e3e765b8ab94c7e989c77ec4b63def210
NiPeGun <nipegun@gmail.com>
2020-05-21 08:52:22 +0200

    Create PonerAudacityEnEspañol.sh

---
+++ ÚnicaEjecución/Escritorio/PonerAudacityEnEspañol.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#----------------------------------------------------
+#  Script de NiPeGun para poner Audacity en español
+#----------------------------------------------------
+
+sed -i -e 's|Language=|Language=es|g' ~/.audacity-data/audacity.cfg
+
