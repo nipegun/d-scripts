6fced87a3ababfe760f16e85d8e8e0fa215ace8d
NiPeGun <nipegun@gmail.com>
2020-05-28 16:18:46 +0200

    Create RepararPartición.sh

---
+++ RepararPartición.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#----------------------------------------------------
+#  Script de NiPeGun para reparar un disco de Deban
+#----------------------------------------------------
+
+CantArgsEsperados=1
+ArgsInsuficientes=65
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+if [ $# -ne $CantArgsEsperados ]
+  then
+    echo ""
+    echo "------------------------------------------------------------------------------"
+    echo -e "${ColorRojo}Mal uso del script.${FinColor} El uso correcto sería:"
+    echo ""
+    echo -e "$0 ${ColorVerde}[Partición]${FinColor}"
+    echo ""
+    echo "Ejemplo:"
+    echo " $0 /dev/sdd1"
+    echo "------------------------------------------------------------------------------"
+    echo ""
+    exit $ArgsInsuficientes
+  else
+    fsck -p -f -v $1
+    echo ""
+fi
+
