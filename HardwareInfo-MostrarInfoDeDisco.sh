2ef87747d1304178a920636419aec8370521afbb
NiPeGun <nipegun@gmail.com>
2020-06-03 06:29:52 +0200

    Update HardwareInfo-MostrarInfoDeDisco.sh

--- HardwareInfo-MostrarInfoDeDisco.sh
+++ HardwareInfo-MostrarInfoDeDisco.sh
@@ -9,18 +9,7 @@
 #  Script de NiPeGun para mostrar información sobre un disco dado 
 #------------------------------------------------------------------
 
-#!/bin/bash
-
-# Pongo a disposición pública este script bajo el término de "software de dominio público".
-# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
-# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
-# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
-
-#--------------------------------------------------------
-#  Script de NiPeGun para bloquear un módulo específico
-#--------------------------------------------------------
-
-CantArgsEsperados=3
+CantArgsEsperados=1
 ArgsInsuficientes=65
 
 ColorRojo='\033[1;31m'
