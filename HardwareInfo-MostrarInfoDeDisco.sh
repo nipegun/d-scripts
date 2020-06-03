f0e3fbdc16ee6fd22d24bfcfc715b4617b3651f4
NiPeGun <nipegun@gmail.com>
2020-06-03 06:28:05 +0200

    Create HardwareInfo-MostrarInfoDeDisco.sh

---
+++ HardwareInfo-MostrarInfoDeDisco.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#------------------------------------------------------------------
+#  Script de NiPeGun para mostrar información sobre un disco dado 
+#------------------------------------------------------------------
+
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#--------------------------------------------------------
+#  Script de NiPeGun para bloquear un módulo específico
+#--------------------------------------------------------
+
+CantArgsEsperados=3
+ArgsInsuficientes=65
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+if [ $# -ne $CantArgsEsperados ]
+  then
+    echo ""
+    echo "------------------------------------------------------------------------------"
+    echo -e "${ColorRojo}Mal uso del script.${FinColor} El uso correcto sería:"
+    echo ""
+    echo -e "$0 ${ColorVerde}[Dispositivo]${FinColor}"
+    echo ""
+    echo "Ejemplo:"
+    echo " $0 /dev/sdc"
+    echo "------------------------------------------------------------------------------"
+    echo ""
+    exit $ArgsInsuficientes
+  else
+    echo ""
+    hdparm -I $1
+    echo ""
+fi
+
