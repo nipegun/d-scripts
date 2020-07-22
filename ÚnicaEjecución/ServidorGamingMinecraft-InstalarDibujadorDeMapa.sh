d2b51bf4e7fdce3c0337b3db878f9117964ba3c3
NiPeGun <nipegun@gmail.com>
2020-07-22 21:06:49 +0200

    Update ServidorGamingMinecraft-InstalarDibujadorDeMapa.sh

--- ÚnicaEjecución/ServidorGamingMinecraft-InstalarDibujadorDeMapa.sh
+++ ÚnicaEjecución/ServidorGamingMinecraft-InstalarDibujadorDeMapa.sh
@@ -26,6 +26,6 @@ cd Minecraft-Overviewer
 python3 setup.py build
 ln -s /root/git/Minecraft-Overviewer/overviewer.py /usr/local/bin/mcoverviewer
 
-ServerVersion=1.15
+ServerVersion=1.16.1
 mkdir -p /root/.minecraft/versions/${ServerVersion}/
 wget https://overviewer.org/textures/${ServerVersion} -O /root/.minecraft/versions/${ServerVersion}/${ServerVersion}.jar
