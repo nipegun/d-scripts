8009843feb22884314855c082393c263f5e5512b
NiPeGun <nipegun@gmail.com>
2020-09-18 04:08:44 +0200

    Update Fail2Ban-InstalarYConfigurar.sh

--- ÚnicaEjecución/debian09/Fail2Ban-InstalarYConfigurar.sh
+++ ÚnicaEjecución/debian09/Fail2Ban-InstalarYConfigurar.sh
@@ -16,20 +16,17 @@ FinColor='\033[0m'
 echo ""
 echo -e "${ColorVerde}Instalando Fail2Ban...${FinColor}"
 apt-get -y install fail2ban sendmail
-service fail2ban stop
-echo "[sshd]"                       > /etc/fail2ban/jail.d/defaults-debian.conf
-echo "enabled = true"              >> /etc/fail2ban/jail.d/defaults-debian.conf
 echo "ignoreip = 127.0.0.1"        >> /etc/fail2ban/jail.d/defaults-debian.conf
 echo "maxretry = 3"                >> /etc/fail2ban/jail.d/defaults-debian.conf
 echo "bantime = 10m"               >> /etc/fail2ban/jail.d/defaults-debian.conf
 echo "findtime = 5m"               >> /etc/fail2ban/jail.d/defaults-debian.conf
 echo ""                            >> /etc/fail2ban/jail.d/defaults-debian.conf
-echo "#Mail"                       >> /etc/fail2ban/jail.d/defaults-debian.conf
+echo "# Mail"                      >> /etc/fail2ban/jail.d/defaults-debian.conf
 echo "#destemail = mail@gmail.com" >> /etc/fail2ban/jail.d/defaults-debian.conf
 echo "#sender = mail@gmail.com"    >> /etc/fail2ban/jail.d/defaults-debian.conf
 echo "#sendername = Fail2Ban"      >> /etc/fail2ban/jail.d/defaults-debian.conf
 echo "#mta = sendmail"             >> /etc/fail2ban/jail.d/defaults-debian.conf
 echo "#action = %(action_mwl)s"    >> /etc/fail2ban/jail.d/defaults-debian.conf
-service fail2ban start
+service fail2ban restart
 # tail -f /var/log/fail2ban.log
 
