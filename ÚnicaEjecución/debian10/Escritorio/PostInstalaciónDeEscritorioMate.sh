aaf02a80e4321e4674adac12acd9b4745e9349e7
NiPeGun <nipegun@gmail.com>
2020-10-29 17:57:12 +0100

    Update PostInstalaciónDeEscritorioMate.sh

--- ÚnicaEjecución/debian10/Escritorio/PostInstalaciónDeEscritorioMate.sh
+++ ÚnicaEjecución/debian10/Escritorio/PostInstalaciónDeEscritorioMate.sh
@@ -12,6 +12,7 @@ FinColor='\033[0m'
 # Sistema
 apt-get -y install caja-open-terminal
 apt-get -y install caja-admin
+
 # Multimedia
 apt-get -y install vlc
 apt-get -y install vlc-plugin-vlsub
@@ -19,12 +20,14 @@ apt-get -y install audacity
 apt-get -y install openshot
 apt-get -y install subtitleeditor
 apt-get -y install easytag
+
 # Redes
 apt-get -y install wireshark
 apt-get -y install virt-viewer
 apt-get -y install remmina
 apt-get -y install etherape
 apt-get -y install sshpass
+
 # Juegos
 apt-get -y install scid
 apt-get -y install scid-rating-data
@@ -32,6 +35,7 @@ apt-get -y install scid-spell-data
 apt-get -y install stockfish
 apt-get -y install dosbox
 apt-get -y install scummvm
+
 # Internet
 apt-get -y install firefox-esr-l10n-es-es
 apt-get -y install thunderbird
@@ -44,11 +48,14 @@ apt-get -y install obs-studio
 apt-get -y install chromium
 apt-get -y install chromium-l10n 
 apt-get -y install filezilla
+
 # Programación
 apt-get -y install ghex
+
 # Seguridad
 apt-get -y install clamav
 apt-get -y install clamtk
+
 # Otros
 apt-get -y install libreoffice-l10n-es
 apt-get -y install unrar
@@ -57,6 +64,7 @@ apt-get -y install simple-scan
 apt-get -y install android-tools-adb # Para poder operar con el contenido de los móviles y relojes android
 apt-get -y install android-tools-fastboot
 apt-get -y install pyrenamer # Hay que agregar el repositorio de stretch antes, o instalar gprename, como reemplazo
+apt-get -y install comix
 
 /root/scripts/d-scripts/ÚnicaEjecución/debian10/Escritorio/TorBrowser-Instalar.sh
 
