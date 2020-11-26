43e1ba57bb0079fe9bbb7903718ec67d8e493b4f
NiPeGun <nipegun@gmail.com>
2020-11-26 21:56:44 +0100

    Create AMDGPU-Pro-Instalar.sh

---
+++ ÚnicaEjecución/debian10/Escritorio/AMDGPU-Pro-Instalar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+URL=https:"//drivers.amd.com/drivers/linux/"
+Archivo="amdgpu-pro-20.30-1109583-ubuntu-20.04.tar.xz"
+
+apt-get -y update
+apt-get -y install wget
+mkdir -p /root/paquetes/amdgpu-pro
+wget $URL$Archivo -O /root/paquetes/amdgpu-pro/amdgpu-pro-20.30-1109583-ubuntu-20.04.tar.xz
+
