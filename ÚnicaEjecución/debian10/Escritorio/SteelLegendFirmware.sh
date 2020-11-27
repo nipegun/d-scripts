467712943b6dfe0e9c130ca345101f1111022113
NiPeGun <nipegun@gmail.com>
2020-11-27 19:28:16 +0100

    Update SteelLegendFirmware.sh

--- ÚnicaEjecución/debian10/Escritorio/SteelLegendFirmware.sh
+++ ÚnicaEjecución/debian10/Escritorio/SteelLegendFirmware.sh
@@ -6,6 +6,27 @@ cd /lib/firmware/rtl_nic
 wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/rtl_nic/rtl8125a-3.fw
 wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/rtl_nic/rtl8168fp-3.fw
 
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8125a-3.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8107e-2.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8107e-1.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168fp-3.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168h-2.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168h-1.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168g-3.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168g-2.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8106e-2.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8106e-1.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8411-2.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8411-1.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8402-1.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168f-2.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168f-1.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8105e-1.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168e-3.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168e-2.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168e-1.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168d-2.fw for module r8169
+W: Possible missing firmware /lib/firmware/rtl_nic/rtl8168d-1.fw for module r8169
 
 
 W: Possible missing firmware /lib/firmware/amdgpu/navi12_gpu_info.bin for module amdgpu
