c96936768c554de6ed06a14e1695f8359cf4a26b
NiPeGun <nipegun@gmail.com>
2020-11-27 17:14:26 +0100

    Update ProxmoxVE-Instalar.sh

--- ÚnicaEjecución/debian10/ProxmoxVE-Instalar.sh
+++ ÚnicaEjecución/debian10/ProxmoxVE-Instalar.sh
@@ -3,7 +3,7 @@
 apt-get -y update
 apt-get -y install wget
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/ProxmoxVE.list
-wget http://download.proxmox.com/debian/proxmomx-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmomx-ve-release-6.x.gpg
+wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmomx-ve-release-6.x.gpg
 apt-get -y update
 apt-get -y install proxmox-ve
 rm -rf /etc/apt/sources.list.d/pve-enterprise.list 
