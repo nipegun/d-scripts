521ca1466b4a627ff30a5359b15ff4efe5155163
NiPeGun <nipegun@gmail.com>
2020-12-18 20:14:35 +0100

    Create ActualizarSistemaOperativoConRepoObsoleto.sh

---
+++ ActualizarSistemaOperativoConRepoObsoleto.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-------------------------------------------------------------------------------------------
+#  Script de NiPeGun para actualizar Debian cuando hay un repositorio con firmas obsoletas
+#-------------------------------------------------------------------------------------------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+echo ""
+echo -e "${ColorVerde}Actualizando sistema operativo con repo obsoleto...${FinColor}"
+echo ""
+apt-get -o Acquire::Check-Valid-Until=false update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y autoremove
