36415a54520ccdafad281edd4f33eb1dc28fef14
NiPeGun <nipegun@gmail.com>
2021-01-19 13:20:13 +0100

    Update DScripts-Sincronizar.sh

--- DScripts-Sincronizar.sh
+++ DScripts-Sincronizar.sh
@@ -29,7 +29,7 @@ wget -q --tries=10 --timeout=20 --spider https://github.com
     mkdir -p /root/scripts/d-scripts/Alias/
     rm /root/scripts/d-scripts/.git -R 2> /dev/null
     find /root/scripts/d-scripts/ -type f -iname "*.sh" -exec chmod +x {} \;
-    /root/scripts/d-scripts/CrearAliasParaLosDScripts.sh
+    /root/scripts/d-scripts/DScripts-CrearAlias.sh
     find /root/scripts/d-scripts/Alias -type f -exec chmod +x {} \;
     
     echo ""
