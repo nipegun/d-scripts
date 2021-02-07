35f73a4773ec04e3b4fc531632dc0deaf0740e8c
NiPeGun <nipegun@gmail.com>
2021-02-07 04:09:40 +0100

    Create Kernel-InstalarFuenteDelKernelEnUso.sh

---
+++ Kernel-InstalarFuenteDelKernelEnUso.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#----------------------------------------------------------------------
+#  Script de NiPeGun para instalar el código fuente del kernel en uso
+#----------------------------------------------------------------------
+
+KernelEnUso=$(uname-r)
+apt-get -y update
+apt-get -y install dpkg-dev
+apt-get -y source linux-image-$KernelEnUso
+
