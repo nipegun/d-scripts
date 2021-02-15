81e918b029111638cbabd3584ffa5b69f00f79d0
NiPeGun <nipegun@gmail.com>
2021-02-16 00:56:42 +0100

    Update CertificadosSSL-BorrarLetsEncrypt.sh

--- CertificadosSSL-BorrarLetsEncrypt.sh
+++ CertificadosSSL-BorrarLetsEncrypt.sh
@@ -7,3 +7,7 @@ rm /etc/letsencrypt/renewal/$Dominio.conf
 
 rm -rf /etc/letsencrypt/live/www.$Dominio
 rm /etc/letsencrypt/renewal/www.$Dominio.conf
+
+sed -i -e 's|SSLCertificateFile /etc/letsencrypt/live/$Dominio/fullchain.pem|#SSLCertificateFile /etc/letsencrypt/live/$Dominio/fullchain.pem|g'   /etc/apache2/sites-available/$Dominio.conf 
+sed -i -e 's|SSLCertificateKeyFile /etc/letsencrypt/live/$Dominio/privkey.pem|#SSLCertificateKeyFile /etc/letsencrypt/live/$Dominio/privkey.pem|g' /etc/apache2/sites-available/$Dominio.conf 
+
