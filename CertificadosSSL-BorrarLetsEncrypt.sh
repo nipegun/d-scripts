0372a08de7b6307ed0b71edd0bc71c8b4b8ebcf9
NiPeGun <nipegun@gmail.com>
2021-02-16 01:14:29 +0100

    Update CertificadosSSL-BorrarLetsEncrypt.sh

--- CertificadosSSL-BorrarLetsEncrypt.sh
+++ CertificadosSSL-BorrarLetsEncrypt.sh
@@ -17,9 +17,9 @@ rm /etc/letsencrypt/renewal/www.$Dominio.conf
 echo ""
 echo ""
 echo ""
-sed -i -e 's|Include /etc/letsencrypt/options-ssl-apache.conf|#Include /etc/letsencrypt/options-ssl-apache.conf|g' /etc/apache2/sites-available/$Dominio.conf 
-sed -i -e 's|SSLCertificateFile /etc/letsencrypt/live/'"$Dominio"'/fullchain.pem|#SSLCertificateFile /etc/letsencrypt/live/'"$Dominio"'/fullchain.pem|g'   /etc/apache2/sites-available/$Dominio.conf 
-sed -i -e 's|SSLCertificateKeyFile /etc/letsencrypt/live/'"$Dominio"'/privkey.pem|#SSLCertificateKeyFile /etc/letsencrypt/live/'"$Dominio"'/privkey.pem|g' /etc/apache2/sites-available/$Dominio.conf 
+sed -i -e 's|Include /etc/letsencrypt/options-ssl-apache.conf|#Include /etc/letsencrypt/options-ssl-apache.conf|g' /etc/apache2/sites-available/$Dominio.conf
+sed -i -e 's|SSLCertificateFile /etc/letsencrypt/live/'"$Dominio"'/fullchain.pem|#SSLCertificateFile /etc/letsencrypt/live/'"$Dominio"'/fullchain.pem|g'   /etc/apache2/sites-available/$Dominio.conf
+sed -i -e 's|SSLCertificateKeyFile /etc/letsencrypt/live/'"$Dominio"'/privkey.pem|#SSLCertificateKeyFile /etc/letsencrypt/live/'"$Dominio"'/privkey.pem|g' /etc/apache2/sites-available/$Dominio.conf
 
 echo ""
 echo "/etc/apache2/sites-available/$Dominio.conf"
