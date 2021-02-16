6b9133216928ef13cccdee1e188ef5a6eade28e8
NiPeGun <nipegun@gmail.com>
2021-02-16 01:52:06 +0100

    Update CertificadosSSL-BorrarLetsEncrypt.sh

--- CertificadosSSL-BorrarLetsEncrypt.sh
+++ CertificadosSSL-BorrarLetsEncrypt.sh
@@ -5,8 +5,9 @@ Dominio="nipegun.com"
 echo ""
 echo ""
 echo ""
+rm -rf /etc/letsencrypt/archive/$Dominio
 rm -rf /etc/letsencrypt/live/$Dominio
-rm /etc/letsencrypt/renewal/$Dominio.conf
+rm -rf /etc/letsencrypt/renewal/$Dominio.conf
 
 echo ""
 echo ""
