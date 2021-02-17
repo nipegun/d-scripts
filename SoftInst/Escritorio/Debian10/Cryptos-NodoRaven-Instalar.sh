836ad23c4942ed41b9e3b4ae0934ea7e089396bb
NiPeGun <nipegun@gmail.com>
2021-02-17 19:34:06 +0100

    Update Cryptos-NodoRaven-Instalar.sh

--- SoftInst/Escritorio/Debian10/Cryptos-NodoRaven-Instalar.sh
+++ SoftInst/Escritorio/Debian10/Cryptos-NodoRaven-Instalar.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Comprobar si el paquete unzip está instalado. Si no está, instalarlo.
+if [ $(dpkg-query -W -f='${Status}' unzip 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+    echo ""
+    echo "NFTables no está instalado. Se procederá a su instalación..."
+    echo ""
+    apt-get -y update
+    apt-get -y install unzip
+fi
+
 Archivo=$(curl -s https://github.com/RavenProject/Ravencoin/releases/ | grep linux | grep gnu | grep zip | grep href | grep -v disable | cut -d '"' -f 2)
-
-echo $Archivo
-
 mkdir -p /root/CodFuente/NodoRaven/
-cd /root/CodFuente/NodoRaven
 wget --no-check-certificate https://github.com$Archivo -O /root/CodFuente/NodoRaven/NodoRaven.zip
+unzip -q /root/CodFuente/NodoRaven/NodoRaven.zip -d /root/CodFuente/NodoRaven/
 
