fae3ad6c334b79f33af78744e0fe13d37b2353c6
NiPeGun <nipegun@gmail.com>
2021-02-17 19:25:10 +0100

    Create Cryptos-NodoRaven-Instalar.sh

---
+++ SoftInst/Escritorio/Debian10/Cryptos-NodoRaven-Instalar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+Archivo=$(curl -s https://github.com/RavenProject/Ravencoin/releases/ | grep linux | grep gnu | grep zip | grep href | grep -v disable | cut -d '"' -f 2)
+
+echo $Archivo
+
