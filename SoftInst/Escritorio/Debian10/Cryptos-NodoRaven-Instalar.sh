79db08919c50a6d26dafbf1a5ad5f4b64f4c2eeb
NiPeGun <nipegun@gmail.com>
2021-02-17 20:14:44 +0100

    Update Cryptos-NodoRaven-Instalar.sh

--- SoftInst/Escritorio/Debian10/Cryptos-NodoRaven-Instalar.sh
+++ SoftInst/Escritorio/Debian10/Cryptos-NodoRaven-Instalar.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 Archivo=$(curl -s https://github.com/RavenProject/Ravencoin/releases/ | grep linux | grep gnu | grep zip | grep href | grep -v disable | cut -d '"' -f 2)
-rm -rf /root/CodFuente/NodoRaven/*
-mkdir -p /root/CodFuente/NodoRaven/Binarios
+rm -rf /root/Software/Binarios/NodoRaven/*
+mkdir -p /root/Software/Binarios/NodoRaven/
 
 echo ""
 echo "Descargando el archivo zip de la última release..."
 echo ""
-wget --no-check-certificate https://github.com$Archivo -O /root/CodFuente/NodoRaven/NodoRaven.zip
+wget --no-check-certificate https://github.com$Archivo -O /root/Software/Binarios/NodoRaven/NodoRaven.zip
 
 echo ""
 echo "Descomprimiendo el archivo zip..."
@@ -20,15 +20,15 @@ if [ $(dpkg-query -W -f='${Status}' unzip 2>/dev/null | grep -c "ok installed") 
     apt-get -y update
     apt-get -y install unzip
 fi
-unzip /root/CodFuente/NodoRaven/NodoRaven.zip -d /root/CodFuente/NodoRaven/
-rm -rf /root/CodFuente/NodoRaven/NodoRaven.zip
+unzip /root/Software/Binarios/NodoRaven/NodoRaven.zip -d /root/Software/Binarios/NodoRaven/
+rm -rf /root/Software/Binarios/NodoRaven.zip
 
 echo ""
 echo "Descomprimiendo el archivo tar.gz de dentro del archivo zip..."
 echo ""
-find /root/CodFuente/NodoRaven/linux/ -type f -name *.tar.gz -exec mv {} /root/CodFuente/NodoRaven/NodoRaven.tar.gz \;
-rm -rf /root/CodFuente/NodoRaven/linux/
-rm -rf /root/CodFuente/NodoRaven/__MACOSX/
+find /root/Software/Binarios/NodoRaven/linux/ -type f -name *.tar.gz -exec mv {} /root/Software/Binarios/NodoRaven/NodoRaven.tar.gz \;
+rm -rf /root/Software/Binarios/NodoRaven/linux/
+rm -rf /root/Software/Binarios/NodoRaven/__MACOSX/
 # Comprobar si el paquete gzip está instalado. Si no está, instalarlo.
 if [ $(dpkg-query -W -f='${Status}' gzip 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     echo ""
@@ -37,11 +37,11 @@ if [ $(dpkg-query -W -f='${Status}' gzip 2>/dev/null | grep -c "ok installed") -
     apt-get -y update
     apt-get -y install gzip
 fi
-tar xzvf /root/CodFuente/NodoRaven/NodoRaven.tar.gz --directory /root/CodFuente/NodoRaven/Binarios/
-rm -rf /root/CodFuente/NodoRaven/NodoRaven.tar.gz
+tar xzvf /root/Software/Binarios/NodoRaven/NodoRaven.tar.gz --directory /root/Software/Binarios/NodoRaven/Binarios/
+rm -rf /root/Software/Binarios/NodoRaven/NodoRaven.tar.gz
 
 echo ""
 echo "Moviendo archivos a la carpeta final..."
 echo ""
-find /root/CodFuente/NodoRaven/Binarios/ -type d -name raven* -exec mv {} /root/CodFuente/NodoRaven/RavenCoin \;
-
+mkdir /root/NodoRaven
+find /root/Software/Binarios/NodoRaven/Binarios/ -type d -name raven* -exec cp {} /root/NodoRaven/ \;
