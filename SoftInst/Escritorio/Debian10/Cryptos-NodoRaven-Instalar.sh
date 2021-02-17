f4b741cf2ffd96aba3463f71238e28c816c52b2c
NiPeGun <nipegun@gmail.com>
2021-02-17 21:22:03 +0100

    Update Cryptos-NodoRaven-Instalar.sh

--- SoftInst/Escritorio/Debian10/Cryptos-NodoRaven-Instalar.sh
+++ SoftInst/Escritorio/Debian10/Cryptos-NodoRaven-Instalar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Comprobar si el paquete curl está instalado. Si no está, instalarlo.
-if [ $(dpkg-query --list | grep -i curl) -eq "  Instalados: ninguno"]; then
+if [ $(dpkg-query -s curl 2>/dev/null | grep installed) -eq 0 ]; then
     echo ""
     echo "curl no está instalado. Se procederá a su instalación..."
     echo ""
