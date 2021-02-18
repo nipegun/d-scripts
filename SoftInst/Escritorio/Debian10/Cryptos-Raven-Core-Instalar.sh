7e1257d994f3b55dc0b0d5f9f0fe5e8b9eb6fcd2
NiPeGun <nipegun@gmail.com>
2021-02-18 04:52:14 +0100

    Update Cryptos-Raven-Core-Instalar.sh

--- SoftInst/Escritorio/Debian10/Cryptos-Raven-Core-Instalar.sh
+++ SoftInst/Escritorio/Debian10/Cryptos-Raven-Core-Instalar.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-------------------------------------------------------
+#  Script de NiPeGun para instalar RavenCore en Debian
+#-------------------------------------------------------
+
 # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
 if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
     echo ""
