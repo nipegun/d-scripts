6da4a0e1f9c00f92366e528fe42c4035d51e9cee
NiPeGun <nipegun@gmail.com>
2021-02-18 05:35:16 +0100

    Create Red-Telnet-Capturar.sh

---
+++ Red-Telnet-Capturar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+InterfazDeseada=eth0
+IPDeseada=192.168.0.1
+
+echo ""
+echo "Capturando el tráfico telnet de la IP $IPDeseada..."
+echo ""
+
+touch /root/telnet.pcap
+truncate -s 0 /root/telnet.pcap
+tshark -P -i $InterfazDeseada -f "tcp port 23 and host $IPDeseada" -w /root/telnet.pcap
+
