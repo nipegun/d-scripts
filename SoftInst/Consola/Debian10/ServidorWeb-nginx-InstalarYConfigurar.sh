b7af7ec58ea0b26f40dbe7e5d8cc61c0df5f89cf
NiPeGun <nipegun@gmail.com>
2021-03-11 21:22:47 +0100

    Update ServidorWeb-nginx-InstalarYConfigurar.sh

--- SoftInst/Consola/Debian10/ServidorWeb-nginx-InstalarYConfigurar.sh
+++ SoftInst/Consola/Debian10/ServidorWeb-nginx-InstalarYConfigurar.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#------------------------------------------------------------------
+#  Script de NiPeGun para instalar y configurar nginx en Debian10
+#------------------------------------------------------------------
+
 echo ""
 echo "Actualizando la lista de paquetes..."
 echo ""
