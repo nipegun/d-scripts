d2be4eb43f5a4eb770cd0d6ec30118358b7d7a88
NiPeGun <nipegun@gmail.com>
2021-03-15 16:28:46 +0100

    Create MySQL-BaseDeDatos-Vaciar.sh

---
+++ MySQL-BaseDeDatos-Vaciar.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#--------------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para borrar una base de datos antes de importar una copia de seguridad de la misma
+#--------------------------------------------------------------------------------------------------------
+
+EXPECTED_ARGS=3
+E_BADARGS=65
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+FechaDeExp=$(date +A%YM%mD%d@%T)
+
+if [ $# -ne $EXPECTED_ARGS ]
+  then
+    echo ""
+    echo "-------------------------------------------------------------------------"
+    echo -e "${ColorRojo}Mal uso del script.${FinColor} El uso correcto sería:"
+    echo ""
+    echo -e "$0 ${ColorVerde}[UsuarioBD] [PasswordBD] [NombreBD] [RutaArchivoSQL]${FinColor}"
+    echo ""
+    echo "Ejemplo 1:"
+    echo "$0 pepe 12345678 Cuentas"
+    echo ""
+    echo "-------------------------------------------------------------------------"
+    echo ""
+    exit $E_BADARGS
+  else
+    mysqldump -u$1 -p$2 --add-drop-table --no-data $3 | grep ^DROP | sed -e 's/DROP TABLE IF EXISTS/TRUNCATE TABLE/g' | mysql -u$1 -p$2 $3
+fi
+
