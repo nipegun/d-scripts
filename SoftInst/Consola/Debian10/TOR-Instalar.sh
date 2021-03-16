dd98535badc7d6dca3e95373243491059d2f9418
NiPeGun <nipegun@gmail.com>
2021-03-16 03:07:41 +0100

    Update TOR-Instalar.sh

--- SoftInst/Consola/Debian10/TOR-Instalar.sh
+++ SoftInst/Consola/Debian10/TOR-Instalar.sh
@@ -9,13 +9,32 @@
 #  Script de NiPeGun para instalar TOR en Debian
 #-------------------------------------------------
 
+echo ""
+echo "Instalando paquetes necesarios..."
+echo ""
 apt-get -y update
 apt-get -y install tor
+
+echo ""
+echo "Configurando TOR..."
+echo ""
 cp /etc/tor/torrc /etc/tor/torrc.bak
 echo "SocksPort 9050"                            > /etc/tor/torrc
 echo "Log notice file /var/log/tor/notices.log" >> /etc/tor/torrc
 echo "Log debug file /var/log/tor/debug.log"    >> /etc/tor/torrc
 echo "RunAsDaemon 1"                            >> /etc/tor/torrc
 
+echo ""
+echo "Reiniciando el servicio..."
+echo ""
 systemctl reload tor.service
 systemctl restart tor.service
+systemctl status tor.service
+echo ""
+
+echo ""
+echo "Comprobando el funcionamiento de TOR..."
+echo ""
+curl --silent --socks5-hostname localhost:9050 https://check.torproject.org | grep ongrat
+echo ""
+
