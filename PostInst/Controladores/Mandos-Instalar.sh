5bdd2270a7a0cf481a452d0cd608fc7f1a0e31fa
NiPeGun <nipegun@gmail.com>
2021-04-09 18:17:30 +0200

    Create Mandos-Instalar.sh

---
+++ PostInst/Controladores/Mandos-Instalar.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-------------------------------------------------------------------------------
+#  Script de NiPeGun para instalar controladores de mandos de juegos en Debian
+#-------------------------------------------------------------------------------
+
+ColorVerde="\033[1;32m"
+FinColor="\033[0m"
+
+apt-get -y update 2> /dev/null
+apt-get -y install git dialog 2> /dev/null
+
+menu=(dialog --timeout 5 --checklist "Elección del adaptador:" 22 76 16)
+  opciones=(1 "XBox One USB Wireless Adapter" off
+            2 "2" off
+            3 "3" off
+            4 "4" off)
+  choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
+  clear
+
+  for choice in $choices
+    do
+      case $choice in
+
+        1)
+          echo ""
+          echo -e "${ColorVerde}Instalando controladores para el adaptador USB de los mandos de la XBox One...${FinColor}"
+          echo ""
+          mkdir /root/Controladores/
+          cd /root/Controladores/
+          apt-get install -y git cabextract
+          git clone https://github.com/medusalix/xow
+          mv /root/Controladores/xow/ /root/Controladores/XBoxOneWirelessAdapter/
+          /root/Controladores/XBoxOneWirelessAdapter/
+          cd /root/Controladores/XBoxOneWirelessAdapter/
+          make BUILD=RELEASE
+          make install
+          systemctl enable xow
+          systemctl start xow
+        ;;
+
+        2)
+        ;;
+
+        3)
+        ;;
+
+        4)
+        ;;
+
+      esac
+
+done
+
+    
