57a87d7bdc49a87380d7683795953c55e169f17b
NiPeGun <nipegun@gmail.com>
2021-04-13 00:00:14 +0200

    Update YTS-DescargarTodosLosTorrentsSciFi.sh

--- YTS-DescargarTodosLosTorrentsSciFi.sh
+++ YTS-DescargarTodosLosTorrentsSciFi.sh
@@ -8,13 +8,16 @@ NroPagIni=2
 NroPagFin=500
 
 ## Construir el archivo con todas las URLs de las páginas de cada peli
+echo ""
+echo "Construyendo el archivo con todas las URLs de cada peli..."
+echo ""
 mkdir -p $CarpetaDeDescarga 2>/dev/null
 touch         /tmp/YTS/URLPagPelis.txt
 truncate -s 0 /tmp/YTS/URLPagPelis.txt
 for NroPag in $(seq $NroPagIni $NroPagFin);
   do
     curl --insecure --silent https://yts.mx/browse-movies/0/all/sci-fi/0/latest/0/all?page=$NroPag | grep href | grep title | grep movies | cut -d '"' -f 2 >> /tmp/YTS/URLPagPelis.txt
-    
+
     ## Comprobar si el resultado del curl es un 404 y si lo es, terminar el bucle
     #if [ $NickName = $1 ]; then
       #
