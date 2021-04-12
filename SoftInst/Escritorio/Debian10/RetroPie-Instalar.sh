ef8e2d02d201bc8e94c7e9d9909d465d4a8a2ca0
NiPeGun <nipegun@gmail.com>
2021-04-12 13:04:56 +0200

    Update RetroPie-Instalar.sh

--- SoftInst/Escritorio/Debian10/RetroPie-Instalar.sh
+++ SoftInst/Escritorio/Debian10/RetroPie-Instalar.sh
@@ -28,6 +28,18 @@ echo ""
 echo "Luego, si quieres instalar el emulador de PSP ve al manejo de paquetes e instala:"
 echo "lr-ppsspp"
 echo ""
+
+#sed -i -e 's|scriptdir="$(dirname "$0")"||g'                                              /root/SoftInst/RetroPie/retropie_setup.sh
+#sed -i -e 's|scriptdir="$(cd "$scriptdir" && pwd)"|scriptdir="/root/SoftInst/RetroPie"|g' /root/SoftInst/RetroPie/retropie_setup.sh
+
+## Packages
+sed -i -e 's|rootdir="/opt/retropie"|rootdir="/RetroPie/opt"|g'                           /root/SoftInst/RetroPie/retropie_packages.sh
+sed -i -e 's|datadir="$home/RetroPie"|datadir="/RetroPie"|g'                              /root/SoftInst/RetroPie/retropie_packages.sh
+sed -i -e 's|scriptdir="$(dirname "$0")"||g'                                              /root/SoftInst/RetroPie/retropie_packages.sh
+sed -i -e 's|scriptdir="$(cd "$scriptdir" && pwd)"|scriptdir="/root/SoftInst/RetroPie"|g' /root/SoftInst/RetroPie/retropie_packages.sh
+
+mkdir /RetroPie/
+
 /root/SoftInst/RetroPie/retropie_setup.sh
 mv /root/RetroPie/ /RetroPie/
 sed -i -e 's|/root/RetroPie/|/RetroPie/|g' /etc/emulationstation/es_systems.cfg
