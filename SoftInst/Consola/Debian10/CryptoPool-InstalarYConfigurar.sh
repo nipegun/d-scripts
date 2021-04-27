34a01c871b5e594bc896df0d237de03455dc4afc
NiPeGun <nipegun@gmail.com>
2021-04-27 04:25:40 +0200

    Update CryptoPool-InstalarYConfigurar.sh

--- SoftInst/Consola/Debian10/CryptoPool-InstalarYConfigurar.sh
+++ SoftInst/Consola/Debian10/CryptoPool-InstalarYConfigurar.sh
@@ -13,6 +13,10 @@ ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
 FinColor='\033[0m'
 
+VersPHP="7.3"
+ContraRootMySQL=root
+ContraBD="mpos"
+
 echo ""
 echo ""
 echo -e "${ColorVerde}---------------------------------------------------------------${FinColor}"
@@ -44,14 +48,18 @@ fi
    apt-get -y update
    apt-get -y install build-essential libcurl4-openssl-dev libdb5.3-dev libdb5.3++-dev mariadb-server
 
+## Servidor Stratum
+   
+
+
 ## Instalar servidor Web
    tasksel install web-server
 
 ## MPOS
    apt-get -y install memcached zip
    apt-get -y install php-dom php-mbstring php-memcached php-zip
-   apt-get -y install libapache2-mod-php7.3
-   apt-get -y install php7.3-curl php7.3-mysqlnd php7.3-json php7.3-xml 
+   apt-get -y install libapache2-mod-php$VersPHP
+   apt-get -y install php$VersPHP-curl php$VersPHP-mysqlnd php$VersPHP-json php$VersPHP-xml 
    apache2ctl -k stop
    cd /var/www/
    ## Comprobar si el paquete git está instalado. Si no lo está, instalarlo.
@@ -88,8 +96,75 @@ fi
    echo "</VirtualHost>"                                     >> /etc/apache2/sites-available/pool.conf
    touch /var/www/MPOS/logs/error.log
    touch /var/www/MPOS/logs/access.log
+   
+   # Permisos
+   chown -Rv www-data /var/www/MPOS/templates/compile
+   chown -Rv www-data /var/www/MPOS/templates/cache
+   chown -Rv www-data /var/www/MPOS/logs
+   
+   # Archivo de configuración
+   cp /var/www/MPOS/include/config/global.inc.dist.php /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['db']['host'] = 'localhost';|$config['db']['host'] = 'localhost';|g'                                     /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['db']['user'] = 'root';|$config['db']['user'] = 'root';|g'                                               /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['db']['pass'] = 'root';|$config['db']['pass'] = 'root';|g'                                               /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['db']['port'] = 3306;|$config['db']['port'] = 3306;|g'                                                   /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['db']['name'] = 'mpos';|$config['db']['name'] = 'mpos';|g'                                               /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['wallet']['type'] = 'http';|$config['wallet']['type'] = 'http';|g'                                       /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['wallet']['host'] = 'localhost:19334';|$config['wallet']['host'] = 'localhost:19334';|g'                 /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['wallet']['username'] = 'testnet';|$config['wallet']['username'] = 'testnet';|g'                         /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['wallet']['password'] = 'testnet';|$config['wallet']['password'] = 'testnet';|g'                         /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['gettingstarted']['stratumurl'] = 'localhost';|$config['gettingstarted']['stratumurl'] = 'localhost';|g' /var/www/MPOS/include/config/global.inc.php
+   #sed -i -e 's|$config['SALT']||g'                                        /var/www/MPOS/include/config/global.inc.php
+   #sed -i -e 's|$config['SALTY']||g'                                       /var/www/MPOS/include/config/global.inc.php
+   #SALT and SALTY must be a minimum of 24 characters or you will get an error message:
+   #'SALT or SALTY is too short, they should be more than 24 characters and changing them will require registering a
+   
+   # Servidor Stratum
+   
+   
+   # Web socket
+   sed -i -e 's|from autobahn.websocket import WebSocketServerProtocol, WebSocketServerFactory|from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerFactory|g' /usr/local/lib/python2.7/dist-packages/stratum-0.2.13-py2.7.egg/stratum/websocket_transport.py
    apache2ctl -k start
 
+## Base de datos
+   /root/scripts/d-scripts/MySQL-BaseDeDatos-Crear.sh mpos mpos $ContraBD
+   # Borrar la base de datos anterior de mpos, si es que existe
+   mysql -e "drop database if exists mpos"
+   # Borrar el usuario mpos, si es que existe
+   mysql -e "drop user if exists mpos@localhost"
+   
+   echo ""
+   echo "Las bases de datos MySQL disponibles actualmente en el sistema son:"
+   echo ""
+   mysql -e "show databases"
+   echo ""
+   echo "Los usuarios MySQL disponibles actualmente en el sistema son:"
+   echo ""
+   mysql -e "select user,host from mysql.user"
+   
+   echo ""
+   echo "Creando la base de datos mpos..."
+   echo ""
+   mysql -e "create database mpos"
+   echo ""
+   echo "Creando el usuario mpos@localhost..."
+   echo ""
+   mysql -e "create user mpos@localhost"
+   echo ""
+   echo "Dando permisos al usuario mpos para que administre la base de datos mpos..."
+   echo ""
+   mysql -e "grant all privileges on mpos.* to mpos@localhost identified by '$ContraBD'"
+   
+   echo ""
+   echo "Las bases de datos MySQL disponibles actualmente en el sistema son:"
+   echo ""
+   mysql -e "show databases"
+   echo ""
+   echo "Los usuarios MySQL disponibles actualmente en el sistema son:"
+   echo ""
+   mysql -e "select user,host from mysql.user"
+   
+   
 ## Litecoin
    cd ~
    git clone git://github.com/litecoin-project/litecoin.git
