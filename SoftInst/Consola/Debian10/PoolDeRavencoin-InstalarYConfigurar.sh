3a13ef01aecbe439f9c2475a4eeb7422994e9104
NiPeGun <nipegun@gmail.com>
2021-04-27 17:14:13 +0200

    Update PoolDeRavencoin-InstalarYConfigurar.sh

--- SoftInst/Consola/Debian10/PoolDeRavencoin-InstalarYConfigurar.sh
+++ SoftInst/Consola/Debian10/PoolDeRavencoin-InstalarYConfigurar.sh
@@ -16,6 +16,7 @@ FinColor='\033[0m'
 VersPHP="7.3"
 ContraRootMySQL=root
 ContraBD="mpos"
+UsuarioDaemon="poolravencoin"
 
 echo ""
 echo ""
@@ -88,18 +89,28 @@ echo ""
     apt-get -y install tar
   fi
   tar -xf /root/SoftInst/Ravencoin/raven-$UltVersRaven-x86_64-linux-gnu.tar.gz
-   # rm raven*gz
-   # cd raven-4.1.0.0/bin
-   # mkdir -p ~/.raven/
-   # touch ~/.raven/raven.conf
-   # echo "rpcuser=user1" > ~/.raven/raven.conf
-   # echo "rpcpassword=pass1" >> ~/.raven/raven.conf
-   # echo "prune=550" >> ~/.raven/raven.conf
-   # echo "daemon=1" >> ~/.raven/raven.conf
-   
+  rm -rf /root/SoftInst/Ravencoin/raven-$UltVersRaven-x86_64-linux-gnu.tar.gz
+
 echo ""
 echo "Creando el usuario para ejecutar el demonio..."
 echo ""
+  useradd -d /home/$UsuarioDaemon/ -s /bin/bash $UsuarioDaemon
+
+echo ""
+echo "Creando carpetas y archivos necesarios para ese usuario..."
+echo ""
+  mkdir -p /home/$UsuarioDaemon/ 2> /dev/null
+  mkdir -p /home/$UsuarioDaemon/.raven/
+  touch /home/$UsuarioDaemon/.raven/raven.conf
+  echo "rpcuser=user1"      > /home/$UsuarioDaemon/.raven/raven.conf
+  echo "rpcpassword=pass1" >> /home/$UsuarioDaemon/.raven/raven.conf
+  echo "prune=550"         >> /home/$UsuarioDaemon/.raven/raven.conf
+  echo "daemon=1"          >> /home/$UsuarioDaemon/.raven/raven.conf
+  chown $UsuarioDaemon:$UsuarioDaemon /home/$UsuarioDaemon/ -R
+  find /home/$UsuarioDaemon -type d -exec chmod 775 {} \;
+  find /home/$UsuarioDaemon -type f -exec chmod 664 {} \;
+
+   
 #adduser pool
 #usermod -aG sudo pool
 #su - pool
