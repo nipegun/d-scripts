d7212f85b66a29fd0c166eecab4d480c540be154
NiPeGun <nipegun@gmail.com>
2021-04-27 02:43:38 +0200

    Update CryptoPool-InstalarYConfigurar.sh

--- SoftInst/Consola/Debian10/CryptoPool-InstalarYConfigurar.sh
+++ SoftInst/Consola/Debian10/CryptoPool-InstalarYConfigurar.sh
@@ -39,5 +39,65 @@ if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
     apt-get -y install dialog
 fi
 
-curl https://raw.githubusercontent.com/cryptopool-builders/Multi-Pool-Installer/master/bootstrap.sh | bash
+
+## Instalar paquetes necesarios
+   apt-get -y update
+   apt-get -y install build-essential libcurl4-openssl-dev libdb5.3-dev libdb5.3++-dev mariadb-server
+
+## Instalar servidor Web
+   tasksel install web-server
+
+## MPOS
+   apt-get -y install memcached zip
+   apt-get -y install php-dom php-mbstring php-memcached php-zip
+   apt-get -y install libapache2-mod-php7.3
+   apt-get -y install php7.3-curl php7.3-mysqlnd php7.3-json php7.3-xml 
+   apache2ctl -k stop
+   cd /var/www/
+   ## Comprobar si el paquete git está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s git 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo "git no está instalado. Iniciando su instalación..."
+        echo ""
+        apt-get -y update
+        apt-get -y install git
+      fi
+   git clone git://github.com/MPOS/php-mpos.git MPOS
+   cd MPOS
+   git checkout master
+   php composer.phar install
+   
+   echo "<VirtualHost *:80>"                                  > /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo "  #Redirect permanent / https://pool.sitioweb.com/" >> /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo "  ServerName pool.sitioweb.com"                     >> /etc/apache2/sites-available/pool.conf
+   echo "  DocumentRoot /var/www/MPOS/public"                >> /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo '  <Directory "/var/www/MPOS/public">'               >> /etc/apache2/sites-available/pool.conf
+   echo "    Require all granted"                            >> /etc/apache2/sites-available/pool.conf
+   echo "    Options FollowSymLinks"                         >> /etc/apache2/sites-available/pool.conf
+   echo "    AllowOverride All"                              >> /etc/apache2/sites-available/pool.conf
+   echo "  </Directory>"                                     >> /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo "  ServerAdmin mail@gmail.com"                       >> /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo "  ErrorLog  /var/www/MPOS/logs/error.log"           >> /etc/apache2/sites-available/pool.conf
+   echo "  CustomLog /var/www/MPOS/logs/access.log combined" >> /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo "</VirtualHost>"                                     >> /etc/apache2/sites-available/pool.conf
+   touch /var/www/MPOS/logs/error.log
+   touch /var/www/MPOS/logs/access.log
+   apache2ctl -k start
+
+## Litecoin
+   cd ~
+   git clone git://github.com/litecoin-project/litecoin.git
+   cd litecoin
+   apt-get -y install autoconf
+   ./autogen.sh
+   ./configure --with-incompatible-bdb
+   make
+   make check
+   make install
 
