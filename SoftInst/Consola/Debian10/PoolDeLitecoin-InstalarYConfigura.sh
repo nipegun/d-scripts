eb7bdacfd54c80d559f0de372cb2af260283a464
NiPeGun <nipegun@gmail.com>
2021-04-28 17:24:51 +0200

    Update PoolDeLitecoin-InstalarYConfigura.sh

--- SoftInst/Consola/Debian10/PoolDeLitecoin-InstalarYConfigura.sh
+++ SoftInst/Consola/Debian10/PoolDeLitecoin-InstalarYConfigura.sh
@@ -25,6 +25,22 @@ echo -e "${ColorVerde}----------------------------------------------------------
 echo ""
 echo ""
 
+## Si se quiere instalar litecoin compilando
+#
+#mkdir -p ~/SoftInst/ 2> /dev/null
+#cd ~/SoftInst/
+#rm -rf ~/SoftInst/Litecoin
+#git clone git://github.com/litecoin-project/litecoin.git
+#mv ~/SoftInst/litecoin/ ~/SoftInst/Litecoin/
+#cd ~/SoftInst/Litecoin/
+#apt-get -y install autoconf
+#~/SoftInst/Litecoin/autogen.sh
+#apt-get -y install libevent-dev doxygen libzmq3-dev
+#~/SoftInst/Litecoin/configure --with-incompatible-bdb
+#make
+#make check
+#make install
+
 echo "Determinando la última versión de lite..."
 echo ""
 ## Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
@@ -81,14 +97,14 @@ echo ""
 echo "Creando carpetas y archivos necesarios para ese usuario..."
 echo ""
   mkdir -p /home/$UsuarioDaemon/ 2> /dev/null
-  mkdir -p /home/$UsuarioDaemon/.raven/
-  touch /home/$UsuarioDaemon/.raven/raven.conf
-  echo "rpcuser=user1"      > /home/$UsuarioDaemon/.raven/raven.conf
-  echo "rpcpassword=pass1" >> /home/$UsuarioDaemon/.raven/raven.conf
-  echo "prune=550"         >> /home/$UsuarioDaemon/.raven/raven.conf
-  echo "daemon=1"          >> /home/$UsuarioDaemon/.raven/raven.conf
+  mkdir -p /home/$UsuarioDaemon/.litecoin/
+  touch /home/$UsuarioDaemon/.litecoin/raven.conf
+  echo "rpcuser=user1"      > /home/$UsuarioDaemon/.litecoin/raven.conf
+  echo "rpcpassword=pass1" >> /home/$UsuarioDaemon/.litecoin/raven.conf
+  echo "prune=550"         >> /home/$UsuarioDaemon/.litecoin/raven.conf
+  echo "daemon=1"          >> /home/$UsuarioDaemon/.litecoin/raven.conf
   rm -rf /home/$UsuarioDaemon/$CarpetaSoft/
-  mv /root/SoftInst/Ravencoin/raven-$UltVersRaven/ /home/$UsuarioDaemon/$CarpetaSoft/
+  mv /root/SoftInst/Litecoin/litecoin-$UltVersLite/ /home/$UsuarioDaemon/$CarpetaSoft/
   chown $UsuarioDaemon:$UsuarioDaemon /home/$UsuarioDaemon/ -R
   find /home/$UsuarioDaemon -type d -exec chmod 775 {} \;
   find /home/$UsuarioDaemon -type f -exec chmod 664 {} \;
@@ -122,45 +138,157 @@ echo ""
   echo ""
 
 
+
+
+
+
+
 echo ""
 echo "Instalando la pool..."
 echo ""
-  ## Comprobar si el paquete git está instalado. Si no lo está, instalarlo.
-  if [[ $(dpkg-query -s git 2>/dev/null | grep installed) == "" ]]; then
+
+
+## Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
     echo ""
-    echo "git no está instalado. Iniciando su instalación..."
+    echo "curl no está instalado. Iniciando su instalación..."
     echo ""
     apt-get -y update
-    apt-get -y install git
-  fi
-  cd /root/SoftInst/
-  git clone https://github.com/notminerproduction/rvn-kawpow-pool.git
-  mv /root/SoftInst/rvn-kawpow-pool/ /home/$UsuarioDaemon/
-  chown $UsuarioDaemon:$UsuarioDaemon /home/$UsuarioDaemon/ -R
-  find /home/$UsuarioDaemon/rvn-kawpow-pool/ -type d -exec chmod 775 {} \;
-  find /home/$UsuarioDaemon/rvn-kawpow-pool/ -type f -exec chmod 664 {} \;
-  find /home/$UsuarioDaemon/rvn-kawpow-pool/ -type f -iname "*.sh" -exec chmod +x {} \;
-  sed -i -e 's|"stratumHost": "192.168.0.200",|"stratumHost": "'"$DominioPool"'",|g'                                            /home/$UsuarioDaemon/rvn-kawpow-pool/config.json
-  sed -i -e 's|"address": "RKopFydExeQXSZZiSTtg66sRAWvMzFReUj",|"address": "'"$DirCart"'",|g'                                   /home/$UsuarioDaemon/rvn-kawpow-pool/pool_configs/ravencoin.json
-  sed -i -e 's|"donateaddress": "RKopFydExeQXSZZiSTtg66sRAWvMzFReUj",|"donateaddress": "RKxPhh36Cz6JoqMuq1nwMuPYnkj8DmUswy",|g' /home/$UsuarioDaemon/rvn-kawpow-pool/pool_configs/ravencoin.json
-  sed -i -e 's|RL5SUNMHmjXtN1AzCRFQrFEhjnf7QQY7Tz|RKxPhh36Cz6JoqMuq1nwMuPYnkj8DmUswy|g'                                         /home/$UsuarioDaemon/rvn-kawpow-pool/pool_configs/ravencoin.json
-  sed -i -e 's|Ta26x9axaDQWaV2bt2z8Dk3R3dN7gHw9b6|RKxPhh36Cz6JoqMuq1nwMuPYnkj8DmUswy|g'                                         /home/$UsuarioDaemon/rvn-kawpow-pool/pool_configs/ravencoin.json
-  apt-get -y install npm
-  
-  apt-get -y install sudo
-  echo "$UsuarioDaemon ALL=(ALL:ALL) ALL" >> /etc/sudoers
-  #find /home/$UsuarioDaemon/rvn-kawpow-pool/install.sh -type f -exec sed -i -e "s|sudo ||g" {} \;
-  
-  echo ""
-  echo "Cambiando la contraseña para el usuario $UsuarioDaemon..."
-  echo ""
-  passwd pool
-  
-  echo ""
-  echo "Ahora tendrás que ejecutar el siguiente comando:"
-  echo ""
-  echo "/home/$UsuarioDaemon/rvn-kawpow-pool/install.sh"
-  echo ""
-  su - pool
+    apt-get -y install curl
+fi
+
+## Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
+if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo "dialog no está instalado. Iniciando su instalación..."
+    echo ""
+    apt-get -y update
+    apt-get -y install dialog
+fi
+
+
+## Instalar paquetes necesarios
+   apt-get -y update
+   apt-get -y install build-essential libcurl4-openssl-dev libdb5.3-dev libdb5.3++-dev mariadb-server
+
+## Servidor Stratum
+   
+
+
+## Instalar servidor Web
+   tasksel install web-server
+
+## MPOS
+   apt-get -y install memcached zip
+   apt-get -y install php-dom php-mbstring php-memcached php-zip
+   apt-get -y install libapache2-mod-php$VersPHP
+   apt-get -y install php$VersPHP-curl php$VersPHP-mysqlnd php$VersPHP-json php$VersPHP-xml 
+   apache2ctl -k stop
+   cd /var/www/
+   ## Comprobar si el paquete git está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s git 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo "git no está instalado. Iniciando su instalación..."
+        echo ""
+        apt-get -y update
+        apt-get -y install git
+      fi
+   git clone git://github.com/MPOS/php-mpos.git MPOS
+   cd MPOS
+   git checkout master
+   php composer.phar install
+   
+   echo "<VirtualHost *:80>"                                  > /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo "  #Redirect permanent / https://pool.sitioweb.com/" >> /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo "  ServerName pool.sitioweb.com"                     >> /etc/apache2/sites-available/pool.conf
+   echo "  DocumentRoot /var/www/MPOS/public"                >> /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo '  <Directory "/var/www/MPOS/public">'               >> /etc/apache2/sites-available/pool.conf
+   echo "    Require all granted"                            >> /etc/apache2/sites-available/pool.conf
+   echo "    Options FollowSymLinks"                         >> /etc/apache2/sites-available/pool.conf
+   echo "    AllowOverride All"                              >> /etc/apache2/sites-available/pool.conf
+   echo "  </Directory>"                                     >> /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo "  ServerAdmin mail@gmail.com"                       >> /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo "  ErrorLog  /var/www/MPOS/logs/error.log"           >> /etc/apache2/sites-available/pool.conf
+   echo "  CustomLog /var/www/MPOS/logs/access.log combined" >> /etc/apache2/sites-available/pool.conf
+   echo ""                                                   >> /etc/apache2/sites-available/pool.conf
+   echo "</VirtualHost>"                                     >> /etc/apache2/sites-available/pool.conf
+   touch /var/www/MPOS/logs/error.log
+   touch /var/www/MPOS/logs/access.log
+   
+   # Permisos
+   chown -Rv www-data /var/www/MPOS/templates/compile
+   chown -Rv www-data /var/www/MPOS/templates/cache
+   chown -Rv www-data /var/www/MPOS/logs
+   
+   # Archivo de configuración
+   cp /var/www/MPOS/include/config/global.inc.dist.php /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['db']['host'] = 'localhost';|$config['db']['host'] = 'localhost';|g'                                     /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['db']['user'] = 'root';|$config['db']['user'] = 'root';|g'                                               /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['db']['pass'] = 'root';|$config['db']['pass'] = 'root';|g'                                               /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['db']['port'] = 3306;|$config['db']['port'] = 3306;|g'                                                   /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['db']['name'] = 'mpos';|$config['db']['name'] = 'mpos';|g'                                               /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['wallet']['type'] = 'http';|$config['wallet']['type'] = 'http';|g'                                       /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['wallet']['host'] = 'localhost:19334';|$config['wallet']['host'] = 'localhost:19334';|g'                 /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['wallet']['username'] = 'testnet';|$config['wallet']['username'] = 'testnet';|g'                         /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['wallet']['password'] = 'testnet';|$config['wallet']['password'] = 'testnet';|g'                         /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['gettingstarted']['stratumurl'] = 'localhost';|$config['gettingstarted']['stratumurl'] = 'localhost';|g' /var/www/MPOS/include/config/global.inc.php
+   sed -i -e 's|$config['check_valid_coinaddress'] = true;|$config['check_valid_coinaddress'] = false;|g'                        /var/www/MPOS/include/config/global.inc.php
+   #sed -i -e 's|$config['SALT']||g'                                        /var/www/MPOS/include/config/global.inc.php
+   #sed -i -e 's|$config['SALTY']||g'                                       /var/www/MPOS/include/config/global.inc.php
+   #SALT and SALTY must be a minimum of 24 characters or you will get an error message:
+   #'SALT or SALTY is too short, they should be more than 24 characters and changing them will require registering a
+   
+   # Servidor Stratum
+   
+   
+   # Web socket
+   sed -i -e 's|from autobahn.websocket import WebSocketServerProtocol, WebSocketServerFactory|from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerFactory|g' /usr/local/lib/python2.7/dist-packages/stratum-0.2.13-py2.7.egg/stratum/websocket_transport.py
+   apache2ctl -k start
+
+## Base de datos
+
+   # Borrar la base de datos anterior de mpos, si es que existe
+   mysql -e "drop database if exists mpos"
+   # Borrar el usuario mpos, si es que existe
+   mysql -e "drop user if exists mpos@localhost"
+   
+   echo ""
+   echo "Las bases de datos MySQL disponibles actualmente en el sistema son:"
+   echo ""
+   mysql -e "show databases"
+   echo ""
+   echo "Los usuarios MySQL disponibles actualmente en el sistema son:"
+   echo ""
+   mysql -e "select user,host from mysql.user"
+   
+   echo ""
+   echo "Creando la base de datos mpos..."
+   echo ""
+   mysql -e "create database mpos"
+   echo ""
+   echo "Creando el usuario mpos@localhost..."
+   echo ""
+   mysql -e "create user mpos@localhost"
+   echo ""
+   echo "Dando permisos al usuario mpos para que administre la base de datos mpos..."
+   echo ""
+   mysql -e "grant all privileges on mpos.* to mpos@localhost identified by '$ContraBD'"
+   
+   echo ""
+   echo "Las bases de datos MySQL disponibles actualmente en el sistema son:"
+   echo ""
+   mysql -e "show databases"
+   echo ""
+   echo "Los usuarios MySQL disponibles actualmente en el sistema son:"
+   echo ""
+   mysql -e "select user,host from mysql.user"
+
+   # Importar la estructura de la base de datos
+   mysql -p mpos < /var/www/MPOS/sql/000_base_structure.sql
   
   
