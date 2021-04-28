c8a93d21ba77298704ddd9bd493c1bcdc2a5ec9f
NiPeGun <nipegun@gmail.com>
2021-04-28 23:06:47 +0200

    Update PoolCrypto-InstalarYConfigurar.sh

--- SoftInst/Consola/Debian10/PoolCrypto-InstalarYConfigurar.sh
+++ SoftInst/Consola/Debian10/PoolCrypto-InstalarYConfigurar.sh
@@ -218,7 +218,7 @@ menu=(dialog --timeout 5 --checklist "Marca lo que quieras instalar:" 22 76 16)
                apt-get -y install zip
              fi
           unzip /root/SoftInst/Ravencoin/raven-$UltVersRaven-x86_64-linux-gnu.zip
-          mv /root/SoftInst/Ravencoin/linux/raven-$UltVersRaven-x86_64-linux-gnu.tar.gz /root/SoftInst/Ravencoin/
+         # mv /root/SoftInst/Ravencoin/linux/raven-$UltVersRaven-x86_64-linux-gnu.tar.gz /root/SoftInst/Ravencoin/
           rm -rf /root/SoftInst/Ravencoin/raven-$UltVersRaven-x86_64-linux-gnu.zip
           rm -rf /root/SoftInst/Ravencoin/linux/
           rm -rf /root/SoftInst/Ravencoin/__MACOSX/
