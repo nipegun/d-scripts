7b9e6dca0c47e147cce6c948e1d6ddc56f8064e8
NiPeGun <nipegun@gmail.com>
2021-05-03 02:01:34 +0200

    Update Hardware-InfoDisco.sh

--- Hardware-InfoDisco.sh
+++ Hardware-InfoDisco.sh
@@ -5,9 +5,25 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------------------
-#  Script de NiPeGun para mostrar información sobre el hardware de red 
-#-----------------------------------------------------------------------
+#-----------------------------------------------------------------
+#  Script de NiPeGun para mostrar información sobre discos duros
+#-----------------------------------------------------------------
+
+ColorVerde="\033[1;32m"
+FinColor="\033[0m"
+
+echo ""
+echo -e "${ColorVerde}Mostrando información sobre discos...${FinColor}"
+echo ""
+
+## Comprobar si el paquete lshw está instalado. Si no lo está, instalarlo.
+   if [[ $(dpkg-query -s lshw 2>/dev/null | grep installed) == "" ]]; then
+     echo ""
+     echo "lshw no está instalado. Iniciando su instalación..."
+     echo ""
+     apt-get -y update
+     apt-get -y install lshw
+   fi
 
 echo ""
 lshw -class disk
