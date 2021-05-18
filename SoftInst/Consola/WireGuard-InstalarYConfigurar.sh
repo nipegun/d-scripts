7d2799ec996f0335d1e974b20cf7763573f0efba
NiPeGun <nipegun@gmail.com>
2021-05-18 03:27:16 +0200

    Update WireGuard-InstalarYConfigurar.sh

--- SoftInst/Consola/WireGuard-InstalarYConfigurar.sh
+++ SoftInst/Consola/WireGuard-InstalarYConfigurar.sh
@@ -60,6 +60,79 @@ elif [ $OS_VERS == "9" ]; then
   echo "------------------------------------------------------------------------------"
   echo ""
 
+  apt-get -y update
+  apt-get -y install wireguard
+          
+  ## Cargar elmódulo
+     modprobe wireguard
+          
+  ## Crear el archivo de configuración#
+     echo "[Interface]" > /etc/wireguard/wg0.conf
+     echo "Address =" >> /etc/wireguard/wg0.conf
+     #echo "SaveConfig = true" >> /etc/wireguard/wg0.conf
+     echo "PrivateKey =" >> /etc/wireguard/wg0.conf
+     echo "ListenPort = 51820" >> /etc/wireguard/wg0.conf
+     echo "PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> /etc/wireguard/wg0.conf
+     echo "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE" >> /etc/wireguard/wg0.conf
+     echo "SaveConfig = true    # Para que se guarden los nuevos clientes en este archivo desde la línea de comandos" >> /etc/wireguard/wg0.conf
+          
+  ## Agregar la dirección IP del servidor al archivo de configuración
+     DirIP=$(ip a | grep eth0 | grep inet | cut -d '/' -f 1 | cut -d 't' -f 2 | cut -d ' ' -f 2)
+     sed -i -e 's|Address =|Address = '$DirIP'|g' /etc/wireguard/wg0.conf
+          
+  ## Crear las claves pública y privada del servidor
+     mkdir /root/WireGuard/
+     wg genkey >                                                  /root/WireGuard/WireGuardServerPrivate.key
+     cat /root/WireGuard/WireGuardServerPrivate.key | wg pubkey > /root/WireGuard/WireGuardServerPublic.key
+          
+  ## Agregar la clave privada al archivo de configuración
+     VarServerPrivKey=$(cat /root/WireGuard/WireGuardServerPrivate.key)
+     sed -i -e 's|PrivateKey =|PrivateKey = '$VarServerPrivKey'|g' /etc/wireguard/wg0.conf
+
+  ## Crear las claves para el primer usuario
+     wg genkey >                                                 /root/WireGuard/WireGuardUser0Private.key
+     cat /root/WireGuard/WireGuardUserOPrivate.key | wg pubkey > /root/WireGuard/WireGuardUser0Public.key
+
+  ## Agregar el primer usuario al archivo de configuración
+     echo ""  >> /etc/wireguard/wg0.conf
+     echo "[Peer]" >> /etc/wireguard/wg0.conf
+     echo "User0PublicKey =" >> /etc/wireguard/wg0.conf
+     echo "AllowedIPs = 0.0.0.0/0" >> /etc/wireguard/wg0.conf
+          
+  ## Agregar la clave pública del primer usuario al archivo de configuración
+     VarUser0PubKey=$(cat /root/WireGuard/WireGuardUser0Public.key)
+     sed -i -e 's|User0PublicKey =|PublicKey = '$VarUser0PubKey'|g' /etc/wireguard/wg0.conf
+          
+  ## Agregar las reglas para tener salida a Internet desde el servidor
+     iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+     iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+     iptables -A INPUT -p udp -m udp --dport 51820 -m conntrack --ctstate NEW -j ACCEPT
+     iptables -A INPUT -s $DirIP/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+     iptables -A INPUT -s $DirIP/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+     # preferiblemente agregarlas a un archivo de ComandosPostArranque
+          
+  ## Habilitar el forwarding
+     sysctl -w net.ipv4.ip_forward=1
+     #sysctl -w net.ipv6.conf.all.forwarding=1
+          
+  ## Hacer permanente el forwarding
+     sed -i -e 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|g' /etc/sysctl.conf
+     #sed -i -e 's|#net.ipv6.conf.all.forwarding=1|net.ipv6.conf.all.forwarding=1|g' /etc/sysctl.conf
+          
+  ## Levantar la conexión
+     echo ""
+     echo "Levantando la interfaz..."
+     echo ""
+     wg-quick up wg0
+     echo ""
+            
+  ## Activar el servicio
+      echo ""
+      echo "Activando el servicio..."
+      echo ""
+      systemctl enable wg-quick@wg0.service
+      echo ""
+
 elif [ $OS_VERS == "10" ]; then
 
   echo ""
@@ -67,6 +140,60 @@ elif [ $OS_VERS == "10" ]; then
   echo "  Iniciando el script de instalación de WireGuard para Debian 10 (Buster)..."
   echo "------------------------------------------------------------------------------"
   echo ""
+
+  echo "deb http://deb.debian.org/debian buster-backports main" > /etc/apt/sources.list.d/backports.list
+  apt-get -y update > /dev/null
+  apt-get -y autoremove > /dev/null
+  apt-get -y -t buster-backports install wireguard
+          
+  ## Generar la llave Privada
+     cd /etc/wireguard
+     umask 077
+     wg genkey > private.key
+     chmod 600 private.key
+            
+  ## Generar la llave Pública en base a la llave privada
+     wg pubkey < private.key > public.key
+            
+  ## Editar el archivo de configuración
+     echo "[Interface]" > /etc/wireguard/wg0.conf
+     echo "Address =" >> /etc/wireguard/wg0.conf
+     #echo "SaveConfig = true" >> /etc/wireguard/wg0.conf
+     echo "PrivateKey =" >> /etc/wireguard/wg0.conf
+     echo "ListenPort = 51820" >> /etc/wireguard/wg0.conf
+     echo "PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> /etc/wireguard/wg0.conf
+     echo "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE" >> /etc/wireguard/wg0.conf
+     echo "SaveConfig = true       # Para que se guarden los nuevos clientes en este archivo desde la línea de comandos" >> /etc/wireguard/wg0.conf
+            
+  ## Agregar la dirección IP del servidor al archivo de configuración
+     DirIP=$(ip a | grep eth0 | grep inet | cut -d '/' -f 1 | cut -d 't' -f 2 | cut -d ' ' -f 2)
+     sed -i -e 's|Address =|Address = '$DirIP'|g' /etc/wireguard/wg0.conf
+            
+  ## Agregar la clave privada al archivo de configuración
+     ClavePub=$(cat /etc/wireguard/private.key)
+     sed -i -e 's|PrivateKey =|PrivateKey = '$ClavePub'|g' /etc/wireguard/wg0.conf
+            
+  ## Habilitar el forwarding
+     sysctl -w net.ipv4.ip_forward=1
+     #sysctl -w net.ipv6.conf.all.forwarding=1
+            
+  ## Hacer permanente el forwarding
+     sed -i -e 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|g' /etc/sysctl.conf
+     #sed -i -e 's|#net.ipv6.conf.all.forwarding=1|net.ipv6.conf.all.forwarding=1|g' /etc/sysctl.conf
+            
+  ## Levantar la conexión
+     echo ""
+     echo "Levantando la interfaz..."
+     echo ""
+     wg-quick up wg0
+     echo ""
+            
+  ## Activar el servicio
+     echo ""
+     echo "Activando el servicio..."
+     echo ""
+     systemctl enable wg-quick@wg0.service
+     echo ""
 
 elif [ $OS_VERS == "11" ]; then
 
