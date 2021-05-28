022b86ac8e581365cfbdfa2f10cab6954a618b61
NiPeGun <nipegun@gmail.com>
2021-05-28 07:42:16 +0200

    Update Hardware-Discos-Info.sh

--- Hardware-Discos-Info.sh
+++ Hardware-Discos-Info.sh
@@ -19,7 +19,7 @@ echo ""
 ## Comprobar si el paquete lshw está instalado. Si no lo está, instalarlo.
    if [[ $(dpkg-query -s lshw 2>/dev/null | grep installed) == "" ]]; then
      echo ""
-     echo "lshw no está instalado. Iniciando su instalación..."
+     echo "  lshw no está instalado. Iniciando su instalación..."
      echo ""
      apt-get -y update
      apt-get -y install lshw
@@ -34,7 +34,7 @@ echo ""
 ## Comprobar si el paquete nvme-cli está instalado. Si no lo está, instalarlo.
    if [[ $(dpkg-query -s nvme-cli 2>/dev/null | grep installed) == "" ]]; then
      echo ""
-     echo "nvme-cli no está instalado. Iniciando su instalación..."
+     echo "  nvme-cli no está instalado. Iniciando su instalación..."
      echo ""
      apt-get -y update
      apt-get -y install nvme-cli
