71a3b5942bc55fa7390d081c72ce852bb12fd61d
NiPeGun <nipegun@gmail.com>
2021-06-04 10:42:43 +0200

    Update MySQL-BaseDeDatos-Importar.sh

--- MySQL-BaseDeDatos-Importar.sh
+++ MySQL-BaseDeDatos-Importar.sh
@@ -19,7 +19,6 @@ FinColor='\033[0m'
 if [ $# -ne $CantArgsEsperados ]
   then
     echo ""
-    echo "-------------------------------------------------------------------------"
     echo -e "${ColorRojo}Mal uso del script.${FinColor} El uso correcto sería:"
     echo ""
     echo -e "ibdd ${ColorVerde}[UsuarioBD] [PasswordBD] [NombreBD] [RutaArchivoSQL]${FinColor}"
@@ -29,8 +28,6 @@ if [ $# -ne $CantArgsEsperados ]
     echo ""
     echo "Ejemplo 2:"
     echo "$0 pepe 12345678 Cuentas '\Copias de seguridad\Base de datos.sql'"   
-    echo ""
-    echo "-------------------------------------------------------------------------"
     echo ""
     exit $ArgsInsuficientes
   else
