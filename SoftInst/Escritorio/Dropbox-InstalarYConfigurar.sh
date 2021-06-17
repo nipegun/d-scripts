5266bcdad23f7da3f9df4fabb91978b1b24d670a
NiPeGun <nipegun@gmail.com>
2021-06-17 16:22:11 +0200

    Create Dropbox-InstalarYConfigurar.sh

---
+++ SoftInst/Escritorio/Dropbox-InstalarYConfigurar.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#--------------------------------------------------------------------
+#  Script de NiPeGun para instalar y configurar Dropbox en Debian
+#--------------------------------------------------------------------
+
+## Determinar la versión de Debian
+
+   if [ -f /etc/os-release ]; then
+       # Para systemd y freedesktop.org
+       . /etc/os-release
+       OS_NAME=$NAME
+       OS_VERS=$VERSION_ID
+   elif type lsb_release >/dev/null 2>&1; then
+       # linuxbase.org
+       OS_NAME=$(lsb_release -si)
+       OS_VERS=$(lsb_release -sr)
+   elif [ -f /etc/lsb-release ]; then
+       # Para algunas versiones de Debian sin el comando lsb_release
+       . /etc/lsb-release
+       OS_NAME=$DISTRIB_ID
+       OS_VERS=$DISTRIB_RELEASE
+   elif [ -f /etc/debian_version ]; then
+       # Para versiones viejas de Debian.
+       OS_NAME=Debian
+       OS_VERS=$(cat /etc/debian_version)
+   else
+       # Para el viejo uname (También funciona para BSD)
+       OS_NAME=$(uname -s)
+       OS_VERS=$(uname -r)
+   fi
+
+
+if [ $OS_VERS == "7" ]; then
+
+  echo ""
+  echo "-----------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación de Dropbox para Debian 7 (Wheezy)..."
+  echo "-----------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "Instalación para Debian 7 todavía no preparada. Prueba instalarlo en otra versión de Debian"
+  echo ""
+
+elif [ $OS_VERS == "8" ]; then
+
+  echo ""
+  echo "-----------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación de Dropbox para Debian 8 (Jessie)..."
+  echo "-----------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "Instalación para Debian 8 todavía no preparada. Prueba instalarlo en otra versión de Debian"
+  echo ""
+
+elif [ $OS_VERS == "9" ]; then
+
+  echo ""
+  echo "------------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación de Dropbox para Debian 9 (Stretch)..."
+  echo "------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Creando la carpeta para descargar el paquete..."
+  echo ""
+  mkdir -p ~/paquetes/dropbox/
+
+  echo ""
+  echo "  Descargando el paquete desde la web de Dropbox..."
+  echo ""
+  wget -O ~/paquetes/dropbox/dropbox.tar https://www.dropbox.com/download?plat=lnx.x86_64
+
+  echo ""
+  echo "  Descomprimiendo el paquete..."
+  echo ""
+  tar xzf ~/paquetes/dropbox/dropbox.tar -C ~/
+
+  echo ""
+  echo "  Descargando el script de python para controlar Dropbox desde la terminal"
+  echo ""
+  wget -O ~/scripts/DemonioDropbox.py "https://www.dropbox.com/download?dl=packages/dropbox.py"
+  chmod +x ~/scripts/DemonioDropbox.py
+
+  echo ""
+  echo "  Configurando dropbox para que se inicie con el sistema..."
+  echo ""
+  echo "~/scripts/DemonioDropbox.py start" >> /root/scripts/ComandosPostArranque.sh
+
+  echo ""
+  echo "  Arrancando el daemon por primera vez..."
+  echo ""
+  echo "  Toma nota de la url que te pondrá abajo porque deberás ingresar a ella"
+  echo "  desde un nbavegador en el que tengas iniciada la sesión de Dropbox"
+  echo "  para activar el dropbox que acabas de instalar."
+  echo ""
+  ~/.dropbox-dist/dropboxd
+  echo ""
+
+elif [ $OS_VERS == "10" ]; then
+
+  echo ""
+  echo "------------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación de Dropbox para Debian 10 (Buster)..."
+  echo "------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "Instalación para Debian 10 todavía no preparada. Prueba instalarlo en otra versión de Debian"
+  echo ""
+
+elif [ $OS_VERS == "11" ]; then
+
+  echo ""
+  echo "--------------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación de Dropbox para Debian 11 (Bullseye)..."
+  echo "--------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "Instalación para Debian 11 todavía no preparada. Prueba instalarlo en otra versión de Debian"
+  echo ""
+
+fi
