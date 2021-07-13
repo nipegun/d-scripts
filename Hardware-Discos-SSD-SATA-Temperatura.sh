fc619d931bc8f2358e720dbf00f04fad657f288b
NiPeGun <nipegun@gmail.com>
2021-07-13 03:06:36 +0200

    Update Hardware-Discos-SSD-SATA-Temperatura.sh

--- Hardware-Discos-SSD-SATA-Temperatura.sh
+++ Hardware-Discos-SSD-SATA-Temperatura.sh
@@ -22,11 +22,12 @@ echo ""
      do
        if [[ -d "/sys/block/sd$LetraDiscoSATA" ]]; then
          ArrayDiscosSATA[$LetraDiscoSATA]=$(echo "sd$LetraDiscoSATA")
+         echo "sd$LetraDiscoSATA"
        fi
      done
 
 echo ""
-echo "  Total de discos SATA instalados en el sistema : ${#ArrayDiscosSATA[@]}"
+echo "  Total de discos SATA instalados en el sistema: ${#ArrayDiscosSATA[@]}"
 
 ## Comprobar si el paquete hddtemp está instalado. Si no lo está, instalarlo.
    if [[ $(dpkg-query -s hddtemp 2>/dev/null | grep installed) == "" ]]; then
