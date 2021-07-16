a6adf3b568675198eeaf3b8e2429f0957c954b57
NiPeGun <nipegun@gmail.com>
2021-07-16 02:34:28 +0200

    Create Cryptos-Minero-XMRig-Ejecutar.sh

---
+++ Cryptos-Minero-XMRig-Ejecutar.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para instalar y configurar diferentes carteras de criptomonedas en Debian10
+#-------------------------------------------------------------------------------------------------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+DirWallet="451K8ZpJTWdLBKb5uCR1EWM5YfCUxdgxWFjYrvKSTaWpH1zdz22JDQBQeZCw7wZjRm3wqKTjnp9NKZpfyUzncXCJ24H4Xtr"
+Hilos=$(dmidecode -t processor | grep ore | grep ount | cut -d ":" -f 2 | cut -d " " -f 2)
+
+echo ""
+echo -e "${ColorVerde}----------------------------------------------${FinColor}"
+echo -e "${ColorVerde}  Iniciando scripts de ejecución de XMRig...${FinColor}"
+echo -e "${ColorVerde}----------------------------------------------${FinColor}"
+echo ""
+
+echo ""
+echo "  Generando un identificador de dispositivo a partir de la MAC de la WiFi..."
+echo ""
+DirMACWlan0=$(ip addr show wlan0 | grep link/ether | cut -d" " -f6)
+Dispositivo=$(echo -n $DirMACWlan0 | md5sum | cut -d" " -f1)
+echo ""
+echo "El identificador del dispositivo es:"
+echo "$Dispositivo"
+echo ""
+
+echo ""
+echo "  Ejecutando minero con identificador $Dispositivo..."
+echo ""
+
+## Con TLS
+   ~/xmrig/build/xmrig -o pool.minexmr.com:443 --threads=$Hilos --rig-id=$Dispositivo -u $DirWallet --tls 
+
+## Sin TLS
+   ~/xmrig/build/xmrig  -o pool.minexmr.com:4444 --threads=$Hilos --rig-id=$Dispositivo -u $DirWallet
+
