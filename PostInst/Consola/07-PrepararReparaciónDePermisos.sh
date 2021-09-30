170bf5dd953a7e5b703e2c7b2f2d62bd2444fb16
NiPeGun <nipegun@gmail.com>
2021-09-30 19:58:34 +0200

    Update 07-PrepararReparaciónDePermisos.sh

--- PostInst/Consola/07-PrepararReparaciónDePermisos.sh
+++ PostInst/Consola/07-PrepararReparaciónDePermisos.sh
@@ -16,24 +16,24 @@ echo "  CREANDO EL ARCHIVO PARA METER LOS COMANDOS"
 echo "----------------------------------------------"
 echo ""
 mkdir -p /root/scripts/ 2> /dev/null
-echo '#!/bin/bash' > /root/scripts/RepararPermisos.sh
-echo "" >> /root/scripts/RepararPermisos.sh
-echo 'FechaDeEjecucion=$(date +A%YM%mD%d@%T)' >> /root/scripts/RepararPermisos.sh
-echo "" >> /root/scripts/RepararPermisos.sh
+echo '#!/bin/bash'                                                                                   > /root/scripts/RepararPermisos.sh
+echo ""                                                                                             >> /root/scripts/RepararPermisos.sh
+echo 'FechaDeEjecucion=$(date +A%YM%mD%d@%T)'                                                       >> /root/scripts/RepararPermisos.sh
+echo ""                                                                                             >> /root/scripts/RepararPermisos.sh
 echo 'echo "La reparación de permisos se ejecutó el $FechaDeEjecucion" >> /var/log/RepPermisos.log' >> /root/scripts/RepararPermisos.sh
-echo "" >> /root/scripts/RepararPermisos.sh
-echo "#  ESCRIBE ABAJO, UNA POR LÍNEA, LAS TAREAS DE REPARACIÓN DE PERMISOS A EJECUTAR"  >> /root/scripts/RepararPermisos.sh
-echo "#▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼" >> /root/scripts/RepararPermisos.sh
-echo "" >> /root/scripts/RepararPermisos.sh
-echo 'echo ""' >> /root/scripts/RepararPermisos.sh
-echo 'echo "-------------------------"' >> /root/scripts/RepararPermisos.sh
-echo 'echo "  Reparando permisos..."' >> /root/scripts/RepararPermisos.sh
-echo 'echo "-------------------------"' >> /root/scripts/RepararPermisos.sh
-echo 'echo ""' >> /root/scripts/RepararPermisos.sh
-echo 'echo "Reparando permisos web..."' >> /root/scripts/RepararPermisos.sh
-echo 'echo ""' >> /root/scripts/RepararPermisos.sh
-echo "chown -R www-data:www-data /var/www" >> /root/scripts/RepararPermisos.sh
-echo "find /var/www -type d -exec chmod 755 {} \;" >> /root/scripts/RepararPermisos.sh
-echo "find /var/www -type f -exec chmod 644 {} \;" >> /root/scripts/RepararPermisos.sh
-echo "chown root:root /var/www # Necesario para enjaular las cuentas" >> /root/scripts/RepararPermisos.sh
-chmod 700 /root/scripts/RepararPermisos.sh
+echo ""                                                                                             >> /root/scripts/RepararPermisos.sh
+echo "#  ESCRIBE ABAJO, UNA POR LÍNEA, LAS TAREAS DE REPARACIÓN DE PERMISOS A EJECUTAR"             >> /root/scripts/RepararPermisos.sh
+echo "#▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼"           >> /root/scripts/RepararPermisos.sh
+echo ""                                                                                             >> /root/scripts/RepararPermisos.sh
+echo 'echo ""'                                                                                      >> /root/scripts/RepararPermisos.sh
+echo 'echo "-------------------------"'                                                             >> /root/scripts/RepararPermisos.sh
+echo 'echo "  Reparando permisos..."'                                                               >> /root/scripts/RepararPermisos.sh
+echo 'echo "-------------------------"'                                                             >> /root/scripts/RepararPermisos.sh
+echo 'echo ""'                                                                                      >> /root/scripts/RepararPermisos.sh
+echo 'echo "Reparando permisos web..."'                                                             >> /root/scripts/RepararPermisos.sh
+echo 'echo ""'                                                                                      >> /root/scripts/RepararPermisos.sh
+echo "chown -R www-data:www-data /var/www"                                                          >> /root/scripts/RepararPermisos.sh
+echo "find /var/www -type d -exec chmod 755 {} \;"                                                  >> /root/scripts/RepararPermisos.sh
+echo "find /var/www -type f -exec chmod 644 {} \;"                                                  >> /root/scripts/RepararPermisos.sh
+echo "chown root:root /var/www # Necesario para enjaular las cuentas"                               >> /root/scripts/RepararPermisos.sh
+chmod 700                                                                                              /root/scripts/RepararPermisos.sh
