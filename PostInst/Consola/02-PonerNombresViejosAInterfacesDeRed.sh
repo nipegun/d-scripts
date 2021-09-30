fbad5aadb6162fabf9e08879de915e6aac9a476e
NiPeGun <nipegun@gmail.com>
2021-09-30 19:24:44 +0200

    Update 02-PonerNombresViejosAInterfacesDeRed.sh

--- PostInst/Consola/02-PonerNombresViejosAInterfacesDeRed.sh
+++ PostInst/Consola/02-PonerNombresViejosAInterfacesDeRed.sh
@@ -16,11 +16,11 @@ FinColor='\033[0m'
 echo ""
 echo -e "${ColorVerde}Configurando las interfaces de red...${FinColor}"
 echo ""
-echo "auto lo" > /etc/network/interfaces
+echo "auto lo"                   > /etc/network/interfaces
 echo "  iface lo inet loopback" >> /etc/network/interfaces
-echo "" >> /etc/network/interfaces
-echo "auto eth0" >> /etc/network/interfaces
-echo "  allow-hotplug eth0" >> /etc/network/interfaces
-echo "  iface eth0 inet dhcp" >> /etc/network/interfaces
-echo "" >> /etc/network/interfaces
+echo ""                         >> /etc/network/interfaces
+echo "auto eth0"                >> /etc/network/interfaces
+echo "  allow-hotplug eth0"     >> /etc/network/interfaces
+echo "  iface eth0 inet dhcp"   >> /etc/network/interfaces
+echo ""                         >> /etc/network/interfaces
 
