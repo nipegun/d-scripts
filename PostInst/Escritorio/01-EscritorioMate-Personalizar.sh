53681fe1a9c0f7b4caa016803073d8dcef2532df
NiPeGun <nipegun@gmail.com>
2021-09-30 19:49:44 +0200

    Update and rename PostInst/Escritorio/Debian10/PostInstalaciónDeEscritorioMate.sh to PostInst/Escritorio/01-EscritorioMate-Personalizar.sh

---
+++ PostInst/Escritorio/01-EscritorioMate-Personalizar.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#----------------------------------------------------------------------------------
+#  Script de NiPeGun para personalizar el escritorio Mate al acabar de instalarlo
+#----------------------------------------------------------------------------------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+## Determinar la versión de Debian
+
+   if [ -f /etc/os-release ]; then
+       # Para systemd y freedesktop.org
+       . /etc/os-release
+       OS_NAME=$NAME
+       OS_VERS=$VERSION_ID
+   elif type lsb_release >/dev/null 2>&1; then
+       # linuxbase.org
+       OS_NAME=$(lsb_release -si)
+       OS_VERS=$(lsb_release -sr)
+   elif [ -f /etc/lsb-release ]; then
+       # Para algunas versiones de Debian sin el comando lsb_release
+       . /etc/lsb-release
+       OS_NAME=$DISTRIB_ID
+       OS_VERS=$DISTRIB_RELEASE
+   elif [ -f /etc/debian_version ]; then
+       # Para versiones viejas de Debian.
+       OS_NAME=Debian
+       OS_VERS=$(cat /etc/debian_version)
+   else
+       # Para el viejo uname (También funciona para BSD)
+       OS_NAME=$(uname -s)
+       OS_VERS=$(uname -r)
+   fi
+
+if [ $OS_VERS == "7" ]; then
+
+  echo ""
+  echo "--------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de personalización del escritorio Mate en Debian 7 (Wheezy)..."
+  echo "--------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Script para Debian 7 todavía no preparado. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "8" ]; then
+
+  echo ""
+  echo "--------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de personalización del escritorio Mate en Debian 8 (Jessie)..."
+  echo "--------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Script para Debian 8 todavía no preparado. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "9" ]; then
+
+  echo ""
+  echo "---------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de personalización del escritorio Mate en Debian 9 (Stretch)..."
+  echo "---------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Script para Debian 9 todavía no preparado. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "10" ]; then
+
+  echo ""
+  echo "---------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de personalización del escritorio Mate en Debian 10 (Buster)..."
+  echo "---------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo -e "${ColorVerde}  Borrando y reemplazando los wallpapers...${FinColor}"
+  echo ""
+
+  ## Cosmos
+     rm -R '/usr/share/backgrounds/cosmos' 
+
+  ## Mate abstract
+     rm '/usr/share/backgrounds/mate/abstract/Flow.png'
+     rm '/usr/share/backgrounds/mate/abstract/Gulp.png'
+     rm '/usr/share/backgrounds/mate/abstract/Silk.png'
+     rm '/usr/share/backgrounds/mate/abstract/Spring.png'
+     rm '/usr/share/backgrounds/mate/abstract/Waves.png' 
+
+  ## Mate desktop
+     rm '/usr/share/backgrounds/mate/desktop/Float-into-MATE.png' 
+     rm '/usr/share/backgrounds/mate/desktop/GreenTraditional.jpg' 
+     rm '/usr/share/backgrounds/mate/desktop/MATE-Stripes-Dark.png' 
+     rm '/usr/share/backgrounds/mate/desktop/MATE-Stripes-Light.png' 
+     rm '/usr/share/backgrounds/mate/desktop/Ubuntu-Mate-Cold-no-logo.png'
+     rm '/usr/share/backgrounds/mate/desktop/Ubuntu-Mate-Radioactive-no-logo.png'
+     rm '/usr/share/backgrounds/mate/desktop/Ubuntu-Mate-Warm-no-logo.png'
+
+  ## Mate nature
+     rm '/usr/share/backgrounds/mate/nature/Aqua.jpg'
+     rm '/usr/share/backgrounds/mate/nature/Blinds.jpg'
+     rm '/usr/share/backgrounds/mate/nature/Dune.jpg'
+     rm '/usr/share/backgrounds/mate/nature/FreshFlower.jpg'
+     rm '/usr/share/backgrounds/mate/nature/Garden.jpg'
+     rm '/usr/share/backgrounds/mate/nature/GreenMeadow.jpg'
+     rm '/usr/share/backgrounds/mate/nature/LadyBird.jpg'
+     rm '/usr/share/backgrounds/mate/nature/RainDrops.jpg'
+     rm '/usr/share/backgrounds/mate/nature/Storm.jpg'
+     rm '/usr/share/backgrounds/mate/nature/TwoWings.jpg'
+     rm '/usr/share/backgrounds/mate/nature/Wood.jpg'
+     rm '/usr/share/backgrounds/mate/nature/YellowFlower.jpg' 
+     rm -R /usr/share/backgrounds/mate/nature
+
+  ## Themes
+     apt-get -y purge arc-theme
+     apt-get -y purge adapta-gtk-theme
+     apt-get -y purge albatross-gtk-theme
+     apt-get -y purge blackbird-gtk-theme
+     apt-get -y purge bluebird-gtk-theme
+     apt-get -y purge breeze-gtk-theme
+     apt-get -y purge darkcold-gtk-theme
+     apt-get -y purge darkmint-gtk-theme
+     apt-get -y purge greybird-gtk-theme
+     apt-get -y purge materia-gtk-theme
+     apt-get -y purge numix-gtk-theme
+
+  rm -rf '/usr/share/themes/Blackbird'
+  rm -rf '/usr/share/themes/BlackMATE'
+  rm -rf '/usr/share/themes/Bluebird'
+  rm -rf '/usr/share/themes/Blue-Submarine'
+  rm -rf '/usr/share/themes/BlueMenta'
+  rm -rf '/usr/share/themes/ContrastHigh'
+  rm -rf '/usr/share/themes/GreenLaguna'
+  rm -rf '/usr/share/themes/Green-Submarine'
+  rm -rf '/usr/share/themes/Greybird'
+  rm -rf '/usr/share/themes/Greybird-accessibility'
+  rm -rf '/usr/share/themes/Greybird-bright'
+  rm -rf '/usr/share/themes/Greybird-compact'
+  rm -rf '/usr/share/themes/HighContrast'
+  rm -rf '/usr/share/themes/HighContrastInverse'
+  rm -rf '/usr/share/themes/Menta'
+  rm -rf '/usr/share/themes/TraditionalOk' 
+
+  apt-get -y install gnome-icon-theme
+  apt-get -y install arc-theme
+
+  rm -rf '/usr/share/themes/Arc'
+
+  rm -R /usr/share/desktop-base/joy-inksplat-theme
+  rm -R /usr/share/desktop-base/joy-theme
+  rm -R /usr/share/desktop-base/lines-theme
+  rm -R /usr/share/desktop-base/softwaves-theme
+  rm -R /usr/share/desktop-base/spacefun-theme
+  unlink /usr/share/desktop-base/active-theme
+  ln -s /usr/share/desktop-base/moonlight-theme /usr/share/desktop-base/active-theme
+
+  echo ""
+
+elif [ $OS_VERS == "11" ]; then
+
+  echo ""
+  echo "-----------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de personalización del escritorio Mate en Debian 11 (Bullseye)..."
+  echo "-----------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Script para Debian 11 todavía no preparado. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+fi
+
