e5ef81979d7fb19635c4fc2b738bf9983b9afc07
NiPeGun <nipegun@gmail.com>
2021-10-07 21:54:21 +0200

    Create LXC-PrepararContenedorDebian.sh

---
+++ PostInst/Consola/LXC-PrepararContenedorDebian.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl --silent https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/Consola/01-Repositorios-PonerTodos.sh | bash
+curl --silent https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/Consola/03-CambiarIdiomaYTecladoAS%C3%B3loEspa%C3%B1ol.sh | bash
+curl --silent https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/Consola/04-TareasCron-Preparar.sh | bash
+curl --silent https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/Consola/05-ComandosPostArranque-Preparar.sh | bash
+
