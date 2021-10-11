111231558ff312a0169dbf1f651c4c2b62005129
NiPeGun <nipegun@gmail.com>
2021-10-11 22:47:58 +0200

    Update Cryptos-CRP-Minero-InstalarOActualizar.sh

--- SoftInst/Consola/Cryptos-CRP-Minero-InstalarOActualizar.sh
+++ SoftInst/Consola/Cryptos-CRP-Minero-InstalarOActualizar.sh
@@ -12,20 +12,7 @@
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
 FinColor='\033[0m'
-
-## Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
-   if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
-     echo ""
-     echo "  dialog no está instalado. Iniciando su instalación..."
-     echo ""
-     apt-get -y update > /dev/null
-     apt-get -y install dialog
-     echo ""
-   fi
-
-## Pedir el nombre del usuario no-root
-   UsuarioCRPNoRoot=$(dialog --keep-tite --title "Ingresa el nombre para el usuario no-root" --inputbox "Nombre de usuario:" 8 60 3>&1 1>&2 2>&3 3>&- )
-     
+ 
 echo ""
 echo -e "${ColorVerde}-----------------------------------------------------------------${FinColor}"
 echo -e "${ColorVerde}  Iniciando el script de instalación de del minero de Utopia...${FinColor}"
@@ -143,8 +130,10 @@ menu=(dialog --timeout 5 --checklist "Marca los mineros que quieras instalar:" 2
         2)
 
           echo ""
-          echo -e "${ColorVerde}  Moviendo el minero de CRP a la carpeta del usuario $UsuarioCRPNoRoot...${FinColor}"
+          echo -e "${ColorVerde}  Moviendo el minero de CRP a la carpeta del usuario no-root...${FinColor}"
           echo ""
+          ## Pedir el nombre del usuario no-root
+              UsuarioCRPNoRoot=$(dialog --keep-tite --title "Ingresa el nombre para el usuario no-root" --inputbox "Nombre de usuario:" 8 60 3>&1 1>&2 2>&3 3>&- )
           if id "$UsuarioCRPNoRoot" &>/dev/null; then
             ## Crear la carpeta
                mkdir -p  /home/$UsuarioCRPNoRoot/Cryptos/CRP/minero/ 2> /dev/null
