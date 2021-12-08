683115c357a9fb42c13366d308f1c546a207d176
NiPeGun <nipegun@gmail.com>
2021-12-08 22:01:00 +0100

    Update ComandosPostArranque-Preparar.sh

--- PostInst/Consola/ComandosPostArranque-Preparar.sh
+++ PostInst/Consola/ComandosPostArranque-Preparar.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#---------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para preparar los comandos post-arranque (antiguo rc.local)
-#---------------------------------------------------------------------------------
+#
+# Ejecución remota:
+# curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/Consola/ComandosPostArranque-Preparar.sh | bash
+#----------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
