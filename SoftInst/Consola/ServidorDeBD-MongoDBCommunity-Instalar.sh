d93af61952a98221106b2a07c1947560c9610890
NiPeGun <nipegun@gmail.com>
2021-12-21 00:07:15 +0100

    Update ServidorDeBD-MongoDBCommunity-Instalar.sh

--- SoftInst/Consola/ServidorDeBD-MongoDBCommunity-Instalar.sh
+++ SoftInst/Consola/ServidorDeBD-MongoDBCommunity-Instalar.sh
@@ -88,8 +88,159 @@ elif [ $OS_VERS == "10" ]; then
   echo ""
 
   echo ""
-  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo "  Instalando dependencias..."
   echo ""
+  apt-get -y update
+  apt-get -y install dirmngr
+  apt-get -y install gnupg
+  apt-get -y install apt-transport-https
+  apt-get -y install software-properties-common
+  apt-get -y install ca-certificates
+  apt-get -y install curl
+
+  echo ""
+  echo "  Agregando la clave GPG..."
+  echo ""
+  curl -fsSL https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+  
+  echo ""
+  echo "  Agregando el repositorio..."
+  echo ""
+  add-apt-repository 'deb https://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main'
+  
+  echo ""
+  echo "  Instalando paquetes..."
+  echo ""
+  apt-get -y update
+  apt-get -y install mongodb-org
+
+  echo ""
+  echo "  Activando el servicio..."
+  echo ""
+  systemctl enable mongod --now
+
+
+
+
+
+
+
+o verify whether the installation has completed successfully, connect to the MongoDB database server using the mongo tool and print the connection status:
+
+mongo --eval 'db.runCommand({ connectionStatus: 1 })'
+
+The output will look like this:
+
+MongoDB shell version v4.2.1
+connecting to: mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb
+Implicit session: session { "id" : UUID("09f11c53-605f-44ad-abec-ec5801bb6b06") }
+MongoDB server version: 4.2.1
+{
+	"authInfo" : {
+		"authenticatedUsers" : [ ],
+		"authenticatedUserRoles" : [ ]
+	},
+	"ok" : 1
+}
+
+    A value of 1 for the ok field indicates success.
+
+Configuring MongoDB
+
+The MongoDB configuration file is named mongod.conf and is located in the /etc directory. The file is in YAML format.
+
+The default configuration settings are sufficient for most users. However, for production environments, it is recommended to uncomment the security section and enable authorization, as shown below:
+/etc/mongod.conf
+
+security:
+  authorization: enabled
+
+The authorization option enables Role-Based Access Control (RBAC) that regulates users access to database resources and operations. If this option is disabled, each user can access all databases and perform any action.
+
+After editing the configuration file, restart the mongod service for changes to take effect:
+
+sudo systemctl restart mongod
+
+To find more information about the configuration options available in MongoDB 4.2, visit the Configuration File Options documentation page.
+Creating Administrative MongoDB User
+
+If you enabled the MongoDB authentication, you’ll need to create an administrative user that can access and manage the MongoDB instance. To do so, access the mongo shell with:
+
+mongo
+
+From inside the MongoDB shell, type the following command to connect to the admin database:
+
+use admin
+
+switched to db admin
+
+Issue the following command to create a new user named mongoAdmin with the userAdminAnyDatabase role:
+
+db.createUser(
+  {
+    user: "mongoAdmin", 
+    pwd: "changeMe", 
+    roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
+  }
+)
+
+Successfully added user: {
+	"user" : "mongoAdmin",
+	"roles" : [
+		{
+			"role" : "userAdminAnyDatabase",
+			"db" : "admin"
+		}
+	]
+}
+
+You can name the administrative MongoDB user as you want.
+
+Exit the mongo shell with:
+
+quit()
+
+To test the changes, access the mongo shell using the administrative user you have previously created:
+
+mongo -u mongoAdmin -p --authenticationDatabase admin
+
+Enter the password when prompted. Once you are inside the MongoDB shell connect to the admin database:
+
+use admin
+
+switched to db admin
+
+Now, print the users with:
+
+show users
+
+{
+	"_id" : "admin.mongoAdmin",
+	"userId" : UUID("cdc81e0f-db58-4ec3-a6b8-829ad0c31f5c"),
+	"user" : "mongoAdmin",
+	"db" : "admin",
+	"roles" : [
+		{
+			"role" : "userAdminAnyDatabase",
+			"db" : "admin"
+		}
+	],
+	"mechanisms" : [
+		"SCRAM-SHA-1",
+		"SCRAM-SHA-256"
+	]
+}
+
+
+
+
+
+
+
+
+
+
+
 
 elif [ $OS_VERS == "11" ]; then
 
