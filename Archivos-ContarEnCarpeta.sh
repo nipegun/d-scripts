d9c574da126ddaf7746665818fd14538d2728512
NiPeGun <nipegun@gmail.com>
2021-12-25 14:30:06 +0100

    Update Archivos-ContarEnCarpeta.sh

--- Archivos-ContarEnCarpeta.sh
+++ Archivos-ContarEnCarpeta.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para contar la cantidad de archivos que hay en una carpeta
-#------------------------------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Archivos-ContarEnCarpeta.sh | bash
+#-------------------------------------------------------------------------------------------------------
 
 CantArgsEsperados=1
 ArgsInsuficientes=65
