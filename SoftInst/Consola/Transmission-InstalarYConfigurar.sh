befdff7f7d3e679024fc77dbf162adc817c1bcce
NiPeGun <nipegun@gmail.com>
2021-12-26 20:16:28 +0100

    Update Transmission-InstalarYConfigurar.sh

--- SoftInst/Consola/Transmission-InstalarYConfigurar.sh
+++ SoftInst/Consola/Transmission-InstalarYConfigurar.sh
@@ -5,9 +5,9 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------
+#-----------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar Transmission en Debian
-#--------------------------------------------------------------------
+#-----------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
@@ -120,14 +120,14 @@ elif [ $OS_VERS == "10" ]; then
       echo -e "${ColorVerde}Realizando cambios en la configuración...${FinColor}"
       echo ""
       cp /etc/transmission-daemon/settings.json /etc/transmission-daemon/settings.json.bak
-      sed -i -e 's|"download-dir": "/var/lib/transmission-daemon/downloads",|"download-dir": "'$1'",|g' /etc/transmission-daemon/settings.json
+      sed -i -e 's|"download-dir": "/var/lib/transmission-daemon/downloads",|"download-dir": "'$1'",|g'     /etc/transmission-daemon/settings.json
       sed -i -e 's|"incomplete-dir": "/var/lib/transmission-daemon/Downloads",|"incomplete-dir": "'$2'",|g' /etc/transmission-daemon/settings.json
-      sed -i -e 's|"incomplete-dir-enabled": false,|"incomplete-dir-enabled": true,|g' /etc/transmission-daemon/settings.json
-      sed -i -e 's|"ratio-limit-enabled": false,|"ratio-limit-enabled": true,|g' /etc/transmission-daemon/settings.json
-      sed -i -e 's|^.*"rpc-password":.*|    "rpc-password": "'$3'",|g' /etc/transmission-daemon/settings.json
-      sed -i -e 's|"rpc-whitelist": "127.0.0.1",|"rpc-whitelist": "127.0.0.1, 192.168.*.*, 10.0.*.*",|g' /etc/transmission-daemon/settings.json
-      sed -i -e 's|"trash-original-torrent-files": false,|"trash-original-torrent-files": true,|g' /etc/transmission-daemon/settings.json
-      sed -i -e 's|"umask": 18,|"umask": 2,|g' /etc/transmission-daemon/settings.json
+      sed -i -e 's|"incomplete-dir-enabled": false,|"incomplete-dir-enabled": true,|g'                      /etc/transmission-daemon/settings.json
+      sed -i -e 's|"ratio-limit-enabled": false,|"ratio-limit-enabled": true,|g'                            /etc/transmission-daemon/settings.json
+      sed -i -e 's|^.*"rpc-password":.*|    "rpc-password": "'$3'",|g'                                      /etc/transmission-daemon/settings.json
+      sed -i -e 's|"rpc-whitelist": "127.0.0.1",|"rpc-whitelist": "127.0.0.1, 192.168.*.*, 10.0.*.*",|g'    /etc/transmission-daemon/settings.json
+      sed -i -e 's|"trash-original-torrent-files": false,|"trash-original-torrent-files": true,|g'          /etc/transmission-daemon/settings.json
+      sed -i -e 's|"umask": 18,|"umask": 2,|g'                                                              /etc/transmission-daemon/settings.json
 
       echo ""
       echo -e "${ColorVerde}Agregando el usuario al grupo transmission-daemon...${FinColor}"
@@ -174,8 +174,88 @@ elif [ $OS_VERS == "11" ]; then
   echo "--------------------------------------------------------------------------------"
   echo ""
 
-  echo ""
-  echo "Instalación para Debian 11 todavía no preparada. Prueba instalarlo en otra versión de Debian"
-  echo ""
+CantArgsRequeridos=4
+  ArgumentosInsuficientes=65
+
+  if [ $# -ne $CantArgsRequeridos ]
+    then
+      echo ""
+      echo "--------------------------------------------------------------------------------------------"
+      echo -e "${ColorRojo}Mal uso del script.${FinColor} El uso correcto sería:"
+      echo ""
+      echo -e "$0 ${ColorVerde}[CarpetaDeDescargas] [CarpetaDeIncompletos] [Password] [Usuario]${FinColor}"
+      echo ""
+      echo "  Ejemplo:"
+      echo "  $0 /var/tmp/transmission/completos /var/tmp/transmission/incompletos 12345678 nico"
+      echo "--------------------------------------------------------------------------------------------"
+      echo ""
+      exit $ArgumentosInsuficientes
+    else
+      echo ""
+      echo -e "${ColorVerde}Creando las carpetas para las descargas...${FinColor}"
+      echo ""
+      mkdir -p $1
+      mkdir -p $2
+    
+      echo ""
+      echo -e "${ColorVerde}Instalando el paquete transission-daemon...${FinColor}"
+      echo ""
+      apt-get -y install transmission-daemon
+
+      echo ""
+      echo -e "${ColorVerde}Deteniendo el servicio transmission-daemon${FinColor}"
+      echo ""
+      service transmission-daemon stop
+
+      echo ""
+      echo -e "${ColorVerde}Realizando cambios en la configuración...${FinColor}"
+      echo ""
+      cp /etc/transmission-daemon/settings.json /etc/transmission-daemon/settings.json.bak
+      sed -i -e 's|"download-dir": "/var/lib/transmission-daemon/downloads",|"download-dir": "'$1'",|g'     /etc/transmission-daemon/settings.json
+      sed -i -e 's|"incomplete-dir": "/var/lib/transmission-daemon/Downloads",|"incomplete-dir": "'$2'",|g' /etc/transmission-daemon/settings.json
+      sed -i -e 's|"incomplete-dir-enabled": false,|"incomplete-dir-enabled": true,|g'                      /etc/transmission-daemon/settings.json
+      sed -i -e 's|"ratio-limit-enabled": false,|"ratio-limit-enabled": true,|g'                            /etc/transmission-daemon/settings.json
+      sed -i -e 's|^.*"rpc-password":.*|    "rpc-password": "'$3'",|g'                                      /etc/transmission-daemon/settings.json
+      sed -i -e 's|"rpc-whitelist": "127.0.0.1",|"rpc-whitelist": "127.0.0.1, 192.168.*.*, 10.0.*.*",|g'    /etc/transmission-daemon/settings.json
+      sed -i -e 's|"trash-original-torrent-files": false,|"trash-original-torrent-files": true,|g'          /etc/transmission-daemon/settings.json
+      sed -i -e 's|"umask": 18,|"umask": 2,|g'                                                              /etc/transmission-daemon/settings.json
+
+      echo ""
+      echo -e "${ColorVerde}Agregando el usuario al grupo transmission-daemon...${FinColor}"
+      echo ""
+      usermod -a -G debian-transmission $4
+
+      echo ""
+      echo -e "${ColorVerde}Cambiando el grupo propietario de lsa carpetas $1 y $2...${FinColor}"
+      echo ""
+      chgrp debian-transmission $1
+      chgrp debian-transmission $2
+    
+      echo ""
+      echo -e "${ColorVerde}Dando permisos de escritura al grupo...${FinColor}"
+      echo ""
+      chmod 770 $1
+      chmod 770 $2
+
+      echo ""
+      echo -e "${ColorVerde}Iniciando el servicio transmission-daemon...${FinColor}"
+      echo ""
+      service transmission-daemon start
+
+      echo ""
+      echo "---------------------------------------------------------"
+      echo "  EL DEMONIO TRANSMISSION HA SIDO INSTALADO E INICIADO."
+      echo ""
+      echo "  Deberías poder administrarlo mediante web en la IP de"
+      echo "  este ordenador seguida por :9091"
+      echo ""
+      echo "  Ejemplo: 192.168.0.120:9091"
+      echo ""
+      echo "  Nombre de usuario: transmission"
+      echo "  Contraseña: $3"
+      echo "---------------------------------------------------------"
+      echo ""
+
+  fi
 
 fi
