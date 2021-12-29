a59b0e0e9bbfb8457b4170ea3401dd3a64344683
NiPeGun <nipegun@gmail.com>
2021-12-29 12:40:51 +0100

    Update SistemaOperativo-Actualizar.sh

--- SistemaOperativo-Actualizar.sh
+++ SistemaOperativo-Actualizar.sh
@@ -15,41 +15,41 @@ FinColor='\033[0m'
 
 echo ""
 #echo "$(tput setab 2)$(tput setaf 7)Iniciando el script de actualización del sistema operativo...$(tput sgr 0)"
-echo -e "${ColorVerde}-------------------------------------------------------------${FinColor}"
-echo -e "${ColorVerde}Iniciando el script de actualización del sistema operativo...${FinColor}"
-echo -e "${ColorVerde}-------------------------------------------------------------${FinColor}"
+echo -e "${ColorVerde}-----------------------------------------------------------------${FinColor}"
+echo -e "${ColorVerde}  Iniciando el script de actualización del sistema operativo...${FinColor}"
+echo -e "${ColorVerde}-----------------------------------------------------------------${FinColor}"
 echo ""
 
 echo ""
-echo -e "${ColorVerde}Reparando permisos de la carpeta /tmp/ ...${FinColor}"
+echo -e "${ColorVerde}  Reparando permisos de la carpeta /tmp/ ...${FinColor}"
 echo ""
 chmod 1777 /tmp
 
 echo ""
-echo -e "${ColorVerde}Ejecutando apt-get update...${FinColor}"
+echo -e "${ColorVerde}  Ejecutando apt-get update...${FinColor}"
 echo ""
 apt-get -y update
 
 echo ""
-echo -e "${ColorVerde}Ejecutando apt-get -y upgrade...${FinColor}"
+echo -e "${ColorVerde}  Ejecutando apt-get -y upgrade...${FinColor}"
 echo ""
 apt-get -y --allow-downgrades upgrade
 
 echo ""
-echo -e "${ColorVerde}Ejecutando apt-get -y dist-upgrade...${FinColor}"
+echo -e "${ColorVerde}  Ejecutando apt-get -y dist-upgrade...${FinColor}"
 echo ""
 apt-get -y --allow-downgrades dist-upgrade
 
 echo ""
-echo -e "${ColorVerde}Ejecutando apt-get -y autoremove...${FinColor}"
+echo -e "${ColorVerde}  Ejecutando apt-get -y autoremove...${FinColor}"
 echo ""
 apt-get -y autoremove
 
 echo ""
 echo ""
-echo -e "${ColorVerde}--------------------------------------------------------${FinColor}"
-echo -e "${ColorVerde}Script para actualizar el sistema operativo, finalizado.${FinColor}"
-echo -e "${ColorVerde}--------------------------------------------------------${FinColor}"
+echo -e "${ColorVerde}------------------------------------------------------------${FinColor}"
+echo -e "${ColorVerde}  Script para actualizar el sistema operativo, finalizado.${FinColor}"
+echo -e "${ColorVerde}------------------------------------------------------------${FinColor}"
 echo ""
 echo ""
 
