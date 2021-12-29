46d9a641fefe3841079c263f41a3e1f70b265a4e
NiPeGun <nipegun@gmail.com>
2021-12-29 12:38:54 +0100

    Update Cryptos-CRP-Minero-ResetearMineroCRP.sh

--- Cryptos-CRP-Minero-ResetearMineroCRP.sh
+++ Cryptos-CRP-Minero-ResetearMineroCRP.sh
@@ -54,7 +54,7 @@ if [ $OS_VERS == "7" ]; then
   echo ""
 
   echo ""
-  echo "  Script para Debian 7 todavía no preparado. Prueba instalarlo en otra versión de Debian."
+  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
   echo ""
 
 elif [ $OS_VERS == "8" ]; then
@@ -66,7 +66,7 @@ elif [ $OS_VERS == "8" ]; then
   echo ""
 
   echo ""
-  echo "  Script para Debian 8 todavía no preparado. Prueba instalarlo en otra versión de Debian."
+  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
   echo ""
 
 elif [ $OS_VERS == "9" ]; then
@@ -78,7 +78,7 @@ elif [ $OS_VERS == "9" ]; then
   echo ""
 
   echo ""
-  echo "  Script para Debian 9 todavía no preparado. Prueba instalarlo en otra versión de Debian."
+  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
   echo ""
 
 elif [ $OS_VERS == "10" ]; then
@@ -90,7 +90,7 @@ elif [ $OS_VERS == "10" ]; then
   echo ""
 
   echo ""
-  echo "  Script para Debian 10 todavía no preparado. Prueba instalarlo en otra versión de Debian."
+  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
   echo ""
 
 elif [ $OS_VERS == "11" ]; then
