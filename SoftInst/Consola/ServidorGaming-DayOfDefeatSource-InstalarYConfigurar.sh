a6260855fa980de5a2c9c7ea6f1c370615e2be40
NiPeGun <nipegun@gmail.com>
2021-12-31 17:38:59 +0100

    Update and rename SoftInst/Consola/Debian10/ServidorGaming-DayOfDefeatSource-InstalarYConfigurar.sh to SoftInst/Consola/ServidorGaming-DayOfDefeatSource-InstalarYConfigurar.sh

---
+++ SoftInst/Consola/ServidorGaming-DayOfDefeatSource-InstalarYConfigurar.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#------------------------------------------------------------------------------
+#  Script de NiPeGun para instalar el servidor gaming de Day of Defeat Source
+#
+#  Ejecución remota:
+#  curl -s | bash
+#------------------------------------------------------------------------------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+## Determinar la versión de Debian
+
+   if [ -f /etc/os-release ]; then
+       # Para systemd y freedesktop.org
+       . /etc/os-release
+       OS_NAME=$NAME
+       OS_VERS=$VERSION_ID
+   elif type lsb_release >/dev/null 2>&1; then
+       # linuxbase.org
+       OS_NAME=$(lsb_release -si)
+       OS_VERS=$(lsb_release -sr)
+   elif [ -f /etc/lsb-release ]; then
+       # Para algunas versiones de Debian sin el comando lsb_release
+       . /etc/lsb-release
+       OS_NAME=$DISTRIB_ID
+       OS_VERS=$DISTRIB_RELEASE
+   elif [ -f /etc/debian_version ]; then
+       # Para versiones viejas de Debian.
+       OS_NAME=Debian
+       OS_VERS=$(cat /etc/debian_version)
+   else
+       # Para el viejo uname (También funciona para BSD)
+       OS_NAME=$(uname -s)
+       OS_VERS=$(uname -r)
+   fi
+
+if [ $OS_VERS == "7" ]; then
+
+  echo ""
+  echo "-----------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación de xxxxxxxxx para Debian 7 (Wheezy)..."
+  echo "-----------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "8" ]; then
+
+  echo ""
+  echo "-----------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación de xxxxxxxxx para Debian 8 (Jessie)..."
+  echo "-----------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "9" ]; then
+
+  echo ""
+  echo "------------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación de xxxxxxxxx para Debian 9 (Stretch)..."
+  echo "------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "10" ]; then
+
+  echo ""
+  echo "------------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación de xxxxxxxxx para Debian 10 (Buster)..."
+  echo "------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo -e "${ColorVerde}Instalando el servidor dodsserver...${FinColor}"
+  echo ""
+
+  echo ""
+  echo "  Instalando dependencias..."
+  echo ""
+  dpkg --add-architecture i386
+  apt-get update
+  apt-get -y install mailutils
+  apt-get -y install postfix
+  apt-get -y install curl
+  apt-get -y install wget
+  apt-get -y install file
+  apt-get -y install tar
+  apt-get -y install bzip2
+  apt-get -y insta ll gzip
+  apt-get -y installunzip
+  apt-get -y install bsdmainutils
+  apt-get -y install python
+  apt-get -y install util-linux
+  apt-get -y install ca-certificates
+  apt-get -y install binutils
+  apt-get -y install bc
+  apt-get -y install jq
+  apt-get -y install tmux
+  apt-get -y install lib32gcc1
+  apt-get -y install libstdc++6
+  apt-get -y install lib32stdc++6
+  apt-get -y install steamcmd
+
+  # comandos a ejecutar manualmente
+  # adduser dodsserver
+  # su - dodsserver
+  # wget -O linuxgsm.sh https://linuxgsm.sh
+  # chmod +x linuxgsm.sh
+  # bash linuxgsm.sh dodsserver
+  #./dodsserver install
+
+elif [ $OS_VERS == "11" ]; then
+
+  echo ""
+  echo "--------------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación de xxxxxxxxx para Debian 11 (Bullseye)..."
+  echo "--------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 11 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+fi
+
