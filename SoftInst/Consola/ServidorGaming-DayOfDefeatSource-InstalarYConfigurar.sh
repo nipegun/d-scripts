d7249674598c8b94f597c896810756efd4f4e2ad
NiPeGun <nipegun@gmail.com>
2021-12-31 18:08:55 +0100

    Update ServidorGaming-DayOfDefeatSource-InstalarYConfigurar.sh

--- SoftInst/Consola/ServidorGaming-DayOfDefeatSource-InstalarYConfigurar.sh
+++ SoftInst/Consola/ServidorGaming-DayOfDefeatSource-InstalarYConfigurar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar el servidor gaming de Day of Defeat Source
 #
 #  Ejecución remota:
-#  curl -s | bash
-#------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/Consola/ServidorGaming-DayOfDefeatSource-InstalarYConfigurar.sh | bash
+#----------------------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
