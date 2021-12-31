9e2bc3f762b79ecc51db15b0f6103230bcfd6950
NiPeGun <nipegun@gmail.com>
2021-12-31 20:51:45 +0100

    Update ServidorGaming-MinecraftJE-InstalarYConfigurar.sh

--- SoftInst/Consola/ServidorGaming-MinecraftJE-InstalarYConfigurar.sh
+++ SoftInst/Consola/ServidorGaming-MinecraftJE-InstalarYConfigurar.sh
@@ -115,6 +115,12 @@ elif [ $OS_VERS == "10" ]; then
   apt-get -y install netcat
   apt-get -y install default-jre
 
+  echo ""
+  echo "  Dependencias instaladas."
+  echo "  Revisa el script porque hay comandos que tendrás que ejecutar manualmente"
+  echo "  para terminar de instalar el servidor de MinecraftJE."
+  echo ""
+
   # Comandos a ejecutar manualmente
   # adduser mcserver
   # su - mcserver
@@ -132,8 +138,47 @@ elif [ $OS_VERS == "11" ]; then
   echo ""
 
   echo ""
-  echo "  Comandos para Debian 11 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo -e "${ColorVerde}Instalando el servidor mcserver...${FinColor}"
   echo ""
+
+  echo ""
+  echo "  Instalando dependencias..."
+  echo ""
+  dpkg --add-architecture i386
+  apt-get -y update
+  apt-get -y install mailutils
+  apt-get -y install postfix
+  apt-get -y install curl
+  apt-get -y install wget
+  apt-get -y install file
+  apt-get -y install tar
+  apt-get -y install bzip2
+  apt-get -y install gzip
+  apt-get -y install unzip
+  apt-get -y install bsdmainutils
+  apt-get -y install python
+  apt-get -y install util-linux
+  apt-get -y install ca-certificates
+  apt-get -y install binutils
+  apt-get -y install bc
+  apt-get -y install jq
+  apt-get -y install tmux
+  apt-get -y install netcat
+  apt-get -y install default-jre
+
+  echo ""
+  echo "  Dependencias instaladas."
+  echo "  Revisa el script porque hay comandos que tendrás que ejecutar manualmente"
+  echo "  para terminar de instalar el servidor de MinecraftJE."
+  echo ""
+
+  # Comandos a ejecutar manualmente
+  # adduser mcserver
+  # su - mcserver
+  # wget -O linuxgsm.sh https://linuxgsm.sh
+  # chmod +x linuxgsm.sh
+  # bash linuxgsm.sh mcserver
+  # ./mcserver install
 
 fi
 
