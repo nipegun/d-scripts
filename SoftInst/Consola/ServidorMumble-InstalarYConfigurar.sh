c88dfc590481932e01310a8109c09af202787b40
NiPeGun <nipegun@gmail.com>
2022-01-01 06:36:30 +0100

    Update and rename SoftInst/Consola/Debian09/ServidorMumble-InstalarYConfigurar.sh to SoftInst/Consola/ServidorMumble-InstalarYConfigurar.sh

--- SoftInst/Consola/ServidorMumble-InstalarYConfigurar.sh
+++ SoftInst/Consola/ServidorMumble-InstalarYConfigurar.sh
@@ -12,18 +12,6 @@
 #  curl -s | bash
 #--------------------------------------------------------------------
 
-
-#!/bin/bash
-
-# Pongo a disposición pública este script bajo el término de "software de dominio público".
-# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
-# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
-# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
-
-#--------------------------------------------------------------------
-#  Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
-#--------------------------------------------------------------------
-
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
 FinColor='\033[0m'
