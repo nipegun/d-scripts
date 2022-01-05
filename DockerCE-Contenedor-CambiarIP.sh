f3c9690b7e547760c10fc5e9f0e70d78ba5fac44
NiPeGun <nipegun@gmail.com>
2022-01-05 19:16:44 +0100

    Update DockerCE-Contenedor-CambiarIP.sh

--- DockerCE-Contenedor-CambiarIP.sh
+++ DockerCE-Contenedor-CambiarIP.sh
@@ -12,20 +12,28 @@
 #  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/DockerCE-Contenedor-CambiarIP.sh | bash
 #------------------------------------------------------------------------------------------------------------
 
-NombreContenedor="portainer"
+NombreContenedor="portainers"
 NuevaIP="172.17.0.1"
 
-## Comprobar si el paquete jq está instalado. Si no lo está, instalarlo.
-   if [[ $(dpkg-query -s jq 2>/dev/null | grep installed) == "" ]]; then
+## Comprobar si el contenedor está corriendo. Si no lo está, abortar.
+   if [[ $(docker ps 2>/dev/null | grep $NombreContenedor) == "" ]]; then
      echo ""
-     echo "  jq no está instalado. Iniciando su instalación..."
+     echo "  El contenedor $NombreContenedor no se está ejecutando."
+     echo "  No se puede cambiar la IP de un contenedor que no se está ejecutando."
      echo ""
-     apt-get -y update > /dev/null
-     apt-get -y install jq
-     echo ""
+   else
+     ## Comprobar si el paquete jq está instalado. Si no lo está, instalarlo.
+        if [[ $(dpkg-query -s jq 2>/dev/null | grep installed) == "" ]]; then
+          echo ""
+          echo "  jq no está instalado. Iniciando su instalación..."
+          echo ""
+          apt-get -y update > /dev/null
+          apt-get -y install jq
+          echo ""
+        fi
+     Red=$(docker inspect $NombreContenedor -f "{{json .NetworkSettings.Networks }}" | jq .bridge.NetworkID | cut -d'"' -f2)
+     #docker network ls
+     docker network disconnect $Red $NombreContenedor
+     docker network connect --ip $NuevaIP $Red $NombreContenedor
    fi
-Red=$(docker inspect $NombreContenedor -f "{{json .NetworkSettings.Networks }}" | jq .bridge.NetworkID | cut -d'"' -f2)
 
-#docker network ls
-docker network disconnect $Red $NombreContenedor
-docker network connect --ip $NuevaIP $Red $NombreContenedor
