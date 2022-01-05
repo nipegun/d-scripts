9d7e1191a731bd3155da7a189e4e1609520e0884
NiPeGun <nipegun@gmail.com>
2022-01-05 19:07:41 +0100

    Update DockerCE-Contenedor-CambiarIP.sh

--- DockerCE-Contenedor-CambiarIP.sh
+++ DockerCE-Contenedor-CambiarIP.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#------------------------------------------------------------------
+#  Script de NiPeGun para cambiar la IP de un container de Docker
+#------------------------------------------------------------------
+
 NombreContenedor="portainer"
 NuevaIP="172.17.0.1"
 
