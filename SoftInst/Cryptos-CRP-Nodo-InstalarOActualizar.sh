01d66ece574d3c3a1999b13c8183bfca8e8db60e
NiPeGun <nipegun@gmail.com>
2022-01-08 21:50:15 +0100

    Update Cryptos-CRP-Nodo-InstalarOActualizar.sh

--- SoftInst/Cryptos-CRP-Nodo-InstalarOActualizar.sh
+++ SoftInst/Cryptos-CRP-Nodo-InstalarOActualizar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar el messenger de Utopia
 #
 #  Ejecución remota:
-#  curl -s | bash
-#-----------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/Cryptos-CRP-Nodo-InstalarOActualizar.sh | bash
+#----------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
