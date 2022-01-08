c71f8c2b8a1989c2cf9d838debad906584ce76d5
NiPeGun <nipegun@gmail.com>
2022-01-08 21:52:09 +0100

    Update Cryptos-CRP-Minero-InstalarOActualizar.sh

--- SoftInst/Consola/Cryptos-CRP-Minero-InstalarOActualizar.sh
+++ SoftInst/Consola/Cryptos-CRP-Minero-InstalarOActualizar.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar los mineros para las diferentes criptomonedas
-#----------------------------------------------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/Consola/Cryptos-CRP-Minero-InstalarOActualizar.sh | bash
+#--------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
