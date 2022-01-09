bc5e690b8904ef47df051b0752ba8d9c107fd2d8
NiPeGun <nipegun@gmail.com>
2022-01-09 03:50:07 +0100

    Create LVM-Grupos-MostrarInfo.sh

---
+++ LVM-Grupos-MostrarInfo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#----------------------------------------------------------------------
+#  Script de NiPeGun para mostrar info de los grupos de volúmenes LVM
+#
+#  Ejecución remota:
+#  curl -s | bash
+#----------------------------------------------------------------------
+
+echo ""
+echo "  Mostrando info de grupos de volúmenes..."
+echo ""
+vgs
+echo ""
+
+echo ""
+echo "  Mostrando info de grupos de volúmenes..."
+echo ""
+vgdisplay
+echo ""
+
