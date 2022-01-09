9f62abb6979cb7afbce99a8f410b70f59f403187
NiPeGun <nipegun@gmail.com>
2022-01-09 01:45:45 +0100

    Update Telegram-EnviarHTML.sh

--- Telegram-EnviarHTML.sh
+++ Telegram-EnviarHTML.sh
@@ -7,6 +7,27 @@
 
 #--------------------------------------------------------------------------
 #  Script de NiPeGun para enviar HTML a Telegram desde Bash usando un bot
+#
+#  Etiquetas soportadas:
+#
+#  <b>bold</b>
+#  <strong>bold</strong>
+#  <i>italic</i>
+#  <em>italic</em>
+#  <u>underline</u>
+#  <ins>underline</ins>
+#  <s>strikethrough</s>
+#  <strike>strikethrough</strike>
+#  <del>strikethrough</del>
+#  <span class="tg-spoiler">spoiler</span>
+#  <tg-spoiler>spoiler</tg-spoiler>
+#  <b>bold <i>italic bold <s>italic bold strikethrough <span class="tg-spoiler">italic bold strikethrough spoiler</span></s> <u>underline italic bold</u></i> bold</b>
+#  <a href="http://www.example.com/">inline URL</a>
+#  <a href="tg://user?id=123456789">inline mention of a user</a>
+#  <code>inline fixed-width code</code>
+#  <pre></pre>
+#  <pre><code class="language-python"></code></pre>
+#
 #--------------------------------------------------------------------------
 
 ColorAdvertencia="\033[1;31m"
