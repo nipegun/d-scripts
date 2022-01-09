93fc04bb1ccb6b76322025faeed69656095d92ca
NiPeGun <nipegun@gmail.com>
2022-01-09 03:54:26 +0100

    Create LVM-GrupVol-Crear.sh

---
+++ LVM-GrupVol-Crear.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-------------------------------------------------------------
+#  Script de NiPeGun para crear un grupo de volúmenes LVM
+#
+#  Ejecución remota:
+#  curl -s | bash
+#----------------------------------------------------------------------
+
+vgcreate $1
+
