62e2d12519da42ff18707e33445917fb3b19ac3f
NiPeGun <nipegun@gmail.com>
2022-01-09 04:12:20 +0100

    Create LVM-VolumLogi-Extender.sh

---
+++ LVM-VolumLogi-Extender.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#--------------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para extender volúmenes lógicos LVM
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/LVM-VolumLogi-MostrarInfo.sh | bash
+#--------------------------------------------------------------------------------------------------------
+
+lvextend $1
+
