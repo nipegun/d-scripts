5acb76b0b559b01b571316db2ab29a7b59fc2df7
NiPeGun <nipegun@gmail.com>
2022-01-27 00:25:59 +0100

    Update ServidorDeBD-OracleDatabaseXE-Instalar.sh

--- SoftInst/Consola/ServidorDeBD-OracleDatabaseXE-Instalar.sh
+++ SoftInst/Consola/ServidorDeBD-OracleDatabaseXE-Instalar.sh
@@ -281,6 +281,7 @@ elif [ $OS_VERS == "11" ]; then
                  echo ""
                  echo "  Configurando instancia ejecutando:"
                  echo ""
+                 sed -i -e 's|LISTENER_PORT=|LISTENER_PORT=1521|g' /etc/sysconfig/oracle-xe-21c.conf
                  echo "  $ArchivoInitD configure"
                  echo ""
                  echo -e "Oracle0\nOracle0" | $ArchivoInitD configure
