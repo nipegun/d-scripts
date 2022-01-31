9dba31b5e51c6db3595edb5538b9d7c285a040c3
NiPeGun <nipegun@gmail.com>
2022-01-31 08:52:52 +0100

    Update Usuario-UsuarioX-AutologuearEnMateDesktop-Desactivar.sh

--- Usuario-UsuarioX-AutologuearEnMateDesktop-Desactivar.sh
+++ Usuario-UsuarioX-AutologuearEnMateDesktop-Desactivar.sh
@@ -5,10 +5,13 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para desactivar el logueo automático del usuariox en mate-desktop
 #
 #  Ejecución remota:
-#  curl -s  | bash
-#--------------------------------------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Usuario-UsuarioX-AutologuearEnMateDesktop-Desactivar.sh | bash
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+sed -i -e 's|autologin-user=usuariox|#autologin-user=|g'           /etc/lightdm/lightdm.conf
+sed -i -e 's|autologin-user-timeout=2|#autologin-user-timeout=0|g' /etc/lightdm/lightdm.conf
 
