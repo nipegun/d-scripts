3185691cdcc01c67561e989183ba0c4d8d8c66d9
NiPeGun <nipegun@gmail.com>
2022-02-06 14:14:51 +0100

    Update and rename Servidor-NextCloud-InstalarYConfigurar.s to Servidor-NextCloud-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Servidor-NextCloud-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Servidor-NextCloud-InstalarYConfigurar.sh
@@ -106,9 +106,46 @@ elif [ $OS_VERS == "9" ]; then
   echo "------------------------------------------------------------------------------"
   echo ""
 
-  echo ""
-  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
+  CantArgsCorrectos=1
+  ArgsInsuficientes=65
+
+  if [ $# -ne $CantArgsCorrectos ]
+    then
+      echo ""
+      echo "------------------------------------------------------------------------------"
+      echo "Mal uso del script."
+      echo ""
+      echo "El uso correcto sería: InstalarYConfigurarServidorNextCloud [Password]"
+      echo ""
+      echo "Ejemplo:"
+      echo ' InstalarYConfigurarServidorNextCloud ausdhg76atsd'
+      echo "------------------------------------------------------------------------------"
+      echo ""
+      exit $ArgsInsuficientes
+    else
+      echo ""
+      echo "------------------------------------------------------"
+      echo "  Instalando y configurando el servidor NextCloud..."
+      echo "------------------------------------------------------"
+      echo ""
+      apt-get -y update
+      tasksel install web-server
+      systemctl enable apache2
+      apt-get -y install mariadb-server
+      systemctl enable mysql
+      apt-get -y install php7.0-xml php7.0-cgi php7.0-cli php7.0-gd php7.0-curl php7.0-zip php7.0-mysql php7.0-mbstring 
+      systemctl start apache2
+      systemctl start mysql
+      mysql_secure_installation
+      cbddyu nextcloud nextcloud $1
+      mkdir /var/www/html/nextcloud
+      cd /var/www/html
+      apt-get -y install wget
+      wget --no-check-certificate http://hacks4geeks.com/_/premium/descargas/debian/root/paquetes/nextcloud/nextcloud.zip
+      apt-get -y install unzip
+      unzip nextcloud.zip
+  
+  fi
 
 elif [ $OS_VERS == "10" ]; then
 
