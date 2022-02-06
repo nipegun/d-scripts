64745bcd273e86924910c6f07d9f02b060117716
NiPeGun <nipegun@gmail.com>
2022-02-06 13:21:55 +0100

    Update ServidorMumble-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/ServidorMumble-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/ServidorMumble-InstalarYConfigurar.sh
@@ -124,9 +124,48 @@ elif [ $OS_VERS == "10" ]; then
   echo "------------------------------------------------------------------------------"
   echo ""
 
-  echo ""
-  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
+  # Actualizar los repositorios
+    apt-get -y update
+
+  # Instalar el paquete
+    apt-get -y install mumble-server
+
+  # Reconfigurarlo
+    echo ""
+    echo "----------------------------------------------------------------------"
+    echo "  RECONFIGURANDO EL SERVIDOR MUMBLE..."
+    echo "  TE PEDIRÁ QUE INGRESES DOS VECES LA NUEVA CONTRASEÑA DEL SuperUser"
+    echo "----------------------------------------------------------------------"
+    dpkg-reconfigure mumble-server
+
+  # Detener el servicio para hacerle cambios a la configuración
+    service mumble-server stop
+
+  # Hacer copia de seguridad del archivo de configuración
+    cp /etc/mumble-server.ini /etc/mumble-server.ini.bak
+
+  # Modificar el archivo de configuración
+    sed -i -e 's|welcometext="<br />Welcome to this server running <b>Murmur</b>.<br />Enjoy your stay!<br />"|welcometext="<br />Bienvenido al servidor <b>Mumble</b>.<br />"|g' /etc/mumble-server.ini
+    sed -i -e 's|bandwidth=72000|bandwidth=128000|g' /etc/mumble-server.ini
+    sed -i -e 's|;bonjour=True|bonjour=True|g' /etc/mumble-server.ini
+    sed -i -e 's|;sslCert=|;sslCert=/etc/letsencrypt/live/dominioejemplo.com/cert.pem|g' /etc/mumble-server.ini
+    sed -i -e 's|;sslKey=|;sslKey=/etc/letsencrypt/live/dominioejemplo.com/privkey.pem|g' /etc/mumble-server.ini
+    sed -i "/;sslKey=/a ;sslCA=/etc/letsencrypt/live/dominioejemplo.com/fullchain.pem" /etc/mumble-server.ini
+
+  # Borrar los datos del certificado SSL autogenerado
+  # Hacer siempre despùés de que se agregue el certificado en /etc/mumble-server.ini
+  # De hecho, lo correcto, después de modificar /etc/mumble-server.ini, sera hacer:
+  # service mumble-server stop
+  # murmurd -wipessl
+  # pkill murmurd
+  # service mumble-server start
+    murmurd -wipessl
+
+  # Matar todos los procesos murmurd
+    pkill murmurd
+
+  # Re-arrancar el servicio
+    service mumble-server start
 
 elif [ $OS_VERS == "11" ]; then
 
@@ -137,30 +176,31 @@ elif [ $OS_VERS == "11" ]; then
   echo ""
 
   # Actualizar los repositorios
-  apt-get -y update
+    apt-get -y update
 
   # Instalar el paquete
-  apt-get -y install mumble-server
+    apt-get -y install mumble-server
 
   # Detener el servicio para hacerle cambios a la configuración
-  service mumble-server stop
+    service mumble-server stop
+
   # Reconfigurarlo
-  echo ""
-  echo "----------------------------------------------------------------------"
-  echo "  RECONFIGURANDO EL SERVIDOR MUMBLE..."
-  echo "  TE PEDIRÁ QUE INGRESES DOS VECES LA NUEVA CONTRASEÑA DEL SuperUser"
-  echo "----------------------------------------------------------------------"
-  dpkg-reconfigure mumble-server
+    echo ""
+    echo "----------------------------------------------------------------------"
+    echo "  RECONFIGURANDO EL SERVIDOR MUMBLE..."
+    echo "  TE PEDIRÁ QUE INGRESES DOS VECES LA NUEVA CONTRASEÑA DEL SuperUser"
+    echo "----------------------------------------------------------------------"
+    dpkg-reconfigure mumble-server
 
   # Hacer copia de seguridad del archivo de configuración
-  cp /etc/mumble-server.ini /etc/mumble-server.ini.bak
+    cp /etc/mumble-server.ini /etc/mumble-server.ini.bak
 
   # Modificar el archivo de configuración
-  sed -i -e 's|welcometext="<br />Welcome to this server running <b>Murmur</b>.<br />Enjoy your stay!<br />"|welcometext="<br />Bienvenido al servidor <b>Mumble</b>.<br />"|g' /etc/mumble-server.ini
-  sed -i -e 's|#bonjour=True|bonjour=True|g'                                            /etc/mumble-server.ini
-  sed -i -e 's|#sslCert=|#sslCert=/etc/letsencrypt/live/dominioejemplo.com/cert.pem|g'  /etc/mumble-server.ini
-  sed -i -e 's|#sslKey=|#sslKey=/etc/letsencrypt/live/dominioejemplo.com/privkey.pem|g' /etc/mumble-server.ini
-  sed -i "/#sslKey=/a #sslCA=/etc/letsencrypt/live/dominioejemplo.com/fullchain.pem"    /etc/mumble-server.ini
+    sed -i -e 's|welcometext="<br />Welcome to this server running <b>Murmur</b>.<br />Enjoy your stay!<br />"|welcometext="<br />Bienvenido al servidor <b>Mumble</b>.<br />"|g' /etc/mumble-server.ini
+    sed -i -e 's|#bonjour=True|bonjour=True|g'                                            /etc/mumble-server.ini
+    sed -i -e 's|#sslCert=|#sslCert=/etc/letsencrypt/live/dominioejemplo.com/cert.pem|g'  /etc/mumble-server.ini
+    sed -i -e 's|#sslKey=|#sslKey=/etc/letsencrypt/live/dominioejemplo.com/privkey.pem|g' /etc/mumble-server.ini
+    sed -i "/#sslKey=/a #sslCA=/etc/letsencrypt/live/dominioejemplo.com/fullchain.pem"    /etc/mumble-server.ini
 
   # Borrar los datos del certificado SSL autogenerado
   # Hacer siempre despùés de que se agregue el certificado en /etc/mumble-server.ini
@@ -169,13 +209,13 @@ elif [ $OS_VERS == "11" ]; then
   # murmurd -wipessl
   # pkill murmurd
   # service mumble-server start
-  murmurd -wipessl
+    murmurd -wipessl
 
   # Matar todos los procesos murmurd
-  pkill murmurd
+    pkill murmurd
 
   # Re-arrancar el servicio
-  service mumble-server start
+    service mumble-server start
 
 fi
 
