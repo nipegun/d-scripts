d498f5aba9d268fa9d68589702487e15097d13e7
NiPeGun <nipegun@gmail.com>
2022-02-10 22:12:31 +0100

    Update xrdp-Sesiones-Monitorizar.sh

--- xrdp-Sesiones-Monitorizar.sh
+++ xrdp-Sesiones-Monitorizar.sh
@@ -27,6 +27,7 @@
   tail -f /var/log/xrdp-sesman.log | grep --line-buffered "reated session" | while read line
     do
       echo "${line}" | tee -a /var/log/XRDPWatcher.log
-      /root/scripts/xrdp-NotificarSes.sh ""${line}""
+      TextoAEnviar=$(echo "${line}")
+      /root/scripts/xrdp-NotificarSes.sh "$TextoAEnviar"
     done
 
