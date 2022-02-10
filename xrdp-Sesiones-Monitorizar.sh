3ac0489b33ec9f897dc475400d324f5d69bc982b
NiPeGun <nipegun@gmail.com>
2022-02-10 21:23:28 +0100

    Update xrdp-Sesiones-Monitorizar.sh

--- xrdp-Sesiones-Monitorizar.sh
+++ xrdp-Sesiones-Monitorizar.sh
@@ -26,7 +26,7 @@
   echo ""
   tail -f /var/log/xrdp-sesman.log | grep "reated session" | while read line;
     do
-      echo "${line}" >> touch /var/log/XRDPWatcher.log
+      echo "${line}" #>> touch /var/log/XRDPWatcher.log
       #if [ $? = 0 ] ; then
       #  echo "${line}" | mailx -s "error in messages file" your@emailaddress.com
       #fi
