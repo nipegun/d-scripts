b41a7bf4dd15fcb78fcbc492985c27cac72c9ffe
NiPeGun <nipegun@gmail.com>
2022-02-10 20:07:52 +0100

    Update xrdp-Monitorear-Conexion.sh

--- xrdp-Monitorear-Conexion.sh
+++ xrdp-Monitorear-Conexion.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para monitorizar las conexiones xrdp
-#----------------------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/xrdp-Monitorear-Conexion.sh | bash
+#---------------------------------------------------------------------------------------------------------
 
 ## Comprobar si el paquete disown está instalado. Si no lo está, instalarlo.
    if [[ $(dpkg-query -s disown 2>/dev/null | grep installed) == "" ]]; then
