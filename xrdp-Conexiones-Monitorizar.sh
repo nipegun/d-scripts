f093ac7988431dbce48dc733b093bf0894e017fb
NiPeGun <nipegun@gmail.com>
2022-02-10 22:36:34 +0100

    Update xrdp-Conexiones-Monitorizar.sh

--- xrdp-Conexiones-Monitorizar.sh
+++ xrdp-Conexiones-Monitorizar.sh
@@ -29,8 +29,8 @@
       echo "${line}" | tee -a /var/log/XRDPWatcher.log
       LineaGrep1=$(echo "${line}")
       LineaGrep2=$(echo "$LineaGrep1" | sed 's-[--g' | 's-]--g' | sed 's-INFO--g')
-      echo "$LineaGrep1"
-      echo "$LineaGrep2"
+      echo "$LineaGrep1" | tee -a /var/log/XRDPWatcher.log
+      echo "$LineaGrep2" | tee -a /var/log/XRDPWatcher.log
       TextoAEnviar=$(echo "$LineaGrep2" | sed 's-connected client computer name-Host intentando conectarse por xrdp-g')
       /root/scripts/xrdp-NotificarCon.sh "$TextoAEnviar"
     done
