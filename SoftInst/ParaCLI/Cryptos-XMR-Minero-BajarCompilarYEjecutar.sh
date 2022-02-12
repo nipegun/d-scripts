3e16b98452252c9fb4a658052c1ac03660b2028c
NiPeGun <nipegun@gmail.com>
2022-02-12 23:01:03 +0100

    Update Cryptos-XMR-Minero-BajarCompilarYEjecutar.sh

--- SoftInst/ParaCLI/Cryptos-XMR-Minero-BajarCompilarYEjecutar.sh
+++ SoftInst/ParaCLI/Cryptos-XMR-Minero-BajarCompilarYEjecutar.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------------------------------------------------------------
-#  Script de NiPeGun para instalar y configurar diferentes carteras de criptomonedas en Debian10
-#-------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para instalar y configurar diferentes carteras de criptomonedas en Debian
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Cryptos-XMR-Minero-BajarCompilarYEjecutar.sh | bash
+#-------------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
