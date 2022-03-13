ce7e8123787d89ffb18a22e30793471d03ea69d9
NiPeGun <nipegun@gmail.com>
2022-03-13 19:25:53 +0100

    Update Servidor-ERP-Odoo-InstalarDesdeWebs.sh

--- SoftInst/ParaCLI/Servidor-ERP-Odoo-InstalarDesdeWebs.sh
+++ SoftInst/ParaCLI/Servidor-ERP-Odoo-InstalarDesdeWebs.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#---------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar el servidor ERP de Odoo
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-ERP-Odoo-InstalarYConfigurar.sh | bash
-#---------------------------------------------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-ERP-Odoo-InstalarDesdeWebs.sh | bash
+#-------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
@@ -101,7 +101,7 @@ elif [ $OS_VERS == "11" ]; then
   echo ""
 
   echo ""
-  echo "  Iniciando la instalación de la base de datos PostgreSQL para Odoo..."
+  echo "  Instalando la base de datos PostgreSQL..."
   echo ""
   # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
     if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
@@ -129,7 +129,7 @@ elif [ $OS_VERS == "11" ]; then
     #su - postgres -c "createdb $BaseDeDatosPSQL"
 
   echo ""
-  echo "  Iniciando la instalación de wkhtmltopdf..."
+  echo "  Instalando wkhtmltopdf..."
   echo ""
   # Determinar la URL del archivo a bajar
     # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
@@ -145,39 +145,36 @@ elif [ $OS_VERS == "11" ]; then
     rm -rf /root/SoftInst/wkhtmltopdf/
     mkdir -p /root/SoftInst/wkhtmltopdf/ 2> /dev/null
     cd /root/SoftInst/wkhtmltopdf/
-    echo ""
-    echo "  Descargando el .deb de wkhtmltopdf..."
-    echo ""
     wget https://github.com/$vSubURL -O wkhtmltopdf.deb
-    echo ""
-    echo "  Instalando el .deb de wkhtmltopdf..."
-    echo ""
     dpkg -i /root/SoftInst/wkhtmltopdf/wkhtmltopdf.deb
 
   echo ""
   echo "  Iniciando la instalación de Odoo..."
   echo ""
-  # Comprobar si el paquete gnupg2 está instalado. Si no lo está, instalarlo.
-    if [[ $(dpkg-query -s gnupg2 2>/dev/null | grep installed) == "" ]]; then
-      echo ""
-      echo "  gnupg2 no está instalado. Iniciando su instalación..."
-      echo ""
-      apt-get -y update > /dev/null
-      apt-get -y install gnupg2
-      echo ""
-    fi
   # Agregar llave para firmar repositorio
     mkdir -p /root/aptkeys/ 2> /dev/null
     wget -q -O- https://nightly.odoo.com/odoo.key -O /root/aptkeys/odoo.key
     gpg --dearmor /root/aptkeys/odoo.key
     cp /root/aptkeys/odoo.key.gpg /usr/share/keyrings/odoo.gpg
   # Agregar repositorio
+    # Comprobar si el paquete gnupg2 está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s gnupg2 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo "  gnupg2 no está instalado. Iniciando su instalación..."
+        echo ""
+        apt-get -y update > /dev/null
+        apt-get -y install gnupg2
+        echo ""
+      fi
     vUltVersOdoo=$(curl -s http://nightly.odoo.com/ | grep blob | grep -v master | head -n1 | cut -d '"' -f2 | sed 's-blob/-\n-g' | grep -v http | cut -d '/' -f1)
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/odoo.gpg] http://nightly.odoo.com/$vUltVersOdoo/nightly/deb/ ./" > /etc/apt/sources.list.d/odoo.list
   # Actualizar el cache de paquetes
     apt-get -y update
   # Instalar la última versión de Odoo
     apt-get -y install odoo
-
+    systemctl enable --now odoo
+    echo ""
+    ss -tunelp | grep 8069
+    echo ""
 fi
 
