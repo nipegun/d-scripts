b565b6f1eff475e1f45042155f83f84046f3f0ba
NiPeGun <nipegun@gmail.com>
2022-03-13 16:42:46 +0100

    Update Servidor-ERP-Odoo-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Servidor-ERP-Odoo-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Servidor-ERP-Odoo-InstalarYConfigurar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar el servidor ERP de Odoo
 #
 #  Ejecución remota:
-#  curl -s x | bash
-#------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-ERP-Odoo-InstalarYConfigurar.sh | bash
+#---------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
