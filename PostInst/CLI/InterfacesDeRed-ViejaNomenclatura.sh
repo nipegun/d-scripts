29f91f39a795addda4c0fdbb81e2ff39315676ff
NiPeGun <nipegun@gmail.com>
2022-03-19 11:10:37 +0100

    Update InterfacesDeRed-ViejaNomenclatura.sh

--- PostInst/CLI/InterfacesDeRed-ViejaNomenclatura.sh
+++ PostInst/CLI/InterfacesDeRed-ViejaNomenclatura.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------------------------------------------
-#  Script de NiPeGun para poner los nombres de las interfces de red a la nomenclatura usada en Debian 8
-#--------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para poner los nombres de las interfaces de red a la nomenclatura usada en Debian 8
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/InterfacesDeRed-ViejaNomenclatura.sh | bash
+#-------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
