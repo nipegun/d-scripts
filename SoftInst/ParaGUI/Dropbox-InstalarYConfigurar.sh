895fd9f08cc569e8f1ad19680f611536c46a1236
NiPeGun <nipegun@gmail.com>
2022-03-19 12:40:48 +0100

    Update Dropbox-InstalarYConfigurar.sh

--- SoftInst/ParaGUI/Dropbox-InstalarYConfigurar.sh
+++ SoftInst/ParaGUI/Dropbox-InstalarYConfigurar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar Dropbox en Debian
 #
 #  Ejecución remota:
-#  curl -s x | bash
-#--------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/Dropbox-InstalarYConfigurar.sh | bash
+#-----------------------------------------------------------------------------------------------------------------------------
 
 ## Determinar la versión de Debian
 
