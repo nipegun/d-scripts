aba9cd466f8f1769c557c28f628bd734426027fb
NiPeGun <nipegun@gmail.com>
2022-03-19 12:26:59 +0100

    Update Ethernet-Realtek10ec-8125.sh

--- PostInst/Controladores/Ethernet-Realtek10ec-8125.sh
+++ PostInst/Controladores/Ethernet-Realtek10ec-8125.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar el controlador de la tarjeta ethernet Realtek 10ec-8125
-#-------------------------------------------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/Controladores/Ethernet-Realtek10ec-8125.sh | bash
+#---------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
