d886e757d6477026c0bd866d11c9b282b86639bc
NiPeGun <nipegun@gmail.com>
2022-03-19 12:38:44 +0100

    Update Cryptos-XCH-Nodo-InstalarYConfigurar.sh

--- SoftInst/ParaGUI/Cryptos-XCH-Nodo-InstalarYConfigurar.sh
+++ SoftInst/ParaGUI/Cryptos-XCH-Nodo-InstalarYConfigurar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar la cadena de bloques de Chia (XCH)
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/Cryptos-XCH-Nodo-InstalarYConfigurar.sh | bash
-#----------------------------------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/Cryptos-XCH-Nodo-InstalarYConfigurar.sh | bash
+#--------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
