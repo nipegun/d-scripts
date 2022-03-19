8770fd8c6ce801fe3649a498eab6d22175437c4d
NiPeGun <nipegun@gmail.com>
2022-03-19 12:37:50 +0100

    Update Cryptos-LTC-Nodo-InstalarYConfigurar.sh

--- SoftInst/ParaGUI/Cryptos-LTC-Nodo-InstalarYConfigurar.sh
+++ SoftInst/ParaGUI/Cryptos-LTC-Nodo-InstalarYConfigurar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar la cadena de bloques de Litecoin (LTC)
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/Cryptos-LTC-Nodo-InstalarYConfigurar.sh | bash
-#----------------------------------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/Cryptos-LTC-Nodo-InstalarYConfigurar.sh | bash
+#--------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
