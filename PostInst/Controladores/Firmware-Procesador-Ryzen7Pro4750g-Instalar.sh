eddc6c7aa06bbe93fe3e701b180f1498b3b72cba
NiPeGun <nipegun@gmail.com>
2022-03-19 12:27:42 +0100

    Update Firmware-Procesador-Ryzen7Pro4750g-Instalar.sh

--- PostInst/Controladores/Firmware-Procesador-Ryzen7Pro4750g-Instalar.sh
+++ PostInst/Controladores/Firmware-Procesador-Ryzen7Pro4750g-Instalar.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar el firmware para el procesador Ryzen 7 Pro 4750G en Debian
-#----------------------------------------------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/Controladores/Firmware-Procesador-Ryzen7Pro4750g-Instalar.sh | bash
+#---------------------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
