fa6e66b7b79d3e5e1d23a60acf58881d366ec5db
NiPeGun <nipegun@gmail.com>
2022-03-19 11:12:26 +0100

    Update InterfacesDeRed-LoopBackYeth0ConNombresViejos.sh

--- PostInst/CLI/InterfacesDeRed-LoopBackYeth0ConNombresViejos.sh
+++ PostInst/CLI/InterfacesDeRed-LoopBackYeth0ConNombresViejos.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para re-escribir /etc/network/interfaceds con los nombres antiguos de las interfaces de red
-#-----------------------------------------------------------------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/InterfacesDeRed-LoopBackYeth0ConNombresViejos.sh
+#------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
