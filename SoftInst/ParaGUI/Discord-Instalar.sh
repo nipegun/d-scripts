d3982ff2869a9c032d3a712842cebe6d04e181a3
NiPeGun <nipegun@gmail.com>
2022-03-19 12:39:29 +0100

    Update Discord-Instalar.sh

--- SoftInst/ParaGUI/Discord-Instalar.sh
+++ SoftInst/ParaGUI/Discord-Instalar.sh
@@ -7,6 +7,9 @@
 
 #--------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar Discord en Debian
+#
+#  Ejecución remota:
+#  curl -s x | bash
 #--------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
