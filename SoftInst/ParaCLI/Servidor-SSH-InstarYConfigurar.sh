7327bce66d209755d6a6316a7d0d93c2f73ef050
NiPeGun <nipegun@gmail.com>
2022-03-21 16:26:05 +0100

    Update Servidor-SSH-InstarYConfigurar.sh

--- SoftInst/ParaCLI/Servidor-SSH-InstarYConfigurar.sh
+++ SoftInst/ParaCLI/Servidor-SSH-InstarYConfigurar.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------
-#  Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
-#--------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#  Script de NiPeGun para instalar y configurar el servidor OpenSSH en Debian
+#
+#  Ejecución remota:
+#  curl -s x | bash
+#------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
