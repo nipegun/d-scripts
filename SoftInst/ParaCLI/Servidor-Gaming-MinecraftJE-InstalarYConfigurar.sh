09c05fe625dbc33e4a8d59236f1a4467e5dbc27b
NiPeGun <nipegun@gmail.com>
2022-03-21 16:16:25 +0100

    Update Servidor-Gaming-MinecraftJE-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Servidor-Gaming-MinecraftJE-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Servidor-Gaming-MinecraftJE-InstalarYConfigurar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar el servidor gaming de MinecraftJE (mcserver)
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/Consola/ServidorGaming-MinecraftJE-InstalarYConfigurar.sh | bash
-#----------------------------------------------------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-Gaming-MinecraftJE-InstalarYConfigurar.sh | bash
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
