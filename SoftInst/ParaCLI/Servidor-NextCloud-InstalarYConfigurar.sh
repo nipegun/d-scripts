456bb613596ecf165113816be60bd4165abf1441
NiPeGun <nipegun@gmail.com>
2022-03-21 16:23:21 +0100

    Update Servidor-NextCloud-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Servidor-NextCloud-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Servidor-NextCloud-InstalarYConfigurar.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------
-#  SCRIPT DE NIPEGUN PARA INSTALAR Y CONFIGURAR NEXTCLOUD
-#----------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para instalar y configurar NextCloud en Debian
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-NextCloud-InstalarYConfigurar.sh
+#---------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
