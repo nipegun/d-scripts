b25021300e2cc1d99c3e27a9b0aa1a9ed6f698d9
NiPeGun <nipegun@gmail.com>
2022-03-21 16:10:08 +0100

    Update Servidor-CompartArchivos-uhub-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Servidor-CompartArchivos-uhub-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Servidor-CompartArchivos-uhub-InstalarYConfigurar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar uhub en Debian
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/Consola/uhub-InstalarYConfigurar.sh | bash
-#------------------------------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-CompartArchivos-uhub-InstalarYConfigurar.sh | bash
+#---------------------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
