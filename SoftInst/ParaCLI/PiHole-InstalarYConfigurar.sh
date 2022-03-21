b6f7a6c21baf0210483e29d5a51b26caf2eb355b
NiPeGun <nipegun@gmail.com>
2022-03-21 15:59:31 +0100

    Update PiHole-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/PiHole-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/PiHole-InstalarYConfigurar.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar PiHole en Debian
-#--------------------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/PiHole-InstalarYConfigurar.sh | bash
+#----------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
