983b5e3bd5e1ddb4d6b1682f6a2a350c652e7647
NiPeGun <nipegun@gmail.com>
2022-03-21 16:56:09 +0100

    Update Cryptos-Pools-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Cryptos-Pools-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Cryptos-Pools-InstalarYConfigurar.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar Pools Cripto en Debian
-#--------------------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Cryptos-Pools-InstalarYConfigurar.sh | bash
+#-----------------------------------------------------------------------------------------------------------------------------------
 
 UsuarioNoRoot="pooladmin"
 DominioPool="localhost"
