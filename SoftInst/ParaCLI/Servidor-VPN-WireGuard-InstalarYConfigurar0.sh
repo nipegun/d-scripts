efc56491e432a7e8ad9ebcf9596798d697f65eab
NiPeGun <nipegun@gmail.com>
2022-03-22 11:04:55 +0100

    Update and rename Servidor-VPN-WireGuard-InstalarYConfigurar.sh to Servidor-VPN-WireGuard-InstalarYConfigurar0.sh

--- SoftInst/ParaCLI/Servidor-VPN-WireGuard-InstalarYConfigurar0.sh
+++ SoftInst/ParaCLI/Servidor-VPN-WireGuard-InstalarYConfigurar0.sh
@@ -414,15 +414,98 @@ elif [ $OS_VERS == "11" ]; then
   echo "--------------------------------------------------------------------------------"
   echo ""
 
-  ## Borrar WireGuard si ya está instalado
-     mv /etc/wireguard/wg0.conf /etc/wireguard/wg0.conf.bak 2> /dev/null
-     mv /root/WireGuard/WireGuardServerPrivate.key /root/WireGuard/WireGuardServerPrivate.key.bak 2> /dev/null
-     mv /root/WireGuard/WireGuardServerPublic.key  /root/WireGuard/WireGuardServerPublic.key.bak  2> /dev/null
-     mv /root/WireGuard/WireGuardUser0Private.key  /root/WireGuard/WireGuardUser0Private.key.bak  2> /dev/null
-     mv /root/WireGuard/WireGuardUser0Public.key   /root/WireGuard/WireGuardUser0Public.key.bak   2> /dev/null
-     apt-get -y purge wireguard
-     apt-get -y purge
-     apt-get -y autoremove
+  # Borrar WireGuard si ya está instalado
+    mv /etc/wireguard/wg0.conf /etc/wireguard/wg0.conf.bak 2> /dev/null
+    mv /root/WireGuard/WireGuardServerPrivate.key /root/WireGuard/WireGuardServerPrivate.key.bak 2> /dev/null
+    mv /root/WireGuard/WireGuardServerPublic.key  /root/WireGuard/WireGuardServerPublic.key.bak  2> /dev/null
+    mv /root/WireGuard/WireGuardUser0Private.key  /root/WireGuard/WireGuardUser0Private.key.bak  2> /dev/null
+    mv /root/WireGuard/WireGuardUser0Public.key   /root/WireGuard/WireGuardUser0Public.key.bak   2> /dev/null
+    apt-get -y purge wireguard
+    apt-get -y purge
+    apt-get -y autoremove
+
+# Instalar el paquete WireGuard
+    apt-get -y update > /dev/null
+    apt-get -y autoremove > /dev/null
+    apt-get -y install wireguard
+
+  # Crear el archivo de configuración#
+    echo "[Interface]"                                                                                                                                                                                                                    > /etc/wireguard/wg0.conf
+    echo "Address ="                                                                                                                                                                                                                     >> /etc/wireguard/wg0.conf
+    echo "PrivateKey ="                                                                                                                                                                                                                  >> /etc/wireguard/wg0.conf
+    echo "ListenPort = 51820"                                                                                                                                                                                                            >> /etc/wireguard/wg0.conf
+    echo "PostUp =   iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $InterfazEthernet -j MASQUERADE; ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o $InterfazEthernet -j MASQUERADE" >> /etc/wireguard/wg0.conf
+    echo "PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $InterfazEthernet -j MASQUERADE; ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o $InterfazEthernet -j MASQUERADE" >> /etc/wireguard/wg0.conf
+    echo "SaveConfig = true    # Para que se guarden los nuevos clientes en este archivo desde la línea de comandos"                                                                                                                     >> /etc/wireguard/wg0.conf
+
+  # Agregar la dirección IP del servidor al archivo de configuración
+    DirIP=$(ip a | grep $InterfazEthernet | grep inet | cut -d '/' -f 1 | cut -d 't' -f 2 | cut -d ' ' -f 2)
+    sed -i -e 's|Address =|Address = "$DirIP"|g' /etc/wireguard/wg0.conf
+
+  # Crear las claves pública y privada del servidor
+    mkdir /root/WireGuard/
+    wg genkey >                                                  /root/WireGuard/WireGuardServerPrivate.key
+    cat /root/WireGuard/WireGuardServerPrivate.key | wg pubkey > /root/WireGuard/WireGuardServerPublic.key
+    chmod 600 /root/WireGuard/WireGuardServerPrivate.key
+
+  # Agregar la clave privada al archivo de configuración
+    VarServerPrivKey=$(cat /root/WireGuard/WireGuardServerPrivate.key)
+    sed -i -e 's|PrivateKey =|PrivateKey = "$VarServerPrivKey"|g' /etc/wireguard/wg0.conf
+
+  # Crear las claves para el primer usuario
+    wg genkey >                                                 /root/WireGuard/WireGuardUser0Private.key
+    cat /root/WireGuard/WireGuardUser0Private.key | wg pubkey > /root/WireGuard/WireGuardUser0Public.key
+
+  # Agregar el primer usuario al archivo de configuración
+    echo ""                       >> /etc/wireguard/wg0.conf
+    echo "[Peer]"                 >> /etc/wireguard/wg0.conf
+    echo "User0PublicKey ="       >> /etc/wireguard/wg0.conf
+    echo "AllowedIPs = 0.0.0.0/0" >> /etc/wireguard/wg0.conf
+
+  # Agregar la clave pública del primer usuario al archivo de configuración
+    VarUser0PubKey=$(cat /root/WireGuard/WireGuardUser0Public.key)
+    sed -i -e 's|User0PublicKey =|PublicKey = "$VarUser0PubKey"|g' /etc/wireguard/wg0.conf
+
+  # Agregar las reglas para tener salida a Internet desde el servidor
+    iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    iptables -A INPUT -p udp -m udp --dport 51820 -m conntrack --ctstate NEW -j ACCEPT
+    iptables -A INPUT -s $DirIP/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+    iptables -A INPUT -s $DirIP/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+
+  # Agregar las reglas a los ComandosPostArranque
+    touch /root/scripts/ReglasIPTablesWireGuard.sh
+    echo "iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"                        > /root/scripts/ReglasIPTablesWireGuard.sh
+    echo "iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"                     >> /root/scripts/ReglasIPTablesWireGuard.sh
+    echo "iptables -A INPUT -p udp -m udp --dport 51820 -m conntrack --ctstate NEW -j ACCEPT"           >> /root/scripts/ReglasIPTablesWireGuard.sh
+    echo "iptables -A INPUT -s $DirIP/24 -p tcp -m tcp --dport 53 -m conntrack --ctstate NEW -j ACCEPT" >> /root/scripts/ReglasIPTablesWireGuard.sh
+    echo "iptables -A INPUT -s $DirIP/24 -p udp -m udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT" >> /root/scripts/ReglasIPTablesWireGuard.sh
+    chmod +x /root/scripts/ReglasIPTablesWireGuard.sh
+    touch /root/scripts/ComandosPostArranque.sh
+    echo "/root/scripts/ReglasIPTablesWireGuard.sh" >> /root/scripts/ComandosPostArranque.sh
+    chmod +x /root/scripts/ComandosPostArranque.sh
+
+  # Habilitar el forwarding
+    sysctl -w net.ipv4.ip_forward=1
+    #sysctl -w net.ipv6.conf.all.forwarding=1
+
+  # Hacer permanente el forwarding
+    sed -i -e 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|g' /etc/sysctl.conf
+    #sed -i -e 's|#net.ipv6.conf.all.forwarding=1|net.ipv6.conf.all.forwarding=1|g' /etc/sysctl.conf
+
+  # Levantar la conexión
+    echo ""
+    echo "  Levantando la interfaz..."
+    echo ""
+    wg-quick up wg0
+    echo ""
+
+  # Activar el servicio
+    echo ""
+    echo "  Activando el servicio..."
+    echo ""
+    systemctl enable wg-quick@wg0.service
+    echo ""
 
 fi
 
