db523c255a171924620e3c21d88a7d69973e223a
NiPeGun <nipegun@gmail.com>
2022-03-27 22:50:17 +0200

    Update EscritorioMate-InstalarSoftware.sh

--- PostInst/GUI/EscritorioMate-InstalarSoftware.sh
+++ PostInst/GUI/EscritorioMate-InstalarSoftware.sh
@@ -88,72 +88,70 @@ elif [ $OS_VERS == "10" ]; then
 
   apt-get -y update
 
-  ## Sistema
-     apt-get -y install gparted
-     apt-get -y install caja-open-terminal
-     apt-get -y install caja-admin
-     apt-get -y install hardinfo
-     apt-get -y install bleachbit
+  # Sistema
+    apt-get -y install gparted
+    apt-get -y install caja-open-terminal
+    apt-get -y install caja-admin
+    apt-get -y install hardinfo
+    apt-get -y install bleachbit
 
-  ## Multimedia
-     apt-get -y install vlc
-     apt-get -y install vlc-plugin-vlsub
-     apt-get -y install audacity
-     apt-get -y install subtitleeditor
-     apt-get -y install easytag
-     apt-get -y install openshot
+  # Multimedia
+    apt-get -y install vlc
+    apt-get -y install vlc-plugin-vlsub
+    apt-get -y install audacity
+    apt-get -y install subtitleeditor
+    apt-get -y install easytag
+    apt-get -y install openshot
 
-  ## Redes
-     apt-get -y install gufw
-     apt-get -y install wireshark
-     apt-get -y install etherape
-     apt-get -y install sshpass
-     apt-get -y install virt-viewer
-     apt-get -y install remmina
+  # Juegos
+    apt-get -y install scid
+    apt-get -y install scid-rating-data
+    apt-get -y install scid-spell-data
+    apt-get -y install stockfish
+    apt-get -y install dosbox
+    apt-get -y install scummvm
 
-  ## Juegos
-     apt-get -y install scid
-     apt-get -y install scid-rating-data
-     apt-get -y install scid-spell-data
-     apt-get -y install stockfish
-     apt-get -y install dosbox
-     apt-get -y install scummvm
+  # Fuentes
+    apt-get -y install fonts-freefont-ttf
+    apt-get -y install fonts-freefont-otf
+    apt-get -y install ttf-mscorefonts-installer
 
-  ## Fuentes
-     apt-get -y install fonts-freefont-ttf
-     apt-get -y install fonts-freefont-otf
-     apt-get -y install ttf-mscorefonts-installer
+  # Redes e internet
+    apt-get -y install whois
+    apt-get -y install gufw
+    apt-get -y install wireshark
+    apt-get -y install etherape
+    apt-get -y install sshpass
+    apt-get -y install virt-viewer
+    apt-get -y install firefox-esr-l10n-es-es
+    apt-get -y install thunderbird
+    apt-get -y install thunderbird-l10n-es-es
+    apt-get -y install lightning-l10n-es-es
+    apt-get -y install eiskaltdcpp
+    apt-get -y install amule
+    apt-get -y install chromium
+    apt-get -y install chromium-l10n
+    apt-get -y install filezilla
+    apt-get -y install mumble
+    apt-get -y install obs-studio
+    apt-get -y install remmina
 
-  ## Internet
-     apt-get -y install firefox-esr-l10n-es-es
-     apt-get -y install thunderbird
-     apt-get -y install thunderbird-l10n-es-es
-     apt-get -y install lightning-l10n-es-es
-     apt-get -y install eiskaltdcpp
-     apt-get -y install amule
-     apt-get -y install chromium
-     apt-get -y install chromium-l10n
-     apt-get -y install filezilla
-     apt-get -y install mumble
-     apt-get -y install obs-studio
-     apt-get -y install remmina
+  # Programación
+    apt-get -y install ghex
 
-  ## Programación
-     apt-get -y install ghex
+  # Seguridad
+    apt-get -y install clamav
+    apt-get -y install clamtk
 
-  ## Seguridad
-     apt-get -y install clamav
-     apt-get -y install clamtk
-
-  ## Otros
-     apt-get -y install libreoffice-l10n-es
-     apt-get -y install unrar
-     apt-get -y install htop
-     apt-get -y install simple-scan
-     apt-get -y install android-tools-adb # Para poder operar con el contenido de los móviles y relojes android
-     apt-get -y install android-tools-fastboot
-     apt-get -y install pyrenamer # Hay que agregar el repositorio de stretch antes, o instalar gprename, como reemplazo
-     apt-get -y install comix
+  # Otros
+    apt-get -y install libreoffice-l10n-es
+    apt-get -y install unrar
+    apt-get -y install htop
+    apt-get -y install simple-scan
+    apt-get -y install android-tools-adb # Para poder operar con el contenido de los móviles y relojes android
+    apt-get -y install android-tools-fastboot
+    apt-get -y install pyrenamer # Hay que agregar el repositorio de stretch antes, o instalar gprename, como reemplazo
+    apt-get -y install comix
 
   # Lanzador de chromium para el root
     mkdir -p /root/.local/share/applications/ 2> /dev/null
@@ -185,71 +183,70 @@ elif [ $OS_VERS == "11" ]; then
 
   apt-get -y update
 
-  ## Sistema
-     apt-get -y install gparted
-     apt-get -y install caja-open-terminal
-     apt-get -y install caja-admin
-     apt-get -y install hardinfo
-     apt-get -y install bleachbit
+  # Sistema
+    apt-get -y install gparted
+    apt-get -y install caja-open-terminal
+    apt-get -y install caja-admin
+    apt-get -y install hardinfo
+    apt-get -y install bleachbit
 
-  ## Multimedia
-     apt-get -y install vlc
-     apt-get -y install vlc-plugin-vlsub
-     apt-get -y install audacity
-     apt-get -y install subtitleeditor
-     apt-get -y install easytag
-     apt-get -y install openshot
+  # Multimedia
+    apt-get -y install vlc
+    apt-get -y install vlc-plugin-vlsub
+    apt-get -y install audacity
+    apt-get -y install subtitleeditor
+    apt-get -y install easytag
+    apt-get -y install openshot
 
-  ## Redes
-     apt-get -y install gufw
-     apt-get -y install wireshark
-     apt-get -y install etherape
-     apt-get -y install sshpass
-     apt-get -y install virt-viewer
-     apt-get -y install remmina
+  # Redes e internet
+    apt-get -y install gufw
+    apt-get -y install wireshark
+    apt-get -y install etherape
+    apt-get -y install sshpass
+    apt-get -y install virt-viewer
+    apt-get -y install whois
+    apt-get -y install remmina
+    apt-get -y install firefox-esr-l10n-es-es
+    apt-get -y install thunderbird
+    apt-get -y install thunderbird-l10n-es-es
+    apt-get -y install lightning-l10n-es-es
+    apt-get -y install eiskaltdcpp
+    apt-get -y install amule
+    apt-get -y install chromium
+    apt-get -y install chromium-l10n
+    apt-get -y install filezilla
+    apt-get -y install mumble
+    apt-get -y install obs-studio
 
-  ## Juegos
-     apt-get -y install scid
-     apt-get -y install scid-rating-data
-     apt-get -y install scid-spell-data
-     apt-get -y install stockfish
-     apt-get -y install dosbox
-     apt-get -y install scummvm
+  # Juegos
+    apt-get -y install scid
+    apt-get -y install scid-rating-data
+    apt-get -y install scid-spell-data
+    apt-get -y install stockfish
+    apt-get -y install dosbox
+    apt-get -y install scummvm
 
-  ## Fuentes
-     apt-get -y install fonts-freefont-ttf
-     apt-get -y install fonts-freefont-otf
-     apt-get -y install ttf-mscorefonts-installer
+  # Fuentes
+    apt-get -y install fonts-freefont-ttf
+    apt-get -y install fonts-freefont-otf
+    apt-get -y install ttf-mscorefonts-installer
 
-  ## Internet
-     apt-get -y install firefox-esr-l10n-es-es
-     apt-get -y install thunderbird
-     apt-get -y install thunderbird-l10n-es-es
-     apt-get -y install lightning-l10n-es-es
-     apt-get -y install eiskaltdcpp
-     apt-get -y install amule
-     apt-get -y install chromium
-     apt-get -y install chromium-l10n
-     apt-get -y install filezilla
-     apt-get -y install mumble
-     apt-get -y install obs-studio
+  # Programación
+    apt-get -y install ghex
 
-  ## Programación
-     apt-get -y install ghex
+  # Seguridad
+    apt-get -y install clamav
+    apt-get -y install clamtk
 
-  ## Seguridad
-     apt-get -y install clamav
-     apt-get -y install clamtk
-
-  ## Otros
-     apt-get -y install libreoffice-l10n-es
-     apt-get -y install unrar
-     apt-get -y install htop
-     apt-get -y install simple-scan
-     apt-get -y install android-tools-adb # Para poder operar con el contenido de los móviles y relojes android
-     apt-get -y install android-tools-fastboot
-     apt-get -y install pyrenamer # Hay que agregar el repositorio de stretch antes, o instalar gprename, como reemplazo
-     apt-get -y install comix
+  # Otros
+    apt-get -y install libreoffice-l10n-es
+    apt-get -y install unrar
+    apt-get -y install htop
+    apt-get -y install simple-scan
+    apt-get -y install android-tools-adb # Para poder operar con el contenido de los móviles y relojes android
+    apt-get -y install android-tools-fastboot
+    apt-get -y install pyrenamer # Hay que agregar el repositorio de stretch antes, o instalar gprename, como reemplazo
+    apt-get -y install comix
 
   # Lanzador de chromium para el root
     mkdir -p /root/.local/share/applications/ 2> /dev/null
