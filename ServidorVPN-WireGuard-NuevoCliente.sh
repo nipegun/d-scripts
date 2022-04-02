da7c13de520c73fc747e99dac4f92d038ceccb05
NiPeGun <nipegun@gmail.com>
2022-04-02 14:55:19 +0200

    Update ServidorVPN-WireGuard-NuevoCliente.sh

--- ServidorVPN-WireGuard-NuevoCliente.sh
+++ ServidorVPN-WireGuard-NuevoCliente.sh
@@ -18,26 +18,24 @@ vIPsPermitidas=$(cat /etc/wireguard/wg0.conf | grep Allowed | head -n1 | cut -d'
 for i in {0..9}
   do
     if [ -f /root/WireGuard/WireGuardUser"$i"Private.key ] and [ -f /root/WireGuard/WireGuardUser"$i"Public.key ]; then
-      echo "El peer User$i ya existe. Intentando crear el peer User$(($i+1))"
+      echo "El peer User$i ya existe. Intentando crear el peer User$(($i+1))..."
     else
-      echo ""
-     # wg genkey >                                                    /root/WireGuard/WireGuardUser"$i"Private.key
-     # cat /root/WireGuard/WireGuardUser"$i"Private.key | wg pubkey > /root/WireGuard/WireGuardUser"$i"Public.key
+      # Generar claves para el nuevo usuario
+        wg genkey >                                                    /root/WireGuard/WireGuardUser"$i"Private.key
+        cat /root/WireGuard/WireGuardUser"$i"Private.key | wg pubkey > /root/WireGuard/WireGuardUser"$i"Public.key
+      # Agregar la configuración a /etc/wireguard/wg0.conf
+        echo ""                             >> /etc/wireguard/wg0.conf
+        echo "[Peer]"                       >> /etc/wireguard/wg0.conf
+        echo 'User"$i"PublicKey ='          >> /etc/wireguard/wg0.conf
+        echo "AllowedIPs = $vIPsPermitidas" >> /etc/wireguard/wg0.conf
+      # Agregar la clave pública del primer usuario al archivo de configuración
+        vUser"$i"PubKey=$(cat /root/WireGuard/WireGuardUser"$i"Public.key)
+        sed -i -e 's|User"$i"PublicKey =|PublicKey = "$vUser"$i"PubKey"|g' /etc/wireguard/wg0.conf
+      # Terminar el script
+        exit
     fi
   done
 
-
-
-
-
-
-# Agregar el primer usuario al archivo de configuración
-  # echo ""                            >> /etc/wireguard/wg0.conf
-  # echo "[Peer]"                      >> /etc/wireguard/wg0.conf
-  # echo "User0PublicKey ="            >> /etc/wireguard/wg0.conf
-  # echo "AllowedIPs = 192.168.0.0/16" >> /etc/wireguard/wg0.conf # No poner nunca 0.0.0.0/0. wg-quick añade rutas para las IPs permitidas en los peers. Al añadir 0.0.0.0/0 intentará enrutar todo internet a través de ese peer y el servidor se quedará sin conexión.
-
-
-#ClavePúblicaDelNuevoCliente IPDelCliente 
-
+# Otra forma de crear peers
+# ClavePúblicaDelNuevoCliente IPDelCliente 
 # wg set wg0 peer <Client Public Key> endpoint <Client IP address>:51820 allowed-ips 203.0.113.12/24,fd86:ea04:1115::5/64
