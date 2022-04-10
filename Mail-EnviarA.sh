bf04217fac6428455da8721630b8ae5cdb671750
NiPeGun <nipegun@gmail.com>
2022-04-10 13:16:54 +0200

    Update Mail-EnviarA.sh

--- Mail-EnviarA.sh
+++ Mail-EnviarA.sh
@@ -5,11 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------
+#---------------------------------------------------------------------------------------------
 #  Script de NiPeGun para enviar mail en una sola línea
-#  Asegúrate de tener instalados los siguientes paquetes:
-#  postfix mailutils
-#----------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Mail-EnviarA.sh | bash
+#---------------------------------------------------------------------------------------------
 
 CantArgsEsperados=3
 ArgsInsuficientes=65
