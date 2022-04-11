fb29b918af5c7459f041c830ebf67893f6ee6bc6
NiPeGun <nipegun@gmail.com>
2022-04-11 18:30:19 +0200

    Update and rename Mail-Enviar-UsandoMutt.sh to Mail-Enviar-Texto-UsandoMutt.sh

--- Mail-Enviar-Texto-UsandoMutt.sh
+++ Mail-Enviar-Texto-UsandoMutt.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para enviar mail en una sola línea usando mutt
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Mail-Enviar-UsandoMutt.sh | bash
-#--------------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Mail-Enviar-Texto-UsandoMutt.sh | bash
+#-------------------------------------------------------------------------------------------------------------
 
 CantArgsEsperados=3
 ArgsInsuficientes=65
