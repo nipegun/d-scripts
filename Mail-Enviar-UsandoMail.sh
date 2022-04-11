8fc7f22df031acdc821cfaa96b17f6110eec957c
NiPeGun <nipegun@gmail.com>
2022-04-11 18:09:30 +0200

    Update Mail-Enviar-UsandoMail.sh

--- Mail-Enviar-UsandoMail.sh
+++ Mail-Enviar-UsandoMail.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#---------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para enviar mail en una sola línea
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Mail-EnviarA.sh | bash
-#---------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Mail-Enviar-UsandoMail.sh | bash
+#--------------------------------------------------------------------------------------------------------
 
 CantArgsEsperados=3
 ArgsInsuficientes=65
