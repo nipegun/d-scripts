b9aceded371d48b285c848e39dab5c7cab04455d
NiPeGun <nipegun@gmail.com>
2022-04-12 12:31:17 +0200

    Update Centreon-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Centreon-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Centreon-InstalarYConfigurar.sh
@@ -47,7 +47,7 @@ if [ $OS_VERS == "7" ]; then
 
   echo ""
   echo "-----------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación de Calibre para Debian 7 (Wheezy)..."
+  echo "  Iniciando el script de instalación de Centreon para Debian 7 (Wheezy)..."
   echo "-----------------------------------------------------------------------------"
   echo ""
 
@@ -59,7 +59,7 @@ elif [ $OS_VERS == "8" ]; then
 
   echo ""
   echo "-----------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación de Calibre para Debian 8 (Jessie)..."
+  echo "  Iniciando el script de instalación de Centreon para Debian 8 (Jessie)..."
   echo "-----------------------------------------------------------------------------"
   echo ""
 
@@ -71,7 +71,7 @@ elif [ $OS_VERS == "9" ]; then
 
   echo ""
   echo "------------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación de Calibre para Debian 9 (Stretch)..."
+  echo "  Iniciando el script de instalación de Centreon para Debian 9 (Stretch)..."
   echo "------------------------------------------------------------------------------"
   echo ""
 
@@ -79,12 +79,11 @@ elif [ $OS_VERS == "9" ]; then
   echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
   echo ""
 
-
 elif [ $OS_VERS == "10" ]; then
 
   echo ""
   echo "------------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación de Calibre para Debian 10 (Buster)..."
+  echo "  Iniciando el script de instalación de Centreon para Debian 10 (Buster)..."
   echo "------------------------------------------------------------------------------"
   echo ""
 
@@ -92,17 +91,46 @@ elif [ $OS_VERS == "10" ]; then
   echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
   echo ""
 
-
 elif [ $OS_VERS == "11" ]; then
 
   echo ""
   echo "--------------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación de Calibre para Debian 11 (Bullseye)..."
+  echo "  Iniciando el script de instalación de Centreon para Debian 11 (Bullseye)..."
   echo "--------------------------------------------------------------------------------"
   echo ""
 
+  hostnamectl set-hostname centreon
+
   echo ""
-  echo "  Comandos para Debian 11 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo "  Agregando el repositorio de Centreon..."
   echo ""
+  echo "deb https://apt.centreon.com/repository/centreon-bullseye/ bullseye main" > /etc/apt/sources.list.d/centreon.list
+
+  echo ""
+  echo "  Actualizando la lsita de paquetes diponibles..."
+  echo ""
+  apt-get -y update
+
+  echo ""
+  echo " Instalando paquetes necesarios..."
+  echo ""
+  apt-get -y install wget
+  apt-get -y install gnupg2
+
+  echo ""
+  echo "  Descargando las llaves para firmar el repositorio..."
+  echo ""
+  wget -O- https://apt-key.centreon.com | gpg --dearmor | tee /etc/apt/trusted.gpg.d/centreon.gpg
+
+  echo ""
+  echo "  Re-actualizando la lsita de paquetes diponibles..."
+  echo ""
+  apt-get -y update
+
+  echo ""
+  echo " Instalando centreon..."
+  echo ""
+  apt-get -y install centreon
 
 fi
+
