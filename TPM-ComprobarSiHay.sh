00b88f0be1219e2945ecdb4b99baa24b5e6277e2
NiPeGun <nipegun@gmail.com>
2022-04-12 17:35:40 +0200

    Create TPM-ComprobarSiHay.sh

---
+++ TPM-ComprobarSiHay.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#---------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para comprobar si el sistema tiene disponible un chip TPM
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/TPM-ComprobarSiHay.sh | bash 
+#---------------------------------------------------------------------------------------------------
+
+dmesg | grep -i tpm
+
+ls -d /sys/kernel/security/tpm* 2>/dev/null | head -1
+
