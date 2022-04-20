5e20d993929a121312535c824f84351a5146f1b1
NiPeGun <nipegun@gmail.com>
2022-04-20 04:57:45 +0200

    Update and rename NagiosCore-InstalarYConfigurar5.sh to NagiosCore-InstalarYConfigurar6.sh

--- SoftInst/ParaCLI/NagiosCore-InstalarYConfigurar6.sh
+++ SoftInst/ParaCLI/NagiosCore-InstalarYConfigurar6.sh
@@ -154,8 +154,8 @@ elif [ $OS_VERS == "11" ]; then
       apt-get -y install tar
       echo ""
     fi
-  tar -xfv /root/SoftInst/NagiosCore/nagiosweb.tar.gz -C /root/SoftInst/NagiosCore/
-  tar -xfv /root/SoftInst/NagiosCore/nagiosgithub.tar.gz -C /root/SoftInst/NagiosCore/
+  tar -xv -f /root/SoftInst/NagiosCore/nagiosweb.tar.gz    -C /root/SoftInst/NagiosCore/
+  tar -xv -f /root/SoftInst/NagiosCore/nagiosgithub.tar.gz -C /root/SoftInst/NagiosCore/
 
 fi
 
