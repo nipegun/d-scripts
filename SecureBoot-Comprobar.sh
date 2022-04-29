c4f03c69d341414413978eab065b516f4cfc9a14
NiPeGun <nipegun@gmail.com>
2022-04-30 00:35:29 +0200

    Create SecureBoot-Comprobar.sh

---
+++ SecureBoot-Comprobar.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#----------------------------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para instalar nueva llave para firmar repositorios en Debian
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/APTKey-BajarEInstalar.sh | bash -s URL Servicio
+#
+#  Ejemplo:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/APTKey-BajarEInstalar.sh | bash -s https://nightly.odoo.com/odoo.key Odoo
+#----------------------------------------------------------------------------------------------------------------------
+
+mokutil --sb-state
+
