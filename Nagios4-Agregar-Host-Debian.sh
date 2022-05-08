169634c59f87e3a86ac1cd3d1f9594a356841af0
NiPeGun <nipegun@gmail.com>
2022-05-08 15:18:14 +0200

    Update and rename Nagios-Agregar-Host-Debian.sh to Nagios4-Agregar-Host-Debian.sh

--- Nagios4-Agregar-Host-Debian.sh
+++ Nagios4-Agregar-Host-Debian.sh
@@ -9,10 +9,10 @@
 #  Script de NiPeGun para agregar un ordenador Debian a Nagios
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios-Agregar-Host-Debian.sh | bash -s URL Servicio
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios4-Agregar-Host-Debian.sh | bash -s URL Servicio
 #
 #  Ejemplo:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios-Agregar-Host-Debian.sh | bash -s midebian "Mi ordenador Debian" "192.168.0.123"
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios4-Agregar-Host-Debian.sh | bash -s midebian "Mi ordenador Debian" "192.168.0.123"
 # ----------
 
 NombreDelHost=$1
