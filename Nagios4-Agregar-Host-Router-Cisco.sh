eda7bf8ec1d474b0052a520c1855c9e8e3b1ec5f
NiPeGun <nipegun@gmail.com>
2022-05-08 15:18:22 +0200

    Update and rename Nagios-Agregar-Host-Router-Cisco.sh to Nagios4-Agregar-Host-Router-Cisco.sh

--- Nagios4-Agregar-Host-Router-Cisco.sh
+++ Nagios4-Agregar-Host-Router-Cisco.sh
@@ -9,10 +9,10 @@
 #  Script de NiPeGun para agregar un host Router a Nagios
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios-Agregar-Host-Router-Cisco.sh | bash -s URL Servicio
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios4-Agregar-Host-Router-Cisco.sh | bash -s URL Servicio
 #
 #  Ejemplo:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios-Agregar-Host-Router-Cisco.sh | bash -s mirouter "Mi router" "192.168.0.123"
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Nagios4-Agregar-Host-Router-Cisco.sh | bash -s mirouter "Mi router" "192.168.0.123"
 # ----------
 
 NombreDelHost=$1
