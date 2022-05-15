e438a49d35296576e16e464c002ffbbef10c91f1
NiPeGun <nipegun@gmail.com>
2022-05-15 19:24:46 +0200

    Update and rename OracleXE-CopSeg-Inconsistente-Incremental.sh to OracleXE-CopSeg-Inconsistente-Incremental0.sh

--- OracleXE-CopSeg-Inconsistente-Incremental0.sh
+++ OracleXE-CopSeg-Inconsistente-Incremental0.sh
@@ -8,14 +8,16 @@
     exit 1
   fi
 
-vCarpetaCopSeg=/home/oracle/CopSeg
+vCarpetaCopSeg=/home/oracle/CopSeg/Inconsistente/Incremental
 mkdir -p $vCarpetaCopSeg 2> /dev/null
 export DATE=$(date +A%yM%mD%d-%H%M%S)
 
 #rman target / log=/home/oracle/CopSeg/prueba.log << EOF
 rman target / << EOF
 run {
-backup as compressed backupset incremental level 0 database tag 'CopiaInconsistenteIncremental' plus archivelog tag 'CopiaInconsistenteIncremental';
+backup incremental level 0 database format            '$vCarpetaCopSeg/%d-${DATE}-database_%u';
+backup incremental level 0 archivelog all format      '$vCarpetaCopSeg/%d-${DATE}-archivelogs_%u' delete input;
+backup incremental level 0 current controlfile format '$vCarpetaCopSeg/%d-${DATE}-controlfile_%u';
 }
 exit;
 EOF
