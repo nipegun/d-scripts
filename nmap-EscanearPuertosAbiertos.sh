1a58618be60cd53babafb56275094fe6099a9b02
NiPeGun <nipegun@gmail.com>
2022-05-25 07:18:29 +0200

    Update nmap-EscanearPuertosAbiertos.sh

--- nmap-EscanearPuertosAbiertos.sh
+++ nmap-EscanearPuertosAbiertos.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para escanear todos los puertos abiertos de una IP
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/APTKey-BajarEInstalar.sh | bash -s URL Servicio
+#
+#  Ejemplo:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/APTKey-BajarEInstalar.sh | bash -s https://nightly.odoo.com/odoo.key Odoo
+# ----------
+
 vIPWAN=111.111.111.111
 
 vPuertoInicio=10100
