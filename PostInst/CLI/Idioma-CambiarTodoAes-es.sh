0620bc79100d4af73fcb663dd098efd9ffdde416
NiPeGun <nipegun@gmail.com>
2022-06-08 11:30:45 +0200

    Update Idioma-CambiarTodoAes-es.sh

--- PostInst/CLI/Idioma-CambiarTodoAes-es.sh
+++ PostInst/CLI/Idioma-CambiarTodoAes-es.sh
@@ -62,32 +62,33 @@ elif [ $OS_VERS == "8" ]; then
   echo "-----------------------------------------------------------------------------"
   echo ""
 
-  ## Poner que sólo se genere el español de España cuando se creen locales
-     echo "es_ES.UTF-8 UTF-8" > /etc/locale.gen
+  # Poner que sólo se genere el español de España cuando se creen locales
+    echo "es_ES.UTF-8 UTF-8" > /etc/locale.gen
 
-  ## Compilar los locales borrando primero los existentes y dejando nada más que el español de España
-     locale-gen --purge es_ES.UTF-8
+  # Compilar los locales borrando primero los existentes y dejando nada más que el español de España
+    apt-get -y update && apt-get -y install locales
+    locale-gen --purge es_ES.UTF-8
 
-  ## Modificar el archivo /etc/default/locale reflejando los cambios
-     echo 'LANG="es_ES.UTF-8"'   > /etc/default/locale
-     echo 'LANGUAGE="es_ES:es"' >> /etc/default/locale
+  # Modificar el archivo /etc/default/locale reflejando los cambios
+    echo 'LANG="es_ES.UTF-8"'   > /etc/default/locale
+    echo 'LANGUAGE="es_ES:es"' >> /etc/default/locale
 
-  ## Poner el teclado en español de España
-     echo 'XKBMODEL="pc105"'   > /etc/default/keyboard
-     echo 'XKBLAYOUT="es"'    >> /etc/default/keyboard
-     echo 'XKBVARIANT=""'     >> /etc/default/keyboard
-     echo 'XKBOPTIONS=""'     >> /etc/default/keyboard
-     echo ''                  >> /etc/default/keyboard
-     echo 'BACKSPACE="guess"' >> /etc/default/keyboard
-     echo ''                  >> /etc/default/keyboard
+  # Poner el teclado en español de España
+    echo 'XKBMODEL="pc105"'   > /etc/default/keyboard
+    echo 'XKBLAYOUT="es"'    >> /etc/default/keyboard
+    echo 'XKBVARIANT=""'     >> /etc/default/keyboard
+    echo 'XKBOPTIONS=""'     >> /etc/default/keyboard
+    echo ''                  >> /etc/default/keyboard
+    echo 'BACKSPACE="guess"' >> /etc/default/keyboard
+    echo ''                  >> /etc/default/keyboard
 
-  ## Notificar cambios
-     echo ""
-     echo "------------------------------------------------------------------"
-     echo "  Cambios realizados."
-     echo "  Debes reiniciar el sistema para que los cambios tengan efecto."
-     echo "------------------------------------------------------------------"
-     echo ""
+  # Notificar cambios
+    echo ""
+    echo "------------------------------------------------------------------"
+    echo "  Cambios realizados."
+    echo "  Debes reiniciar el sistema para que los cambios tengan efecto."
+    echo "------------------------------------------------------------------"
+    echo ""
 
 elif [ $OS_VERS == "9" ]; then
 
@@ -97,32 +98,33 @@ elif [ $OS_VERS == "9" ]; then
   echo "------------------------------------------------------------------------------"
   echo ""
 
-  ## Poner que sólo se genere el español de España cuando se creen locales
-     echo "es_ES.UTF-8 UTF-8" > /etc/locale.gen
+  # Poner que sólo se genere el español de España cuando se creen locales
+    echo "es_ES.UTF-8 UTF-8" > /etc/locale.gen
 
-  ## Compilar los locales borrando primero los existentes y dejando nada más que el español de España
-     locale-gen --purge es_ES.UTF-8
+  # Compilar los locales borrando primero los existentes y dejando nada más que el español de España
+    apt-get -y update && apt-get -y install locales
+    locale-gen --purge es_ES.UTF-8
 
-  ## Modificar el archivo /etc/default/locale reflejando los cambios
-     echo 'LANG="es_ES.UTF-8"'   > /etc/default/locale
-     echo 'LANGUAGE="es_ES:es"' >> /etc/default/locale
+  # Modificar el archivo /etc/default/locale reflejando los cambios
+    echo 'LANG="es_ES.UTF-8"'   > /etc/default/locale
+    echo 'LANGUAGE="es_ES:es"' >> /etc/default/locale
 
-  ## Poner el teclado en español de España
-     echo 'XKBMODEL="pc105"'   > /etc/default/keyboard
-     echo 'XKBLAYOUT="es"'    >> /etc/default/keyboard
-     echo 'XKBVARIANT=""'     >> /etc/default/keyboard
-     echo 'XKBOPTIONS=""'     >> /etc/default/keyboard
-     echo ''                  >> /etc/default/keyboard
-     echo 'BACKSPACE="guess"' >> /etc/default/keyboard
-     echo ''                  >> /etc/default/keyboard
+  # Poner el teclado en español de España
+    echo 'XKBMODEL="pc105"'   > /etc/default/keyboard
+    echo 'XKBLAYOUT="es"'    >> /etc/default/keyboard
+    echo 'XKBVARIANT=""'     >> /etc/default/keyboard
+    echo 'XKBOPTIONS=""'     >> /etc/default/keyboard
+    echo ''                  >> /etc/default/keyboard
+    echo 'BACKSPACE="guess"' >> /etc/default/keyboard
+    echo ''                  >> /etc/default/keyboard
 
-  ## Notificar cambios
-     echo ""
-     echo "------------------------------------------------------------------"
-     echo "  Cambios realizados."
-     echo "  Debes reiniciar el sistema para que los cambios tengan efecto."
-     echo "------------------------------------------------------------------"
-     echo ""
+  # Notificar cambios
+    echo ""
+    echo "------------------------------------------------------------------"
+    echo "  Cambios realizados."
+    echo "  Debes reiniciar el sistema para que los cambios tengan efecto."
+    echo "------------------------------------------------------------------"
+    echo ""
 
 elif [ $OS_VERS == "10" ]; then
 
@@ -132,32 +134,33 @@ elif [ $OS_VERS == "10" ]; then
   echo "------------------------------------------------------------------------------"
   echo ""
 
-  ## Poner que sólo se genere el español de España cuando se creen locales
-     echo "es_ES.UTF-8 UTF-8" > /etc/locale.gen
+  # Poner que sólo se genere el español de España cuando se creen locales
+    echo "es_ES.UTF-8 UTF-8" > /etc/locale.gen
 
-  ## Compilar los locales borrando primero los existentes y dejando nada más que el español de España
-     locale-gen --purge es_ES.UTF-8
+  # Compilar los locales borrando primero los existentes y dejando nada más que el español de España
+    apt-get -y update && apt-get -y install locales
+    locale-gen --purge es_ES.UTF-8
 
-  ## Modificar el archivo /etc/default/locale reflejando los cambios
-     echo 'LANG="es_ES.UTF-8"'   > /etc/default/locale
-     echo 'LANGUAGE="es_ES:es"' >> /etc/default/locale
+  # Modificar el archivo /etc/default/locale reflejando los cambios
+    echo 'LANG="es_ES.UTF-8"'   > /etc/default/locale
+    echo 'LANGUAGE="es_ES:es"' >> /etc/default/locale
 
-  ## Poner el teclado en español de España
-     echo 'XKBMODEL="pc105"'   > /etc/default/keyboard
-     echo 'XKBLAYOUT="es"'    >> /etc/default/keyboard
-     echo 'XKBVARIANT=""'     >> /etc/default/keyboard
-     echo 'XKBOPTIONS=""'     >> /etc/default/keyboard
-     echo ''                  >> /etc/default/keyboard
-     echo 'BACKSPACE="guess"' >> /etc/default/keyboard
-     echo ''                  >> /etc/default/keyboard
+  # Poner el teclado en español de España
+    echo 'XKBMODEL="pc105"'   > /etc/default/keyboard
+    echo 'XKBLAYOUT="es"'    >> /etc/default/keyboard
+    echo 'XKBVARIANT=""'     >> /etc/default/keyboard
+    echo 'XKBOPTIONS=""'     >> /etc/default/keyboard
+    echo ''                  >> /etc/default/keyboard
+    echo 'BACKSPACE="guess"' >> /etc/default/keyboard
+    echo ''                  >> /etc/default/keyboard
 
-  ## Notificar cambios
-     echo ""
-     echo "------------------------------------------------------------------"
-     echo "  Cambios realizados."
-     echo "  Debes reiniciar el sistema para que los cambios tengan efecto."
-     echo "------------------------------------------------------------------"
-     echo ""
+  # Notificar cambios
+    echo ""
+    echo "------------------------------------------------------------------"
+    echo "  Cambios realizados."
+    echo "  Debes reiniciar el sistema para que los cambios tengan efecto."
+    echo "------------------------------------------------------------------"
+    echo ""
 
 elif [ $OS_VERS == "11" ]; then
 
@@ -167,32 +170,33 @@ elif [ $OS_VERS == "11" ]; then
   echo "--------------------------------------------------------------------------------"
   echo ""
 
-  ## Poner que sólo se genere el español de España cuando se creen locales
-     echo "es_ES.UTF-8 UTF-8" > /etc/locale.gen
+  # Poner que sólo se genere el español de España cuando se creen locales
+    echo "es_ES.UTF-8 UTF-8" > /etc/locale.gen
 
-  ## Compilar los locales borrando primero los existentes y dejando nada más que el español de España
-     locale-gen --purge es_ES.UTF-8
+  # Compilar los locales borrando primero los existentes y dejando nada más que el español de España
+    apt-get -y update && apt-get -y install locales
+    locale-gen --purge es_ES.UTF-8
 
-  ## Modificar el archivo /etc/default/locale reflejando los cambios
-     echo 'LANG="es_ES.UTF-8"'   > /etc/default/locale
-     echo 'LANGUAGE="es_ES:es"' >> /etc/default/locale
+  # Modificar el archivo /etc/default/locale reflejando los cambios
+    echo 'LANG="es_ES.UTF-8"'   > /etc/default/locale
+    echo 'LANGUAGE="es_ES:es"' >> /etc/default/locale
 
-  ## Poner el teclado en español de España
-     echo 'XKBMODEL="pc105"'   > /etc/default/keyboard
-     echo 'XKBLAYOUT="es"'    >> /etc/default/keyboard
-     echo 'XKBVARIANT=""'     >> /etc/default/keyboard
-     echo 'XKBOPTIONS=""'     >> /etc/default/keyboard
-     echo ''                  >> /etc/default/keyboard
-     echo 'BACKSPACE="guess"' >> /etc/default/keyboard
-     echo ''                  >> /etc/default/keyboard
+  # Poner el teclado en español de España
+    echo 'XKBMODEL="pc105"'   > /etc/default/keyboard
+    echo 'XKBLAYOUT="es"'    >> /etc/default/keyboard
+    echo 'XKBVARIANT=""'     >> /etc/default/keyboard
+    echo 'XKBOPTIONS=""'     >> /etc/default/keyboard
+    echo ''                  >> /etc/default/keyboard
+    echo 'BACKSPACE="guess"' >> /etc/default/keyboard
+    echo ''                  >> /etc/default/keyboard
 
-  ## Notificar cambios
-     echo ""
-     echo "------------------------------------------------------------------"
-     echo "  Cambios realizados."
-     echo "  Debes reiniciar el sistema para que los cambios tengan efecto."
-     echo "------------------------------------------------------------------"
-     echo ""
+  # Notificar cambios
+    echo ""
+    echo "------------------------------------------------------------------"
+    echo "  Cambios realizados."
+    echo "  Debes reiniciar el sistema para que los cambios tengan efecto."
+    echo "------------------------------------------------------------------"
+    echo ""
 
 fi
 
