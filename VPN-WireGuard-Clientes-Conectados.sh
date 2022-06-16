cb99cb1f2eb56e60fc00f1458cd3531cea0e8e8f
NiPeGun <nipegun@gmail.com>
2022-06-16 20:07:19 +0200

    Create VPN-WireGuard-Clientes-Conectados.sh

---
+++ VPN-WireGuard-Clientes-Conectados.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para agregar un nuevo cliente al servidor WireGuard
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/VPN-WireGuard-Clientes-Conectados.sh | bash
+# ----------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+wg show
+
