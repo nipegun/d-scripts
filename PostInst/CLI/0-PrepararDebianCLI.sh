b075794d165a927672db01f01de243f4f51edb9a
NiPeGun <nipegun@gmail.com>
2022-06-24 17:00:37 +0200

    Create 0-PrepararDebianCLI.sh

---
+++ PostInst/CLI/0-PrepararDebianCLI.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para preparar Debian sin entorno gráfico a gusto de NiPeGun
+#
+#  Ejecución remota:
+#  curl -s x | bash
+# ----------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+# Preparar ComandosPostArranque (rc.local)
+  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/ComandosPostArranque-Preparar.sh | bash
+
+# Preparar tareas Cropn
+  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/TareasCron-Preparar.sh | bash
+
+# Preparar cortafuegos
+  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/Cortafuegos-Preparar.sh | bash
+
+# Poner idioma sólo en español
+  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/Idioma-CambiarTodoAes-es.sh | bash
+
+# Poner todos los repositorios
+  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/Repositorios-PonerTodos.sh | bash
+
