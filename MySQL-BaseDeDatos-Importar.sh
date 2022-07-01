7d7d52f24925cc74c8d3dd2ca20e6fbc09a0a974
NiPeGun <nipegun@gmail.com>
2022-07-01 07:52:01 +0200

    Update MySQL-BaseDeDatos-Importar.sh

--- MySQL-BaseDeDatos-Importar.sh
+++ MySQL-BaseDeDatos-Importar.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para importar una base de datos de MySQL
-#--------------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/MySQL-BaseDeDatos-Importar.sh | bash -s UsuarioBD PasswordBD NombreBD RutaArchivoSQL
+# ----------
 
 CantArgsEsperados=4
 ArgsInsuficientes=65
