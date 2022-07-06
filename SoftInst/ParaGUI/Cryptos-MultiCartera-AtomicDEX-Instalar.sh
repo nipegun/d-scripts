b3faa6b330f4f6a0a1e3224cbc247e14f08abb6e
NiPeGun <nipegun@gmail.com>
2022-07-06 03:49:06 +0200

    Update Cryptos-MultiCartera-AtomicDEX-Instalar.sh

--- SoftInst/ParaGUI/Cryptos-MultiCartera-AtomicDEX-Instalar.sh
+++ SoftInst/ParaGUI/Cryptos-MultiCartera-AtomicDEX-Instalar.sh
@@ -163,7 +163,7 @@ elif [ $OS_VERS == "11" ]; then
     echo ""
     mkdir -p /home/$vUsuarioNoRoot/AtomicDEX/ 2> /dev/null
     cp -rf /root/SoftInst/AtomicDEX/AntaraAtomicDexAppDir/usr/* /home/$vUsuarioNoRoot/AtomicDEX/
-    cp /root/SoftInst/AtomicDEX/AntaraAtomicDexAppDir/dex-logo-64.png /home/$vUsuarioNoRoot/AtomicDEX/AtomicDEX.png
+    cp /root/SoftInst/AtomicDEX/AntaraAtomicDexAppDir/dex-logo-64.png /home/$vUsuarioNoRoot/AtomicDEX/logo.png
 
   # Agregar aplicación al menú
     echo ""
@@ -172,7 +172,7 @@ elif [ $OS_VERS == "11" ]; then
     mkdir -p /home/$vUsuarioNoRoot/.local/share/applications/ 2> /dev/null
     cp -f /root/SoftInst/AtomicDEX/AntaraAtomicDexAppDir/dex.desktop                                    /home/$vUsuarioNoRoot/.local/share/applications/atomicdex.desktop
     sed -i -e 's|Exec=atomicdex-desktop|Exec=/home/'$vUsuarioNoRoot'/AtomicDEX/bin/atomicdex-desktop|g' /home/$vUsuarioNoRoot/.local/share/applications/atomicdex.desktop
-    sed -i -e "s|Icon=dex-logo-64|Icon=/home/$vUsuarioNoRoot/AtomicDEX/AtomicDEX.png|g"                 /home/$vUsuarioNoRoot/.local/share/applications/atomicdex.desktop
+    sed -i -e "s|Icon=dex-logo-64|Icon=/home/$vUsuarioNoRoot/AtomicDEX/logo.png|g"                      /home/$vUsuarioNoRoot/.local/share/applications/atomicdex.desktop
     sed -i -e "s|Name=atomicdex-desktop|Name=AtomicDEX|g"                                               /home/$vUsuarioNoRoot/.local/share/applications/atomicdex.desktop
     chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/.local/share/applications/atomicdex.desktop
     gio set /home/$vUsuarioNoRoot/.local/share/applications/atomicdex.desktop "metadata::trusted" yes
@@ -184,7 +184,7 @@ elif [ $OS_VERS == "11" ]; then
     mkdir -p /home/$vUsuarioNoRoot/.config/autostart/ 2> /dev/null
     cp -f /root/SoftInst/AtomicDEX/AntaraAtomicDexAppDir/dex.desktop                                    /home/$vUsuarioNoRoot/.config/autostart/atomicdex.desktop
     sed -i -e 's|Exec=atomicdex-desktop|Exec=/home/'$vUsuarioNoRoot'/AtomicDEX/bin/atomicdex-desktop|g' /home/$vUsuarioNoRoot/.config/autostart/atomicdex.desktop
-    sed -i -e "s|Icon=dex-logo-64|Icon=/home/$vUsuarioNoRoot/AtomicDEX/AtomicDEX.png|g"                 /home/$vUsuarioNoRoot/.config/autostart/atomicdex.desktop
+    sed -i -e "s|Icon=dex-logo-64|Icon=/home/$vUsuarioNoRoot/AtomicDEX/logo.png|g"                      /home/$vUsuarioNoRoot/.config/autostart/atomicdex.desktop
     sed -i -e "s|Name=atomicdex-desktop|Name=AtomicDEX|g"                                               /home/$vUsuarioNoRoot/.config/autostart/atomicdex.desktop
     chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/.config/autostart/atomicdex.desktop
     gio set /home/$vUsuarioNoRoot/.config/autostart/atomicdex.desktop "metadata::trusted" yes
