d58c897d90ee918244d3c47e72313d705057b064
NiPeGun <nipegun@gmail.com>
2022-08-07 18:00:00 +0200

    Update Grub2-InstalarEn-Pendrive.sh

--- SoftInst/ParaCLI/Grub2-InstalarEn-Pendrive.sh
+++ SoftInst/ParaCLI/Grub2-InstalarEn-Pendrive.sh
@@ -196,6 +196,7 @@ elif [ $OS_VERS == "11" ]; then
   echo "      insmod part_msdos"                                                    >> /Particiones/USB/PendriveGrub2/boot/grub/grub.cfg
   echo "      insmod ext2"                                                          >> /Particiones/USB/PendriveGrub2/boot/grub/grub.cfg
   echo "      search --no-floppy --set=root --file /vmlinuz --hint-bios=hd0,msdos1" >> /Particiones/USB/PendriveGrub2/boot/grub/grub.cfg
+  echo '      echo "  La variable root tiene el siguiente valor: $root"'            >> /Particiones/USB/PendriveGrub2/boot/grub/grub.cfg
   echo "      linux  /vmlinuz root=/dev/sda3 ro net.ifnames=0 biosdevname=0 quiet"  >> /Particiones/USB/PendriveGrub2/boot/grub/grub.cfg
   echo "      initrd /initrd.img"                                                   >> /Particiones/USB/PendriveGrub2/boot/grub/grub.cfg
   echo "    }"                                                                      >> /Particiones/USB/PendriveGrub2/boot/grub/grub.cfg
