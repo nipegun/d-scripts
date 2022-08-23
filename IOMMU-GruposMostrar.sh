223513aded197e5d721f4033a643206afdd0acdb
NiPeGun <nipegun@gmail.com>
2022-08-23 03:53:19 +0200

    Update IOMMU-GruposMostrar.sh

--- IOMMU-GruposMostrar.sh
+++ IOMMU-GruposMostrar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para mostrar el aislamiento/agrupamiento IOMMU en Debian
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/IOMMU-GruposMostrar.sh.sh | bash
-#-------------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/IOMMU-GruposMostrar.sh | bash
+# ----------
 
 echo ""
 echo "-------------------------------------------"
