ccc450d15de01ebb03da6c9b1aaa785f5745fb12
NiPeGun <nipegun@gmail.com>
2022-09-29 18:15:22 +0200

    Create Heimdall-Items-Mostrar.sh

---
+++ Heimdall-Items-Mostrar.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para mostrar items de Heimdall desde Bash
+#
+#  Ejecución remota:
+#  curl -s x | bash
+#
+#  Ejecución remota sin caché:
+#  curl -s -H 'Cache-Control: no-cache, no-store' x | bash
+# ----------
+
+# Comprobar que sqlite3 esté instalado
+  if [[ $(dpkg-query -s sqlite3 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${vColorRojo}sqlite3 no está instalado. Iniciando su instalación...${vFinColor}"
+    echo ""
+    apt-get -y update && apt-get -y install sqlite3
+    echo ""
+  fi
+
+# Mostrar items
+  echo ""
+  echo "select title, url from items" | sqlite3 /var/www/heimdall/database/app.sqlite | sed 's-|- | -g'
+  echo ""
+
