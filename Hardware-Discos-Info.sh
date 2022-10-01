979b102d45fe2e2f5ad6d633dc25598d398f5bfa
NiPeGun <nipegun@gmail.com>
2022-10-01 22:27:55 +0200

    Update Hardware-Discos-Info.sh

--- Hardware-Discos-Info.sh
+++ Hardware-Discos-Info.sh
@@ -5,9 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para mostrar información sobre discos duros
-#-----------------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s 
+# ----------
 
 ColorVerde="\033[1;32m"
 FinColor="\033[0m"
