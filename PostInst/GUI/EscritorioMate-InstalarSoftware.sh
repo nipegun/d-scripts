930a81dc97483b949aa024c19b5d3ae85e85bb1a
NiPeGun <nipegun@gmail.com>
2022-10-08 15:40:58 +0200

    Update EscritorioMate-InstalarSoftware.sh

--- PostInst/GUI/EscritorioMate-InstalarSoftware.sh
+++ PostInst/GUI/EscritorioMate-InstalarSoftware.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para instalar software en el escritorio Mate de Debian
 #
 #  Ejecución remota:
 #  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/GUI/EscritorioMate-InstalarSoftware.sh | bash
-#-----------------------------------------------------------------------------------------------------------------------------
+# ----------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
