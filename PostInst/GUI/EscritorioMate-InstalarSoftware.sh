97526ef84f69b17dddd712aa20845cc6f0868bc1
NiPeGun <nipegun@gmail.com>
2022-10-12 13:32:42 +0200

    Update EscritorioMate-InstalarSoftware.sh

--- PostInst/GUI/EscritorioMate-InstalarSoftware.sh
+++ PostInst/GUI/EscritorioMate-InstalarSoftware.sh
@@ -80,93 +80,9 @@ elif [ $OS_VERS == "10" ]; then
   echo "---------------------------------------------------------------------------------------------------"
   echo ""
 
-  apt-get -y update
-
-  # Sistema
-    apt-get -y install gparted
-    apt-get -y install caja-open-terminal
-    apt-get -y install caja-admin
-    apt-get -y install hardinfo
-    apt-get -y install bleachbit
-
-  # Multimedia
-    apt-get -y install vlc
-    apt-get -y install vlc-plugin-vlsub
-    apt-get -y install audacity
-    apt-get -y install subtitleeditor
-    apt-get -y install easytag
-    apt-get -y install openshot
-
-  # Juegos
-    apt-get -y install scid
-    apt-get -y install scid-rating-data
-    apt-get -y install scid-spell-data
-    apt-get -y install stockfish
-    apt-get -y install dosbox
-    apt-get -y install scummvm
-
-  # Fuentes
-    apt-get -y install fonts-freefont-ttf
-    apt-get -y install fonts-freefont-otf
-    apt-get -y install ttf-mscorefonts-installer
-
-  # Redes e internet
-    apt-get -y install whois
-    apt-get -y install gufw
-    apt-get -y install wireshark
-    apt-get -y install etherape
-      setcap cap_net_raw,cap_net_admin+eip /usr/bin/etherape
-    apt-get -y install sshpass
-    apt-get -y install virt-viewer
-    apt-get -y install firefox-esr-l10n-es-es
-    apt-get -y install thunderbird
-    apt-get -y install thunderbird-l10n-es-es
-    apt-get -y install lightning-l10n-es-es
-    apt-get -y install eiskaltdcpp
-    apt-get -y install amule
-    apt-get -y install chromium
-    apt-get -y install chromium-l10n
-    apt-get -y install filezilla
-    apt-get -y install mumble
-    apt-get -y install obs-studio
-    apt-get -y install remmina
-
-  # Programación
-    apt-get -y install ghex
-
-  # Seguridad
-    apt-get -y install clamav
-    apt-get -y install clamtk
-
-  # Otros
-    apt-get -y install libreoffice-l10n-es
-    apt-get -y install unrar
-    apt-get -y install htop
-    apt-get -y install simple-scan
-    apt-get -y install android-tools-adb # Para poder operar con el contenido de los móviles y relojes android
-    apt-get -y install android-tools-fastboot
-    apt-get -y install pyrenamer # Hay que agregar el repositorio de stretch antes, o instalar gprename, como reemplazo
-    apt-get -y install comix
-
-  # Lanzador de chromium para el root
-    mkdir -p /root/.local/share/applications/ 2> /dev/null
-    echo "[Desktop Entry]"                      > /root/.local/share/applications/chromiumroot.desktop
-    echo "Name=Chromium (para root)"           >> /root/.local/share/applications/chromiumroot.desktop
-    echo "Comment=Accede a Internet"           >> /root/.local/share/applications/chromiumroot.desktop
-    echo "GenericName=Navegador web"           >> /root/.local/share/applications/chromiumroot.desktop
-    echo "Exec=/usr/bin/chromium --no-sandbox" >> /root/.local/share/applications/chromiumroot.desktop
-    echo "Icon=chromium"                       >> /root/.local/share/applications/chromiumroot.desktop
-    echo "Type=Application"                    >> /root/.local/share/applications/chromiumroot.desktop
-    echo "StartupNotify=false"                 >> /root/.local/share/applications/chromiumroot.desktop
-    echo "StartupWMClass=Code"                 >> /root/.local/share/applications/chromiumroot.desktop
-    echo "Categories=Network;WebBrowser;"      >> /root/.local/share/applications/chromiumroot.desktop
-    echo "MimeType=text/html;text/xml;application/xhtml_xml;application/x-mimearchive;x-scheme-handler/http;x-scheme-handler/https;" >> /root/.local/share/applications/chromiumroot.desktop
-    gio set /root/.local/share/applications/chromiumroot.desktop "metadata::trusted" yes
-
-  /root/scripts/d-scripts/SoftInst/Escritorio/TorBrowser-Instalar.sh
-
-  apt-get -y remove xterm reportbug blender imagemagick inkscape gnome-disk-utility
-  apt-get -y autoremove
+  echo ""
+  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
 
 elif [ $OS_VERS == "11" ]; then
 
@@ -232,6 +148,7 @@ elif [ $OS_VERS == "11" ]; then
     apt-get -y install scummvm
 
   # Fuentes
+    apt-get -y install fonts-ubuntu
     apt-get -y install fonts-freefont-ttf
     apt-get -y install fonts-freefont-otf
     apt-get -y install ttf-mscorefonts-installer
