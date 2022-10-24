5dec8117dc4f3212bf1ca52a5cd1377f7169d96a
NiPeGun <nipegun@gmail.com>
2022-10-24 13:30:51 +0200

    Update Servidor-OpenLDAP-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Servidor-OpenLDAP-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Servidor-OpenLDAP-InstalarYConfigurar.sh
@@ -6,9 +6,92 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-#  Script de NiPeGun para instalar y configurar el actualizador de NoIP en Debian
+#  Script de NiPeGun para instalar y configurar el servidor OpenLDAP en Debian
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/ActualizadorNoIP-InstalarYConfigurar.sh | bash
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-OpenLDAP-InstalarYConfigurar.sh | bash
 # ----------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org 
+     . /etc/os-release
+     OS_NAME=$NAME
+     OS_VERS=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # linuxbase.org
+     OS_NAME=$(lsb_release -si)
+     OS_VERS=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release
+     . /etc/lsb-release
+     OS_NAME=$DISTRIB_ID
+     OS_VERS=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+     OS_NAME=Debian
+     OS_VERS=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD)
+     OS_NAME=$(uname -s)
+     OS_VERS=$(uname -r)
+  fi
+
+if [ $OS_VERS == "7" ]; then
+
+  echo ""
+  echo "--------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación del servidor OpenLDAP en Debian 7 (Wheezy)..."
+  echo "--------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "8" ]; then
+
+  echo ""
+  echo "--------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación del servidor OpenLDAP en Debian 8 (Jessie)..."
+  echo "--------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "9" ]; then
+
+  echo ""
+  echo "---------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación del servidor OpenLDAP en Debian 9 (Stretch)..."
+  echo "---------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "10" ]; then
+
+  echo ""
+  echo "---------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación del servidor OpenLDAP en Debian 10 (Buster)..."
+  echo "---------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "11" ]; then
+
+  echo ""
+  echo "---------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación del servidor OpenLDAP en Debian 11 (Bullseye)..."
+  echo "---------------------------------------------------------------------------------------"
+  echo ""
+
+
+fi
 
