98c364bb942c608012831d2f98327a47634a27e0
NiPeGun <nipegun@gmail.com>
2022-12-10 15:43:26 +0100

    Update SistemaOperativo-Reiniciar.sh

--- SistemaOperativo-Reiniciar.sh
+++ SistemaOperativo-Reiniciar.sh
@@ -5,18 +5,16 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------
+# ----------
 #  Script de NiPeGun para reiniciar Debian
-#-------------------------------------------
+# ----------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
 FinColor='\033[0m'
 
 echo ""
-echo ""
 echo -e "${ColorVerde}Reiniciando sistema operativo...${FinColor}"
-echo ""
 echo ""
 shutdown -r now
 
