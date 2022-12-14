99f3e17a0cc0f2902b35499e717bcb766f19453d
NiPeGun <nipegun@gmail.com>
2022-12-14 12:59:11 +0100

    Update Repositorios-PonerTodos.sh

--- PostInst/CLI/Repositorios-PonerTodos.sh
+++ PostInst/CLI/Repositorios-PonerTodos.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#---------------------------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para poner todos los repos en Debian
 #
 #  Ejecución remota:
 #  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/Repositorios-PonerTodos.sh | bash
-#---------------------------------------------------------------------------------------------------------------------
+# ----------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
