228cb00a2d1bd6b67cb04a7a05da295ed875f983
NiPeGun <nipegun@gmail.com>
2023-01-07 03:02:27 +0100

    Update DScripts-Sincronizar.sh

--- DScripts-Sincronizar.sh
+++ DScripts-Sincronizar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para sincronizar los d-scripts
 #
 # Ejecución remota:
 # curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/DScripts-Sincronizar.sh | bash
-#--------------------------------------------------------------------------------------------------
+# ----------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
