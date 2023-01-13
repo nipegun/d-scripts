a718adaae663885f4b106b10b99214f5d086c756
NiPeGun <nipegun@gmail.com>
2023-01-13 12:26:56 +0100

    Create MariaDB-Servidor-ComprobarYLevantar.sh

---
+++ MariaDB-Servidor-ComprobarYLevantar.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para comprobar si un servidor MariaDB está caido y, si eso, 
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/APTKey-BajarEInstalar.sh | bash -s URL Servicio
+#
+#  Ejemplo:
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/APTKey-BajarEInstalar.sh | bash -s https://nightly.odoo.com/odoo.key Odoo
+# ----------
+
+vEstadoServBD=$(systemctl status mariadb.service | grep "atus:" | cut -d'"' -f2)
+
+if [[ $vEstadoServBD == "MariaDB server is down" ]]; then
+  systemctl start mariadb
+fi
+
