80411978c22c610a9ea3a9b5daf7bda21f26aeb7
NiPeGun <nipegun@gmail.com>
2023-01-23 11:20:15 +0100

    Update Servidor-DNS-Bind9-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Servidor-DNS-Bind9-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Servidor-DNS-Bind9-InstalarYConfigurar.sh
@@ -95,7 +95,7 @@ elif [ $OS_VERS == "11" ]; then
   # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
     if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
       echo ""
-      echo -e "${vColorRojo}El paquete dialog no está instalado. Iniciando su instalación...${vFinColor}"
+      echo -e "${vColorRojo}    El paquete dialog no está instalado. Iniciando su instalación...${vFinColor}"
       echo ""
       apt-get -y update && apt-get -y install dialog
       echo ""
