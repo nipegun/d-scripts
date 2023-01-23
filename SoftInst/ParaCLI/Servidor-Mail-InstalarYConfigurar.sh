48151d92294151be53c9d2b59123f450169d2297
NiPeGun <nipegun@gmail.com>
2023-01-23 12:16:20 +0100

    Update Servidor-Mail-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Servidor-Mail-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Servidor-Mail-InstalarYConfigurar.sh
@@ -36,14 +36,14 @@ vFinColor='\033[0m'
 
 # Comprobar si el script está corriendo como root
   if [ $(id -u) -ne 0 ]; then
-    echo -e "${vColorRojo}Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
+    echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
     exit 1
   fi
 
 # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
   if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
     echo ""
-    echo -e "${vColorRojo}curl no está instalado. Iniciando su instalación...${vFinColor}"
+    echo -e "${vColorRojo}  curl no está instalado. Iniciando su instalación...${vFinColor}"
     echo ""
     apt-get -y update && apt-get -y install curl
     echo ""
@@ -128,9 +128,9 @@ elif [ $OS_VERS == "11" ]; then
   menu=(dialog --checklist "Marca las opciones que quieras instalar:" 22 96 16)
     opciones=(
       1 "Instalar servidor DNS en el propio servidor de correo." on
-      2 "Opción 2" off
-      3 "Opción 3" off
-      4 "Opción 4" off
+      2 "Instalar el MTA (Mail Transport Agent) postfix." off
+      3 "Instalar el MDA (Mail Delivery Agent) dovecot." off
+      4 "Instalar el MUA (Mail User Agent) roundcube." off
       5 "Opción 5" off
     )
   choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
@@ -206,8 +206,8 @@ elif [ $OS_VERS == "11" ]; then
             # El servidor dns del servidor de correo debe ser el servidor DNS donde están configurados los registros:
             # Hay que crear una zona en el servidor DNS que sea el dominio y la extensión, por ejemplo: hacks4geeks.com
             # En bind 9 (zona directa, db.hacks4geeks.com):
-       //     echo "correo     IN A     $vIPDirecta" >> /etc/bind/db.$vDominio
-       //     echo "$vDominio. IN MX 10 correo"      >> /etc/bind/db.$vDominio # Las cuentas de correo que entren en @hacsk4geeks.com serán redirigidas a la entrada correos
+       ##     echo "correo     IN A     $vIPDirecta" >> /etc/bind/db.$vDominio
+       ##//     echo "$vDominio. IN MX 10 correo"      >> /etc/bind/db.$vDominio # Las cuentas de correo que entren en @hacsk4geeks.com serán redirigidas a la entrada correos
             # Comprobamos que la zona esté bien escrita:
              echo ""
              echo "  Comprobando la sintaxis de la zona directa..."
@@ -226,115 +226,18 @@ elif [ $OS_VERS == "11" ]; then
           2)
 
             echo ""
-            echo "  Opción 2..."
+            echo "  Instalando el MTA (Mail Transport Agent) postfix..."
             echo ""
-   
-            # Instalar el servidor de bases de datos
-              apt-get -y install mariadb-server
-            # Instalar roundcube (asume que la contraseña root de MySQL está vacía, si no, debería preguntar la contraseña root)
-              apt-get -y install roundcube
-              # dbconfig-common: si
-              # Poner contraseña de aplicación.
-            # Activar el alias para ingresar mediante /roundcube
-              sed -i -e 's-#    Alias /roundcube /var/lib/roundcube/public_html-Alias /roundcube /var/lib/roundcube/public_html-g' /etc/roundcube/apache.conf
-            # Hacer que se sirva en la carpeta raíz de apache
-              # Modificar el sitio por defecto para HTTP
-                sed -i -e 's|</VirtualHost>|\n  Include /etc/roundcube/apache.conf\n  Alias / /var/lib/roundcube/public_html/\n\n</VirtualHost>|g' /etc/apache2/sites-available/000-default.conf
-                a2ensite 000-default
-                # Modificar el sitio por defecto para HTTPS
-                sed -i -e 's|</VirtualHost>|\n  Include /etc/roundcube/apache.conf\n  Alias / /var/lib/roundcube/public_html/\n\n</VirtualHost>|g' /etc/apache2/sites-available/default-ssl.conf
-               a2ensite default-ssl.conf
-               a2enmod ssl
-              # Crear un certificado autofirmado
-              openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/roundcube.key -out /etc/ssl/certs/roundcube.crt -subj "/C=ES/ST=Madrid/L=Arganda/O=MiEmpresa/CN=dominio.com/emailAddress=mail@gmail.com"
-              # Reemplazar la ubicación del certificado en el archivo de configuración
-              sed -i -e 's|/etc/ssl/certs/ssl-cert-snakeoil.pem|/etc/ssl/certs/roundcube.crt|g'     /etc/apache2/sites-available/default-ssl.conf
-              sed -i -e 's|/etc/ssl/private/ssl-cert-snakeoil.key|/etc/ssl/private/roundcube.key|g' /etc/apache2/sites-available/default-ssl.conf
-              # Desactivar la configuración por defecto de roundcube
-              a2disconf roundcube
-              # Comprobar sintaxis de apache
-              echo ""
-              echo "  Comprobando sintaxis de los archivos de apache..."
-              echo ""
-              apache2ctl configtest
-              # Reiniciar el servidor apache
-                echo ""
-                echo "  Reiniciando el servidor apache..."
-                echo ""
-                systemctl restart apache2
-                systemctl restart apache2
 
-              # Reiniciar el servicio apache
-                service apache2 restart
-              # Instalar el demonio para IMAP
-                apt-get -y install dovecot-imapd
-              # Modificar la configuración
-                #sed -i -e 's|$config['default_host'] = '';|$config['default_host'] = 'localhost';|g'                                            /etc/roundcube/config.inc.php
-                sed -i -e 's|$config['default_host'] = '';|$config['default_host'] = 'correo.festivalehz.local';|g'                              /etc/roundcube/config.inc.php
-                sed -i -e 's|$config['smtp_port'] = 587;|$config['smtp_port'] = 25;|g'                                                           /etc/roundcube/config.inc.php
-                sed -i -e 's|$config['smtp_user'] = '%u';|$config['smtp_user'] = '';|g'                                                          /etc/roundcube/config.inc.php
-                sed -i -e 's|$config['plugins'] = array(|$config['plugins'] = array(\n'archive',\n'zipdownload',\n'managesieve',\n'password',|g' /etc/roundcube/config.inc.php
-                echo ""                                                                                                                       >> /etc/roundcube/config.inc.php
-                echo ""'$config'"['session_lifetime'] = 60;"                                                                                  >> /etc/roundcube/config.inc.php
-                echo ""'$config'"['skin_logo'] = './ispmail-logo.png';"                                                                       >> /etc/roundcube/config.inc.php
-                echo ""
-                echo "  Cambiando el logo por defecto..."
-                echo ""
-              # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
-                if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
-                  echo ""
-                  echo -e "${vColorRojo}    wget no está instalado. Iniciando su instalación...${vFinColor}"
-                  echo ""
-                  apt-get -y update && apt-get -y install wget
-                  echo ""
-                fi
-              wget --no-check-certificate http://hacks4geeks.com/_/cosas/LogoMailEHZ.png -O /var/lib/roundcube/public_html/logo.png
+            apt-get -y install postfix
 
-              apt-get -y install postfix
-              apt-get -y install dovecot-imapd
-              #apt-get -y install dovecot-pop3d
-              #dpkg-reconfigure postfix
-              # -> Sitio de intenet
-              # -> festivalehz.ddns.net 
-              # -> En blanco
-              # -> Enter
-              # -> Forzar actualizaciones síncronas, si
-              # -> 127.0.0.0/24 192.168.0.0/24 192.168.1.0/24 192.168.2.0/24 192.168.3.0/24 192.168.4.0/24 192.168.255.0/24
-              # -> Limite 0, ilimitado
-              # -> Caracter de extension de direcciones locales +
-              # -> Protocolos de internet a usar IPv4
-              echo "home_mailbox = Maildir/" >> /etc/postfix/main.cf
-              maildirmake.dovecot /etc/skel/Maildir
-              sed -i -e 's|#   mail_location = maildir:~/Maildir|mail_location = maildir:~/Maildir|g' /etc/dovecot/conf.d/10-mail.conf
-              service dovecot restart
-              service postfix restart
+            echo "home_mailbox = Maildir/" >> /etc/postfix/main.cf
 
-              apt-get -y install mariadb-server
-              apt-get -y install mariadb-client
-              apt-get -y install phpmyadmin
-              # Marcar apache2
-              # Configurar la base para dbconfig-common: si
-              # Poner contraseña de aplicacion
-              mysql_secure_installation
-              #mysql -uroot -prootMySQL -e "update mysql.user set plugin='' where user = 'root';"
-              mysql -e "create user 'roundcube'@'localhost' identified by 'roundcube';"
-              mysql -e "grant all privileges on mysql.* to 'roundcube'@'localhost';"
-              mysql -e "create database roundcube;"
-              mysql -e "grant all privileges on roundcube.* to 'roundcube'@'localhost'";
-              apt-get -y install roundcube
-                # dbconfig common, si, app
-                # TCP/IP
-                # Servidor de bases de datos para roundcube: localhost
-                # Número de puerto del servicio mysql: 3306
-                # Authentication: mysql_native_password
-                # Nombre de la base de datos: roundcube
-                # Usuario roundcube @ localhost
-                # contraseña de aplicación: roundcube (Es la contraseña de :identified by 'roundcube')
-              sed -i -e 's-#    Alias /roundcube /var/lib/roundcube/public_html-Alias /roundcube /var/lib/roundcube/public_html-g' /etc/roundcube/apache.conf
-              service apache2 restart
-              adduser nico
-              adduser pablo
-              adduser gorka
+            service postfix restart
+
+            adduser nico
+            adduser pablo
+            adduser gorka
 
             # Instalar paquetes
               apt-get -y install roundcube
@@ -357,30 +260,128 @@ elif [ $OS_VERS == "11" ]; then
             echo "dbname = roundcube"                                                >> /etc/postfix/mysql-virtual-mailbox-maps.cf
             echo "query = SELECT maildir FROM virtual_users WHERE email='%s'"        >> /etc/postfix/mysql-virtual-mailbox-maps.cf
             systemctl restart postfix
-  
-            # Indicar el servidor IMAP y SMTP en el archivo de confioguración de roundcube
-              sed -i -e 's|$config['default_host'] = '';|$config['default_host'] = 'tuservidor';|g'        /etc/roundcube/config.inc.php
-              sed -i -e 's|$config['smtp_server'] = 'localhost';|$config['smtp_server'] = 'tuservidor';|g' /etc/roundcube/config.inc.php
-            #apt-get install roundcube-plugins
-              echo "home_mailbox = Maildir/" >> /etc/postfix/main.cf
-              maildirmake.dovecot /etc/skel/Maildir
-             apt-get -y install postfix
+
+            echo "home_mailbox = Maildir/" >> /etc/postfix/main.cf
+            maildirmake.dovecot /etc/skel/Maildir
+            apt-get -y install postfix
 
           ;;
 
           3)
 
             echo ""
-            echo "  Opción 3..."
+            echo "  Instalando el MDA (Mail Delivery Agent) dovecot..."
             echo ""
+
+            # Reiniciar el servicio apache
+              service apache2 restart
+            # Instalar el demonio para IMAP
+              apt-get -y install dovecot-imapd
+            # Modificar la configuración
+              #sed -i -e 's|$config['default_host'] = '';|$config['default_host'] = 'localhost';|g'                                            /etc/roundcube/config.inc.php
+              sed -i -e 's|$config['default_host'] = '';|$config['default_host'] = 'correo.festivalehz.local';|g'                              /etc/roundcube/config.inc.php
+              sed -i -e 's|$config['smtp_port'] = 587;|$config['smtp_port'] = 25;|g'                                                           /etc/roundcube/config.inc.php
+              sed -i -e 's|$config['smtp_user'] = '%u';|$config['smtp_user'] = '';|g'                                                          /etc/roundcube/config.inc.php
+              sed -i -e 's|$config['plugins'] = array(|$config['plugins'] = array(\n'archive',\n'zipdownload',\n'managesieve',\n'password',|g' /etc/roundcube/config.inc.php
+              echo ""                                                                                                                       >> /etc/roundcube/config.inc.php
+              echo ""'$config'"['session_lifetime'] = 60;"                                                                                  >> /etc/roundcube/config.inc.php
+              echo ""'$config'"['skin_logo'] = './ispmail-logo.png';"                                                                       >> /etc/roundcube/config.inc.php
+              echo ""
+              echo "  Cambiando el logo por defecto..."
+              echo ""
+            # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
+              if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
+                echo ""
+                echo -e "${vColorRojo}    wget no está instalado. Iniciando su instalación...${vFinColor}"
+                echo ""
+                apt-get -y update && apt-get -y install wget
+                echo ""
+              fi
+            wget --no-check-certificate http://hacks4geeks.com/_/cosas/LogoMailEHZ.png -O /var/lib/roundcube/public_html/logo.png
+
+
+            maildirmake.dovecot /etc/skel/Maildir
+            sed -i -e 's|#   mail_location = maildir:~/Maildir|mail_location = maildir:~/Maildir|g' /etc/dovecot/conf.d/10-mail.conf
+            service dovecot restart
 
           ;;
 
           4)
 
             echo ""
-            echo "  Opción 4..."
+            echo "  Instalando el MUA (Mail User Agent) roundcube."
             echo ""
+
+            # Instalar el servidor de bases de datos
+              apt-get -y install mariadb-server
+            # Instalar roundcube (asume que la contraseña root de MySQL está vacía, si no, debería preguntar la contraseña root)
+              apt-get -y install roundcube
+              # dbconfig-common: si
+              # Poner contraseña de aplicación.
+            # Activar el alias para ingresar mediante /roundcube
+              sed -i -e 's-#    Alias /roundcube /var/lib/roundcube/public_html-Alias /roundcube /var/lib/roundcube/public_html-g' /etc/roundcube/apache.conf
+            # Hacer que se sirva en la carpeta raíz de apache
+              # Modificar el sitio por defecto para HTTP
+                sed -i -e 's|</VirtualHost>|\n  Include /etc/roundcube/apache.conf\n  Alias / /var/lib/roundcube/public_html/\n\n</VirtualHost>|g' /etc/apache2/sites-available/000-default.conf
+                a2ensite 000-default
+              # Modificar el sitio por defecto para HTTPS
+                sed -i -e 's|</VirtualHost>|\n  Include /etc/roundcube/apache.conf\n  Alias / /var/lib/roundcube/public_html/\n\n</VirtualHost>|g' /etc/apache2/sites-available/default-ssl.conf
+              a2ensite default-ssl.conf
+              a2enmod ssl
+              # Crear un certificado autofirmado
+                openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/roundcube.key -out /etc/ssl/certs/roundcube.crt -subj "/C=ES/ST=Madrid/L=Arganda/O=MiEmpresa/CN=dominio.com/emailAddress=mail@gmail.com"
+              # Reemplazar la ubicación del certificado en el archivo de configuración
+                sed -i -e 's|/etc/ssl/certs/ssl-cert-snakeoil.pem|/etc/ssl/certs/roundcube.crt|g'     /etc/apache2/sites-available/default-ssl.conf
+                sed -i -e 's|/etc/ssl/private/ssl-cert-snakeoil.key|/etc/ssl/private/roundcube.key|g' /etc/apache2/sites-available/default-ssl.conf
+              # Desactivar la configuración por defecto de roundcube
+                a2disconf roundcube
+              # Comprobar sintaxis de apache
+                echo ""
+                echo "  Comprobando sintaxis de los archivos de apache..."
+                echo ""
+                apache2ctl configtest
+              # Reiniciar el servidor apache
+                echo ""
+                echo "  Reiniciando el servidor apache..."
+                echo ""
+                systemctl restart apache2
+                systemctl restart apache2
+
+
+
+
+
+
+              apt-get -y install mariadb-server
+              apt-get -y install mariadb-client
+              apt-get -y install phpmyadmin
+              # Marcar apache2
+              # Configurar la base para dbconfig-common: si
+              # Poner contraseña de aplicacion
+              mysql_secure_installation
+              #mysql -uroot -prootMySQL -e "update mysql.user set plugin='' where user = 'root';"
+              mysql -e "create user 'roundcube'@'localhost' identified by 'roundcube';"
+              mysql -e "grant all privileges on mysql.* to 'roundcube'@'localhost';"
+              mysql -e "create database roundcube;"
+              mysql -e "grant all privileges on roundcube.* to 'roundcube'@'localhost'";
+              apt-get -y install roundcube
+
+                # dbconfig common, si, app
+                # TCP/IP
+                # Servidor de bases de datos para roundcube: localhost
+                # Número de puerto del servicio mysql: 3306
+                # Authentication: mysql_native_password
+                # Nombre de la base de datos: roundcube
+                # Usuario roundcube @ localhost
+                # contraseña de aplicación: roundcube (Es la contraseña de :identified by 'roundcube')
+              sed -i -e 's-#    Alias /roundcube /var/lib/roundcube/public_html-Alias /roundcube /var/lib/roundcube/public_html-g' /etc/roundcube/apache.conf
+              service apache2 restart
+
+  
+            # Indicar el servidor IMAP y SMTP en el archivo de confioguración de roundcube
+              sed -i -e 's|$config['default_host'] = '';|$config['default_host'] = 'tuservidor';|g'        /etc/roundcube/config.inc.php
+              sed -i -e 's|$config['smtp_server'] = 'localhost';|$config['smtp_server'] = 'tuservidor';|g' /etc/roundcube/config.inc.php
+            #apt-get install roundcube-plugins
 
           ;;
 
@@ -389,6 +390,20 @@ elif [ $OS_VERS == "11" ]; then
             echo ""
             echo "  Opción 5..."
             echo ""
+
+            apt-get -y install dovecot-imapd
+              #apt-get -y install dovecot-pop3d
+              #dpkg-reconfigure postfix
+              # -> Sitio de intenet
+              # -> festivalehz.ddns.net 
+              # -> En blanco
+              # -> Enter
+              # -> Forzar actualizaciones síncronas, si
+              # -> 127.0.0.0/24 192.168.0.0/24 192.168.1.0/24 192.168.2.0/24 192.168.3.0/24 192.168.4.0/24 192.168.255.0/24
+              # -> Limite 0, ilimitado
+              # -> Caracter de extension de direcciones locales +
+              # -> Protocolos de internet a usar IPv4
+
 
           ;;
 
