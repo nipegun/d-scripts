d9c8c20230285cca9cb080e5ef5fefddbc7df887
NiPeGun <nipegun@gmail.com>
2023-01-25 18:59:48 +0100

    Update Idioma-CambiarTodoAes-es.sh

--- PostInst/CLI/Idioma-CambiarTodoAes-es.sh
+++ PostInst/CLI/Idioma-CambiarTodoAes-es.sh
@@ -18,22 +18,22 @@ FinColor='\033[0m'
 
 # Determinar la versión de Debian
   if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org
-       . /etc/os-release
-       OS_NAME=$NAME
-       OS_VERS=$VERSION_ID
+    . /etc/os-release
+    OS_NAME=$NAME
+    OS_VERS=$VERSION_ID
   elif type lsb_release >/dev/null 2>&1; then # linuxbase.org
-       OS_NAME=$(lsb_release -si)
-       OS_VERS=$(lsb_release -sr)
+    OS_NAME=$(lsb_release -si)
+    OS_VERS=$(lsb_release -sr)
   elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release
-       . /etc/lsb-release
-       OS_NAME=$DISTRIB_ID
-       OS_VERS=$DISTRIB_RELEASE
+    . /etc/lsb-release
+    OS_NAME=$DISTRIB_ID
+    OS_VERS=$DISTRIB_RELEASE
   elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
-       OS_NAME=Debian
-       OS_VERS=$(cat /etc/debian_version)
+    OS_NAME=Debian
+    OS_VERS=$(cat /etc/debian_version)
   else                                        # Para el viejo uname (También funciona para BSD)
-       OS_NAME=$(uname -s)
-       OS_VERS=$(uname -r)
+    OS_NAME=$(uname -s)
+    OS_VERS=$(uname -r)
   fi
 
 if [ $OS_VERS == "7" ]; then
