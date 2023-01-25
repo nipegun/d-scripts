e40d6b745090903420136234980009325796e885
NiPeGun <nipegun@gmail.com>
2023-01-25 18:56:34 +0100

    Update TareasCron-Preparar.sh

--- PostInst/CLI/TareasCron-Preparar.sh
+++ PostInst/CLI/TareasCron-Preparar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para preparar las tareas cron
 #
 #  Ejecución remota:
 #  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/TareasCron-Preparar.sh | bash
-#-----------------------------------------------------------------------------------------------------------------
+# ----------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
