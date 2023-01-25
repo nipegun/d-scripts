fdffb8028587e370c6140a27cbd5ae9dd7312c6e
NiPeGun <nipegun@gmail.com>
2023-01-25 19:17:36 +0100

    Update Servicio-xrdpMonitorCon-Instalar.sh

--- PostInst/GUI/Servicio-xrdpMonitorCon-Instalar.sh
+++ PostInst/GUI/Servicio-xrdpMonitorCon-Instalar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#------------------------------------------------------------------------------------------------------------------------------
+# ----------
 #  Servicio de NiPeGun para monitorizar las conexiones xrdp
 #
 #  Ejecución remota:
 #  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/GUI/Servicio-xrdpMonitorCon-Instalar.sh | bash
-#------------------------------------------------------------------------------------------------------------------------------
+# ----------
 
 # Crear el servicio
   echo "[Unit]"                                                                      > /etc/systemd/system/xrdpMonitorCon.service
