a11ea282c040d0bc0c8e87281ec69a3dd74871d0
NiPeGun <nipegun@gmail.com>
2023-01-29 15:37:28 +0100

    Create Argumentos.sh

---
+++ SoftInst/Argumentos.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+vCantArgsCorrectos=2
+vArgsInsuficientes=65
+
+vColorAzul="\033[0;34m"
+vColorAzulClaro="\033[1;34m"
+vColorVerde='\033[1;32m'
+vColorRojo='\033[1;31m'
+vFinColor='\033[0m'
+
+if [ $# -ne $vCantArgsCorrectos ]
+  then
+    echo ""
+    echo "------------------------------------------------------------------------------"
+    echo "Mal uso del script."
+    echo ""
+    echo "El uso correcto sería: $0 [Argumento1] [Argumento2]"
+    echo ""
+    echo "Ejemplo:"
+    echo ' $0 "/etc/pepe/" "pepe.conf"'
+    echo "------------------------------------------------------------------------------"
+    echo ""
+    exit $vArgsInsuficientes
+  else
+    echo ""
+    echo ""
+    echo ""
+    cat $1$2
+
+fi
+
