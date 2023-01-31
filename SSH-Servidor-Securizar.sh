0e20f1937d17779e7064067c5163a33197155d16
NiPeGun <nipegun@gmail.com>
2023-01-31 13:16:02 +0100

    Update SSH-Servidor-Securizar.sh

--- SSH-Servidor-Securizar.sh
+++ SSH-Servidor-Securizar.sh
@@ -9,7 +9,7 @@
 #  Script de NiPeGun para securizar el servidor SSH por defecto de Debian
 #
 #  Ejecución remota:
-#  curl -s x | bash
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SSH-Servidor-Securizar.sh | bash
 # ----------
 
 # Comprobar si el script está corriendo como root
@@ -83,15 +83,83 @@ elif [ $OS_VERS == "11" ]; then
   echo ""
   echo -e "${vColorAzulClaro}Iniciando el script de securización de SSH para Debian 11 (Bullseye)...${vFinColor}"
   echo ""
-  # Cambiar el puerto
-    sed -i -e 's|#Port 22|Port 22222|g' /etc/ssh/sshd_config
-  # Deshabilitar el logueo del root
-    sed -i -e 's|PermitRootLogin yes|PermitRootLogin no|g' /etc/ssh/sshd_config
-  # Configurar el tiempo máximo para introducir la contraseña en 15 segundos
-    sed -i -e 's|#LoginGraceTime 2m|LoginGraceTime 15|g' /etc/ssh/sshd_config
-  # Limitar ingresar la contraseña mal a sólo 3 veces
-    sed -i -e 's|#MaxAuthTries 6|MaxAuthTries 3|g' /etc/ssh/sshd_config
-  # Denegar ssh a los usuarios mortadelo y filemon
-    echo "DenyUsers mortadelo filemon" >> /etc/ssh/sshd_config
+
+ #menu=(dialog --timeout 5 --checklist "Marca las opciones que quieras instalar:" 22 96 16)
+  menu=(dialog --checklist "Marca las opciones que quieras instalar:" 22 96 16)
+    opciones=(
+      1 "Ejecutar securización básica (casi mandatorio)" on
+      2 "Opción 2" off
+      3 "Opción 3" off
+      4 "Opción 4" off
+      5 "Opción 5" off
+    )
+  choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
+  #clear
+
+    for choice in $choices
+      do
+        case $choice in
+
+          1)
+
+            echo ""
+            echo "  Ejecutando securización básica..."
+            echo ""
+            echo "    Los cambios serán los siguientes:"
+            echo ""
+            echo "    - Se cambiará el puerto del 2 al 22222"
+            echo "    - Se deshabilitará el logueo con la cuenta del root"
+            echo "    - Se limitará el tiempo para introducir la contraseña a 15 segundos"
+            echo "    - Se limitará el ingreso de contraseña incorrecta a sólo 3 veces."
+            echo "    - Se denegará permanentemente el acceso a los usuarios mortadelo y filemon"
+            echo ""
+            # Cambiar el puerto
+              sed -i -e 's|#Port 22|Port 22222|g' /etc/ssh/sshd_config
+            # Deshabilitar el logueo del root
+              sed -i -e 's|PermitRootLogin yes|PermitRootLogin no|g' /etc/ssh/sshd_config
+            # Configurar el tiempo máximo para introducir la contraseña en 15 segundos
+              sed -i -e 's|#LoginGraceTime 2m|LoginGraceTime 15|g' /etc/ssh/sshd_config
+            # Limitar ingresar la contraseña mal a sólo 3 veces
+              sed -i -e 's|#MaxAuthTries 6|MaxAuthTries 3|g' /etc/ssh/sshd_config
+            # Denegar ssh a los usuarios mortadelo y filemon
+              echo "DenyUsers mortadelo filemon" >> /etc/ssh/sshd_config
+
+          ;;
+
+          2)
+
+            echo ""
+            echo "  Opción 2..."
+            echo ""
+
+          ;;
+
+          3)
+
+            echo ""
+            echo "  Opción 3..."
+            echo ""
+
+          ;;
+
+          4)
+
+            echo ""
+            echo "  Opción 4..."
+            echo ""
+
+          ;;
+
+          5)
+
+            echo ""
+            echo "  Opción 5..."
+            echo ""
+
+          ;;
+
+      esac
+
+  done
 
 fi
