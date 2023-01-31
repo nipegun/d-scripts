02c0f694eae484a3969f0292f013f1b52b3ff185
NiPeGun <nipegun@gmail.com>
2023-01-31 09:20:43 +0100

    Update MariaDB-Cluster-Galera-ReInicializar.sh

--- MariaDB-Cluster-Galera-ReInicializar.sh
+++ MariaDB-Cluster-Galera-ReInicializar.sh
@@ -9,7 +9,7 @@
 #   Script de NiPeGun para volver a inicializar el cluster de Galera cuando no se levanta al no encontrar los otros nodos
 #
 #   Ejecución remota:
-#   curl -s x | bash
+#   curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/MariaDB-Cluster-Galera-ReInicializar.sh | bash
 # ----------
 
 echo ""
