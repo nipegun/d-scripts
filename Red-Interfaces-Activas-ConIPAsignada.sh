ad155ef2bfffbc3ab23baac307bb43c5f68ee0c4
NiPeGun <nipegun@gmail.com>
2023-02-03 21:46:31 +0100

    Update Red-Interfaces-Activas-ConIPAsignada.sh

--- Red-Interfaces-Activas-ConIPAsignada.sh
+++ Red-Interfaces-Activas-ConIPAsignada.sh
@@ -127,9 +127,9 @@ elif [ $OS_VERS == "11" ]; then
     aInterfazActivaConIP=()
     for (( j=0; j<$vCantIntActivas; j++ ));
       do
-        vInterfazSinArroba=$(echo "${aIntRedActivas[$j]}" | cut -d '@' -f1)
-        #vIPInt=$(ip a show ${aIntRedActivas[$j]} 2> /dev/null | grep "scope" | grep -Po '(?<=inet )[\d.]+')
-        vIPInt=$(ip a show $vInterfazSinArroba 2> /dev/null | grep "scope" | grep -Po '(?<=inet )[\d.]+')
+        #vInterfazSinArroba=$(echo "${aIntRedActivas[$j]}" | cut -d '@' -f1)
+        #vIPInt=$(ip a show $vInterfazSinArroba 2> /dev/null | grep "scope" | grep -Po '(?<=inet )[\d.]+')
+        vIPInt=$(ip a show ${aIntRedActivas[$j]} 2> /dev/null | grep "scope" | grep -Po '(?<=inet )[\d.]+')
         if [ "$vIPInt" != ""  ]; then
           echo "    → :${aIntRedActivas[$j]}:$vIPInt"
           aInterfazActivaConIP[$j]=${aIntRedActivas[$j]}
