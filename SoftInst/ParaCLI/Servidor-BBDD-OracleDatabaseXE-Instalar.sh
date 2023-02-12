35c1ba5ca4deae66fa63880a2df7035429aad7ad
NiPeGun <nipegun@gmail.com>
2023-02-12 14:25:52 +0100

    Update Servidor-BBDD-OracleDatabaseXE-Instalar.sh

--- SoftInst/ParaCLI/Servidor-BBDD-OracleDatabaseXE-Instalar.sh
+++ SoftInst/ParaCLI/Servidor-BBDD-OracleDatabaseXE-Instalar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para instalar el servidor de bases de datos de Oracle Database XE en Debian
 #
 #  Ejecución remota
 #  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-BBDD-OracleDatabaseXE-Instalar.sh | bash
-#-----------------------------------------------------------------------------------------------------------------------------------------
+# ----------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
