5ec10a83aa3d3d6ccdbaeae8519e845cb02f8750
NiPeGun <nipegun@gmail.com>
2023-02-27 17:06:21 +0100

    Create Zabbix-InstalarYConfigurar.sh

---
+++ SoftInst/ParaCLI/Zabbix-InstalarYConfigurar.sh
@@ -0,0 +1,245 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para instalar y configurar Zabbix en Debian
+#
+#  Ejecución remota:
+#  curl -s x | bash
+#
+#  Ejecución remota sin caché:
+#  curl -s -H 'Cache-Control: no-cache, no-store' x | bash
+#
+#  Ejecución remota con parámetros:
+#  curl -s x | bash -s Parámetro1 Parámetro2
+# ----------
+
+vColorAzul="\033[0;34m"
+vColorAzulClaro="\033[1;34m"
+vColorVerde='\033[1;32m'
+vColorRojo='\033[1;31m'
+vFinColor='\033[0m'
+
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
+    exit 1
+  fi
+
+# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${vColorRojo}  curl no está instalado. Iniciando su instalación...${vFinColor}"
+    echo ""
+    apt-get -y update
+    apt-get -y install curl
+    echo ""
+  fi
+
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
+    . /etc/os-release
+    OS_NAME=$NAME
+    OS_VERS=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # Para linuxbase.org.
+    OS_NAME=$(lsb_release -si)
+    OS_VERS=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release.
+    . /etc/lsb-release
+    OS_NAME=$DISTRIB_ID
+    OS_VERS=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    OS_NAME=Debian
+    OS_VERS=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD).
+    OS_NAME=$(uname -s)
+    OS_VERS=$(uname -r)
+  fi
+
+if [ $OS_VERS == "7" ]; then
+
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de Zabbix para Debian 7 (Wheezy)...${vFinColor}"
+  echo ""
+
+  echo ""
+  echo -e "${vColorRojo}    Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
+  echo ""
+
+elif [ $OS_VERS == "8" ]; then
+
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de Zabbix para Debian 8 (Jessie)...${vFinColor}"
+  echo ""
+
+  echo ""
+  echo -e "${vColorRojo}    Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
+  echo ""
+
+elif [ $OS_VERS == "9" ]; then
+
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de Zabbix para Debian 9 (Stretch)...${vFinColor}"
+  echo ""
+
+  echo ""
+  echo -e "${vColorRojo}    Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
+  echo ""
+
+elif [ $OS_VERS == "10" ]; then
+
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de Zabbix para Debian 10 (Buster)...${vFinColor}"
+  echo ""
+
+  echo ""
+  echo -e "${vColorRojo}    Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
+  echo ""
+
+elif [ $OS_VERS == "11" ]; then
+
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de Zabbix para Debian 11 (Bullseye)...${vFinColor}"
+  echo ""
+
+  # Obtener el númer de la última versión LTS de Zabbix
+    echo ""
+    echo "    Obteniendo el número de la última versión LTS de Zabbix..."
+    echo ""
+    # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo -e "${vColorRojo}      El paquete curl no está instalado. Iniciando su instalación...${vFinColor}"
+        echo ""
+        apt-get -y update
+        apt-get -y install curl
+        echo ""
+      fi
+    vUltVerLTS)
+  # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${vColorRojo}    El paquete dialog no está instalado. Iniciando su instalación...${vFinColor}"
+      echo ""
+      apt-get -y update
+      apt-get -y install dialog
+      echo ""
+    fi
+
+  menu=(dialog --checklist "Marca las opciones que quieras instalar:" 22 96 16)
+    opciones=(
+      1 "Instalar Zabbix LTS con servidor Web apache2 y base de datos MySQL." on
+      2 "Instalar Zabbix LTS con servidor Web apache2 y base de datos PostGreSQL." off
+      3 "Instalar Zabbix LTS con servidor Web nginx y base de datos MySQL." off
+      4 "Instalar Zabbix LTS con servidor Web nginx y base de datos PostgreSQL." off
+      5 "Opción 5" off
+    )
+  choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
+
+    for choice in $choices
+      do
+        case $choice in
+
+          1)
+
+            echo ""
+            echo "  Instalando Zabbix LTS con servidor Web apache2 y base de datos MySQL..."
+            echo ""
+
+            # Agregar el repositorio
+              echo ""
+              echo "    Agregando el repositorio..."
+              echo ""
+              # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
+                if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
+                  echo ""
+                  echo -e "${vColorRojo}    El paquete wget no está instalado. Iniciando su instalación...${vFinColor}"
+                  echo ""
+                  apt-get -y update
+                  apt-get -y install wget
+                  echo ""
+                fi
+              wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-4%2Bdebian11_all.deb
+              dpkg -i zabbix-release_6.0-4+debian11_all.deb
+              apt-get -y update
+
+            # Instalar el frontend y el agente
+              echo ""
+              echo "    Instalando el frontend y el repositorio..."
+              echo ""
+              apt-get -y install zabbix-server-mysql
+              apt-get -y install zabbix-frontend-php
+              apt-get -y install zabbix-apache-conf
+              apt-get -y install zabbix-sql-scripts
+              apt-get -y install zabbix-agent
+
+            # Crear la base de datos
+              echo ""
+              echo "    Creando la base de datos..."
+              echo ""
+              mysql -e "create database zabbix character set utf8mb4 collate utf8mb4_bin"
+              mysql -e "create user zabbix@localhost identified by 'Pass123'"
+              mysql -e "grant all privileges on zabbix.* to zabbix@localhost"
+              mysql -e "set global log_bin_trust_function_creators = 1"
+
+            # Importando el esquema y los datos iniciales
+              zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p Pass123
+              # Deshabilitar log_bin_trust_function_creators
+                mysql -e "set global log_bin_trust_function_creators = 0"
+ 
+            # Indicar el password de la base de datos en el archivo de configuración
+              sed -i -e 's-DBPassword=-DBPassword=Pass123-g' /etc/zabbix/zabbix_server.conf
+
+            # Iniciar el servidor y el agente
+              systemctl restart zabbix-server zabbix-agent apache2
+              systemctl enable zabbix-server --now
+              systemctl zabbix-agent apache2 --now
+
+f. Open Zabbix UI web page
+
+The default URL for Zabbix UI when using Apache web server is http://host/zabbix 
+          ;;
+
+          2)
+
+            echo ""
+            echo "  Opción 2..."
+            echo ""
+
+          ;;
+
+          3)
+
+            echo ""
+            echo "  Opción 3..."
+            echo ""
+
+          ;;
+
+          4)
+
+            echo ""
+            echo "  Opción 4..."
+            echo ""
+
+          ;;
+
+          5)
+
+            echo ""
+            echo "  Opción 5..."
+            echo ""
+
+          ;;
+
+      esac
+
+  done
+
+
+
+fi
