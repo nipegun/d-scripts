afcdf51d5c11ed23e21dd0135a855e874d72a5fb
NiPeGun <nipegun@gmail.com>
2023-03-06 01:38:35 +0100

    Update DockerCE-Contenedor-Instalar-PortainerCE.sh

--- SoftInst/ParaCLI/DockerCE-Contenedor-Instalar-PortainerCE.sh
+++ SoftInst/ParaCLI/DockerCE-Contenedor-Instalar-PortainerCE.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------------------------------------------------------------------------
-#  Script de NiPeGun para instalar PortainerCE en el DockerCE de Debian
+# ----------
+# Script de NiPeGun para instalar PortainerCE en el DockerCE de Debian
 #
 # Ejecución remota
-# curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/DockerCE-InstalarContenedor-PortainerCE.sh | bash
-#--------------------------------------------------------------------------------------------------------------------------------------
+#   curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/DockerCE-InstalarContenedor-PortainerCE.sh | bash
+# ----------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
