195bcac6a825af7166bd818e9be049046a18feec
NiPeGun <nipegun@gmail.com>
2023-03-06 01:38:33 +0100

    Update DockerCE-Contenedor-Instalar-OracleLinux.sh

--- SoftInst/ParaCLI/DockerCE-Contenedor-Instalar-OracleLinux.sh
+++ SoftInst/ParaCLI/DockerCE-Contenedor-Instalar-OracleLinux.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------------------------------------------------------------------
-#  Script de NiPeGun para instalar OracleLinux en el DockerCE de Debian
+# ----------
+# Script de NiPeGun para instalar OracleLinux en el DockerCE de Debian
 #
 # Ejecución remota
-# curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/DockerCE-InstalarContenedor-OracleLinux.sh | bash
-#----------------------------------------------------------------------------------------------------------------------------------------
+#   curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/DockerCE-InstalarContenedor-OracleLinux.sh | bash
+# ----------
 
 VerOLDeseada=8
 
