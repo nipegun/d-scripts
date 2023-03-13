2c4a8faeead5da47bded5da937c6cabf69e17f5b
NiPeGun <nipegun@gmail.com>
2023-03-13 23:55:48 +0100

    Update ArrancarEnModoTexto.sh

--- Interfaz-ModoCLI.sh
+++ Interfaz-ModoCLI.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#---------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para hacer que Debian arranque en modo texto
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/ArrancarEnModoTexto.sh | bash
-#---------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Interfaz-ModoCLI.sh | bash
+# ----------
 
 echo ""
 echo "-------------------------"
