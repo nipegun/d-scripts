798f8046920dc0e2518e44007855ccb3bb1c6abe
NiPeGun <nipegun@gmail.com>
2023-03-14 04:52:12 +0100

    Update MySQLWorkbench-Instalar.sh

--- SoftInst/ParaGUI/MySQLWorkbench-Instalar.sh
+++ SoftInst/ParaGUI/MySQLWorkbench-Instalar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para instalar y configurar MySQL WorkBench en Debian
 #
 #  Ejecución remota:
 #  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/MySQLWorkbench-Instalar.sh | bash
-#-------------------------------------------------------------------------------------------------------------------------
+# ----------
 
 ## Determinar la versión de Debian
 
