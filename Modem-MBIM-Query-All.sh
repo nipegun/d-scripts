05a09e047fae8e415fecde0b7f1d6ffe6872bffc
NiPeGun <nipegun@gmail.com>
2023-03-16 01:02:13 +0100

    Update Modem-MBIM-Query-All.sh

--- Modem-MBIM-Query-All.sh
+++ Modem-MBIM-Query-All.sh
@@ -6,7 +6,7 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
+# Script de NiPeGun para obtener información del modem 4G mediante el protocolo MBIM
 #
 # Ejecución remota:
 #   curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Modem-MBIM-Query-All.sh | bash
@@ -24,212 +24,213 @@ vFinColor='\033[0m'
     exit 1
   fi
 
-# Comprobar si el paquete mbimcli está instalado. Si no lo está, instalarlo.
-  if [[ $(dpkg-query -s mbimcli 2>/dev/null | grep installed) == "" ]]; then
+# Comprobar si el paquete libmbim-utils está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s libmbim-utils 2>/dev/null | grep installed) == "" ]]; then
     echo ""
-    echo -e "${vColorRojo}  mbimcli no está instalado. Iniciando su instalación...${vFinColor}"
+    echo -e "${vColorRojo}  libmbim-utils no está instalado. Iniciando su instalación...${vFinColor}"
     echo ""
     apt-get -y update
-    apt-get -y install mbimcli
+    apt-get -y install libmbim-utils
     echo ""
   fi
 
 echo ""
-echo "  Ejecutando query-device-caps..."
+echo -e "${vColorAzulClaro}  Ejecutando query-device-caps...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-device-caps
 
 echo ""
-echo "  Ejecutando query-subscriber-ready-status..."
+echo -e "${vColorAzulClaro}  Ejecutando query-subscriber-ready-status...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-subscriber-ready-status
 
 echo ""
-echo "  Ejecutando query-radio-state..."
+echo -e "${vColorAzulClaro}  Ejecutando query-radio-state...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-radio-state
 
 echo ""
-echo "  Ejecutando query-device-services..."
+echo -e "${vColorAzulClaro}  Ejecutando query-device-services...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-device-services
 
 echo ""
-echo "  Ejecutando query-pin-state..."
+echo -e "${vColorAzulClaro}  Ejecutando query-pin-state...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-pin-state
 
 echo ""
-echo "  Ejecutando query-pin-list..."
+echo -e "${vColorAzulClaro}  Ejecutando query-pin-list...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-pin-list
 
 echo ""
-echo "  Ejecutando query-home-provider..."
+echo -e "${vColorAzulClaro}  Ejecutando query-home-provider...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-home-provider
 
 echo ""
-echo "  Ejecutando query-preferred-providers..."
+echo -e "${vColorAzulClaro}  Ejecutando query-preferred-providers...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-preferred-providers
 
 echo ""
-echo "  Ejecutando query-visible-providers..."
+echo -e "${vColorAzulClaro}  Ejecutando query-visible-providers...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-visible-providers
 
 echo ""
-echo "  Ejecutando query-registration-state..."
+echo -e "${vColorAzulClaro}  Ejecutando query-registration-state...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-registration-state
 
 echo ""
-echo "  Ejecutando query-signal-state..."
+echo "  Ejecutando query-signal-state...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-signal-state
 
 echo ""
-echo "  Ejecutando query-packet-service-state..."
+echo -e "${vColorAzulClaro}  Ejecutando query-packet-service-state...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-packet-service-state
 
 echo ""
-echo "  Ejecutando query-packet-statistics..."
+echo -e "${vColorAzulClaro}  Ejecutando query-packet-statistics...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-packet-statistics
 
 echo ""
-echo "  Ejecutando query-provisioned-contexts..."
+echo -e "${vColorAzulClaro}  Ejecutando query-provisioned-contexts...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-provisioned-contexts
 
 echo ""
-echo "  Ejecutando query-network-idle-hint..."
+echo -e "${vColorAzulClaro}  Ejecutando query-network-idle-hint...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-network-idle-hint
 
 echo ""
-echo "  Ejecutando query-emergency-mode..."
+echo -e "${vColorAzulClaro}  Ejecutando query-emergency-mode...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --query-emergency-mode
 
 echo ""
-echo "  Ejecutando phonebook-query-configuration..."
+echo -e "${vColorAzulClaro}  Ejecutando phonebook-query-configuration...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --phonebook-query-configuration
 
 echo ""
-echo "  Ejecutando ms-query-firmware-id..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-firmware-id...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-firmware-id
 
 echo ""
-echo "  Ejecutando ms-query-sar-config..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-sar-config...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-sar-config
 
 echo ""
-echo "  Ejecutando ms-query-transmission-status..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-transmission-status...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-transmission-status
 
 echo ""
-echo "  Ejecutando atds-query-signal..."
+echo -e "${vColorAzulClaro}  Ejecutando atds-query-signal...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --atds-query-signal
 
 echo ""
-echo "  Ejecutando atds-query-location..."
+echo -e "${vColorAzulClaro}  Ejecutando atds-query-location...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --atds-query-location
 
 echo ""
-echo "  Ejecutando ms-query-lte-attach-configuration..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-lte-attach-configuration...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-lte-attach-configuration
 
 echo ""
-echo "  Ejecutando ms-query-lte-attach-info..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-lte-attach-info...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-lte-attach-info
 
 echo ""
-echo "  Ejecutando ms-query-sys-caps..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-sys-caps...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-sys-caps
 
 echo ""
-echo "  Ejecutando ms-query-device-caps..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-device-caps...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-device-caps
 
 echo ""
-echo "  Ejecutando ms-query-device-slot-mappings..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-device-slot-mappings...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-device-slot-mappings
 
 echo ""
-echo "  Ejecutando ms-query-location-info-status..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-location-info-status...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-location-info-status
 
 echo ""
-echo "  Ejecutando ms-query-provisioned-contexts..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-provisioned-contexts...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-provisioned-contexts
 
 echo ""
-echo "  Ejecutando ms-query-base-stations-info..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-base-stations-info...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-base-stations-info
 
 echo ""
-echo "  Ejecutando ms-query-registration-parameters..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-registration-parameters...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-registration-parameters
 
 echo ""
-echo "  Ejecutando ms-query-modem-configuration..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-modem-configuration...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-modem-configuration
 
 echo ""
-echo "  Ejecutando ms-query-wake-reason..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-wake-reason...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-wake-reason
 
 echo ""
-echo "  Ejecutando quectel-query-radio-state..."
+echo -e "${vColorAzulClaro}  Ejecutando quectel-query-radio-state...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --quectel-query-radio-state
 
 echo ""
-echo "  Ejecutando intel-query-rfim..."
+echo -e "${vColorAzulClaro}  Ejecutando intel-query-rfim...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --intel-query-rfim
 
 echo ""
-echo "  Ejecutando ms-query-nitz..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-nitz...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-nitz
 
 echo ""
-echo "  Ejecutando ms-query-uicc-application-list..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-uicc-application-list...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-uicc-application-list
 
 echo ""
-echo "  Ejecutando ms-query-uicc-atr..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-uicc-atr...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-uicc-atr
 
 echo ""
-echo "  Ejecutando ms-query-uicc-reset..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-uicc-reset...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-uicc-reset
 
 echo ""
-echo "  Ejecutando ms-query-uicc-terminal-capability..."
+echo -e "${vColorAzulClaro}  Ejecutando ms-query-uicc-terminal-capability...${vFinColor}"
 echo ""
 mbimcli --device=/dev/cdc-wdm0 --device-open-proxy --ms-query-uicc-terminal-capability
+
