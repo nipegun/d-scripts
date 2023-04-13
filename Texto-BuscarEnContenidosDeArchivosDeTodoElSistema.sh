359076bbe0d5ce8fde5d7b9b0deaf487fd90bb37
NiPeGun <nipegun@gmail.com>
2023-04-13 18:00:34 +0200

    Update Texto-BuscarEnContenidosDeArchivosDeTodoElSistema.sh

--- Texto-BuscarEnContenidosDeArchivosDeTodoElSistema.sh
+++ Texto-BuscarEnContenidosDeArchivosDeTodoElSistema.sh
@@ -27,27 +27,26 @@ if [ $# -ne $EXPECTED_ARGS ]
     exit $E_BADARGS
   else
     echo ""
-    grep -rnw --color -e $1 /bin
-    grep -rnw --color -e $1 /boot
-    grep -rnw --color -e $1 /dev
-    grep -rnw --color -e $1 /etc
-    grep -rnw --color -e $1 /home
-    grep -rnw --color -e $1 /lib
-    grep -rnw --color -e $1 /lib64
-    grep -rnw --color -e $1 /lost+found
-    grep -rnw --color -e $1 /media
-    grep -rnw --color -e $1 /mnt
-    grep -rnw --color -e $1 /opt
-    grep -rnw --color -e $1 /root
-    grep -rnw --color -e $1 /run
-    grep -rnw --color -e $1 /sbin
-    grep -rnw --color -e $1 /srv
-    grep -rnw --color -e $1 /tmp
-    grep -rnw --color -e $1 /usr
-    grep -rnw --color -e $1 /var
+    grep -rnw --color -e "$1" /bin
+    grep -rnw --color -e "$1" /boot
+    grep -rnw --color -e "$1" /dev
+    grep -rnw --color -e "$1" /etc
+    grep -rnw --color -e "$1" /home
+    grep -rnw --color -e "$1" /lib
+    grep -rnw --color -e "$1" /lib64
+    grep -rnw --color -e "$1" /lost+found
+    grep -rnw --color -e "$1" /media
+    grep -rnw --color -e "$1" /mnt
+    grep -rnw --color -e "$1" /opt
+    grep -rnw --color -e "$1" /root
+    grep -rnw --color -e "$1" /run
+    grep -rnw --color -e "$1" /sbin
+    grep -rnw --color -e "$1" /srv
+    grep -rnw --color -e "$1" /tmp
+    grep -rnw --color -e "$1" /usr
+    grep -rnw --color -e "$1" /var
     echo ""
 fi
-
 
 # De esta forma sólo buscará en archivos con extensión .c o .h:
   #grep --include=\*.{c,h} -rnw '/path/to/somewhere/' -e "pattern"
@@ -55,7 +54,7 @@ fi
 # De esta otra forma se excluirá la búsqueda dentro delos archivos con extensión .o:
   #grep --exclude=\*.o -rnw '/path/to/somewhere/' -e "pattern"
 
-#Es posible excluir una o varias carpetas usando e parámetro "--exclude-dir".
-#Por ejemplo, lo siguiente excluirá las carpetas dir1/, dir2/ y todas las que hagan match con *.dst/:
-  #grep --exclude-dir={dir1,dir2,*.dst} -rnw '/path/to/search/' -e "pattern"
+# Es posible excluir una o varias carpetas usando e parámetro "--exclude-dir".
+  # Por ejemplo, lo siguiente excluirá las carpetas dir1/, dir2/ y todas las que hagan match con *.dst/:
+    # grep --exclude-dir={dir1,dir2,*.dst} -rnw '/path/to/search/' -e "pattern"
 
