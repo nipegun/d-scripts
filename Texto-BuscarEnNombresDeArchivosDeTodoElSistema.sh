8180db75c760150b21ec4d37ccafea4e6841d1f9
NiPeGun <nipegun@gmail.com>
2023-04-13 17:37:46 +0200

    Update Texto-BuscarEnNombresDeArchivosDeTodoElSistema.sh

--- Texto-BuscarEnNombresDeArchivosDeTodoElSistema.sh
+++ Texto-BuscarEnNombresDeArchivosDeTodoElSistema.sh
@@ -30,24 +30,24 @@ if [ $# -ne $EXPECTED_ARGS ]
     exit $E_BADARGS
   else
     echo ""
-    find /bin        -type f -name *$2* -print
-    find /boot       -type f -name *$2* -print
-    find /dev        -type f -name *$2* -print
-    find /etc        -type f -name *$2* -print
-    find /home       -type f -name *$2* -print
-    find /lib        -type f -name *$2* -print
-    find /lib64      -type f -name *$2* -print
-    find /lost+found -type f -name *$2* -print
-    find /media      -type f -name *$2* -print
-    find /mnt        -type f -name *$2* -print
-    find /opt        -type f -name *$2* -print
-    find /root       -type f -name *$2* -print
-    find /run        -type f -name *$2* -print
-    find /sbin       -type f -name *$2* -print
-    find /srv        -type f -name *$2* -print
-    find /tmp        -type f -name *$2* -print
-    find /usr        -type f -name *$2* -print
-    find /var        -type f -name *$2* -print
+    find /bin        -type f -name "*$2*" -print
+    find /boot       -type f -name "*$2*" -print
+    find /dev        -type f -name "*$2*" -print
+    find /etc        -type f -name "*$2*" -print
+    find /home       -type f -name "*$2*" -print
+    find /lib        -type f -name "*$2*" -print
+    find /lib64      -type f -name "*$2*" -print
+    find /lost+found -type f -name "*$2*" -print
+    find /media      -type f -name "*$2*" -print
+    find /mnt        -type f -name "*$2*" -print
+    find /opt        -type f -name "*$2*" -print
+    find /root       -type f -name "*$2*" -print
+    find /run        -type f -name "*$2*" -print
+    find /sbin       -type f -name "*$2*" -print
+    find /srv        -type f -name "*$2*" -print
+    find /tmp        -type f -name "*$2*" -print
+    find /usr        -type f -name "*$2*" -print
+    find /var        -type f -name "*$2*" -print
     echo ""
     echo ""
     #find /bin        -type f -exec grep --color=auto "$1" {} \;
