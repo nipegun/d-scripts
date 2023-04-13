94049be0cbae7668f01c591643a3936f80d2585c
NiPeGun <nipegun@gmail.com>
2023-04-13 23:58:32 +0200

    Update Fail2Ban-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Fail2Ban-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Fail2Ban-InstalarYConfigurar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------------------------------------------------------
-#  Script de NiPeGun para instalar y configurar Fail2Ban en Debian
+# ----------
+# Script de NiPeGun para instalar y configurar Fail2Ban en Debian
 #
-#  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Fail2Ban-InstalarYConfigurar.sh | bash
-#----------------------------------------------------------------------------------------------------------------------------
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Fail2Ban-InstalarYConfigurar.sh | bash
+# ----------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
