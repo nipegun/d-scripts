517d40287b32482694496c7f5d9c36bf0702e799
NiPeGun <nipegun@gmail.com>
2023-04-19 02:00:13 +0200

    Update Proxmox-BackupServer-Cliente-Instalar.sh

--- SoftInst/ParaCLI/Proxmox-BackupServer-Cliente-Instalar.sh
+++ SoftInst/ParaCLI/Proxmox-BackupServer-Cliente-Instalar.sh
@@ -9,8 +9,13 @@
 # Script de NiPeGun para instalar y configurar el cliente de PBS (Proxmox BackUpServer) en Debian
 #
 # Ejecución remota:
-#   curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Proxmox-BackupServer-Cliente-Instalar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Proxmox-BackupServer-Cliente-Instalar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Proxmox-BackupServer-Cliente-Instalar.sh | sed 's-192.168.1.9-192.168.1.10-g' | sed 's-pbsdt-pbsdatastorage-g' | bash
 # ----------
+
+vArch=amd64
+vIPpbs="xxx.xxx.xxx.xxx"
+vDataStorage="pbsdt"
 
 vColorAzul="\033[0;34m"
 vColorAzulClaro="\033[1;34m"
@@ -22,16 +27,6 @@ vFinColor='\033[0m'
   if [ $(id -u) -ne 0 ]; then
     echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
     exit 1
-  fi
-
-# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
-  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
-    echo ""
-    echo -e "${vColorRojo}  curl no está instalado. Iniciando su instalación...${vFinColor}"
-    echo ""
-    apt-get -y update
-    apt-get -y install curl
-    echo ""
   fi
 
 # Determinar la versión de Debian
@@ -100,7 +95,31 @@ elif [ $OS_VERS == "11" ]; then
   echo -e "${vColorAzulClaro}  Iniciando el script de instalación del cliente de Proxmox Backup Server para Debian 11 (Bullseye)...${vFinColor}"
   echo ""
 
+  echo ""
+  echo "    Agregando el repositorio PBS Client..."
+  echo ""
   vArch=amd64
   echo "deb [arch=$vArch] http://download.proxmox.com/debian/pbs-client bullseye main" > /etc/apt/sources.list.d/pbs-client.list
+  # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${vColorRojo}  wget no está instalado. Iniciando su instalación...${vFinColor}"
+      echo ""
+      apt-get -y update
+      apt-get -y install wget
+      echo ""
+    fi
+  wget http://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
+  apt-get -y update
+  echo ""
+  echo "    Instalando el paquete proxmox-backup-client..."
+  echo ""
+  apt-get -y install proxmox-backup-client
+  echo ""
+  echo "    Creando el script para copias de seguridad completas..."
+  echo ""
+  mkdir -p /root/scripts/EsteDebian/ 2> /dev/null
+  echo '#!/bin/bash'                                                                   > /root/scripts/EsteDebian/CopSeg-SistemaDeArchivosCompletoHaciaPBS.sh
+  echo "proxmox-backup-client backup root.pxar:/ --repository $vIPpbs:$vDataStorage " >> /root/scripts/EsteDebian/CopSeg-SistemaDeArchivosCompletoHaciaPBS.sh
 
 fi
