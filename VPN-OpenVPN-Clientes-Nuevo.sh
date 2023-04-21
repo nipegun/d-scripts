d5013599facd35767685fab430f8826d625bf540
NiPeGun <nipegun@gmail.com>
2023-04-21 14:27:46 +0200

    Update and rename VPN-OpenVPN-NuevoCliente.sh to VPN-OpenVPN-Clientes-Nuevo.sh

--- VPN-OpenVPN-Clientes-Nuevo.sh
+++ VPN-OpenVPN-Clientes-Nuevo.sh
@@ -5,9 +5,9 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#---------------------------------------------------------
-#  Script de NiPeGun para crear un nuevo cliente OpenVPN
-#---------------------------------------------------------
+# ----------
+# Script de NiPeGun para crear un nuevo cliente OpenVPN
+# ----------
 
 # TERMINARLO SIGUIENDO LAS INSTRUCCIONES ESTAS:
 # https://www.digitalocean.com/community/tutorials/how-to-set-up-an-openvpn-server-on-debian-8#step-8-—-generate-ce$
