f344d40796150e1b3f03c52131c9762428ed9b75
NiPeGun <nipegun@gmail.com>
2023-04-24 23:18:53 +0200

    Update RetroPie-Instalar.sh

--- SoftInst/ParaGUI/RetroPie-Instalar.sh
+++ SoftInst/ParaGUI/RetroPie-Instalar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------------------------------------------------------------------------------
-#  Script de NiPeGun para instalar y configurar RetroPie en Debian
+# ----------
+# Script de NiPeGun para instalar y configurar RetroPie en Debian
 #
-#  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/RetroPie-Instalar.sh | bash
-#-------------------------------------------------------------------------------------------------------------------
+# Ejecución remota:
+#   curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/RetroPie-Instalar.sh | bash
+# ----------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
