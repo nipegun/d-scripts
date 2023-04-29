40c9fb8c148833e9ff29a86ee121954fdc79ad72
NiPeGun <nipegun@gmail.com>
2023-04-29 05:32:39 +0200

    Update SistemaOperativo-ActualizarALaVersionSiguiente.sh

--- SistemaOperativo-ActualizarALaVersionSiguiente.sh
+++ SistemaOperativo-ActualizarALaVersionSiguiente.sh
@@ -5,9 +5,9 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------------
-#  Script de NiPeGun para actualizar Debian a la versión inmediatamente posterior
-#----------------------------------------------------------------------------------
+# ----------
+# Script de NiPeGun para actualizar Debian a la versión inmediatamente posterior
+# ----------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
