9ffa137a82c2ec455e6e240454f8fb1db4111bc3
NiPeGun <nipegun@gmail.com>
2023-04-29 05:32:06 +0200

    Update SistemaOperativo-ActualizarConRepoCaducado.sh

--- SistemaOperativo-ActualizarConRepoCaducado.sh
+++ SistemaOperativo-ActualizarConRepoCaducado.sh
@@ -5,21 +5,27 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------------------------------------------------------
-#  Script de NiPeGun para actualizar Debian cuando hay un repositorio con firmas caducadas
-#-------------------------------------------------------------------------------------------
+# ----------
+# Script de NiPeGun para actualizar Debian cuando hay un repositorio con firmas caducadas
+# ----------
 
-ColorRojo='\033[1;31m'
-ColorVerde='\033[1;32m'
-FinColor='\033[0m'
+vColorAzul="\033[0;34m"
+vColorAzulClaro="\033[1;34m"
+vColorVerde='\033[1;32m'
+vColorRojo='\033[1;31m'
+vFinColor='\033[0m'
 
 echo ""
-echo -e "${ColorVerde}Reparando permisos de la carpeta /tmp/ ...${FinColor}"
+echo -e "${vColorAzulClaro}  Iniciando el script de actualización del sistema opeartivo con firmas caducadas...${vFinColor}"
+echo ""
+
+echo ""
+echo "    Reparando permisos de la carpeta /tmp/ ..."
 echo ""
 chmod 1777 /tmp
 
 echo ""
-echo -e "${ColorVerde}Actualizando sistema operativo con repo caducado...${FinColor}"
+echo "    Intentando actualización..."
 echo ""
 apt-get -o Acquire::Check-Valid-Until=false update
 apt-get -y --allow-downgrades upgrade
