9c246cbe8313184c89fba9c44f5572d40fb56f73
NiPeGun <nipegun@gmail.com>
2023-05-06 05:09:54 +0200

    Update and rename Hardware-Discos-SSD-SATA-Salud.sh to Hardware-Discos-SATA-SSD-Salud.sh

--- Hardware-Discos-SATA-SSD-Salud.sh
+++ Hardware-Discos-SATA-SSD-Salud.sh
@@ -5,9 +5,9 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para comprobar el estado de salud de los discos duros SSD SATA
-#------------------------------------------------------------------------------------
+# ----------
 
 ColorVerde="\033[1;32m"
 FinColor="\033[0m"
