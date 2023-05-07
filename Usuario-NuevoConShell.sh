744933e97566120023e612c6884a92e7cf9bdb08
NiPeGun <nipegun@gmail.com>
2023-05-07 19:54:44 +0200

    Update Usuario-NuevoConShell.sh

--- Usuario-NuevoConShell.sh
+++ Usuario-NuevoConShell.sh
@@ -5,9 +5,9 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#---------------------------------------------------------------------
-#  Script de NiPeGun para agregar usuarios nuevos con shell a Debian
-#---------------------------------------------------------------------
+# ----------
+# Script de NiPeGun para agregar usuarios nuevos con shell a Debian
+# ----------
 
 ArgumentosEsperados=1
 ArgumentosInsuficientes=65
