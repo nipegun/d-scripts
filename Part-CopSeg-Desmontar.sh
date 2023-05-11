4073744c2be774bbcf9e60a2af1e55ea0e0e2fa0
NiPeGun <nipegun@gmail.com>
2023-05-11 05:59:16 +0200

    Create Part-CopSeg-Desmontar.sh

---
+++ Part-CopSeg-Desmontar.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para desmontar la partición donde se van a ejecutar las copias de seguridad
+#
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/Part-CopSeg-Desmontar.sh | bash
+# ----------
+
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
+
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
+    exit 1
+  fi
+
+echo ""
+echo -e "${vColorAzulClaro}  Desmontando la partición de copias de seguridad...${vFinColor}"
+echo ""
+umount /dev/disk/by-partlabel/PartCopSeg
+
