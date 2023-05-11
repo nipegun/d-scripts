620884342891f90dc25b156580353613399c1bf2
NiPeGun <nipegun@gmail.com>
2023-05-11 06:52:28 +0200

    Create Part-CopSegExt-Montar.sh

---
+++ Part-CopSegExt-Montar.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para montar la partición donde se van a ejecutar las copias de seguridad externas
+#
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/Part-CopSegExt-Montar.sh | bash
+# ----------
+
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
+
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
+    exit 1
+  fi
+
+echo ""
+echo -e "${vColorAzulClaro}  Montando la partición de copias de seguridad externas...${vFinColor}"
+echo ""
+mkdir -p /Particiones/CopSegExt 2> /dev/null
+mount -t auto -v /dev/disk/by-partlabel/PartCopSegExt /Particiones/CopSegExt
+
+echo ""
+echo "    Indicando que el disco que tiene la partición de copias de seguridad externas se apague después de 5 min. sin usar..."
+echo ""
+hdparm -S 60 /dev/disk/by-partlabel/PartCopSegExt
