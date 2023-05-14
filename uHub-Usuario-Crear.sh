93d379c6f69c0b2c8eacde25563b11ee2b290af6
NiPeGun <nipegun@gmail.com>
2023-05-14 07:22:41 +0200

    Update uHub-Usuario-Crear.sh

--- uHub-Usuario-Crear.sh
+++ uHub-Usuario-Crear.sh
@@ -5,9 +5,9 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------
-#  Script de NiPeGun para agregar usuarios a uHub
-#--------------------------------------------------
+# ----------
+# Script de NiPeGun para agregar usuarios a uHub
+# ----------
 
 CantArgsEsperados=2
 ArgsInsuficientes=65
