45de96b4c9e2492c36f275ef4e8fcf9baeb2dccc
NiPeGun <nipegun@gmail.com>
2023-05-14 06:51:38 +0200

    Update Argumentos.sh

--- SoftInst/Argumentos.sh
+++ SoftInst/Argumentos.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-
-vColorAzul="\033[0;34m"
-vColorAzulClaro="\033[1;34m"
-vColorVerde='\033[1;32m'
-vColorRojo='\033[1;31m'
-vFinColor='\033[0m'
+# Definir las variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
 
 vCantArgsCorrectos=2
 vArgsInsuficientes=65
@@ -19,10 +19,10 @@ if [ $# -ne $vCantArgsCorrectos ]
   then
     echo ""
     echo -e "${vColorRojo}  Mal uso del script. El uso correcto sería: ${vFinColor}"
-    echo                 "    script [Argumento1] [Argumento2]"
+    echo "    $0 [Argumento1] [Argumento2]"
     echo ""
-    echo                 "  Ejemplo:"
-    echo                 '    script "/etc/pepe/" "jpg"'
+    echo "  Ejemplo:"
+    echo "    $0 'Hola' 'Mundo'"
     echo ""
     exit $vArgsInsuficientes
   else
