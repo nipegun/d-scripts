3e31803b82f644abcbb5e2057b4066af35b24f06
NiPeGun <nipegun@gmail.com>
2023-05-15 08:20:18 +0200

    Update InstalacionNueva.sh

--- SoftInst/InstalacionNueva.sh
+++ SoftInst/InstalacionNueva.sh
@@ -34,7 +34,7 @@
 # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
   if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
     echo ""
-    echo -e "${vColorRojo}  curl no está instalado. Iniciando su instalación...${vFinColor}"
+    echo -e "${vColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${vFinColor}"
     echo ""
     apt-get -y update
     apt-get -y install curl
