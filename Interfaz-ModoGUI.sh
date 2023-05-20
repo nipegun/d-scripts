be9045008d7296ac4454e8c845bf1d533aa9b0f7
NiPeGun <nipegun@gmail.com>
2023-05-20 05:30:16 +0200

    Update Interfaz-ModoGUI.sh

--- Interfaz-ModoGUI.sh
+++ Interfaz-ModoGUI.sh
@@ -12,20 +12,26 @@
 #  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/Interfaz-ModoGUI.sh | bash
 # ----------
 
-echo ""
-echo "-------------------------"
-echo "  REALIZANDO CAMBIOS..."
-echo "-------------------------"
-echo ""
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
 
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
+    exit 1
+  fi
+
+echo ""
+echo -e "${vColorAzulClaro}  Iniciando script de configuración de inicio en modo GUI para Debian...${vFinColor}"
+echo ""
 systemctl set-default -f graphical.target
 
 echo ""
-echo "-----------------------------------------------------------------------"
-echo "  CAMBIOS REALIZADOS: "
-echo "  EN EL PRÓXIMO INICIO DEBIAN ARRANCARÁ EN MODO GRÁFICO MULTI-USUARIO"
-echo "-----------------------------------------------------------------------"
+echo -e "${vColorVerde}    Cambios realizados. En el próximo inicio Debian iniciará en modo GUI multi-usuario.${vFinColor}"
 echo ""
-
 shutdown -r now
 
