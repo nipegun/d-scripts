58c02e002f8d17ea8e1a7a0c5eba078fc1026873
NiPeGun <nipegun@gmail.com>
2023-05-24 11:42:24 +0200

    Update Servidor-BBDD-InfluxDB-Instalar.sh

--- SoftInst/ParaCLI/Servidor-BBDD-InfluxDB-Instalar.sh
+++ SoftInst/ParaCLI/Servidor-BBDD-InfluxDB-Instalar.sh
@@ -5,89 +5,84 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------------------------------------------------------------------
-#  Script de NiPeGun para instalar el servidor de bases InfluxDB en Debian
+# ----------
+# Script de NiPeGun para instalar el servidor de bases InfluxDB en Debian
 #
 # Ejecución remota
-# curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-BBDD-InfluxDB-Instalar.sh | bash
-#--------------------------------------------------------------------------------------------------------------------------------
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-BBDD-InfluxDB-Instalar.sh | bash
+# ----------
 
-ColorRojo='\033[1;31m'
-ColorVerde='\033[1;32m'
-FinColor='\033[0m'
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
 
-## Determinar la versión de Debian
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
+    exit 1
+  fi
 
-   if [ -f /etc/os-release ]; then
-       # Para systemd y freedesktop.org
-       . /etc/os-release
-       OS_NAME=$NAME
-       OS_VERS=$VERSION_ID
-   elif type lsb_release >/dev/null 2>&1; then
-       # linuxbase.org
-       OS_NAME=$(lsb_release -si)
-       OS_VERS=$(lsb_release -sr)
-   elif [ -f /etc/lsb-release ]; then
-       # Para algunas versiones de Debian sin el comando lsb_release
-       . /etc/lsb-release
-       OS_NAME=$DISTRIB_ID
-       OS_VERS=$DISTRIB_RELEASE
-   elif [ -f /etc/debian_version ]; then
-       # Para versiones viejas de Debian.
-       OS_NAME=Debian
-       OS_VERS=$(cat /etc/debian_version)
-   else
-       # Para el viejo uname (También funciona para BSD)
-       OS_NAME=$(uname -s)
-       OS_VERS=$(uname -r)
-   fi
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org
+    . /etc/os-release
+    OS_NAME=$NAME
+    OS_VERS=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # linuxbase.org
+    OS_NAME=$(lsb_release -si)
+    OS_VERS=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release
+    . /etc/lsb-release
+    OS_NAME=$DISTRIB_ID
+    OS_VERS=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    OS_NAME=Debian
+    OS_VERS=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD)
+    OS_NAME=$(uname -s)
+    OS_VERS=$(uname -r)
+  fi
 
 if [ $OS_VERS == "7" ]; then
 
   echo ""
-  echo "--------------------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación del servidor InfluxDB para Debian 7 (Wheezy)..."
-  echo "--------------------------------------------------------------------------------------"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación del servidor InfluxDB para Debian 7 (Wheezy)...${vFinColor}"
   echo ""
 
   echo ""
-  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo -e "${vColorRojo}    Comandos para Debian 7 todavía no preparados. Prueba ejecutar el script en otra versión de Debian.${vFinColor}"
   echo ""
 
 elif [ $OS_VERS == "8" ]; then
 
   echo ""
-  echo "--------------------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación del servidor InfluxDB para Debian 8 (Jessie)..."
-  echo "--------------------------------------------------------------------------------------"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación del servidor InfluxDB para Debian 8 (Jessie)...${vFinColor}"
   echo ""
 
   echo ""
-  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo -e "${vColorRojo}    Comandos para Debian 8 todavía no preparados. Prueba ejecutar el script en otra versión de Debian.${vFinColor}"
   echo ""
 
 elif [ $OS_VERS == "9" ]; then
 
   echo ""
-  echo "---------------------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación del servidor InfluxDB para Debian 9 (Stretch)..."
-  echo "---------------------------------------------------------------------------------------"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación del servidor InfluxDB para Debian 9 (Stretch)...${vFinColor}"
   echo ""
 
   echo ""
-  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo -e "${vColorRojo}    Comandos para Debian 9 todavía no preparados. Prueba ejecutar el script en otra versión de Debian.${vFinColor}"
   echo ""
 
 elif [ $OS_VERS == "10" ]; then
 
   echo ""
-  echo "---------------------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación del servidor InfluxDB para Debian 10 (Buster)..."
-  echo "---------------------------------------------------------------------------------------"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación del servidor InfluxDB para Debian 10 (Buster)...${vFinColor}"
   echo ""
 
   echo ""
-  echo "  Instalando paquetes..."
+  echo "    Instalando paquetes..."
   echo ""
   apt-get -y update
   apt-get -y install influxdb
@@ -95,27 +90,25 @@ elif [ $OS_VERS == "10" ]; then
   apt-get -y install python3-influxdb
 
   echo ""
-  echo "  Activando el servicio"
+  echo "    Activando el servicio"
   echo ""
   systemctl enable influxdb.service --now
 
 elif [ $OS_VERS == "11" ]; then
 
   echo ""
-  echo "----------------------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación del servidor InfluxDB para Debian 11 (Bullseye)..."
-  echo "----------------------------------------------------------------------------------------"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación del servidor InfluxDB para Debian 11 (Bullseye)...${vFinColor}"
   echo ""
 
   echo ""
-  echo "  Instalando paquetes de InfluxDB..."
+  echo "    Instalando paquetes de InfluxDB..."
   echo ""
   apt-get -y update
   apt-get -y install influxdb
   apt-get -y install influxdb-client
 
   echo ""
-  echo "  Instalando paquetes de Python..."
+  echo "    Instalando paquetes de Python..."
   echo ""
   apt-get -y install python3-dev
   apt-get -y install python3-pip
@@ -123,7 +116,7 @@ elif [ $OS_VERS == "11" ]; then
   pip3 install influxdb
 
   echo ""
-  echo "  Activando el servicio"
+  echo "    Activando el servicio"
   echo ""
   systemctl enable influxdb.service --now
 
