52c4530ea85514df10a78281ba547f99a523118b
NiPeGun <nipegun@gmail.com>
2023-05-25 08:18:31 +0200

    Create Hardware-Procesador-Temperatura-Medir.sh

---
+++ Hardware-Procesador-Temperatura-Medir.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para medir la temperatura del procesador
+#
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/Hardware-Procesador-Temperatura-Medir.sh | bash
+# ----------
+
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
+
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
+    exit 1
+  fi
+
+# Comprobar si el paquete lm-sensors está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s lm-sensors 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${vColorRojo}  El paquete lm-sensors no está instalado. Iniciando su instalación...${vFinColor}"
+    echo ""
+    apt-get -y update
+    apt-get -y install lm-sensors
+    echo ""
+  fi
+
+sensors -u k10temp-pci-00c3 | grep emp1 | cut -d':' -f2 | sed 's- --g'
+
