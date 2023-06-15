d7c8be69bbeba83a3d8e47144adaf07cf052cbf1
NiPeGun <nipegun@gmail.com>
2023-06-15 04:16:45 +0200

    Update VirtualBox-Instalar.sh

--- SoftInst/ParaGUI/VirtualBox-Instalar.sh
+++ SoftInst/ParaGUI/VirtualBox-Instalar.sh
@@ -5,43 +5,45 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#---------------------------------------------------------------------------------------------------------------------
-#  Script de NiPeGun para instalar y configurar VirtualBox en Debian
+# ----------
+# Script de NiPeGun para instalar y configurar VirtualBox en Debian
 #
-#  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/VirtualBox-Instalar.sh | bash
-#---------------------------------------------------------------------------------------------------------------------
+# Ejecución remota:
+#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/VirtualBox-Instalar.sh | bash
+# ----------
 
-ColorRojo='\033[1;31m'
-ColorVerde='\033[1;32m'
-FinColor='\033[0m'
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
 
-## Determinar la versión de Debian
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
+    exit 1
+  fi
 
-   if [ -f /etc/os-release ]; then
-       # Para systemd y freedesktop.org
-       . /etc/os-release
-       OS_NAME=$NAME
-       OS_VERS=$VERSION_ID
-   elif type lsb_release >/dev/null 2>&1; then
-       # linuxbase.org
-       OS_NAME=$(lsb_release -si)
-       OS_VERS=$(lsb_release -sr)
-   elif [ -f /etc/lsb-release ]; then
-       # Para algunas versiones de Debian sin el comando lsb_release
-       . /etc/lsb-release
-       OS_NAME=$DISTRIB_ID
-       OS_VERS=$DISTRIB_RELEASE
-   elif [ -f /etc/debian_version ]; then
-       # Para versiones viejas de Debian.
-       OS_NAME=Debian
-       OS_VERS=$(cat /etc/debian_version)
-   else
-       # Para el viejo uname (También funciona para BSD)
-       OS_NAME=$(uname -s)
-       OS_VERS=$(uname -r)
-   fi
-
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
+    . /etc/os-release
+    OS_NAME=$NAME
+    OS_VERS=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # Para linuxbase.org.
+    OS_NAME=$(lsb_release -si)
+    OS_VERS=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release.
+    . /etc/lsb-release
+    OS_NAME=$DISTRIB_ID
+    OS_VERS=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    OS_NAME=Debian
+    OS_VERS=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD).
+    OS_NAME=$(uname -s)
+    OS_VERS=$(uname -r)
+  fi
 
 if [ $OS_VERS == "7" ]; then
 
@@ -165,6 +167,63 @@ elif [ $OS_VERS == "11" ]; then
       echo ""
       Usuario1000=$(id 1000 | cut -d'(' -f2 | cut -d')' -f1)
       usermod -a -G vboxusers $Usuario1000
- 
+
+elif [ $OS_VERS == "12" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de VirtualBox para Debian 11 (Bullseye)..."
+  echo ""
+
+  # Instalar paquetes necesarios
+    echo ""
+    echo "  Instalando paquetes necesarios..."
+    echo ""
+    apt-get -y update
+    apt-get -y install linux-headers-$(uname -r)
+    apt-get -y install dkms
+
+  # Agregar repositorio
+    echo ""
+    echo "  Agregando repositorio de VirtualBox..."
+    echo ""
+    apt-get -y install gnupg2
+    # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo "  wget no está instalado. Iniciando su instalación..."
+        echo ""
+        apt-get -y update > /dev/null
+        apt-get -y install wget
+        echo ""
+      fi
+    wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
+    wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
+    echo "deb [arch=$(dpkg --print-architecture)] http://download.virtualbox.org/virtualbox/debian bullseye contrib" > /etc/apt/sources.list.d/virtualbox.list
+    apt-get -y update
+
+  # Instalar virtualbox
+    echo ""
+    echo "  Instalando el paquete virtualbox..."
+    echo ""
+    PaqueteAInstalar=$(apt-cache search virtualbox | grep "virtualbox-" | tail -n1 | cut -d' ' -f1)
+    apt-get -y install $PaqueteAInstalar
+
+  # Instalar el pack de extensiones
+    echo ""
+    echo "  Instalando el pack de extensiones..."
+    echo ""
+    mkdir -p /root/SoftInst/VirtualBox/
+    cd /root/SoftInst/VirtualBox/
+    VersDeVBoxInstalada=$(virtualbox -h | grep elector | cut -d'v' -f2)
+    wget https://download.virtualbox.org/virtualbox/$VersDeVBoxInstalada/Oracle_VM_VirtualBox_Extension_Pack-$VersDeVBoxInstalada.vbox-extpack
+    vboxmanage extpack install --replace /root/SoftInst/VirtualBox/Oracle_VM_VirtualBox_Extension_Pack-$VersDeVBoxInstalada.vbox-extpack
+
+   # Agregar el usuario 1000 al grupo virtualbox
+     echo ""
+     echo "  Agregando el usuario 1000 en el grupo virtualbox..."
+     echo ""
+     Usuario1000=$(id 1000 | cut -d'(' -f2 | cut -d')' -f1)
+     usermod -a -G vboxusers $Usuario1000
+
 fi
 
