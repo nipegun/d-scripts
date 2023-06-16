afaff291319f37b78a9f08ea2650d6841bc14978
NiPeGun <nipegun@gmail.com>
2023-06-16 03:54:09 +0200

    Update Sensores-InstalarYDetectar.sh

--- PostInst/CLI/Sensores-InstalarYDetectar.sh
+++ PostInst/CLI/Sensores-InstalarYDetectar.sh
@@ -134,7 +134,7 @@ elif [ $OS_VERS == "11" ]; then
 elif [ $OS_VERS == "12" ]; then
 
   echo ""
-  echo -e "${vColorAzulClaro}  Iniciando el script ... Debian 12 (Bookworm)...${vFinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación y configuración de sensores de hardware en Debian 12 (Bookworm)...${vFinColor}"
   echo ""
 
   echo ""
