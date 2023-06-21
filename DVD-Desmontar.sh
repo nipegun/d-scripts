ea68acdd1cd0e74e43f8041f1bec1b5045898fdd
NiPeGun <nipegun@gmail.com>
2023-06-21 04:30:09 +0200

    Create DVD-Desmontar.sh

---
+++ DVD-Desmontar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para desmontar la unidad de DVD
+#
+# Ejecución remota:
+#   curl -sL x | bash
+# ----------
+
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
+
+echo ""
+umount -v /dev/sr0
+echo ""
