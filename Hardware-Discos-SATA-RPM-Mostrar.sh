4666ec20bc6fcfc264cefc13e97b6de0268dd510
NiPeGun <nipegun@gmail.com>
2023-06-21 04:34:07 +0200

    Create Hardware-Discos-SATA-RPM-Mostrar.sh

---
+++ Hardware-Discos-SATA-RPM-Mostrar.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para mostrar la temperatura de los discos duros SATA
+#
+# Ejecución remota:
+#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/Hardware-Discos-SATA-RPM-Mostrar.sh | bash
+# ----------
+
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
+
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
+    exit 1
+  fi
+
+# Comprobar si el paquete smartmontools está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s smartmontools 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${vColorRojo}  El paquete smartmontools no está instalado. Iniciando su instalación...${vFinColor}"
+    echo ""
+    apt-get -y update
+    apt-get -y install smartmontools
+    echo ""
+  fi
+
+echo ""
+echo "  DISCO sda"
+echo ""
+smartctl -i /dev/sda
+
+echo ""
+echo "  DISCO sdb"
+echo ""
+smartctl -i /dev/sdb
+
+echo ""
+echo "  DISCO sdc"
+echo ""
+smartctl -i /dev/sdc
+
+echo ""
+echo "  DISCO sdd"
+echo ""
+smartctl -i /dev/sdd
+
+echo ""
+echo "  DISCO sde"
+echo ""
+smartctl -i /dev/sde
+
+echo ""
+echo "  DISCO sdf"
+echo ""
+smartctl -i /dev/sdf
+
+echo ""
+echo "  DISCO sdg"
+echo ""
+smartctl -i /dev/sdg
+
