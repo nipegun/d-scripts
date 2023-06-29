ec38b8e0a65edf8c568a6cdc6290e7c0c4034af5
NiPeGun <nipegun@gmail.com>
2023-06-29 04:21:21 +0200

    Update LogsDelSistema-Mostrar.sh

--- LogsDelSistema-Mostrar.sh
+++ LogsDelSistema-Mostrar.sh
@@ -5,9 +5,19 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-------------------------------------------------------
-#  Script de NiPeGun para mostrar los logs del sistema
-#-------------------------------------------------------
+# ----------
+# Script de NiPeGun para nostrar los logs del sistema
+#
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/Logs-VarLog-PonerACero.sh | bash
+# ----------
+
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
 
 if [ $# -eq 0 ]
   then
