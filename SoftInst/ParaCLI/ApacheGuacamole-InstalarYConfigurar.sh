20a895465af40888784230db28541942f1527e3f
NiPeGun <nipegun@gmail.com>
2023-07-07 16:39:55 +0200

    Update ApacheGuacamole-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/ApacheGuacamole-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/ApacheGuacamole-InstalarYConfigurar.sh
@@ -161,7 +161,68 @@ elif [ $OS_VERS == "11" ]; then
       ./configure --with-init-dir=/etc/init.d --enable-allow-freerdp-snapshots
       make
       make install
+      ldconfig
+      systemctl daemon-reload
+      systemctl start guacd
+      systemctl enable guacd
+      mkdir -p /etc/guacamole/{extensions,lib}
+    # Apache Tomcat
+      apt install tomcat9 tomcat9-admin tomcat9-common tomcat9-user
+      wget https://downloads.apache.org/guacamole/1.3.0/binary/guacamole-1.3.0.war
+      mv guacamole-1.3.0.war /var/lib/tomcat9/webapps/guacamole.war
+      systemctl restart tomcat9 guacd
+    # Auntnticación de bases de datos
+      apt install mariadb-server
+      mysql_secure_installation
+      wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.26.tar.gz
+      tar -xf mysql-connector-java-8.0.26.tar.gz
+      cp mysql-connector-java-8.0.26/mysql-connector-java-8.0.26.jar /etc/guacamole/lib/
+      wget https://downloads.apache.org/guacamole/1.3.0/binary/guacamole-auth-jdbc-1.3.0.tar.gz
+      tar -xf guacamole-auth-jdbc-1.3.0.tar.gz
+      mv guacamole-auth-jdbc-1.3.0/mysql/guacamole-auth-jdbc-mysql-1.3.0.jar /etc/guacamole/extensions/
+      mysql -u root -p
+        ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
+        CREATE DATABASE guacamole_db;
+        CREATE USER 'guacamole_user'@'localhost' IDENTIFIED BY 'password';
+        GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'localhost';
+        FLUSH PRIVILEGES;
+        cd guacamole-auth-jdbc-1.3.0/mysql/schema
+        cat *.sql | mysql -u root -p guacamole_db
+        nano /etc/guacamole/guacamole.properties
+        #Paste in the following configuration settings, replacing [password] with the password of the new guacamole_user that you created for the database.
+        # MySQL properties
+        #mysql-hostname: 127.0.0.1
+        #mysql-port: 3306
+        #mysql-database: guacamole_db
+        #mysql-username: guacamole_user
+        #mysql-password: [password]
+        systemctl restart tomcat9 guacd mysql
+        #Navigate to the URL: [ip]:8080/guacamole
+        Enter guacadmin as the username and guacadmin as the password. Then click Login.
+        Create a New Admin User
+        Before continuing with configuring Guacamole, it’s recommended that you create a new admin account and delete the original.
+        Click the guacadmin user dropdown menu on the top right and select Settings.
+        Navigate to the Users tab and click the New User button.
+        Under the Edit User section, enter your preferred username and a secure password.
+        Under the Permissions section, check all the permissions.
+        Click Save to create the new user.
+        Log out of the current user and log in as the newly created user.
+        Click your username on the top left and select Settings from the dropdown menu.
+        Navigate to the Users tab and click the guacadmin user.
+        At the bottom of the Edit User screen, click Delete to remove the default user.
+        Create an SSH Connection
+        To test Guacamole, let’s create an new connection in Guacamole that opens up an SSH connection to the server.
+        After logging in to Guacamole, click your username on the top left and select Settings from the dropdown menu.
+        Navigate to the Connections tab and click New Connection.
+        Under Edit Connection, enter a name for your new connection (such as “Guacamole SSH”) and select SSH as the Protocol.
 
+        Under Parameters, enter your IP address as the Hostname, 22 as the Port, your username as the Username and your user’s password as the Password. Other parameters as available if you wish to edit the connection further.
+        Click Save to create the new connection.
+        Navigate back to your user’s home screen by clicking your username on the top left and select Home from the dropdown menu.
+        Click on the new connection under the All Connections list.
+
+        Más info: https://www.linode.com/docs/guides/installing-apache-guacamole-on-ubuntu-and-debian/
+        
 elif [ $OS_VERS == "12" ]; then
 
   echo ""
