725394c3bd13c64db2ad9a7cb65cb75a6cf3204c
NiPeGun <nipegun@gmail.com>
2023-07-14 17:26:01 +0200

    Create CrowdSec-Hubs-Listar.sh

---
+++ CrowdSec-Hubs-Listar.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para mostrar Hubs de CrowdSec en Debian
+#
+# Ejecución remota:
+#   curl -sL x | bash
+# ----------
+
+# Definir variables de color
+  vColorAzul="\033[0;34m"
+  vColorAzulClaro="\033[1;34m"
+  vColorVerde='\033[1;32m'
+  vColorRojo='\033[1;31m'
+  vFinColor='\033[0m'
+
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
+    exit 1
+  fi
+
+# Listar hubs
+  echo ""
+  echo "  Listando hubs de CrowdSed activados en el sistema..."
+  echo ""
+  cscli hub list
+
