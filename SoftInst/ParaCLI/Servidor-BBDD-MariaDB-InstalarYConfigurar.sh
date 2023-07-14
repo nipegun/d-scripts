40ccc427da379edc30d6cdbaeab13de3e777ef2c
NiPeGun <nipegun@gmail.com>
2023-07-14 23:28:40 +0200

    Update Servidor-BBDD-MariaDB-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Servidor-BBDD-MariaDB-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Servidor-BBDD-MariaDB-InstalarYConfigurar.sh
@@ -6,16 +6,16 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-#  Script de NiPeGun para instalar y configurar MariaDB en Debian
+# Script de NiPeGun para instalar y configurar MariaDB en Debian
 #
-#  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-BBDD-MariaDB-InstalarYConfigurar.sh | bash
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-BBDD-MariaDB-InstalarYConfigurar.sh | bash
 #
-#  Ejecución remota sin caché:
-#  curl -s -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-BBDD-MariaDB-InstalarYConfigurar.sh | bash
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-BBDD-MariaDB-InstalarYConfigurar.sh | bash
 #
-#  Ejecución remota con parámetros:
-#  curl -s https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-BBDD-MariaDB-InstalarYConfigurar.sh | bash -s Parámetro1 Parámetro2
+# Ejecución remota con parámetros:
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-BBDD-MariaDB-InstalarYConfigurar.sh | bash -s Parámetro1 Parámetro2
 # ----------
 
 # Definir las variables de color
@@ -24,20 +24,14 @@
   vColorVerde='\033[1;32m'
   vColorRojo='\033[1;31m'
   vFinColor='\033[0m'
+ 
+# Registrar la fecha de ejecución del script
+  vFecha=$(date +a%Ym%md%d@%T)
 
 # Comprobar si el script está corriendo como root
   if [ $(id -u) -ne 0 ]; then
     echo -e "${vColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${vFinColor}" >&2
     exit 1
-  fi
-
-# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
-  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
-    echo ""
-    echo -e "${vColorRojo}  curl no está instalado. Iniciando su instalación...${vFinColor}"
-    echo ""
-    apt-get -y update && apt-get -y install curl
-    echo ""
   fi
 
 # Determinar la versión de Debian
@@ -63,41 +57,41 @@
 if [ $OS_VERS == "7" ]; then
 
   echo ""
-  echo -e "${vColorAzulClaro}Iniciando el script de instalación de MariaDB para Debian 7 (Wheezy)...${vFinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de MariaDB para Debian 7 (Wheezy)...${vFinColor}"
   echo ""
 
   echo ""
-  echo -e "${vColorRojo}  Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
+  echo -e "${vColorRojo}    Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
   echo ""
 
 elif [ $OS_VERS == "8" ]; then
 
   echo ""
-  echo -e "${vColorAzulClaro}Iniciando el script de instalación de MariaDB para Debian 8 (Jessie)...${vFinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de MariaDB para Debian 8 (Jessie)...${vFinColor}"
   echo ""
 
   echo ""
-  echo -e "${vColorRojo}  Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
+  echo -e "${vColorRojo}    Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
   echo ""
 
 elif [ $OS_VERS == "9" ]; then
 
   echo ""
-  echo -e "${vColorAzulClaro}Iniciando el script de instalación de MariaDB para Debian 9 (Stretch)...${vFinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de MariaDB para Debian 9 (Stretch)...${vFinColor}"
   echo ""
 
   echo ""
-  echo -e "${vColorRojo}  Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
+  echo -e "${vColorRojo}    Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
   echo ""
 
 elif [ $OS_VERS == "10" ]; then
 
   echo ""
-  echo -e "${vColorAzulClaro}Iniciando el script de instalación de MariaDB para Debian 10 (Buster)...${vFinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de MariaDB para Debian 10 (Buster)...${vFinColor}"
   echo ""
 
   echo ""
-  echo -e "${vColorRojo}  Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
+  echo -e "${vColorRojo}    Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${vFinColor}"
   echo ""
 
 elif [ $OS_VERS == "11" ]; then
@@ -182,6 +176,117 @@ elif [ $OS_VERS == "11" ]; then
               systemctl restart mariadb
             else
               vFecha=$(date +a%Ym%md%d@%T)
+              cp /etc/mysql/my.cnf /etc/mysql/my.cnf.bak-$vFecha
+              sed -i -e 's|bind-address = 0.0.0.0||g'                       /etc/mysql/my.cnf
+              sed -i -e 's|\[mysqld]|\[mysqld]\nbind-address = $vIPExtra|g' /etc/mysql/my.cnf
+              systemctl restart mariadb
+            fi
+
+        ;;
+
+        4)
+
+          # Instalar phpmydmin
+            echo ""
+            echo "  Instalando phpmyadmin..."
+            echo ""
+            apt-get -y install phpmyadmin
+
+        ;;
+
+        4)
+
+          # Securizar la instalación
+            echo ""
+            echo "  Securizando instalación con script oficial..."
+            echo ""
+            mysql_secure_installation
+
+        ;;
+
+      esac
+
+  done
+
+
+elif [ $OS_VERS == "12" ]; then
+
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de MariaDB para Debian 12 (Bookworm)...${vFinColor}"
+  echo ""
+
+  # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${vColorRojo}    El paquete dialog no está instalado. Iniciando su instalación...${vFinColor}"
+      echo ""
+      apt-get -y update
+      apt-get -y install dialog
+      echo ""
+    fi
+
+  menu=(dialog --checklist "Marca las opciones que quieras instalar:" 22 96 16)
+    opciones=(
+      1 "Instalar paquete mariadb-server." on
+      2 "Permitir conexiones desde todas las IPs (no sólo localhost)." off
+      3 "Permitir conexión SÓLO desde una IP específica (sin localhost)." off
+      4 "Instalar phpmyadmin." off
+      5 "Securizar instalación con script oficial." off
+    )
+  choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
+
+  for choice in $choices
+    do
+      case $choice in
+
+        1)
+
+          # Instalar el paquete mmariadb-server
+            echo ""
+            echo "  Instalando el paquete mariadb-server..."
+            echo ""
+            apt-get -y update
+            apt-get -y install mariadb-server
+
+        ;;
+
+        2)
+
+          # Permitir la conexión desde todas las IPs
+            echo ""
+            echo "  Permitiendo conexión desde todas las IPs (no sólo desde localhost)..."
+            echo ""
+            vExisteSec=$(cat /etc/mysql/my.cnf | grep ^'\[mysqld]')
+            if [[ $vExisteSec == "" ]]; then
+              cp /etc/mysql/my.cnf /etc/mysql/my.cnf.bak-$vFecha
+              echo "[mysqld]"               >> /etc/mysql/my.cnf
+              echo "bind-address = 0.0.0.0" >> /etc/mysql/my.cnf
+              systemctl restart mariadb
+            else
+              cp /etc/mysql/my.cnf /etc/mysql/my.cnf.bak-$vFecha
+              sed -i -e 's|\[mysqld]|\[mysqld]\nbind-address = 0.0.0.0|g' /etc/mysql/my.cnf
+              systemctl restart mariadb
+            fi
+
+        ;;
+
+        3)
+
+          # Permitir conexiones sólo desde una IP específica
+            echo ""
+            echo "  Permitiendo conexión SÓLO desde una IP específica (sin localhost)..."
+            echo ""
+            echo "    Introduce la IP desde la que se deberían escuchar las conexiones y presiona Enter:"
+            echo ""
+            read vIPExtra < /dev/tty
+            echo ""
+            vExisteSec=$(cat /etc/mysql/my.cnf | grep ^'\[mysqld]')
+            if [[ $vExisteSec == "" ]]; then
+              cp /etc/mysql/my.cnf /etc/mysql/my.cnf.bak-$vFecha
+              echo "[mysqld]"                 >> /etc/mysql/my.cnf
+              echo "bind-address = $vIPExtra" >> /etc/mysql/my.cnf
+              systemctl restart mariadb
+            else
               cp /etc/mysql/my.cnf /etc/mysql/my.cnf.bak-$vFecha
               sed -i -e 's|bind-address = 0.0.0.0||g'                       /etc/mysql/my.cnf
               sed -i -e 's|\[mysqld]|\[mysqld]\nbind-address = $vIPExtra|g' /etc/mysql/my.cnf
