8be0eadaec841c3f8f93e04f535b00145a54f7db
NiPeGun <nipegun@gmail.com>
2023-07-19 02:17:38 +0200

    Update CopSegInt-CrearScript.sh

--- PostInst/CLI/CopSegInt-CrearScript.sh
+++ PostInst/CLI/CopSegInt-CrearScript.sh
@@ -9,7 +9,7 @@
 # Script de NiPeGun para preparar Debian sin entorno gráfico a gusto de NiPeGun
 #
 # Ejecución remota:
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/0-PrepararDebianCLI.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/CopSegInt-CrearScript.sh | bash
 # ----------
 
 # Definir constantes de color
