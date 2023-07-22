926dbd2d1ae274a19cd00173f8b509b889a04f91
NiPeGun <nipegun@gmail.com>
2023-07-23 00:19:50 +0200

    Update Proxmox-VirtualizationEnvironment-Instalar.sh

--- SoftInst/ParaCLI/Proxmox-VirtualizationEnvironment-Instalar.sh
+++ SoftInst/ParaCLI/Proxmox-VirtualizationEnvironment-Instalar.sh
@@ -12,9 +12,23 @@
 #  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Proxmox-VirtualizationEnvironment-Instalar.sh | bash
 ---------
 
-cColorRojo='\033[1;31m'
-cColorVerde='\033[1;32m'
-cFinColor='\033[0m'
+# Definir fecha de ejecución del script
+  cFechaDeEjec=$(date +a%Ym%md%d@%T)
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${cFinColor}"
+    exit
+  fi
 
 # Determinar la versión de Debian
   if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
@@ -75,14 +89,14 @@ elif [ $cVerSO == "10" ]; then
   apt-get -y update
   apt-get -y install wget
   echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/ProxmoxVE.list
-  wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmomx-ve-release-6.x.gpg
+  wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmomx-ve-release-buster.gpg
   apt-get -y update
   echo "127.0.0.1 $HOSTNAME" > /etc/hosts
   apt-get -y install proxmox-ve
   rm -rf /etc/apt/sources.list.d/pve-enterprise.list
   apt-get -y update
   apt-get -y install pve-headers
-  cp /etc/network/interfaces /etc/network/interfaces.bak
+  cp /etc/network/interfaces /etc/network/interfaces.bak.$cFechaDeEjec
 
   # Desinstalar
   # touch /please-remove-proxmox-ve
@@ -94,9 +108,39 @@ elif [ $cVerSO == "11" ]; then
   echo "  Iniciando el script de instalación de ProxmoxVE para Debian 11 (Bullseye)..."  
   echo ""
 
+  apt-get -y update
+  apt-get -y install wget
+  echo "deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/ProxmoxVE.list
+  wget http://download.proxmox.com/debian/proxmox-ve-release-buulseye.gpg -O /etc/apt/trusted.gpg.d/proxmomx-ve-release-bullseye.gpg
+  apt-get -y update
+  echo "127.0.0.1 $HOSTNAME" > /etc/hosts
+  apt-get -y install proxmox-ve
+  rm -rf /etc/apt/sources.list.d/pve-enterprise.list
+  apt-get -y update
+  apt-get -y install pve-headers
+  cp /etc/network/interfaces /etc/network/interfaces.bak.$cFechaDeEjec
+
+elif [ $cVerSO == "12" ]; then
+
   echo ""
-  echo "  Comandos para Debian 11 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo "  Iniciando el script de instalación de ProxmoxVE para Debian 12 (Bookworm)..."  
   echo ""
+
+  apt-get -y update
+  apt-get -y install wget
+  echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/ProxmoxVE.list
+  wget http://download.proxmox.com/debian/proxmox-ve-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmomx-ve-release-bookworm.gpg
+  apt-get -y update
+  echo "127.0.0.1 $HOSTNAME" > /etc/hosts
+  apt-get -y install proxmox-ve
+  rm -rf /etc/apt/sources.list.d/pve-enterprise.list
+  apt-get -y update
+  apt-get -y install pve-headers
+  cp /etc/network/interfaces /etc/network/interfaces.bak.$cFechaDeEjec
+
+  # Desinstalar
+  # touch /please-remove-proxmox-ve
+  # apt-get -y purge proxmox-ve
 
 fi
 
