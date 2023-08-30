3e57312dd931e9f88766ed14f659817ce9eae680
NiPeGun <nipegun@gmail.com>
2023-08-30 18:48:44 +0200

    Update Cryptos-Cartera-XCH-InstalarOActualizar1.sh

--- SoftInst/ParaGUI/Cryptos-Cartera-XCH-InstalarOActualizar1.sh
+++ SoftInst/ParaGUI/Cryptos-Cartera-XCH-InstalarOActualizar1.sh
@@ -27,40 +27,40 @@ echo ""
 
 # Obtener el número de la última versión estable desde github
   echo ""
-  echo "  Obteniendo el número de la última versión estable desde github..." 
-echo ""
+  echo "    Obteniendo el número de la última versión estable desde github..." 
+  echo ""
   vUltVersEstable=$(curl -sL https://github.com/Chia-Network/chia-blockchain/releases/latest | sed 's->-\n-g' | grep "releases/tag/" | sed 's-releases/tag/-\nversion"-g' | grep version | grep -v name | head -n1 | cut -d '"' -f2)
   echo ""
-  echo "  La última versión estable es la: $vUltVersEstable."
+  echo "      La última versión estable es la: $vUltVersEstable."
   echo ""
 
 # Obtener enlace de descarga de la última versión estable
   echo ""
-  echo "  Obteniendo enlace de descarga de la versión $vUltVersEstable..." 
+  echo "    Obteniendo enlace de descarga de la versión $vUltVersEstable..." 
 echo ""
   vURLDelArchivoDeb=$(curl -sL https://github.com/Chia-Network/chia-blockchain/releases | sed 's->-\n-g' | sed 's-href-\nhref-g' | grep $vUltVersEstable | grep href | grep .deb | grep amd64 | grep -v orrent | grep -v cli | cut -d'"' -f2)
   if [[ "$vURLDelArchivoDeb" == "" ]]; then
     vURLDelArchivoDeb="Chia-Network/chia-blockchain/releases/download/$vUltVersEstable/chia-blockchain_"$vUltVersEstable"_amd64.deb"
   fi
   echo ""
-  echo "  La URL de descarga es: https://github.com/$vURLDelArchivoDeb."
+  echo "    La URL de descarga es: https://github.com/$vURLDelArchivoDeb."
   echo ""
 
 # Crear carpeta de descarga
   echo ""
-  echo "  Creando carpeta de descarga..." 
-echo ""
+  echo "   Creando carpeta de descarga..." 
+  echo ""
   rm -rf /root/SoftInst/Cryptos/XCH/ 2> /dev/null
   mkdir -p /root/SoftInst/Cryptos/XCH/ 2> /dev/null
 
 # Descargar y descomprimir todos los archivos
   echo ""
-  echo "  Descargando el paquete .deb..." 
-echo ""
+  echo "    Descargando el paquete .deb..." 
+  echo ""
   # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
     if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
       echo ""
-      echo "  wget no está instalado. Iniciando su instalación..."
+      echo "     El paquete wget no está instalado. Iniciando su instalación..."
       echo ""
       apt-get -y update && apt-get -y install wget
       echo ""
@@ -70,12 +70,12 @@ echo ""
   wget https://github.com/$vURLDelArchivoDeb -O /root/SoftInst/Cryptos/XCH/chia.deb
 
   echo ""
-  echo "  Extrayendo los archivos de dentro del paquete .deb..." 
-echo ""
+  echo "    Extrayendo los archivos de dentro del paquete .deb..." 
+  echo ""
   # Comprobar si el paquete binutils está instalado. Si no lo está, instalarlo.
     if [[ $(dpkg-query -s binutils 2>/dev/null | grep installed) == "" ]]; then
       echo ""
-      echo "  binutils no está instalado. Iniciando su instalación..."
+      echo "      El paquete binutils no está instalado. Iniciando su instalación..."
       echo ""
       apt-get -y update > /dev/null
       apt-get -y install binutils
@@ -84,12 +84,12 @@ echo ""
   ar xv /root/SoftInst/Cryptos/XCH/chia.deb
 
   echo ""
-  echo "  Descomprimiendo el archivo data.tar.xz..." 
-echo ""
+  echo "    Descomprimiendo el archivo data.tar.xz..." 
+  echo ""
   # Comprobar si el paquete tar está instalado. Si no lo está, instalarlo.
     if [[ $(dpkg-query -s tar 2>/dev/null | grep installed) == "" ]]; then
       echo ""
-      echo "  tar no está instalado. Iniciando su instalación..."
+      echo "      El paquete tar no está instalado. Iniciando su instalación..."
       echo ""
       apt-get -y update && apt-get -y install tar
       echo ""
@@ -99,8 +99,8 @@ echo ""
 
 # Instalar dependencias necesarias
   echo ""
-  echo "  Instalando dependencias necesarias..." 
-echo ""
+  echo "    Instalando dependencias necesarias..." 
+  echo ""
   apt-get -y install libgtk-3-0
   apt-get -y install libnotify4
   apt-get -y install libnss3
@@ -123,8 +123,8 @@ echo ""
 
 # Crear la carpeta para el usuario no root
   echo ""
-  echo "  Creando la carpeta para el usuario no root..." 
-echo ""
+  echo "    Creando la carpeta para el usuario no root..." 
+  echo ""
   mkdir -p /home/$vUsuarioNoRoot/Cryptos/XCH/ 2> /dev/null
   rm -rf /home/$vUsuarioNoRoot/Cryptos/XCH/chia-blockchain/ 2> /dev/null
   mv /root/SoftInst/Cryptos/XCH/opt/chia/ /home/$vUsuarioNoRoot/Cryptos/XCH/chia-blockchain/
@@ -137,16 +137,16 @@ echo ""
 
 # Borrar archivos sobrantes
   echo ""
-  echo "  Borrando archivos sobrantes..." 
-echo ""
+  echo "    Borrando archivos sobrantes..." 
+  echo ""
   rm -rf /root/SoftInst/Cryptos/XCH/debian-binary
   rm -rf /root/SoftInst/Cryptos/XCH/control.tar.xz
   rm -rf /root/SoftInst/Cryptos/XCH/data.tar.xz
 
 # Agregar aplicación al menú
   echo ""
-  echo "  Agregando la aplicación gráfica al menú..." 
-echo ""
+  echo "    Agregando la aplicación gráfica al menú..." 
+  echo ""
   mkdir -p /home/$vUsuarioNoRoot/.local/share/applications/ 2> /dev/null
   echo "[Desktop Entry]"                                                            > /home/$vUsuarioNoRoot/.local/share/applications/xch-gui.desktop
   echo "Name=xch GUI"                                                              >> /home/$vUsuarioNoRoot/.local/share/applications/xch-gui.desktop
@@ -161,8 +161,8 @@ echo ""
 
 # Crear el archivo de auto-ejecución
   echo ""
-  echo "  Creando el archivo de autoejecución de chia-blockchain para el escritorio..." 
-echo ""
+  echo "    Creando el archivo de autoejecución de chia-blockchain para el escritorio..." 
+  echo ""
   mkdir -p /home/$vUsuarioNoRoot/.config/autostart/ 2> /dev/null
   echo "[Desktop Entry]"                                                            > /home/$vUsuarioNoRoot/.config/autostart/xch-gui.desktop
   echo "Name=xch GUI"                                                              >> /home/$vUsuarioNoRoot/.config/autostart/xch-gui.desktop
@@ -177,8 +177,8 @@ echo ""
 
 # Instalar los c-scripts
   echo ""
-  echo "  Instalando los c-scripts..." 
-echo ""
+  echo "    Instalando los c-scripts..." 
+  echo ""
   su $vUsuarioNoRoot -c "curl --silent https://raw.githubusercontent.com/nipegun/c-scripts/main/CScripts-Instalar.sh | bash"
   find /home/$vUsuarioNoRoot/scripts/c-scripts/ -type f -iname "*.sh" -exec chmod +x {} \;
 
@@ -189,8 +189,8 @@ echo ""
 
 # Reparar permisos
   echo ""
-  echo "  Reparando permisos..." 
-echo ""
+  echo "    Reparando permisos..." 
+  echo ""
   chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/Cryptos/
   chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/Cryptos/XCH/ -R
   find /home/$vUsuarioNoRoot/Cryptos/XCH/ -type d -exec chmod 750 {} \;
