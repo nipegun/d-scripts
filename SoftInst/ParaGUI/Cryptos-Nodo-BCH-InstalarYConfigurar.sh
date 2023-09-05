286feaf07d1312f13b9f378e08c34c8ca3fbf448
NiPeGun <nipegun@gmail.com>
2023-09-05 07:52:00 +0200

    Update Cryptos-Nodo-BCH-InstalarYConfigurar.sh

--- SoftInst/ParaGUI/Cryptos-Nodo-BCH-InstalarYConfigurar.sh
+++ SoftInst/ParaGUI/Cryptos-Nodo-BCH-InstalarYConfigurar.sh
@@ -37,9 +37,8 @@ echo ""
 
 menu=(dialog --checklist "Marca las opciones que quieras instalar:" 22 96 16)
   opciones=(
-    1 "Instalar BitcoinCashNode en modo CLI desde la web oficial." on
-    2 "Instalar BitcoinCashNode en modo CLI desde la web de GitHub." off
-    3 "Agregar la configuración para modo GUI." off
+    1 "Instalar BitcoinCashNode en modo CLI desde la web de GitHub." on
+    2 "Agregar las opciones para poder lanzar la app de modo gráfico." on
   )
 choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
 
@@ -48,126 +47,6 @@ choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
       case $choice in
 
         1)
-
-          echo ""
-          echo "  Instalando BitcoinCashNode para modo CLI desde la web oficial..."
-          echo ""
-
-          echo ""
-          echo "    Determinando la última versión del nodo BitcoinCashNode disponible en la web oficial..."
-          echo ""
-          # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
-            if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
-              echo ""
-              echo -e "${cColorAzulClaro}      El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
-              echo "  "
-              echo ""
-              apt-get -y update
-              apt-get -y install curl
-              echo ""
-            fi
-          $vUltVersBitcoinCashNode=$(curl -sL https://bitcoin.org/wallet/ | sed 's->->\n-g' | sed 's-"-\n-g' | grep tar.gz | sed 's|.*raven-||' | cut -d'-' -f1)
-          echo ""
-          echo "      La última versión de bitcoin cash disponible en la web oficial es la $vUltVersBitcoinCashNode"
-          echo ""
-
-          echo ""
-          echo "    Determinando la URL del archivo a descargar..."          echo ""
-          vURLArchivo=$(curl -sL https://bitcoin.org/wallet/ | sed 's->->\n-g' | sed 's-"-\n-g' | grep tar.gz)
-          echo ""
-          echo "      La URL del archivo es: $vURLArchivo"
-          echo ""
-
-          echo ""
-          echo "    Intentando descargar el archivo..."          echo ""
-          mkdir -p /root/SoftInst/Cryptos/BCH/ 2> /dev/null
-          rm -rf /root/SoftInst/Cryptos/BCH/*
-          cd /root/SoftInst/Cryptos/BCH/
-          # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
-            if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
-              echo ""
-              echo -e "${cColorAzulClaro}      El paquete wget no está instalado. Iniciando su instalación...${cFinColor}"
-              echo ""
-              apt-get -y update
-              apt-get -y install wget
-              echo ""
-            fi
-          wget $vURLArchivo -O /root/SoftInst/Cryptos/BCH/raven$vUltVersBitcoinCashNode.tar.gz
-
-          echo ""
-          echo "    Descomprimiendo el archivo..."          echo ""
-          # Comprobar si el paquete tar está instalado. Si no lo está, instalarlo.
-            if [[ $(dpkg-query -s tar 2>/dev/null | grep installed) == "" ]]; then
-              echo ""
-              echo -e "${cColorAzulClaro}      El paquete tar no está instalado. Iniciando su instalación...${cFinColor}"
-              echo ""
-              apt-get -y update
-              apt-get -y install tar
-              echo ""
-            fi
-          tar -xf /root/SoftInst/Cryptos/BCH/raven$vUltVersBitcoinCashNode.tar.gz
-          rm -f /root/SoftInst/Cryptos/BCH/raven$vUltVersBitcoinCashNode.tar.gz
-          find /root/SoftInst/Cryptos/BCH/ -type d -name "raven*" -exec mv {} /root/SoftInst/Cryptos/BCH/"raven-$vUltVersBitcoinCashNode"/ \; 2> /dev/null
-
-          echo ""
-          echo "    Creando carpetas y archivos necesarios para ese usuario..."          echo ""
-          mkdir -p /home/$vUsuarioNoRoot/.raven/
-          touch    /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "rpcuser=bchrpc"           > /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "rpcpassword=bchrpcpass"  >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "rpcallowip=127.0.0.1"    >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "#Default RPC port 8766"  >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "rpcport=20401"           >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "server=1"                >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "listen=1"                >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "prune=550"               >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "daemon=1"                >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "gen=0"                   >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          rm -rf   /home/$vUsuarioNoRoot/Cryptos/BCH/
-          mkdir -p /home/$vUsuarioNoRoot/Cryptos/BCH/ 2> /dev/null
-          mv /root/SoftInst/Cryptos/BCH/raven-$vUltVersBitcoinCashNode/* /home/$vUsuarioNoRoot/Cryptos/BCH/
-          chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/Cryptos/
-          chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/Cryptos/BCH/ -R
-          find /home/$vUsuarioNoRoot/Cryptos/BCH/ -type d -exec chmod 775 {} \;
-          find /home/$vUsuarioNoRoot/Cryptos/BCH/ -type f -exec chmod 664 {} \;
-          find /home/$vUsuarioNoRoot/Cryptos/BCH/bin -type f -exec chmod +x {} \;
-
-          # Instalar los c-scripts
-            su $vUsuarioNoRoot -c "curl -sL https://raw.githubusercontent.com/nipegun/c-scripts/main/CScripts-Instalar.sh | bash"
-            find /home/$vUsuarioNoRoot/scripts/c-scripts/ -type f -iname "*.sh" -exec chmod +x {} \;
-
-          # Reparación de permisos
-            chmod +x /home/$vUsuarioNoRoot/Cryptos/BCH/bin/*
-            chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/Cryptos/BCH/ -R
-            chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/.raven/ -R
-
-          # Iniciar el demonio
-            echo ""
-            echo "  Arrancando bitcoind..."
-            echo ""
-            su $vUsuarioNoRoot -c "/home/$vUsuarioNoRoot/scripts/c-scripts/bch-daemon-iniciar.sh"
-            sleep 5
-
-            #su $vUsuarioNoRoot -c "/home/$vUsuarioNoRoot/Cryptos/BCH/bin/raven-cli getnewaddress" > /home/$vUsuarioNoRoot/pooladdress-bch.txt
-            #chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/pooladdress-bch.txt
-            #echo ""
-            #echo "  La dirección para recibir bitcoins es:"
-            #echo ""
-            #cat /home/$vUsuarioNoRoot/pooladdress-bch.txt
-            #vDirCartBCH=$(cat /home/$vUsuarioNoRoot/pooladdress-bch.txt)
-            #echo ""
-
-          # Autoejecución del nodo al iniciar el sistema
-            echo ""
-            echo "    Agregando ravend a los ComandosPostArranque..."
-            echo ""
-            chmod +x /home/$vUsuarioNoRoot/scripts/c-scripts/bch-daemon-iniciar.sh
-            echo "su $vUsuarioNoRoot -c '/home/"$vUsuarioNoRoot"/scripts/c-scripts/bch-daemon-iniciar.sh'" >> /root/scripts/ComandosPostArranque.sh
-            echo ""
-
-        ;;
-
-        2)
 
           echo ""
           echo "  Instalando el nodo BitcoinCashNode desde la web de GitHub..."
@@ -186,8 +65,8 @@ choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
               apt-get -y install curl
               echo ""
             fi
-          $vUltVersBitcoinCashNode=$(curl -sL https://github.com/bitcoin-cash-node/bitcoin-cash-node/releases | sed 's->->\n-g' | grep href | grep tar | grep inux | grep 686 | grep -v debug | cut -d'"' -f2 | cut -d'/' -f6 |
-cut -d'v' -f2)
+          vUltVersBitcoinCashNode=$(curl -sL https://github.com/bitcoin-cash-node/bitcoin-cash-node/releases | sed 's->->\n-g' | grep href | grep tar | grep inux | grep "x86_64" | grep -v debug | cut -d'"' -f2 | cut -d'/' -f6 | cut -d'v' -f2)
+        ##vUltVersBitcoinCashNode="26.0.0"
           echo ""
           echo "      La última versión de BitcoinCashNode disponible en GitHub es la $vUltVersBitcoinCashNode"
           echo ""
@@ -195,14 +74,16 @@ cut -d'v' -f2)
           echo ""
           echo "    Determinando la URL del archivo a descargar..."
           echo ""
-          vNombreArchivo=$(curl -sL https://github.com/bitcoin-cash-node/bitcoin-cash-node/releases | sed 's->->\n-g' | grep href | grep tar | grep inux | grep 686 | grep -v debug | cut -d'"' -f2)
-          vURLArchivo="https://github.com/$vNombreArchivo"
+          vNombreArchivo=$(curl -sL https://github.com/bitcoin-cash-node/bitcoin-cash-node/releases | sed 's->->\n-g' | grep href | grep tar | grep inux | grep "x86_64" | grep -v debug | cut -d'"' -f2)
+         ##vNombreArchivo="/bitcoin-cash-node/bitcoin-cash-node/releases/download/v$vUltVersBitcoinCashNode/bitcoin-cash-node-$vUltVersBitcoinCashNode-x86_64-linux-gnu.tar.gz"
+          vURLArchivo="https://github.com$vNombreArchivo"
           echo ""
           echo "      La URL del archivo es: $vURLArchivo"
           echo ""
 
           echo ""
-          echo "    Intentando descargar el archivo..."          echo ""
+          echo "    Intentando descargar el archivo..."
+          echo ""
           mkdir -p /root/SoftInst/Cryptos/BCH/ 2> /dev/null
           rm -rf /root/SoftInst/Cryptos/BCH/*
           cd /root/SoftInst/Cryptos/BCH/
@@ -218,7 +99,8 @@ cut -d'v' -f2)
           wget $vURLArchivo -O /root/SoftInst/Cryptos/BCH/bitcoincashnode$vUltVersBitcoinCashNode.tar.gz
 
           echo ""
-          echo "    Descomprimiendo el archivo..."          echo ""
+          echo "    Descomprimiendo el archivo..."
+          echo ""
           # Comprobar si el paquete tar está instalado. Si no lo está, instalarlo.
             if [[ $(dpkg-query -s tar 2>/dev/null | grep installed) == "" ]]; then
               echo ""
@@ -230,30 +112,32 @@ cut -d'v' -f2)
             fi
           tar -xf /root/SoftInst/Cryptos/BCH/bitcoincashnode$vUltVersBitcoinCashNode.tar.gz
           rm -f /root/SoftInst/Cryptos/BCH/bitcoincashnode$vUltVersBitcoinCashNode.tar.gz
-          find /root/SoftInst/Cryptos/BCH/ -type d -name "raven*" -exec mv {} /root/SoftInst/Cryptos/BCH/"raven-$vUltVersBitcoinCashNode"/ \; 2> /dev/null
+          find /root/SoftInst/Cryptos/BCH/ -type d -name "bitcoin*" -exec mv {} /root/SoftInst/Cryptos/BCH/"bitcoin-cash-node"/ \; 2> /dev/null
 
           echo ""
-          echo "    Creando carpetas y archivos necesarios para ese usuario..."          echo ""
-          mkdir -p /home/$vUsuarioNoRoot/.raven/
-          touch    /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "rpcuser=bchrpc"           > /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "rpcpassword=bchrpcpass"  >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "rpcallowip=127.0.0.1"    >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "#Default RPC port 8766"  >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "rpcport=20401"           >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "server=1"                >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "listen=1"                >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "prune=550"               >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "daemon=1"                >> /home/$vUsuarioNoRoot/.raven/raven.conf
-          echo "gen=0"                   >> /home/$vUsuarioNoRoot/.raven/raven.conf
+          echo "    Creando carpetas y archivos necesarios para el usuario $vUsuarioNoRoot..."
+          echo ""
           rm -rf   /home/$vUsuarioNoRoot/Cryptos/BCH/
           mkdir -p /home/$vUsuarioNoRoot/Cryptos/BCH/ 2> /dev/null
-          mv /root/SoftInst/Cryptos/BCH/raven-$vUltVersBitcoinCashNode/* /home/$vUsuarioNoRoot/Cryptos/BCH/
+          mv /root/SoftInst/Cryptos/BCH/bitcoin-cash-node/* /home/$vUsuarioNoRoot/Cryptos/BCH/
           chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/Cryptos/
           chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/Cryptos/BCH/ -R
           find /home/$vUsuarioNoRoot/Cryptos/BCH/ -type d -exec chmod 775 {} \;
           find /home/$vUsuarioNoRoot/Cryptos/BCH/ -type f -exec chmod 664 {} \;
           find /home/$vUsuarioNoRoot/Cryptos/BCH/bin -type f -exec chmod +x {} \;
+
+          #mkdir -p /home/$vUsuarioNoRoot/.bitcoin/
+          #touch    /home/$vUsuarioNoRoot/.bitcoin/bitcoin.conf
+          #echo "rpcuser=bchrpc"           > /home/$vUsuarioNoRoot/.bitcoin/bitcoin.conf
+          #echo "rpcpassword=bchrpcpass"  >> /home/$vUsuarioNoRoot/.bitcoin/bitcoin.conf
+          #echo "rpcallowip=127.0.0.1"    >> /home/$vUsuarioNoRoot/.bitcoin/bitcoin.conf
+          #echo "#Default RPC port 8766"  >> /home/$vUsuarioNoRoot/.bitcoin/bitcoin.conf
+          #echo "rpcport=20401"           >> /home/$vUsuarioNoRoot/.bitcoin/bitcoin.conf
+          #echo "server=1"                >> /home/$vUsuarioNoRoot/.bitcoin/bitcoin.conf
+          #echo "listen=1"                >> /home/$vUsuarioNoRoot/.bitcoin/bitcoin.conf
+          #echo "prune=550"               >> /home/$vUsuarioNoRoot/.bitcoin/bitcoin.conf
+          #echo "daemon=1"                >> /home/$vUsuarioNoRoot/.bitcoin/bitcoin.conf
+          #echo "gen=0"                   >> /home/$vUsuarioNoRoot/.bitcoin/bitcoin.conf
 
           # Instalar los c-scripts
             su $vUsuarioNoRoot -c "curl -sL https://raw.githubusercontent.com/nipegun/c-scripts/main/CScripts-Instalar.sh | bash"
@@ -262,7 +146,7 @@ cut -d'v' -f2)
           # Reparación de permisos
             chmod +x /home/$vUsuarioNoRoot/Cryptos/BCH/bin/*
             chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/Cryptos/BCH/ -R
-            chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/.raven/ -R
+            chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/.bitcoin/ -R
 
           # Iniciar el demonio
             echo ""
@@ -271,30 +155,38 @@ cut -d'v' -f2)
             su $vUsuarioNoRoot -c "/home/$vUsuarioNoRoot/scripts/c-scripts/bch-daemon-iniciar.sh"
             sleep 5
 
-            #su $vUsuarioNoRoot -c "/home/$vUsuarioNoRoot/Cryptos/BCH/bin/raven-cli getnewaddress" > /home/$vUsuarioNoRoot/pooladdress-bch.txt
-            #chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/pooladdress-bch.txt
-            #echo ""
-            #echo "  La dirección para recibir bitcoins es:"
-            #echo ""
-            #cat /home/$vUsuarioNoRoot/pooladdress-bch.txt
-            #vDirCartBCH=$(cat /home/$vUsuarioNoRoot/pooladdress-bch.txt)
-            #echo ""
+          # Obtener dirección de recepción
+            su $vUsuarioNoRoot -c "/home/$vUsuarioNoRoot/Cryptos/BCH/bin/bitcoin-cli getnewaddress" > /home/$vUsuarioNoRoot/address-bch.txt
+            chown $vUsuarioNoRoot:$vUsuarioNoRoot /home/$vUsuarioNoRoot/address-bch.txt
+            echo ""
+            echo "  La dirección para recibir bitcoins es:"
+            echo ""
+            cat /home/$vUsuarioNoRoot/address-bch.txt
+            vDirCartBCH=$(cat /home/$vUsuarioNoRoot/address-bch.txt)
+            echo ""
 
           # Autoejecución del nodo al iniciar el sistema
             echo ""
             echo "    Agregando ravend a los ComandosPostArranque..."
             echo ""
             chmod +x /home/$vUsuarioNoRoot/scripts/c-scripts/bch-daemon-iniciar.sh
-            echo "su $vUsuarioNoRoot -c '/home/"$vUsuarioNoRoot"/scripts/c-scripts/bch-daemon-iniciar.sh'" >> /root/scripts/ComandosPostArranque.sh
+            echo "su $vUsuarioNoRoot -c '/home/"$vUsuarioNoRoot"/scripts/c-scripts/bch-daemon-iniciar.sh'" >> /root/scripts/ParaEsteDebian/ComandosPostArranque.sh
             echo ""
 
         ;;
 
-        3)
+        2)
 
           echo ""
           echo "  Agregando configuración para el modo GUI..."
           echo ""
+
+          # Instalación de dependencias gráficas
+            apt-get -y update
+            apt-get -y install libxcb-icccm4
+            apt-get -y install libxcb-image0
+            apt-get -y install libxcb-keysyms1
+            apt-get -y install libxcb-render-util0
 
           # Icono de lanzamiento en el menú gráfico
             echo ""
@@ -312,7 +204,6 @@ cut -d'v' -f2)
             #echo "Icon="                                                           >> /home/$vUsuarioNoRoot/.local/share/applications/bch.desktop
             chown $vUsuarioNoRoot:$vUsuarioNoRoot                                      /home/$vUsuarioNoRoot/.local/share/applications/bch.desktop
             gio set /home/$vUsuarioNoRoot/.local/share/applications/bch.desktop "metadata::trusted" yes
-            
 
           # Autoejecución gráfica de bitcoin
             echo ""
