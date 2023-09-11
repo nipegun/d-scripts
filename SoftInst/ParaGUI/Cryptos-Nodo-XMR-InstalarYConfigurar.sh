0a8212112402bd44dd6ff03be3698263323b9776
NiPeGun <nipegun@gmail.com>
2023-09-11 18:47:10 +0200

    Update Cryptos-Nodo-XMR-InstalarYConfigurar.sh

--- SoftInst/ParaGUI/Cryptos-Nodo-XMR-InstalarYConfigurar.sh
+++ SoftInst/ParaGUI/Cryptos-Nodo-XMR-InstalarYConfigurar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
----
+# ----------
 # Script de NiPeGun para instalar y configurar el nodo de Monero (XMR)
 #
 # Ejecución remota:
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/Cryptos-Nodo-XMR-InstalarYConfigurar.sh | bash
----
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/Cryptos-Nodo-XMR-InstalarYConfigurar.sh | bash
+# ----------
 
 vUsuarioNoRoot="nipegun"
 
