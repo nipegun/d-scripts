be460a7852667857b4cfcac3296c2cb9233a845f
NiPeGun <nipegun@gmail.com>
2024-03-26 21:02:36 +0100

    Update Cryptos-MultiCartera-Exodus-Instalar.sh

--- SoftInst/ParaGUI/Cryptos-MultiCartera-Exodus-Instalar.sh
+++ SoftInst/ParaGUI/Cryptos-MultiCartera-Exodus-Instalar.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para instalar y configurar Exodus en Debian
 #
 # Ejecución remota:
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/Cryptos-MultiCartera-Coinomi-Instalar.sh | bash
+#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/Cryptos-MultiCartera-Exodus-Instalar.sh | bash
 #
 # Ejecución remota sin caché:
-#  curl -sL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/Cryptos-MultiCartera-Coinomi-Instalar.sh | bash
+#  curl -sL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/Cryptos-MultiCartera-Exodus-Instalar.sh | bash
 # ----------
 
 vUsuarioNoRoot="nipegun"
