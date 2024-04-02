c6878cf44a1fa812348eabfe1cbc151f94b96002
NiPeGun <nipegun@gmail.com>
2024-04-02 20:17:06 +0200

    Update Proxmox-VirtualizationEnvironment-Instalar.sh

--- SoftInst/ParaCLI/Proxmox-VirtualizationEnvironment-Instalar.sh
+++ SoftInst/ParaCLI/Proxmox-VirtualizationEnvironment-Instalar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
----------
+# ---------
 # Script de NiPeGun para instalar y configurar ProxmoxVE en Debian
 #
 # Ejecución remota:
 #  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Proxmox-VirtualizationEnvironment-Instalar.sh | bash
----------
+# ---------
 
 # IP Local
   #cIPLocal="$(hostname -I)"
