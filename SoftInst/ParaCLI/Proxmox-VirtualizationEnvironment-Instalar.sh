5253597ad4924f3eb940de59284465f34ecda649
NiPeGun <nipegun@gmail.com>
2024-04-02 20:32:22 +0200

    Update Proxmox-VirtualizationEnvironment-Instalar.sh

--- SoftInst/ParaCLI/Proxmox-VirtualizationEnvironment-Instalar.sh
+++ SoftInst/ParaCLI/Proxmox-VirtualizationEnvironment-Instalar.sh
@@ -215,159 +215,164 @@ elif [ $cVerSO == "12" ]; then
   echo -e "${cColorAzulClaro}  Iniciando el script de instalación de ProxmoxVE para Debian 12 (Bookworm)...${cFinColor}"
   echo ""
 
-  # Agregar el repositorio de proxmox
-    echo ""
-    echo "    Agregando el repositorio de Proxmox..."
-    echo ""
-    echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-no-subscription.list
+  if [[ ! -f "/root/Fase1Completa.txt" ]]; then
 
-  # Agregar a llave para firmar las descargas desde el repositorio de Proxmox
-    echo ""
-    echo "    Agregando a llave para firmar las descargas desde el repositorio de Proxmox..."
-    echo ""
-    # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
-      if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
-        echo ""
-        echo -e "${cColorRojo}      El paquete wget no está instalado. Iniciando su instalación...${cFinColor}"
-        echo ""
-        apt-get -y update
-        apt-get -y install wget
-        echo ""
-      fi
-    wget http://download.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmomx-release-bookworm.gpg
-
-  # Actualizar la lista de paquetes disponibles en los repositorios activados
-    echo ""
-    echo "    Actualizar la lista de paquetes disponibles en los repositorios activados..."
-    echo ""
-    apt-get -y update
-
-  # Configurar el archivo /etc/hosts
-    echo ""
-    echo "      Editando el archivo /etc/hosts..."
-    echo ""
-    cp /etc/hosts /etc/hosts.bak.$cFechaDeEjec
-    echo "127.0.0.1 localhost localhost.localdomain" > /etc/hosts
-    echo "$cIPLocal $HOSTNAME $HOSTNAME.home.arpa"  >> /etc/hosts
-    echo ""
-    echo "        El archivo /etc/hosts ha quedado así:"
-    echo ""
-    cat /etc/hosts
-    echo ""
-
-  # Instalar el kernel por defecto
-    echo ""
-    echo "    Instalando el kernel por defecto..."
-    echo ""
-    apt-get -y install proxmox-default-kernel
-
-  # Reiniciar Debian
-    echo ""
-    echo "    Reiniciando Debian..."
-    echo ""
-    apt-get -y install proxmox-default-kernel
-
-
-##### Fase 2
-
-  # Instalar paquetes
-    echo ""
-    echo "    Instalando paquetes..."
-    echo ""
-    apt-get -y install proxmox-ve
-    apt-get -y install postfix
-    apt-get -y install open-iscsi
-    apt-get -y install chrony
-
-  # Desinstalar el kernel de Debian
-    echo ""
-    echo "    Desinstalando el kernel de Debian..."
-    echo ""
-    apt-get -y remove linux-image-amd64
-    apt-get -y remove linux-image-[5-6]*
-
-  # Actualizar grub
-    echo ""
-    echo "    Actualizando grub..."
-    echo ""
-    update-grub
-
-  # Borrar el repositorio enterprise
-    echo ""
-    echo "    Desactivando el repositorio enterprise..."
-    echo ""
-    mv /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.bak
-
-  # Volver a actualizar la lista de paquetes disponibles en los repositorios activados
-    echo ""
-    echo "    Volviendo a actualizar la lista de paquetes disponibles en los repositorios activados..."
-    echo ""
-    apt-get -y update
-
-  # Instalar las cabeceras del kernel
-    echo ""
-    echo "    Instalando las cabeceras del kernel..."
-    echo ""
-    apt-get -y install pve-headers
-
-  # Desinstalar OSProber
-    #echo ""
-    #echo "    Desinstalando OSProber..."
-    #echo ""
-    #apt-get -y remove os-prober
-
-
-  # Configurar la red
-    echo ""
-    echo "      Editando el archivo /etc/network/interfaces..."
-    echo ""
-    cp /etc/network/interfaces /etc/network/interfaces.bak.$cFechaDeEjec
-    echo "auto lo"                         > /etc/network/interfaces
-    echo "iface lo inet loopback"         >> /etc/network/interfaces
-    echo ""                               >> /etc/network/interfaces
-    echo "iface eth0 inet manual"         >> /etc/network/interfaces
-    echo ""                               >> /etc/network/interfaces
-    echo "auto vmbr0"                     >> /etc/network/interfaces
-    echo "iface vmbr0 inet static"        >> /etc/network/interfaces
-    echo "  address $cIPLocal/$cCIDR"     >> /etc/network/interfaces
-    echo "  gateway $cIPGateway"          >> /etc/network/interfaces
-    echo "  bridge-ports eth0"            >> /etc/network/interfaces
-    echo "  bridge-stp off"               >> /etc/network/interfaces
-    echo "  bridge-fd 0"                  >> /etc/network/interfaces
-    echo "  #hwaddress 00:00:00:00:02:00" >> /etc/network/interfaces
-    echo ""                               >> /etc/network/interfaces
-    echo "auto vmbr1"                     >> /etc/network/interfaces
-    echo "iface vmbr1 inet manual"        >> /etc/network/interfaces
-    echo "bridge-ports none"              >> /etc/network/interfaces
-    echo "  bridge-stp off"               >> /etc/network/interfaces
-    echo "  bridge-fd 0"                  >> /etc/network/interfaces
-    echo "  #Switch 1"                    >> /etc/network/interfaces
-    echo ""
-    echo "        El archivo /etc/network/interfaces ha quedado así:"
-    echo ""
-    cat /etc/network/interfaces
-    # /etc/resolv.conf
+    # Agregar el repositorio de proxmox
       echo ""
-      echo "      Editando el archivo /etc/resolv.conf..."
+      echo "    Agregando el repositorio de Proxmox..."
       echo ""
-      echo "nameserver $cIPGateway"      > /etc/resolv.conf
-      echo "nameserver 9.9.9.9"         >> /etc/resolv.conf
-      echo "nameserver 149.112.112.112" >> /etc/resolv.conf
+      echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-no-subscription.list
 
-    # Reiniciar el Debian
+    # Agregar a llave para firmar las descargas desde el repositorio de Proxmox
       echo ""
-      echo "    Re-iniciando Debian..."
+      echo "    Agregando a llave para firmar las descargas desde el repositorio de Proxmox..."
+      echo ""
+      # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
+        if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
+          echo ""
+          echo -e "${cColorRojo}      El paquete wget no está instalado. Iniciando su instalación...${cFinColor}"
+          echo ""
+          apt-get -y update
+          apt-get -y install wget
+          echo ""
+        fi
+      wget http://download.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmomx-release-bookworm.gpg
+
+    # Actualizar la lista de paquetes disponibles en los repositorios activados
+      echo ""
+      echo "    Actualizar la lista de paquetes disponibles en los repositorios activados..."
+      echo ""
+      apt-get -y update
+
+    # Configurar el archivo /etc/hosts
+      echo ""
+      echo "      Editando el archivo /etc/hosts..."
+      echo ""
+      cp /etc/hosts /etc/hosts.bak.$cFechaDeEjec
+      echo "127.0.0.1 localhost localhost.localdomain" > /etc/hosts
+      echo "$cIPLocal $HOSTNAME $HOSTNAME.home.arpa"  >> /etc/hosts
+      echo ""
+      echo "        El archivo /etc/hosts ha quedado así:"
+      echo ""
+      cat /etc/hosts
+      echo ""
+
+    # Instalar el kernel por defecto
+      echo ""
+      echo "    Instalando el kernel por defecto..."
+      echo ""
+      apt-get -y install proxmox-default-kernel
+
+    # Crear el archivo de Fase1 Completa
+      echo ""
+      echo "    Creando el archivo de fase 1 completa..."
+      echo ""
+      touch /root/Fase1Completa.txt
+
+    # Reiniciar Debian
+      echo ""
+      echo "    Reiniciando Debian..."
       echo ""
       shutdown -r now
 
-  # Notificar fin de la instalación
-    echo ""
-    echo -e "${cColorVerde}    Ejecución del script, finalizada.${cFinColor}"
-    echo ""
-    echo -e "${cColorVerde}      Para desinstalar Proxmox ejecuta:${cFinColor}"
-    echo -e "${cColorVerde}        touch /please-remove-proxmox-ve${cFinColor}"
-    echo -e "${cColorVerde}        apt-get -y purge proxmox-ve${cFinColor}"
-    echo ""
+  fi
+
+  if [[ -f "/root/Fase1Completa.txt" ]]; then
+
+    # Instalar paquetes
+      echo ""
+      echo "    Instalando paquetes..."
+      echo ""
+      apt-get -y update
+      apt-get -y install proxmox-ve
+      apt-get -y install postfix
+      apt-get -y install open-iscsi
+      apt-get -y install chrony
+
+    # Desinstalar el kernel de Debian
+      echo ""
+      echo "    Desinstalando el kernel de Debian..."
+      echo ""
+      apt-get -y remove linux-image-amd64
+      apt-get -y remove linux-image-[5-6]*
+
+    # Actualizar grub
+      echo ""
+      echo "    Actualizando grub..."
+      echo ""
+      update-grub
+
+    # Borrar el repositorio enterprise
+      echo ""
+      echo "    Desactivando el repositorio enterprise..."
+      echo ""
+      mv /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.bak
+
+    # Volver a actualizar la lista de paquetes disponibles en los repositorios activados
+      echo ""
+      echo "    Volviendo a actualizar la lista de paquetes disponibles en los repositorios activados..."
+      echo ""
+      apt-get -y update
+
+    # Instalar las cabeceras del kernel
+      echo ""
+      echo "    Instalando las cabeceras del kernel..."
+      echo ""
+      apt-get -y install pve-headers
+
+    # Desinstalar OSProber
+      #echo ""
+      #echo "    Desinstalando OSProber..."
+      #echo ""
+      #apt-get -y remove os-prober
+
+    # Configurar la red
+      echo ""
+      echo "      Editando el archivo /etc/network/interfaces..."
+      echo ""
+      cp /etc/network/interfaces /etc/network/interfaces.bak.$cFechaDeEjec
+      echo "auto lo"                         > /etc/network/interfaces
+      echo "iface lo inet loopback"         >> /etc/network/interfaces
+      echo ""                               >> /etc/network/interfaces
+      echo "iface eth0 inet manual"         >> /etc/network/interfaces
+      echo ""                               >> /etc/network/interfaces
+      echo "auto vmbr0"                     >> /etc/network/interfaces
+      echo "iface vmbr0 inet static"        >> /etc/network/interfaces
+      echo "  address $cIPLocal/$cCIDR"     >> /etc/network/interfaces
+      echo "  gateway $cIPGateway"          >> /etc/network/interfaces
+      echo "  bridge-ports eth0"            >> /etc/network/interfaces
+      echo "  bridge-stp off"               >> /etc/network/interfaces
+      echo "  bridge-fd 0"                  >> /etc/network/interfaces
+      echo "  #hwaddress 00:00:00:00:02:00" >> /etc/network/interfaces
+      echo ""                               >> /etc/network/interfaces
+      echo "auto vmbr1"                     >> /etc/network/interfaces
+      echo "iface vmbr1 inet manual"        >> /etc/network/interfaces
+      echo "bridge-ports none"              >> /etc/network/interfaces
+      echo "  bridge-stp off"               >> /etc/network/interfaces
+      echo "  bridge-fd 0"                  >> /etc/network/interfaces
+      echo "  #Switch 1"                    >> /etc/network/interfaces
+      echo ""
+      echo "        El archivo /etc/network/interfaces ha quedado así:"
+      echo ""
+      cat /etc/network/interfaces
+      # /etc/resolv.conf
+        echo ""
+        echo "      Editando el archivo /etc/resolv.conf..."
+        echo ""
+        echo "nameserver $cIPGateway"      > /etc/resolv.conf
+        echo "nameserver 9.9.9.9"         >> /etc/resolv.conf
+        echo "nameserver 149.112.112.112" >> /etc/resolv.conf
+
+    # Notificar fin de la instalación
+      echo ""
+      echo -e "${cColorVerde}    Ejecución del script, finalizada.${cFinColor}"
+      echo ""
+      echo -e "${cColorVerde}      Para desinstalar Proxmox ejecuta:${cFinColor}"
+      echo -e "${cColorVerde}        touch /please-remove-proxmox-ve${cFinColor}"
+      echo -e "${cColorVerde}        apt-get -y purge proxmox-ve${cFinColor}"
+      echo ""
+
+  fi
 
 fi
 
