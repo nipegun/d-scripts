30a9b99bfc8ee88cf34b45e6a0c3be5d600fcb07
NiPeGun <nipegun@gmail.com>
2024-04-24 04:05:41 +0200

    Create ROCm-InstalarYConfigurar.sh

---
+++ PostInst/Controladores/ROCm-InstalarYConfigurar.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+
+# Script de NiPeGun para instalar y configurar ROCm en Debian
+#
+# Ejecución remota:
+#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/Controladores/ROCm-InstalarYConfigurar.sh | bash
+
+
+cColorRojo='\033[1;31m'
+cColorVerde='\033[1;32m'
+cFinColor='\033[0m'
+
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
+    . /etc/os-release
+    cNomSO=$NAME
+    cVerSO=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # Para linuxbase.org.
+    cNomSO=$(lsb_release -si)
+    cVerSO=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release.
+    . /etc/lsb-release
+    cNomSO=$DISTRIB_ID
+    cVerSO=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    cNomSO=Debian
+    cVerSO=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD).
+    cNomSO=$(uname -s)
+    cVerSO=$(uname -r)
+  fi
+
+if [ $cVerSO == "7" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de ROCm para Debian 7 (Wheezy)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "8" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de ROCm para Debian 8 (Jessie)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "9" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de ROCm para Debian 9 (Stretch)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "10" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de ROCm para Debian 10 (Buster)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "11" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de ROCm para Debian 11 (Bullseye)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 11 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "12" ]; then
+
+  vFechaDeEjec=$(date +a%Ym%md%d@%T)
+
+  # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${cColorRojo}  El paquete dialog no está instalado. Iniciando su instalación...${cFinColor}"
+      echo ""
+      apt-get -y update
+      apt-get -y install dialog
+      echo ""
+    fi
+
+  menu=(dialog --checklist "Marca la opción que quieras instalar:" 22 96 16)
+    opciones=(
+      1 "Instalar desde los repositorios oficiales de Debian" on
+      2 "Instalar desde fuente externa" off
+    )
+  choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
+  #clear
+
+    for choice in $choices
+      do
+        case $choice in
+
+          1)
+
+            echo ""
+            echo "  Instalando ROCm desde los repositorios oficiales de Debian..."
+            echo ""
+            apt-get -y install librocm-smi-dev
+            apt-get -y install librocm-smi64-1
+            apt-get -y install rocm-cmake
+            apt-get -y install rocm-device-libs
+            apt-get -y install rocm-smi
+            apt-get -y install rocminfo
+            apt-get -y install libamd-comgr2
+            apt-get -y install librocsparse0
+            apt-get -y install libspfft1
+
+          ;;
+
+          2)
+
+            echo ""
+            echo "  Instalando ROCm desde una fuente externa..."
+            echo ""
+
+            # Agregar el repositorio
+              echo ""
+              echo "    Agregando el repositorio"
+              echo ""
+              apt-get -y install curl
+              apt-get -y install gnupg2
+              curl -fsSL https://repo.radeon.com/rocm/rocm.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/rocm-keyring.gpg
+              echo 'deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/rocm-keyring.gpg] https://repo.radeon.com/rocm/apt/5.7/ jammy main' > /etc/apt/sources.list.d/rocm.list
+            
+            # Instalar paquetes
+              echo ""
+              echo "    Instalando paquetes necesarios..."
+              echo ""
+              apt-get -y install wget
+              apt-get -y install gawk
+              apt-get -y install linux-headers-amd64
+              apt-get -y install clinfo
+
+            # Instalar ROCm
+              echo ""
+              echo "    Instalando el paquete rocm-opencl-runtime..."
+              echo ""
+              apt-get -y install rocm-opencl-runtime
+          ;;
+
+      esac
+
+  done
+
+fi
