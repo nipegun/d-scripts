ee0ed1e2f52a549fb4d54428dd1030e860ab87ee
NiPeGun <nipegun@gmail.com>
2024-05-01 23:47:04 +0200

    Update MySQL-BaseDeDatos-Importar.sh

--- MySQL-BaseDeDatos-Importar.sh
+++ MySQL-BaseDeDatos-Importar.sh
@@ -19,7 +19,7 @@ cColorRojo='\033[1;31m'
 cColorVerde='\033[1;32m'
 cFinColor='\033[0m'
 
-if [ $# -ne $cCantArgsEsperados ]
+if [ $# -ne $cCantArgumEsperados ]
   then
     echo ""
     echo -e "${cColorRojo}Mal uso del script.${cFinColor}"
