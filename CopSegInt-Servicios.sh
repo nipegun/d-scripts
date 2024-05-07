325990603cce1ef4efff7b3264705bd1c0b9ba88
NiPeGun <nipegun@gmail.com>
2024-05-08 01:22:55 +0200

    Create CopSegInt-Servicios.sh

---
+++ CopSegInt-Servicios.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para hacer copia de seguridad interna de servicios instalados en Debian
+#
+# Ejecución remota:
+#  curl -sL x | bash
+#
+# Ejecución remota sin caché:
+#  curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+# ----------
