d78b57de250b80b33128b40dc9d634e68990a996
NiPeGun <nipegun@gmail.com>
2024-05-13 06:22:15 +0200

    Update Servidor-Mail-Exim4-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Servidor-Mail-Exim4-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Servidor-Mail-Exim4-InstalarYConfigurar.sh
@@ -157,5 +157,83 @@ elif [ $cVerSO == "11" ]; then
   echo "  Comandos para Debian 11 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
   echo ""
 
+elif [ $cVerSO == "12" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de exim4 para Debian 12 (Bookworm)..."  
+  echo ""
+
+  apt-get -y update
+  apt-get -y install exim4
+  apt-get -y install courier-imap
+
+  # Crear la carpeta mail en cada carpeta de usuario
+    maildirmake /etc/skel/Maildir
+    maildirmake ~root/Maildir
+    chown root:root ~root/Maildir -R
+
+  # Reconfigurar Exim4
+    dpkg-reconfigure exim4-config
+
+  # Generar el certificado SSL
+    /usr/share/doc/exim4-base/examples/exim-gencert
+          
+  # Activar TLS
+    echo "MAIN_TLS_ENABLE = true"      > /etc/exim4/exim4.conf.localmacros
+    echo "tls_on_connect_ports = 465" >> /etc/exim4/exim4.conf.localmacros
+    sed -i -e "s|SMTPLISTENEROPTIONS=''|SMTPLISTENEROPTIONS='-oX 465:25:587 -oP /var/run/exim4/exim.pid'|g" /etc/default/exim4
+    echo "MAIN_TLS_ENABLE = yes"      >> /etc/exim4/exim4.conf.template
+    echo "tls_on_connect_ports=465"   >> /etc/exim4/exim4.conf.template
+    echo "rfc1413_query_timeout = 0s" >> /etc/exim4/exim4.conf.template
+          
+  # Instalar el daemon SaslAuth
+    apt-get -y install sasl2-bin
+    sed -i -e 's|START=no|START=yes|g' /etc/default/saslauthd
+    adduser Debian-exim sasl
+          
+  # Borrar 7 líneas después de "# plain_saslauthd_server:"
+    sed -i -e '/# plain_saslauthd_server:/{n;N;N;N;N;N;N;N;d}' /etc/exim4/exim4.conf.template
+          
+  # Descomentar la línea "# plain_saslauthd_server:" y agregar el resto de líneas antes borradas pero ahora descomentadas
+    sed -i -e 's|# plain_saslauthd_server:|\
+    plain_saslauthd_server: \
+    driver = plaintext \
+    public_name = PLAIN \
+    server_condition = ${if saslauthd{{$auth2}{$auth3}}{1}{0}} \
+    server_set_id = $auth2 \
+    server_prompts = : \
+    .ifndef AUTH_SERVER_ALLOW_NOTLS_PASSWORDS \
+    server_advertise_condition = ${if eq{$tls_in_cipher}{}{}{*}} \
+    .endif|g' /etc/exim4/exim4.conf.template
+          
+  # Configurar IMAP
+    rm -rf /etc/courier/*.pem
+    make-ssl-cert /usr/share/ssl-cert/ssleay.cnf /etc/courier/imapd.pem
+    service courier-imap restart
+    service courier-imap-ssl restart
+    service courier-authdaemon restart
+
+# Comprobar si el paquete net-tools está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s net-tools 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete net-tools no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    apt-get -y update
+    apt-get -y install net-tools
+    echo ""
+  fi
+  netstat -atupln | grep 465
+          
+  # Instalar y configurar SpamAssassin
+    apt-get -y install spamassassin
+    systemctl enable spamassassin.service
+          
+  echo ""
+  echo "Sistema instalado."
+  echo ""
+  echo "Para crear un usuario sin acceso al shell ejecuta:"
+  echo "/root/scripts/d-scripts/UsuarioNuevoSinShell.sh"
+  echo ""
+
 fi
 
