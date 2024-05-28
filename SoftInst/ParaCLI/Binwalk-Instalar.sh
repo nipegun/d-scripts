19813eb5c4b9da1f29c1be121416860f14e25585
NiPeGun <nipegun@gmail.com>
2024-05-28 13:26:21 +0200

    Update Binwalk-Instalar.sh

--- SoftInst/ParaCLI/Binwalk-Instalar.sh
+++ SoftInst/ParaCLI/Binwalk-Instalar.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para instalar la última versión de binwalk en Debian
 #
 # Ejecución remota:
-#   curl -sL x | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Binwalk-Instalar.sh | bash
 #
 # Ejecución remota sin caché:
-#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#   curl -sL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Binwalk-Instalar.sh | bash
 # ----------
 
 # Definir constantes de color
