1c509bad96e5b5bb56e169d0f5b3c224ac79ced7
NiPeGun <nipegun@gmail.com>
2024-06-03 09:00:49 +0200

    Update Escritorio-Mate-InstalarSoftware.sh

--- PostInst/GUI/Escritorio-Mate-InstalarSoftware.sh
+++ PostInst/GUI/Escritorio-Mate-InstalarSoftware.sh
@@ -83,112 +83,8 @@ elif [ $cVerSO == "11" ]; then
   echo ""
 
   echo ""
-  echo "  Iniciando el script de instalación de software para el escritorio Mate en Debian 12 (Bookworm)..."
+  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
   echo ""
-
-  # Desinstalar cosas específicas de mate-esktop
-    apt-get -y remove xterm
-    apt-get -y remove reportbug
-    apt-get -y remove blender
-    apt-get -y remove imagemagick
-    apt-get -y remove inkscape
-    apt-get -y remove gnome-disk-utility
-    apt-get -y autoremove
-
-  # Actualizar el cache de los paquetes
-    apt-get -y update
-
-  # Sistema
-    apt-get -y install gparted
-    apt-get -y install hardinfo
-    apt-get -y install bleachbit
-
-  # Multimedia
-    apt-get -y install vlc
-    apt-get -y install vlc-plugin-vlsub
-    apt-get -y install audacity
-    apt-get -y install subtitleeditor
-    apt-get -y install easytag
-    apt-get -y install openshot
-
-  # Redes e internet
-    apt-get -y install gufw
-    apt-get -y install wireshark
-    apt-get -y install etherape
-      setcap CAP_NET_RAW=pe /usr/bin/etherape
-    apt-get -y install sshpass
-    apt-get -y install virt-viewer
-    apt-get -y install whois
-    apt-get -y install remmina
-    apt-get -y install firefox-esr-l10n-es-es
-    apt-get -y install thunderbird
-    apt-get -y install thunderbird-l10n-es-es
-    apt-get -y install lightning-l10n-es-es
-    apt-get -y install eiskaltdcpp
-    apt-get -y install amule
-    apt-get -y install chromium
-    apt-get -y install chromium-l10n
-    apt-get -y install filezilla
-    apt-get -y install mumble
-    apt-get -y install obs-studio
-    #apt-get -y install telegram-desktop
-    apt-get -y install discord
-
-  # Juegos
-    apt-get -y install scid
-    apt-get -y install scid-rating-data
-    apt-get -y install scid-spell-data
-    apt-get -y install stockfish
-    apt-get -y install dosbox
-    apt-get -y install scummvm
-
-  # Fuentes
-    apt-get -y install fonts-ubuntu
-    apt-get -y install fonts-ubuntu-console
-    apt-get -y install fonts-freefont-ttf
-    apt-get -y install fonts-freefont-otf
-    apt-get -y install ttf-mscorefonts-installer
-
-  # Programación
-    apt-get -y install ghex
-
-  # Seguridad
-    apt-get -y install clamav
-    apt-get -y install clamtk
-
-  # Otros
-    apt-get -y install libreoffice-l10n-es
-    apt-get -y install unrar
-    apt-get -y install htop
-    apt-get -y install simple-scan
-    apt-get -y install android-tools-adb # Para poder operar con el contenido de los móviles y relojes android
-    apt-get -y install android-tools-fastboot
-    apt-get -y install pyrenamer # Hay que agregar el repositorio de stretch antes, o instalar gprename, como reemplazo
-    apt-get -y install comix
-
-  # Lanzador de chromium para el root
-    mkdir -p /root/.local/share/applications/ 2> /dev/null
-    echo "[Desktop Entry]"                      > /root/.local/share/applications/chromiumroot.desktop
-    echo "Name=Chromium (para root)"           >> /root/.local/share/applications/chromiumroot.desktop
-    echo "Comment=Accede a Internet"           >> /root/.local/share/applications/chromiumroot.desktop
-    echo "GenericName=Navegador web"           >> /root/.local/share/applications/chromiumroot.desktop
-    echo "Exec=/usr/bin/chromium --no-sandbox" >> /root/.local/share/applications/chromiumroot.desktop
-    echo "Icon=chromium"                       >> /root/.local/share/applications/chromiumroot.desktop
-    echo "Type=Application"                    >> /root/.local/share/applications/chromiumroot.desktop
-    echo "StartupNotify=false"                 >> /root/.local/share/applications/chromiumroot.desktop
-    echo "StartupWMClass=Code"                 >> /root/.local/share/applications/chromiumroot.desktop
-    echo "Categories=Network;WebBrowser;"      >> /root/.local/share/applications/chromiumroot.desktop
-    echo "MimeType=text/html;text/xml;application/xhtml_xml;application/x-mimearchive;x-scheme-handler/http;x-scheme-handler/https;" >> /root/.local/share/applications/chromiumroot.desktop
-    gio set /root/.local/share/applications/chromiumroot.desktop "metadata::trusted" yes
-
-  # Tor browser
-    apt-get -y install torbrowser-launcher
-
-  # Específicas para mate-desktop
-    apt-get -y install caja-open-terminal
-    apt-get -y install caja-admin
-    #apt-get -y install caja-share     # Para compartir carpetas desde el propio caja
-    #apt-get -y install gvfs-backends  # Para poder ver las comparticiones de red usando samba
 
 elif [ $cVerSO == "12" ]; then
 
@@ -208,6 +104,22 @@ elif [ $cVerSO == "12" ]; then
   # Actualizar el cache de los paquetes
     apt-get -y update
 
+  # Herramientas de terminal
+    apt-get -y install sshpass
+    apt-get -y install whois
+    apt-get -y install shellcheck
+    apt-get -y install grub2
+    apt-get -y install wget
+    apt-get -y install curl
+    apt-get -y install nmap
+    apt-get -y install mc
+    apt-get -y install smartmontools
+    apt-get -y install coreutils
+    apt-get -y install sshpass
+    apt-get -y install unrar
+    apt-get -y install android-tools-adb # Para poder operar con el contenido de los móviles y relojes android
+    apt-get -y install android-tools-fastboot
+
   # Sistema
     apt-get -y install gparted
     apt-get -y install hardinfo
@@ -222,13 +134,10 @@ elif [ $cVerSO == "12" ]; then
     apt-get -y install openshot
 
   # Redes e internet
-    apt-get -y install gufw
     apt-get -y install wireshark
     apt-get -y install etherape
       setcap CAP_NET_RAW=pe /usr/bin/etherape
-    apt-get -y install sshpass
     apt-get -y install virt-viewer
-    apt-get -y install whois
     apt-get -y install remmina
     apt-get -y install firefox-esr-l10n-es-es
     apt-get -y install thunderbird
@@ -262,9 +171,17 @@ elif [ $cVerSO == "12" ]; then
   # Programación
     apt-get -y install ghex
 
-  # Seguridad
-    apt-get -y install clamav
+  # Antivirus
     apt-get -y install clamtk
+    apt-get -y install clamav
+    apt-get -y install clamav-freshclam
+    apt-get -y install clamav-daemon
+    mkdir /var/log/clamav/ 2> /dev/null
+    #touch /var/log/clamav/freshclam.log
+    #chown clamav:clamav /var/log/clamav/freshclam.log
+    #chmod 640 /var/log/clamav/freshclam.log
+    rm -rf /var/log/clamav/freshclam.log
+    freshclam
 
   # Otros
     apt-get -y install libreoffice-l10n-es
