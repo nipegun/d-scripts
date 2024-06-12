8df82eb69515109dad8cae737bd5d1f6f0501a4b
NiPeGun <nipegun@gmail.com>
2024-06-12 07:13:56 +0200

    Update Archivos-MP3-BorrarTags.sh

--- Archivos-MP3-BorrarTags.sh
+++ Archivos-MP3-BorrarTags.sh
@@ -48,7 +48,7 @@ if [ $# -ne $cCantArgumEsperados ]
     # Comprobar si el paquete eyed3 está instalado. Si no lo está, instalarlo.
       if [[ $(dpkg-query -s eyed3 2>/dev/null | grep installed) == "" ]]; then
         echo ""
-        echo "  eyed3 no está instalado. Iniciando su instalación..."
+        echo "  El paquete eyed3 no está instalado. Iniciando su instalación..."
         echo ""
         apt-get -y update > /dev/null
         apt-get -y install eyed3
