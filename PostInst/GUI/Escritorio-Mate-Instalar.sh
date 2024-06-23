34b9a2f7cd60ffe6aca88a03703abc15627f9a73
NiPeGun <nipegun@gmail.com>
2024-06-23 03:52:15 +0200

    Update Escritorio-Mate-Instalar.sh

--- PostInst/GUI/Escritorio-Mate-Instalar.sh
+++ PostInst/GUI/Escritorio-Mate-Instalar.sh
@@ -12,121 +12,141 @@
 #  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/GUI/Escritorio-Mate-Instalar.sh | bash
 # ----------
 
-cColorRojo='\033[1;31m'
-cColorVerde='\033[1;32m'
-cFinColor='\033[0m'
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
 
-# Determinar la versión de Debian
-
-  if [ -f /etc/os-release ]; then
-     # Para systemd y freedesktop.org
-     . /etc/os-release
-     cNomSO=$NAME
-     cVerSO=$VERSION_ID
-  elif type lsb_release >/dev/null 2>&1; then
-     # linuxbase.org
-     cNomSO=$(lsb_release -si)
-     cVerSO=$(lsb_release -sr)
-  elif [ -f /etc/lsb-release ]; then
-     # Para algunas versiones de Debian sin el comando lsb_release
-     . /etc/lsb-release
-     cNomSO=$DISTRIB_ID
-     cVerSO=$DISTRIB_RELEASE
-  elif [ -f /etc/debian_version ]; then
-     # Para versiones viejas de Debian.
-     cNomSO=Debian
-     cVerSO=$(cat /etc/debian_version)
-  else
-     # Para el viejo uname (También funciona para BSD)
-     cNomSO=$(uname -s)
-     cVerSO=$(uname -r)
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo ""
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${cFinColor}"
+    echo ""
+    exit
   fi
 
-if [ $cVerSO == "7" ]; then
+# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    apt-get -y update
+    apt-get -y install curl
+    echo ""
+  fi
 
-  echo ""
-  echo "  Iniciando el script de instalación del escritorio Mate en Debian 7 (Wheezy)..."
-  echo ""
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
+    . /etc/os-release
+    cNomSO=$NAME
+    cVerSO=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # Para linuxbase.org.
+    cNomSO=$(lsb_release -si)
+    cVerSO=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release.
+    . /etc/lsb-release
+    cNomSO=$DISTRIB_ID
+    cVerSO=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    cNomSO=Debian
+    cVerSO=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD).
+    cNomSO=$(uname -s)
+    cVerSO=$(uname -r)
+  fi
 
-  echo ""
-  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
+# Ejecutar comandos dependiendo de la versión de Debian detectada
+  if [ $cVerSO == "13" ]; then
 
-elif [ $cVerSO == "8" ]; then
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Mate en Debian 13 (x)...${cFinColor}"
+    echo ""
 
-  echo ""
-  echo "  Iniciando el script de instalación del escritorio Mate en Debian 8 (Jessie)..."
-  echo ""
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
 
-  echo ""
-  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
+  elif [ $cVerSO == "12" ]; then
 
-elif [ $cVerSO == "9" ]; then
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Mate en Debian 12 (Bookworm)...${cFinColor}"
+    echo ""
 
-  echo ""
-  echo "  Iniciando el script de instalación del escritorio Mate en Debian 9 (Stretch)..." 
-  echo ""
+    apt-get -y update
+    apt-get -y install tasksel
+    tasksel install mate-desktop
+    apt-get -y install caja-open-terminal
+    apt-get -y install caja-admin
+    apt-get -y install firefox-esr-l10n-es-es
+    apt-get -y install libreoffice-l10n-es
 
-  echo ""
-  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
+    # Permitir caja como root
+      mkdir -p /root/.config/autostart/ 2> /dev/null
+      echo "[Desktop Entry]"                > /root/.config/autostart/caja.desktop
+      echo "Type=Application"              >> /root/.config/autostart/caja.desktop
+      echo "Exec=caja --force-desktop"     >> /root/.config/autostart/caja.desktop
+      echo "Hidden=false"                  >> /root/.config/autostart/caja.desktop
+      echo "X-MATE-Autostart-enabled=true" >> /root/.config/autostart/caja.desktop
+      echo "Name[es_ES]=Caja"              >> /root/.config/autostart/caja.desktop
+      echo "Name=Caja"                     >> /root/.config/autostart/caja.desktop
+      echo "Comment[es_ES]="               >> /root/.config/autostart/caja.desktop
+      echo "Comment="                      >> /root/.config/autostart/caja.desktop
+      echo "X-MATE-Autostart-Delay=0"      >> /root/.config/autostart/caja.desktop
+      gio set /root/.config/autostart/caja.desktop "metadata::trusted" yes
 
-elif [ $cVerSO == "10" ]; then
+  elif [ $cVerSO == "11" ]; then
 
-  echo ""
-  echo "  Iniciando el script de personalización del escritorio Mate en Debian 10 (Buster)..." 
-  echo ""
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Mate en Debian 11 (Bullseye)...${cFinColor}"
+    echo ""
 
-  apt-get -y update
-  apt-get -y install tasksel
-  tasksel install mate-desktop
-  apt-get -y install caja-open-terminal
-  apt-get -y install caja-admin
-  apt-get -y install firefox-esr-l10n-es-es
-  apt-get -y install libreoffice-l10n-es
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 11 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
 
-  # Permitir caja como root
-    mkdir -p /root/.config/autostart/ 2> /dev/null
-    echo "[Desktop Entry]"                > /root/.config/autostart/caja.desktop
-    echo "Type=Application"              >> /root/.config/autostart/caja.desktop
-    echo "Exec=caja --force-desktop"     >> /root/.config/autostart/caja.desktop
-    echo "Hidden=false"                  >> /root/.config/autostart/caja.desktop
-    echo "X-MATE-Autostart-enabled=true" >> /root/.config/autostart/caja.desktop
-    echo "Name[es_ES]=Caja"              >> /root/.config/autostart/caja.desktop
-    echo "Name=Caja"                     >> /root/.config/autostart/caja.desktop
-    echo "Comment[es_ES]="               >> /root/.config/autostart/caja.desktop
-    echo "Comment="                      >> /root/.config/autostart/caja.desktop
-    echo "X-MATE-Autostart-Delay=0"      >> /root/.config/autostart/caja.desktop
-    gio set /root/.config/autostart/caja.desktop "metadata::trusted" yes
+  elif [ $cVerSO == "10" ]; then
 
-elif [ $cVerSO == "11" ]; then
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Mate en Debian 10 (Buster)...${cFinColor}"
+    echo ""
 
-  echo ""
-  echo "  Iniciando el script de instalación del escritorio Mate en Debian 11 (Bullseye)..."
-  echo ""
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
 
-  apt-get -y update
-  apt-get -y install tasksel
-  tasksel install mate-desktop
-  apt-get -y install caja-open-terminal
-  apt-get -y install caja-admin
-  apt-get -y install firefox-esr-l10n-es-es
-  apt-get -y install libreoffice-l10n-es
+  elif [ $cVerSO == "9" ]; then
 
-  # Permitir caja como root
-    mkdir -p /root/.config/autostart/ 2> /dev/null
-    echo "[Desktop Entry]"                > /root/.config/autostart/caja.desktop
-    echo "Type=Application"              >> /root/.config/autostart/caja.desktop
-    echo "Exec=caja --force-desktop"     >> /root/.config/autostart/caja.desktop
-    echo "Hidden=false"                  >> /root/.config/autostart/caja.desktop
-    echo "X-MATE-Autostart-enabled=true" >> /root/.config/autostart/caja.desktop
-    echo "Name[es_ES]=Caja"              >> /root/.config/autostart/caja.desktop
-    echo "Name=Caja"                     >> /root/.config/autostart/caja.desktop
-    echo "Comment[es_ES]="               >> /root/.config/autostart/caja.desktop
-    echo "Comment="                      >> /root/.config/autostart/caja.desktop
-    echo "X-MATE-Autostart-Delay=0"      >> /root/.config/autostart/caja.desktop
-    gio set /root/.config/autostart/caja.desktop "metadata::trusted" yes
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Mate en Debian 9 (Stretch)...${cFinColor}"
+    echo ""
 
-fi
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "8" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Mate en Debian 8 (Jessie)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "7" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Mate en Debian 7 (Wheezy)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  fi
 
