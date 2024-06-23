11b0c06bc442770c2023d2bfca63e153556fb057
NiPeGun <nipegun@gmail.com>
2024-06-23 04:05:53 +0200

    Update Escritorio-Gnome-Instalar.sh

--- PostInst/GUI/Escritorio-Gnome-Instalar.sh
+++ PostInst/GUI/Escritorio-Gnome-Instalar.sh
@@ -12,102 +12,140 @@
 #  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/GUI/Escritorio-Gnome-Instalar.sh | bash
 # ----------
 
-cColorRojo='\033[1;31m'
-cColorVerde='\033[1;32m'
-cFinColor='\033[0m'
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
 
-# Determinar la versión de Debian
-
-  if [ -f /etc/os-release ]; then
-     # Para systemd y freedesktop.org
-     . /etc/os-release
-     cNomSO=$NAME
-     cVerSO=$VERSION_ID
-  elif type lsb_release >/dev/null 2>&1; then
-     # linuxbase.org
-     cNomSO=$(lsb_release -si)
-     cVerSO=$(lsb_release -sr)
-  elif [ -f /etc/lsb-release ]; then
-     # Para algunas versiones de Debian sin el comando lsb_release
-     . /etc/lsb-release
-     cNomSO=$DISTRIB_ID
-     cVerSO=$DISTRIB_RELEASE
-  elif [ -f /etc/debian_version ]; then
-     # Para versiones viejas de Debian.
-     cNomSO=Debian
-     cVerSO=$(cat /etc/debian_version)
-  else
-     # Para el viejo uname (También funciona para BSD)
-     cNomSO=$(uname -s)
-     cVerSO=$(uname -r)
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo ""
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${cFinColor}"
+    echo ""
+    exit
   fi
 
-if [ $cVerSO == "7" ]; then
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
+    . /etc/os-release
+    cNomSO=$NAME
+    cVerSO=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # Para linuxbase.org.
+    cNomSO=$(lsb_release -si)
+    cVerSO=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release.
+    . /etc/lsb-release
+    cNomSO=$DISTRIB_ID
+    cVerSO=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    cNomSO=Debian
+    cVerSO=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD).
+    cNomSO=$(uname -s)
+    cVerSO=$(uname -r)
+  fi
 
-  echo ""
-  echo "  Iniciando el script de instalación del escritorio Gnome en Debian 7 (Wheezy)..."
-  echo ""
+# Ejecutar comandos dependiendo de la versión de Debian detectada
+  if [ $cVerSO == "13" ]; then
 
-  echo ""
-  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Gnome en Debian 13 (x)...${cFinColor}"
+    echo ""
 
-elif [ $cVerSO == "8" ]; then
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
 
-  echo ""
-  echo "  Iniciando el script de instalación del escritorio Gnome en Debian 8 (Jessie)..."
-  echo ""
+  elif [ $cVerSO == "12" ]; then
 
-  echo ""
-  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Gnome en Debian 12 (Bookworm)...${cFinColor}"
+    echo ""
 
-elif [ $cVerSO == "9" ]; then
+   # Actualizar la lista de paquetes
+      apt-get -y update
 
-  echo ""
-  echo "  Iniciando el script de instalación del escritorio Gnome en Debian 9 (Stretch)..."
-  echo ""
+    # Instalar tasksel
+      apt-get -y install tasksel
 
-  echo ""
-  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
+    # Instalar gnome con tasksel
+      tasksel install gnome-desktop
 
-elif [ $cVerSO == "10" ]; then
+    # Instalar paquetes en castellano que no se instalaron por defecto
+      apt-get -y install firefox-esr-l10n-es-es
+      apt-get -y install libreoffice-l10n-es
 
-  echo ""
-  echo "  Iniciando el script de personalización del escritorio Gnome en Debian 10 (Buster)..." 
-echo ""
+  elif [ $cVerSO == "11" ]; then
 
-  # Actualizar la lista de paquetes
-    apt-get -y update
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Gnome en Debian 11 (Bullseye)...${cFinColor}"
+    echo ""
 
-  # Instalar tasksel
-    apt-get -y install tasksel
+   # Actualizar la lista de paquetes
+      apt-get -y update
 
-  # Instalar gnome con tasksel
-    tasksel install gnome-desktop
+    # Instalar tasksel
+      apt-get -y install tasksel
 
-  # Instalar paquetes en castellano que no se instalaron por defecto
-    apt-get -y install firefox-esr-l10n-es-es
-    apt-get -y install libreoffice-l10n-es
+    # Instalar gnome con tasksel
+      tasksel install gnome-desktop
 
-elif [ $cVerSO == "11" ]; then
+    # Instalar paquetes en castellano que no se instalaron por defecto
+      apt-get -y install firefox-esr-l10n-es-es
+      apt-get -y install libreoffice-l10n-es
 
-  echo ""
-  echo "  Iniciando el script de instalación del escritorio Gnome en Debian 11 (Bullseye)..."
-  echo ""
+  elif [ $cVerSO == "10" ]; then
 
-  # Actualizar la lista de paquetes
-    apt-get -y update
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Gnome en Debian 10 (Buster)...${cFinColor}"
+    echo ""
 
-  # Instalar tasksel
-    apt-get -y install tasksel
+    # Actualizar la lista de paquetes
+      apt-get -y update
 
-  # Instalar gnome con tasksel
-    tasksel install gnome-desktop
+    # Instalar tasksel
+      apt-get -y install tasksel
 
-  # Instalar paquetes en castellano que no se instalaron por defecto
-    apt-get -y install firefox-esr-l10n-es-es
-    apt-get -y install libreoffice-l10n-es
+    # Instalar gnome con tasksel
+      tasksel install gnome-desktop
 
-fi
+    # Instalar paquetes en castellano que no se instalaron por defecto
+      apt-get -y install firefox-esr-l10n-es-es
+      apt-get -y install libreoffice-l10n-es
+
+  elif [ $cVerSO == "9" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Gnome en Debian 9 (Stretch)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "8" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Gnome en Debian 8 (Jessie)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "7" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación del escritorio Gnome en Debian 7 (Wheezy)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  fi
+
