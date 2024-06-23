e7bb31a1cc7dbd21bb53ac6d314de52e8946c9e0
NiPeGun <nipegun@gmail.com>
2024-06-23 04:19:44 +0200

    Update Servicio-xrdpMonitorSes-Instalar.sh

--- PostInst/GUI/Servicio-xrdpMonitorSes-Instalar.sh
+++ PostInst/GUI/Servicio-xrdpMonitorSes-Instalar.sh
@@ -12,34 +12,141 @@
 #   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/GUI/Servicio-xrdpMonitorSes-Instalar.sh | bash
 # ----------
 
-cColorAzul="\033[0;34m"
-cColorAzulClaro="\033[1;34m"
-cColorVerde='\033[1;32m'
-cColorRojo='\033[1;31m'
-cFinColor='\033[0m'
 
-echo ""
-echo -e "${cColorAzulClaro}  Iniciando el script de instalación de la monitorización de sesiones concedidas al servidor xrdp...${cFinColor}"
-echo ""
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
 
-# Crear el servicio
-  echo "[Unit]"                                                                    > /etc/systemd/system/xrdpMonitorSes.service
-  echo "Description=Monitor de sesiones xrdp"                                     >> /etc/systemd/system/xrdpMonitorSes.service
-  echo "After=network.target"                                                     >> /etc/systemd/system/xrdpMonitorSes.service
-  echo ""                                                                         >> /etc/systemd/system/xrdpMonitorSes.service
-  echo "[Service]"                                                                >> /etc/systemd/system/xrdpMonitorSes.service
-  echo "Type=simple"                                                              >> /etc/systemd/system/xrdpMonitorSes.service
-  echo "ExecStart=/bin/bash /root/scripts/d-scripts/xrdp-Sesiones-Monitorizar.sh" >> /etc/systemd/system/xrdpMonitorSes.service
-  echo "TimeoutStartSec=0"                                                        >> /etc/systemd/system/xrdpMonitorSes.service
-  echo "StartLimitInterval=0"                                                     >> /etc/systemd/system/xrdpMonitorSes.service
-  echo "Restart=always"                                                           >> /etc/systemd/system/xrdpMonitorSes.service
-  echo ""                                                                         >> /etc/systemd/system/xrdpMonitorSes.service
-  echo "[Install]"                                                                >> /etc/systemd/system/xrdpMonitorSes.service
-  echo "WantedBy=default.target"                                                  >> /etc/systemd/system/xrdpMonitorSes.service
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo ""
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${cFinColor}"
+    echo ""
+    exit
+  fi
 
-# Recargar cambios
-  systemctl daemon-reload
+# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    apt-get -y update
+    apt-get -y install curl
+    echo ""
+  fi
 
-# Activar e iniciar el servicio
-  systemctl enable xrdpMonitorSes.service --now
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
+    . /etc/os-release
+    cNomSO=$NAME
+    cVerSO=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # Para linuxbase.org.
+    cNomSO=$(lsb_release -si)
+    cVerSO=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release.
+    . /etc/lsb-release
+    cNomSO=$DISTRIB_ID
+    cVerSO=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    cNomSO=Debian
+    cVerSO=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD).
+    cNomSO=$(uname -s)
+    cVerSO=$(uname -r)
+  fi
+
+# Ejecutar comandos dependiendo de la versión de Debian detectada
+  if [ $cVerSO == "13" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 13 (x)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "12" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 12 (Bookworm)...${cFinColor}"
+    echo ""
+
+    # Crear el servicio
+      echo "[Unit]"                                                                    > /etc/systemd/system/xrdpMonitorSes.service
+      echo "Description=Monitor de sesiones xrdp"                                     >> /etc/systemd/system/xrdpMonitorSes.service
+      echo "After=network.target"                                                     >> /etc/systemd/system/xrdpMonitorSes.service
+      echo ""                                                                         >> /etc/systemd/system/xrdpMonitorSes.service
+      echo "[Service]"                                                                >> /etc/systemd/system/xrdpMonitorSes.service
+      echo "Type=simple"                                                              >> /etc/systemd/system/xrdpMonitorSes.service
+      echo "ExecStart=/bin/bash /root/scripts/d-scripts/xrdp-Sesiones-Monitorizar.sh" >> /etc/systemd/system/xrdpMonitorSes.service
+      echo "TimeoutStartSec=0"                                                        >> /etc/systemd/system/xrdpMonitorSes.service
+      echo "StartLimitInterval=0"                                                     >> /etc/systemd/system/xrdpMonitorSes.service
+      echo "Restart=always"                                                           >> /etc/systemd/system/xrdpMonitorSes.service
+      echo ""                                                                         >> /etc/systemd/system/xrdpMonitorSes.service
+      echo "[Install]"                                                                >> /etc/systemd/system/xrdpMonitorSes.service
+      echo "WantedBy=default.target"                                                  >> /etc/systemd/system/xrdpMonitorSes.service
+
+    # Recargar cambios
+      systemctl daemon-reload
+
+    # Activar e iniciar el servicio
+      systemctl enable xrdpMonitorSes.service --now
+
+  elif [ $cVerSO == "11" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 11 (Bullseye)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 11 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 10 (Buster)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "9" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 9 (Stretch)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "8" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 8 (Jessie)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "7" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 7 (Wheezy)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  fi
 
