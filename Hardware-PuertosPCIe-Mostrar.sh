41e4c3b54dc4cf372f04d7f33b746e18de7c8816
NiPeGun <nipegun@gmail.com>
2024-07-04 00:42:53 +0200

    Update Hardware-PuertosPCIe-Mostrar.sh

--- Hardware-PuertosPCIe-Mostrar.sh
+++ Hardware-PuertosPCIe-Mostrar.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para obtener información de los puertos PCIe de una placa base
+#
+# Ejecución remota:
+#   curl -sL x | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+# ----------
+
 # Definir el array asociativo
   declare -A vPuertosPCIe
 
