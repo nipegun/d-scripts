782f3186c0d10f4a9c2c52ce574faef8963ed861
NiPeGun <nipegun@gmail.com>
2024-07-04 23:54:37 +0200

    Create LMStudio-ModelosLLM-Instalar.sh

---
+++ SoftInst/ParaCLI/LMStudio-ModelosLLM-Instalar.sh
@@ -0,0 +1,12 @@
+gemma-2-9b-it-f32.gguf
+
+
+lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF
+  Meta-Llama-3-8B-Instruct-Q8_0.gguf (9GB)
+https://huggingface.co/lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct-Q4_K_M.gguf
+
+https://huggingface.co/lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF
+https://huggingface.co/lmstudio-community/Meta-Llama-3-70B-Instruct-GGUF
+https://huggingface.co/lmstudio-community/Meta-Llama-3-120B-Instruct-GGUF
+https://huggingface.co/lmstudio-community/gemma-2-9b-it-GGUF
+https://huggingface.co/lmstudio-community/gemma-2-27b-it-GGUF
