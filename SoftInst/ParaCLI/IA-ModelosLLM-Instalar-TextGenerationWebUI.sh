439b71ecf24f1b13503188ecb44542bfac3d8ac2
NiPeGun <nipegun@gmail.com>
2024-07-05 07:46:45 +0200

    Update IA-ModelosLLM-Instalar-TextGenerationWebUI.sh

--- SoftInst/ParaCLI/IA-ModelosLLM-Instalar-TextGenerationWebUI.sh
+++ SoftInst/ParaCLI/IA-ModelosLLM-Instalar-TextGenerationWebUI.sh
@@ -1,5 +1,7 @@
 vCarpeta="/opt/text-generation-webui/models/"
 
 
-            #Use the download-model.py script to automatically download a model from Hugging Face.
-/opt/text-generation-webui/download-model.py
+            #Use the download-model.py script to automatically download a model from Hugging Face. /opt/text-generation-webui/download-model.py
+
+cd /opt/text-generation-webui/
+./download-model.py
