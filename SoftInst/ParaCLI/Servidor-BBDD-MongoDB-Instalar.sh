0d9887f43cdddb7bd7e0e41441a6103f9f1c6348
NiPeGun <nipegun@gmail.com>
2024-07-16 06:04:53 +0200

    Update Servidor-BBDD-MongoDB-Instalar.sh

--- SoftInst/ParaCLI/Servidor-BBDD-MongoDB-Instalar.sh
+++ SoftInst/ParaCLI/Servidor-BBDD-MongoDB-Instalar.sh
@@ -116,18 +116,13 @@ elif [ $cVerSO == "12" ]; then
     echo ""
     apt -y install /root/SoftInst/MongoDBCommunity/MongoDBCommunityServer.deb
 
-  # Instalar Mongo Community Shell
-    echo ""
-    echo "    Descargando e instalando Mongo Community Shell"
-    echo ""
-    curl -L https://repo.mongodb.org/apt/debian/dists/bookworm/mongodb-org/$vUltVersMainMongoDBCommunity/main/binary-amd64/mongodb-org-shell_"$vUltSubVersMongoDBCommunity"_amd64.deb -o /root/SoftInst/MongoDBCommunity/MongoDBCommunityShell.deb 
-    echo ""
-    apt -y install /root/SoftInst/MongoDBCommunity/MongoDBCommunityShell.deb
-
-  echo ""
-  echo "  Activando la autenticación de usuarios..." 
-  echo ""
-  sed -i -e 's|#security:|security:\n  authorization: enabled|g' /etc/mongod.conf
+#  # Instalar Mongo Community Shell
+#    echo ""
+#    echo "    Descargando e instalando Mongo Community Shell"
+#    echo ""
+#    curl -L https://repo.mongodb.org/apt/debian/dists/bookworm/mongodb-org/$vUltVersMainMongoDBCommunity/main/binary-amd64/mongodb-org-shell_"$vUltSubVersMongoDBCommunity"_amd64.deb -o /root/SoftInst/MongoDBCommunity/MongoDBCommunityShell.deb 
+#    echo ""
+#    apt -y install /root/SoftInst/MongoDBCommunity/MongoDBCommunityShell.deb
 
   echo ""
   echo "  Activando el acceso desde todas las IPs..." 
@@ -142,47 +137,50 @@ elif [ $cVerSO == "12" ]; then
   echo ""
   systemctl enable mongod.service --now
 
-  echo ""
-  echo "  Verificando el estado de la conexión con la base de datos..." 
-  echo ""
-  echo "  0 = Incorrecta."
-  echo "  1 = Correcta"
-  echo ""
-  mongo --quiet --eval 'db.runCommand({ connectionStatus: 1 })' | jq .ok
+#  echo ""
+#  echo "  Verificando el estado de la conexión con la base de datos..." 
+#  echo ""
+#  echo "  0 = Incorrecta."
+#  echo "  1 = Correcta"
+#  echo ""
+#  mongo --quiet --eval 'db.runCommand({ connectionStatus: 1 })' | jq .ok
 
+#  echo ""
+#  echo "  Activando la autenticación de usuarios..." 
+#  echo ""
+#  sed -i -e 's|#security:|security:\n  authorization: enabled|g' /etc/mongod.conf
 
+#  echo ""
+#  echo "  Creando el usuario con privilegios de administración..." 
+#  echo ""
+#  echo 'use admin'                            > /tmp/CrearUsuariosMongoDB.js
+#  echo ''                                    >> /tmp/CrearUsuariosMongoDB.js
+#  echo 'db.createUser({'                     >> /tmp/CrearUsuariosMongoDB.js
+#  echo '  user: "root",'                     >> /tmp/CrearUsuariosMongoDB.js
+#  echo '  pwd: "rootMongoDB",'               >> /tmp/CrearUsuariosMongoDB.js
+#  echo '  roles: ['                          >> /tmp/CrearUsuariosMongoDB.js
+#  echo '    {'                               >> /tmp/CrearUsuariosMongoDB.js
+#  echo '      role: "userAdminAnyDatabase",' >> /tmp/CrearUsuariosMongoDB.js
+#  echo '      db: "admin"'                   >> /tmp/CrearUsuariosMongoDB.js
+#  echo '    }'                               >> /tmp/CrearUsuariosMongoDB.js
+#  echo '    ,"readWriteAnyDatabase"'         >> /tmp/CrearUsuariosMongoDB.js
+#  echo '  ]'                                 >> /tmp/CrearUsuariosMongoDB.js
+#  echo '})'                                  >> /tmp/CrearUsuariosMongoDB.js
+#  mongosh --quiet < /tmp/CrearUsuariosMongoDB.js
+#  echo ""
+#  echo "  Se ha creado el usuario root con contraseña rootMySQL."
+#  echo ""
+#  echo "  Para cambiarle la contraseña lanza mongosh como root ejecuta:"
+#  echo ""
+#  echo "  use admin"
+#  echo '  db.changeUserPassword("root", passwordPrompt())'
+#  echo ""
 
-  echo ""
-  echo "  Reiniciando el servicio..." 
-  echo ""
-  systemctl daemon-reload
-  systemctl restart mongod
-
-  echo ""
-  echo "  Creando el usuario con privilegios de administración..." 
-  echo ""
-  echo 'use admin'                            > /tmp/CrearUsuariosMongoDB.js
-  echo ''                                    >> /tmp/CrearUsuariosMongoDB.js
-  echo 'db.createUser({'                     >> /tmp/CrearUsuariosMongoDB.js
-  echo '  user: "root",'                     >> /tmp/CrearUsuariosMongoDB.js
-  echo '  pwd: "rootMongoDB",'               >> /tmp/CrearUsuariosMongoDB.js
-  echo '  roles: ['                          >> /tmp/CrearUsuariosMongoDB.js
-  echo '    {'                               >> /tmp/CrearUsuariosMongoDB.js
-  echo '      role: "userAdminAnyDatabase",' >> /tmp/CrearUsuariosMongoDB.js
-  echo '      db: "admin"'                   >> /tmp/CrearUsuariosMongoDB.js
-  echo '    }'                               >> /tmp/CrearUsuariosMongoDB.js
-  echo '    ,"readWriteAnyDatabase"'         >> /tmp/CrearUsuariosMongoDB.js
-  echo '  ]'                                 >> /tmp/CrearUsuariosMongoDB.js
-  echo '})'                                  >> /tmp/CrearUsuariosMongoDB.js
-  mongosh --quiet < /tmp/CrearUsuariosMongoDB.js
-  echo ""
-  echo "  Se ha creado el usuario root con contraseña rootMySQL."
-  echo ""
-  echo "  Para cambiarle la contraseña lanza mongosh como root ejecuta:"
-  echo ""
-  echo "  use admin"
-  echo '  db.changeUserPassword("root", passwordPrompt())'
-  echo ""
+#  echo ""
+#  echo "  Reiniciando el servicio..." 
+#  echo ""
+#  systemctl daemon-reload
+#  systemctl restart mongod
 
 elif [ $cVerSO == "11" ]; then
 
@@ -302,108 +300,7 @@ elif [ $cVerSO == "10" ]; then
   echo ""
 
   echo ""
-  echo "  Determinando el número de la última versión de MongoDB..." 
-  echo ""
-  UltVersMongoDB=$(curl -sL https://repo.mongodb.org/apt/debian/dists/buster/mongodb-org/ | grep href | grep -v velopmen | grep -v esti | grep -v arent | tail -n1 | cut -d "'" -f2)
-  echo "  La última versión de MongoDB es la $UltVersMongoDB"
-
-  echo ""
-  echo "  Instalando dependencias..." 
-  echo ""
-  apt-get -y update
-  apt-get -y install ca-certificates
-  apt-get -y install curl
-  apt-get -y install jq
-
-  echo ""
-  echo "  Agregando la clave GPG..." 
-  echo ""
-  # Comprobar si el paquete gnupg está instalado. Si no lo está, instalarlo.
-     if [[ $(dpkg-query -s gnupg 2>/dev/null | grep installed) == "" ]]; then
-       echo ""
-       echo "  gnupg no está instalado. Iniciando su instalación..."       echo ""
-       apt-get -y update
-       apt-get -y install gnupg
-       echo ""
-     fi
-  curl -fsSL https://www.mongodb.org/static/pgp/server-$UltVersMongoDB.asc | apt-key add -
-  
-  echo ""
-  echo "  Agregando el repositorio..." 
-  echo ""
-  # Comprobar si el paquete lsb-release está instalado. Si no lo está, instalarlo.
-     if [[ $(dpkg-query -s lsb-release 2>/dev/null | grep installed) == "" ]]; then
-       echo ""
-       echo "  lsb-release no está instalado. Iniciando su instalación..."       echo ""
-       apt-get -y update
-       apt-get -y install lsb-release
-       echo ""
-     fi
-  VersDebian=$(lsb_release -cs)
-  echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/$UltVersMongoDB main" > /etc/apt/sources.list.d/mongodb-$UltVersMongoDB.list
-  #echo "deb http://repo.mongodb.org/apt/debian $VersDebian/mongodb-org/$UltVersMongoDB main" > /etc/apt/sources.list.d/mongodb-$UltVersMongoDB.list
-
-  echo ""
-  echo "  Instalando paquetes..." 
-  echo ""
-  apt-get -y update
-  apt-get -y install mongodb-org
-
-  echo ""
-  echo "  Activando el servicio..." 
-  echo ""
-  systemctl enable mongod --now
-
-  echo ""
-  echo "  Verificando el estado de la conexión con la base de datos..." 
-  echo ""
-  echo "  0 = Incorrecta."
-  echo "  1 = Correcta"
-  echo ""
-  mongo --quiet --eval 'db.runCommand({ connectionStatus: 1 })' | jq .ok
-
-  echo ""
-  echo "  Activando la autenticación de usuarios..." 
-  echo ""
-  sed -i -e 's|#security:|security:\n  authorization: enabled|g' /etc/mongod.conf
-
-  echo ""
-  echo "  Activando el acceso desde todas las IPs..." 
-  echo ""
-  echo "  Si no quieres esto, edita el archivo /etc/mongod.conf"
-  echo '  y cambia la línea IP en la parte que dice "bindIp: 0.0.0.0".'
-  echo ""
-  sed -i -e 's|bindIp: 127.0.0.1|bindIp: 0.0.0.0|g' /etc/mongod.conf
-
-  echo ""
-  echo "  Reiniciando el servicio..." 
-  echo ""
-  systemctl restart mongod
-
-  echo ""
-  echo "  Creando el usuario con privilegios de administración..." 
-  echo ""
-  echo 'use admin'                            > /tmp/CrearUsuariosMongoDB.js
-  echo ''                                    >> /tmp/CrearUsuariosMongoDB.js
-  echo 'db.createUser({'                     >> /tmp/CrearUsuariosMongoDB.js
-  echo '  user: "root",'                     >> /tmp/CrearUsuariosMongoDB.js
-  echo '  pwd: "rootMongoDB",'               >> /tmp/CrearUsuariosMongoDB.js
-  echo '  roles: ['                          >> /tmp/CrearUsuariosMongoDB.js
-  echo '    {'                               >> /tmp/CrearUsuariosMongoDB.js
-  echo '      role: "userAdminAnyDatabase",' >> /tmp/CrearUsuariosMongoDB.js
-  echo '      db: "admin"'                   >> /tmp/CrearUsuariosMongoDB.js
-  echo '    }'                               >> /tmp/CrearUsuariosMongoDB.js
-  echo '    ,"readWriteAnyDatabase"'         >> /tmp/CrearUsuariosMongoDB.js
-  echo '  ]'                                 >> /tmp/CrearUsuariosMongoDB.js
-  echo '})'                                  >> /tmp/CrearUsuariosMongoDB.js
-  mongosh --quiet < /tmp/CrearUsuariosMongoDB.js
-  echo ""
-  echo "  Se ha creado el usuario root con contraseña rootMySQL."
-  echo ""
-  echo "  Para cambiarle la contraseña lanza mongosh como root ejecuta:"
-  echo ""
-  echo "  use admin"
-  echo '  db.changeUserPassword("root", passwordPrompt())'
+  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
   echo ""
 
 elif [ $cVerSO == "9" ]; then
