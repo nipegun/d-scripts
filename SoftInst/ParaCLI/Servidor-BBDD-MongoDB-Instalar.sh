0d142792a61c282f18ea662af32431a8596969d2
NiPeGun <nipegun@gmail.com>
2024-07-16 03:58:48 +0200

    Update Servidor-BBDD-MongoDB-Instalar.sh

--- SoftInst/ParaCLI/Servidor-BBDD-MongoDB-Instalar.sh
+++ SoftInst/ParaCLI/Servidor-BBDD-MongoDB-Instalar.sh
@@ -9,12 +9,26 @@
 # Script de NiPeGun para instalar el servidor de bases MongoDB en Debian
 #
 # Ejecución remota
-# curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-BBDD-MongoDB-Instalar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Servidor-BBDD-MongoDB-Instalar.sh | bash
 # ----------
 
-cColorRojo='\033[1;31m'
-cColorVerde='\033[1;32m'
-cFinColor='\033[0m'
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Comprobar si el script está corriendo como root
+  #if [ $(id -u) -ne 0 ]; then     # Sólo comprueba si es root
+  if [[ $EUID -ne 0 ]]; then       # Comprueba si es root o sudo
+    echo ""
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse con privilegios de administrador (como root o con sudo).${cFinColor}"
+    echo ""
+    exit
+  fi
 
 # Determinar la versión de Debian
   if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
@@ -36,40 +50,20 @@ cFinColor='\033[0m'
     cVerSO=$(uname -r)
   fi
 
-if [ $cVerSO == "7" ]; then
+elif [ $cVerSO == "13" ]; then
 
   echo ""
-  echo "  Iniciando el script de instalación del servidor MongoDB para Debian 7 (Wheezy)..."
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "8" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación del servidor MongoDB para Debian 8 (Jessie)..."
+  echo "  Iniciando el script de instalación del servidor MongoDB para Debian 13 (x)..." 
   echo ""
 
   echo ""
-  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo "  Comandos para Debian 13 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
   echo ""
 
-elif [ $cVerSO == "9" ]; then
+elif [ $cVerSO == "12" ]; then
 
   echo ""
-  echo "  Iniciando el script de instalación del servidor MongoDB para Debian 9 (Stretch)..."
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "10" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación del servidor MongoDB para Debian 10 (Buster)..."
+  echo "  Iniciando el script de instalación del servidor MongoDB para Debian 12 (Bookworm)..." 
   echo ""
 
   echo ""
@@ -286,6 +280,147 @@ elif [ $cVerSO == "11" ]; then
   echo ""
   echo "  use admin"
   echo '  db.changeUserPassword("root", passwordPrompt())'
+  echo ""
+
+elif [ $cVerSO == "10" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación del servidor MongoDB para Debian 10 (Buster)..."
+  echo ""
+
+  echo ""
+  echo "  Determinando el número de la última versión de MongoDB..." 
+  echo ""
+  UltVersMongoDB=$(curl -sL https://repo.mongodb.org/apt/debian/dists/buster/mongodb-org/ | grep href | grep -v velopmen | grep -v esti | grep -v arent | tail -n1 | cut -d "'" -f2)
+  echo "  La última versión de MongoDB es la $UltVersMongoDB"
+
+  echo ""
+  echo "  Instalando dependencias..." 
+  echo ""
+  apt-get -y update
+  apt-get -y install ca-certificates
+  apt-get -y install curl
+  apt-get -y install jq
+
+  echo ""
+  echo "  Agregando la clave GPG..." 
+  echo ""
+  # Comprobar si el paquete gnupg está instalado. Si no lo está, instalarlo.
+     if [[ $(dpkg-query -s gnupg 2>/dev/null | grep installed) == "" ]]; then
+       echo ""
+       echo "  gnupg no está instalado. Iniciando su instalación..."       echo ""
+       apt-get -y update
+       apt-get -y install gnupg
+       echo ""
+     fi
+  curl -fsSL https://www.mongodb.org/static/pgp/server-$UltVersMongoDB.asc | apt-key add -
+  
+  echo ""
+  echo "  Agregando el repositorio..." 
+  echo ""
+  # Comprobar si el paquete lsb-release está instalado. Si no lo está, instalarlo.
+     if [[ $(dpkg-query -s lsb-release 2>/dev/null | grep installed) == "" ]]; then
+       echo ""
+       echo "  lsb-release no está instalado. Iniciando su instalación..."       echo ""
+       apt-get -y update
+       apt-get -y install lsb-release
+       echo ""
+     fi
+  VersDebian=$(lsb_release -cs)
+  echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/$UltVersMongoDB main" > /etc/apt/sources.list.d/mongodb-$UltVersMongoDB.list
+  #echo "deb http://repo.mongodb.org/apt/debian $VersDebian/mongodb-org/$UltVersMongoDB main" > /etc/apt/sources.list.d/mongodb-$UltVersMongoDB.list
+
+  echo ""
+  echo "  Instalando paquetes..." 
+  echo ""
+  apt-get -y update
+  apt-get -y install mongodb-org
+
+  echo ""
+  echo "  Activando el servicio..." 
+  echo ""
+  systemctl enable mongod --now
+
+  echo ""
+  echo "  Verificando el estado de la conexión con la base de datos..." 
+  echo ""
+  echo "  0 = Incorrecta."
+  echo "  1 = Correcta"
+  echo ""
+  mongo --quiet --eval 'db.runCommand({ connectionStatus: 1 })' | jq .ok
+
+  echo ""
+  echo "  Activando la autenticación de usuarios..." 
+  echo ""
+  sed -i -e 's|#security:|security:\n  authorization: enabled|g' /etc/mongod.conf
+
+  echo ""
+  echo "  Activando el acceso desde todas las IPs..." 
+  echo ""
+  echo "  Si no quieres esto, edita el archivo /etc/mongod.conf"
+  echo '  y cambia la línea IP en la parte que dice "bindIp: 0.0.0.0".'
+  echo ""
+  sed -i -e 's|bindIp: 127.0.0.1|bindIp: 0.0.0.0|g' /etc/mongod.conf
+
+  echo ""
+  echo "  Reiniciando el servicio..." 
+  echo ""
+  systemctl restart mongod
+
+  echo ""
+  echo "  Creando el usuario con privilegios de administración..." 
+  echo ""
+  echo 'use admin'                            > /tmp/CrearUsuariosMongoDB.js
+  echo ''                                    >> /tmp/CrearUsuariosMongoDB.js
+  echo 'db.createUser({'                     >> /tmp/CrearUsuariosMongoDB.js
+  echo '  user: "root",'                     >> /tmp/CrearUsuariosMongoDB.js
+  echo '  pwd: "rootMongoDB",'               >> /tmp/CrearUsuariosMongoDB.js
+  echo '  roles: ['                          >> /tmp/CrearUsuariosMongoDB.js
+  echo '    {'                               >> /tmp/CrearUsuariosMongoDB.js
+  echo '      role: "userAdminAnyDatabase",' >> /tmp/CrearUsuariosMongoDB.js
+  echo '      db: "admin"'                   >> /tmp/CrearUsuariosMongoDB.js
+  echo '    }'                               >> /tmp/CrearUsuariosMongoDB.js
+  echo '    ,"readWriteAnyDatabase"'         >> /tmp/CrearUsuariosMongoDB.js
+  echo '  ]'                                 >> /tmp/CrearUsuariosMongoDB.js
+  echo '})'                                  >> /tmp/CrearUsuariosMongoDB.js
+  mongosh --quiet < /tmp/CrearUsuariosMongoDB.js
+  echo ""
+  echo "  Se ha creado el usuario root con contraseña rootMySQL."
+  echo ""
+  echo "  Para cambiarle la contraseña lanza mongosh como root ejecuta:"
+  echo ""
+  echo "  use admin"
+  echo '  db.changeUserPassword("root", passwordPrompt())'
+  echo ""
+
+elif [ $cVerSO == "9" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación del servidor MongoDB para Debian 9 (Stretch)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "8" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación del servidor MongoDB para Debian 8 (Jessie)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "7" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación del servidor MongoDB para Debian 7 (Wheezy)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
   echo ""
 
 fi
