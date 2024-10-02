3cf8d960a94c52f71385635a8eae25d469613c8c
NiPeGun <nipegun@gmail.com>
2024-10-02 09:40:27 +0200

    Create IA-ModelosLLM-Instalar-Ollama-Phi.sh

---
+++ SoftInst/ParaCLI/IA-ModelosLLM-Instalar-Ollama-Phi.sh
@@ -0,0 +1,7 @@
+     43 "phi3 3.8b-mini-128k-instruct-q4_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     44 "phi3 3.8b-mini-128k-instruct-q8_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     45 "phi3 3.8b-mini-128k-instruct-fp16 (x,x GB en disco) (x,x GB en VRAM)" off
+
+     46 "phi3 14b-medium-128k-instruct-q4_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     47 "phi3 14b-medium-128k-instruct-q8_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     48 "phi3 14b-medium-128k-instruct-fp16 (x,x GB en disco) (x,x GB en VRAM)" off
