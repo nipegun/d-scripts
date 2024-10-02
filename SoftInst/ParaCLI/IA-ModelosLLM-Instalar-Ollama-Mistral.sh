1693ac51917f8fe0d15966b41a08cfe1f41cb0a7
NiPeGun <nipegun@gmail.com>
2024-10-02 09:40:30 +0200

    Create IA-ModelosLLM-Instalar-Ollama-Mistral.sh

---
+++ SoftInst/ParaCLI/IA-ModelosLLM-Instalar-Ollama-Mistral.sh
@@ -0,0 +1,3 @@
+     40 "mistral 7b-instruct-q4_0 ( 4,2 GB en disco) ( 7,2 GB en VRAM)" off
+     41 "mistral 7b-instruct-q8_0 ( 7,8 GB en disco) (15,0 GB en VRAM)" off
+     42 "mistral 7b-instruct-fp16 (14,2 GB en disco) ( x,x GB en VRAM)?" off
