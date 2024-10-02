d0fa66b266d9b8b4902018e9b340091bfc363b63
NiPeGun <nipegun@gmail.com>
2024-10-02 09:40:21 +0200

    Create IA-ModelosLLM-Instalar-Ollama-Gemma.sh

---
+++ SoftInst/ParaCLI/IA-ModelosLLM-Instalar-Ollama-Gemma.sh
@@ -0,0 +1,8 @@
+     
+     55 "gemma2 9b-instruct-q4_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     56 "gemma2 9b-instruct-q8_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     57 "gemma2 9b-instruct-fp16 (x,x GB en disco) (x,x GB en VRAM)" off
+     
+     58 "gemma2 27b-instruct-q4_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     59 "gemma2 27b-instruct-q8_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     60 "gemma2 27b-instruct-fp16 (x,x GB en disco) (x,x GB en VRAM)" off
