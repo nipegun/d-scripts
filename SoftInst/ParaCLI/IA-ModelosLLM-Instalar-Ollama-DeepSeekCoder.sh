fc85d1ccf3d667a69e632d926f311eef724d4a28
NiPeGun <nipegun@gmail.com>
2024-10-02 09:40:24 +0200

    Create IA-ModelosLLM-Instalar-Ollama-DeepSeekCoder.sh

---
+++ SoftInst/ParaCLI/IA-ModelosLLM-Instalar-Ollama-DeepSeekCoder.sh
@@ -0,0 +1,7 @@
+     49 "deepseek-coder-v2 16b-lite-instruct-q4_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     50 "deepseek-coder-v2 16b-lite-instruct-q8_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     51 "deepseek-coder-v2 16b-lite-instruct-fp16 (x,x GB en disco) (x,x GB en VRAM)" off
+     
+     52 "deepseek-coder-v2 236b-instruct-q4_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     53 "deepseek-coder-v2 236b-instruct-q8_0 (x,x GB en disco) (x,x GB en VRAM)" off
+     54 "deepseek-coder-v2 236b-instruct-fp16 (x,x GB en disco) (x,x GB en VRAM)" off
