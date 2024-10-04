99ce690b2a5546576724b8a63e343cf814dfd645
NiPeGun <nipegun@gmail.com>
2024-10-04 21:29:34 +0200

    Update Spotify-InstalarYConfigurar.sh

--- SoftInst/ParaGUI/Spotify-InstalarYConfigurar.sh
+++ SoftInst/ParaGUI/Spotify-InstalarYConfigurar.sh
@@ -18,6 +18,29 @@
 #  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/Spotify-InstalarYConfigurar.sh | bash -s Parámetro1 Parámetro2
 # ----------
 
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
+#
+# Ejecución remota:
+#   curl -sL x | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
 # Definir constantes de color
   cColorAzul="\033[0;34m"
   cColorAzulClaro="\033[1;34m"
@@ -27,64 +50,148 @@
     #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
   cFinColor='\033[0m'
 
-# Notificar el inicio de la ejecución del script
-  echo ""
-  echo -e "${cColorAzulClaro}  Iniciando el script de instalación de Spotify en Debian...${cFinColor}"
-  echo ""
-
 # Comprobar si el script está corriendo como root
-  if [ $(id -u) -ne 0 ]; then
+  #if [ $(id -u) -ne 0 ]; then     # Sólo comprueba si es root
+  if [[ $EUID -ne 0 ]]; then       # Comprueba si es root o sudo
     echo ""
-    echo -e "${cColorRojo}    Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${cFinColor}"
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse con privilegios de administrador (como root o con sudo).${cFinColor}"
     echo ""
     exit
   fi
 
-# Agregar el repositorio
-  echo ''
-  echo '    Agregando el repositorio...'
-  echo ''
-  echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list.d/spotify.list
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
+    . /etc/os-release
+    cNomSO=$NAME
+    cVerSO=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # Para linuxbase.org.
+    cNomSO=$(lsb_release -si)
+    cVerSO=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release.
+    . /etc/lsb-release
+    cNomSO=$DISTRIB_ID
+    cVerSO=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    cNomSO=Debian
+    cVerSO=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD).
+    cNomSO=$(uname -s)
+    cVerSO=$(uname -r)
+  fi
 
-# Agregar la clave para firmar el repositorio
-  echo ''
-  echo '    Agregando la clave para firmar el repositorio...'
-  echo ''
-  # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
-    if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
-      echo ""
-      echo -e "${cColorRojo}      El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
-      echo ""
+# Ejecutar comandos dependiendo de la versión de Debian detectada
+
+  if [ $cVerSO == "13" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 13 (x)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "12" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 12 (Bookworm)...${cFinColor}"
+    echo ""
+
+    # Agregar el repositorio
+      echo ''
+      echo '    Agregando el repositorio...'
+      echo ''
+      echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list.d/spotify.list
+
+    # Agregar la clave para firmar el repositorio
+      echo ''
+      echo '    Agregando la clave para firmar el repositorio...'
+      echo ''
+      # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+        if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+          echo ""
+          echo -e "${cColorRojo}      El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+          echo ""
+          apt-get -y update && apt-get -y install curl
+          echo ""
+        fi
+      curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg >> /tmp/Spotify.gpg
+
+    # Convertir la clave al formato apt
+      echo ''
+      echo '    Convirtiendo la clave al formato apt...'
+      echo ''
+      # Comprobar si el paquete gpg está instalado. Si no lo está, instalarlo.
+        if [[ $(dpkg-query -s gpg 2>/dev/null | grep installed) == "" ]]; then
+          echo ""
+          echo -e "${cColorRojo}      El paquete gpg no está instalado. Iniciando su instalación...${cFinColor}"
+          echo ""
+          apt-get -y update && apt-get -y install gpg
+          echo ""
+        fi
+      gpg --yes -o /etc/apt/trusted.gpg.d/spotify.gpg --dearmor /tmp/Spotify.gpg
+
+    # Actualizar la lista de los paquetes disponibles en los repositorios instalados
+      echo ''
+      echo '    Actualizando la lista de los paquetes disponibles en los repositorios instalados...'
+      echo ''
       apt-get -y update
-      apt-get -y install curl
-      echo ""
-    fi
-    curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg >> /tmp/Spotify.gpg
 
-# Convertir la clave al formato apt
-  echo ''
-  echo '    Convirtiendo la clave al formato apt...'
-  echo ''
-  # Comprobar si el paquete gpg está instalado. Si no lo está, instalarlo.
-    if [[ $(dpkg-query -s gpg 2>/dev/null | grep installed) == "" ]]; then
-      echo ""
-      echo -e "${cColorRojo}      El paquete gpg no está instalado. Iniciando su instalación...${cFinColor}"
-      echo ""
-      apt-get -y update
-      apt-get -y install gpg
-      echo ""
-    fi
-  gpg --yes -o /etc/apt/trusted.gpg.d/spotify.gpg --dearmor /tmp/Spotify.gpg
+    # Instalar el paquete
+      echo ''
+      echo '    Instalando el paquete...'
+      echo ''
+      apt-get -y install spotify-client
 
-# Actualizar la lista de los paquetes disponibles en los repositorios instalados
-  echo ''
-  echo '    Actualizando la lista de los paquetes disponibles en los repositorios instalados...'
-  echo ''
-  apt-get -y update
+  elif [ $cVerSO == "11" ]; then
 
-# Instalar el paquete
-  echo ''
-  echo '    Instalando el paquete...'
-  echo ''
-  apt-get -y install spotify-client
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 11 (Bullseye)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 11 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 10 (Buster)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "9" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 9 (Stretch)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "8" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 8 (Jessie)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "7" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Debian 7 (Wheezy)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  fi
 
