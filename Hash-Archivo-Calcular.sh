e0e69061ef7bd691fd320f8fae5c1123fc03b7d4
NiPeGun <nipegun@gmail.com>
2024-10-12 19:33:11 +0200

    Update Hash-Archivo-Calcular.sh

--- Hash-Archivo-Calcular.sh
+++ Hash-Archivo-Calcular.sh
@@ -5,22 +5,11 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-# Pongo a disposición pública este script bajo el término de "software de dominio público".
-# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
-# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
-# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
-
 # ----------
 # Script de NiPeGun para obtener el hash de un archivo con diferentes algoritmos:
 #
-# Ejecución remota:
-#   curl -sL x | bash
-#
-# Ejecución remota sin caché:
-#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
-#
 # Ejecución remota con parámetros:
-#   curl -sL x | bash -s Parámetro1 Parámetro2
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/Hash-Archivo-Calcular.sh | bash -s RutaAlArchivo
 #
 # Bajar y editar directamente el archivo en nano
 #   curl -sL x | nano -
@@ -49,8 +38,6 @@ if [ $# -ne $cCantArgumEsperados ]
     echo ""
     exit
   else
-  # Definir fecha de ejecución del script
-    cFechaDeEjec=$(date +a%Ym%md%d@%T)
 
   # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
     if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
