2f6e0f8c1cdf66673fa87431fb1a1902a3bd707d
NiPeGun <nipegun@gmail.com>
2024-10-14 23:51:02 +0200

    Create Gnome-Automontaje-Activar.sh

---
+++ Gnome-Automontaje-Activar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para activar el auto-montaje de unidades en Gnome
+#
+# Ejecución remota:
+#   curl -sL x | bash
+# ----------
+
+gsettings set org.gnome.desktop.media-handling automount true
+
