ff0d92e8aea9e4a0d67197b8ea2dde427356dfc0
NiPeGun <nipegun@gmail.com>
2024-10-15 00:33:36 +0200

    Create Herramientas-Forense-Instalar.sh

---
+++ SoftInst/ParaGUI/Herramientas-Forense-Instalar.sh
@@ -0,0 +1,10 @@
+
+# dd
+  apt-get -y install coreutils
+# AFF (Advanced Forensic Format)
+  apt-get -y install afflib-tools
+# EWF (Expert Witness Format)
+  apt-get -y install libewf-tools
+
+# Desactivar el automontaje de unidades de Gnome
+  gsettings set org.gnome.desktop.media-handling automount false
