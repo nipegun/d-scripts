368b265d49955a96503b6dca31cf7ae73b6bd295
NiPeGun <nipegun@gmail.com>
2024-10-15 02:14:39 +0200

    Update Herramientas-Forense-Instalar.sh

--- SoftInst/ParaGUI/Herramientas-Forense-Instalar.sh
+++ SoftInst/ParaGUI/Herramientas-Forense-Instalar.sh
@@ -1,13 +1,16 @@
-
-# dd
-  apt-get -y install coreutils
-# AFF (Advanced Forensic Format)
-  apt-get -y install afflib-tools
-# EWF (Expert Witness Format)
-  apt-get -y install libewf-tools
-# dc3dd
-  apt-get -y install dc3dd
-
+# Herramientas de terminal
+  # dd
+    apt-get -y install coreutils
+  # AFF (Advanced Forensic Format)
+    apt-get -y install afflib-tools
+  # EWF (Expert Witness Format)
+    apt-get -y install libewf-tools
+  # dc3dd
+    apt-get -y install dc3dd
+# Herramientas gráficas
+  # guymager
+    apt-get -y install guymager
+  # 
 
 # Desactivar el automontaje de unidades de Gnome
   gsettings set org.gnome.desktop.media-handling automount false
