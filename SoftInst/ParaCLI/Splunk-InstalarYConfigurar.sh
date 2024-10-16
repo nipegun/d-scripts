76123b2fce70a2e2c46d7782925d821c2fdc4279
NiPeGun <nipegun@gmail.com>
2024-10-16 11:45:30 +0200

    Update Splunk-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Splunk-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Splunk-InstalarYConfigurar.sh
@@ -121,7 +121,7 @@
                   echo ""
                 fi
               wget "https://download.splunk.com/products/splunk/releases/9.3.1/linux/splunk-9.3.1-0b8d769cb912-linux-2.6-amd64.deb" -O /root/SoftInst/Splunk/splunk.deb
-              apt install splunk.deb
+              apt install /root/SoftInst/Splunk/splunk.deb
               # Iniciar y aceptar licencia
                 /opt/splunk/bin/splunk start --accept-license
               # Hacer que se auto-inicie
