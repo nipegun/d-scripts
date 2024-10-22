cc010803fa6a25049b587862d00d1f30680b7c4d
NiPeGun <nipegun@gmail.com>
2024-10-22 08:23:55 +0200

    Create Splunk-DesinstalarYPurgar.sh

---
+++ SoftInst/ParaCLI/Splunk-DesinstalarYPurgar.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para desinstalar y purgar Splunk
+#
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Splunk-InstalarYConfigurar.sh | bash
+# ----------
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Comprobar si el script está corriendo como root
+  #if [ $(id -u) -ne 0 ]; then     # Sólo comprueba si es root
+  if [[ $EUID -ne 0 ]]; then       # Comprueba si es root o sudo
+    echo ""
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse con privilegios de administrador (como root o con sudo).${cFinColor}"
+    echo ""
+    exit
+  fi
+
+# Notificar inicio de ejecución del script
+  echo ""
+  echo -e "${cColorAzulClaro}  Iniciando el script de instalación de splunk para Debian 13 (x)...${cFinColor}"
+  echo ""
+
+#
+  /opt/splunk/bin/splunk stop
+  rm -rf /opt/splunk
+  rm -rf /var/log/splunk
+  rm -rf /etc/splunk
+  deluser splunk
+  delgroup splunk
+  /etc/systemd/system/splunk.service
+  systemctl daemon-reload
+  find / -name "*splunk*"
+  apt purge splunk
