2166efbe75d731f53fe400f593f7fd666ba05d2a
NiPeGun <nipegun@gmail.com>
2024-10-24 19:54:06 +0200

    Update JDownloader-InstalarYConfigurar.sh

--- SoftInst/ParaGUI/JDownloader-InstalarYConfigurar.sh
+++ SoftInst/ParaGUI/JDownloader-InstalarYConfigurar.sh
@@ -9,16 +9,10 @@
 # Script de NiPeGun para instalar y configurar jDownloader en Debian
 #
 # Ejecución remota:
-#   curl -sL x | bash
-#
-# Ejecución remota sin caché:
-#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
-#
-# Ejecución remota con parámetros:
-#   curl -sL x | bash -s Parámetro1 Parámetro2
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaGUI/JDownloader-InstalarYConfigurar.sh | bash
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL x | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaGUI/JDownloader-InstalarYConfigurar.sh | nano -
 # ----------
 
 # Definir constantes de color
