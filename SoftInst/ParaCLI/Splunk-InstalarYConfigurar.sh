8ee5220f62d1d1f916a1adb8ac3e1a9a3777d29f
NiPeGun <nipegun@gmail.com>
2024-11-02 20:45:27 +0100

    Update Splunk-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Splunk-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Splunk-InstalarYConfigurar.sh
@@ -141,8 +141,8 @@
                 echo ""
                 echo "    Para configurar la escucha:"
                 echo "      Acceder a la interfaz web y loguearse con la cuenta creada."
-                echo "      Luego ir a Settings >> Add Data >> Forwarding and receiving"
-                echo '      En "Configure receiving" hacer clñick en "Add new".'
+                echo '      Luego ir a "Settings" >> "Add Data" >> "Forwarding and receiving".'
+                echo '      En "Configure receiving" hacer click en "Add new".'
                 echo '      Indicad, por ejemplo, el puerto 9997 y Salvar.'
                 echo ''
 
