7590fc28a9e3fbc637faaae4485a78fe7a5b365d
NiPeGun <nipegun@gmail.com>
2024-11-09 11:30:33 +0100

    Update Hardware-Discos-SATA-Temperatura-Mostrar.sh

--- Hardware-Discos-SATA-Temperatura-Mostrar.sh
+++ Hardware-Discos-SATA-Temperatura-Mostrar.sh
@@ -6,50 +6,160 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para mostrar la temperatura de los discos duros SATA
+# Script de NiPeGun para mostrar la temperatura de los discos duros SATA en Debian
 #
 # Ejecución remota:
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/Hardware-Discos-SATA-Temperatura.sh | bash
+#   curl -sL x | bash
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
 # ----------
 
-cColorVerde="\033[1;32m"
-cFinColor="\033[0m"
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
 
-echo ""
-echo -e "${cColorVerde}  Mostrando temperatura de los discos duros SATA...${cFinColor}"
-echo ""
+# Comprobar si el script está corriendo como root
+  #if [ $(id -u) -ne 0 ]; then     # Sólo comprueba si es root
+  if [[ $EUID -ne 0 ]]; then       # Comprueba si es root o sudo
+    echo ""
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse con privilegios de administrador (como root o con sudo).${cFinColor}"
+    echo ""
+    exit
+  fi
 
-# Obtener la cantidad de discos SATA que hay instalados en el sistema
-   for LetraDiscoSATA in {a..x}
-     do
-       if [[ -d "/sys/block/sd$LetraDiscoSATA" ]]; then
-         # Agregar la unidad al último índice libre en el array
-            DiscoActual=$(echo "sd$LetraDiscoSATA")
-            ArrayDiscosSATA+=("${DiscoActual}")
+# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    apt-get -y update && apt-get -y install curl
+    echo ""
+  fi
+
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
+    . /etc/os-release
+    cNomSO=$NAME
+    cVerSO=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # Para linuxbase.org.
+    cNomSO=$(lsb_release -si)
+    cVerSO=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release.
+    . /etc/lsb-release
+    cNomSO=$DISTRIB_ID
+    cVerSO=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    cNomSO=Debian
+    cVerSO=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD).
+    cNomSO=$(uname -s)
+    cVerSO=$(uname -r)
+  fi
+
+# Ejecutar comandos dependiendo de la versión de Debian detectada
+
+  if [ $cVerSO == "13" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script que muestra la temperatura de los discos duros SATA en Debian 13 (x)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "12" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script que muestra la temperatura de los discos duros SATA en Debian 12 (Bookworm)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 12 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "11" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script que muestra la temperatura de los discos duros SATA en Debian 11 (Bullseye)...${cFinColor}"
+    echo ""
+
+    # Obtener la cantidad de discos SATA que hay instalados en el sistema
+       for LetraDiscoSATA in {a..x}
+         do
+           if [[ -d "/sys/block/sd$LetraDiscoSATA" ]]; then
+             # Agregar la unidad al último índice libre en el array
+                DiscoActual=$(echo "sd$LetraDiscoSATA")
+                ArrayDiscosSATA+=("${DiscoActual}")
+           fi
+         done
+
+    echo ""
+    echo "  Total de discos SATA instalados en el sistema: ${#ArrayDiscosSATA[@]}"
+    echo ""
+
+    # Comprobar si el paquete hddtemp está instalado. Si no lo está, instalarlo.
+       if [[ $(dpkg-query -s hddtemp 2>/dev/null | grep installed) == "" ]]; then
+         echo ""
+         echo "  hddtemp no está instalado. Iniciando su instalación..."
+         echo ""
+         apt-get -y update && apt-get -y install hddtemp
+         echo ""
        fi
-     done
 
-echo ""
-echo "  Total de discos SATA instalados en el sistema: ${#ArrayDiscosSATA[@]}"
-echo ""
+    # Mostrar la temperatura de cada disco detectado
+       for DiscoSATA in "${ArrayDiscosSATA[@]}"
+         do
+           #echo ""
+           #echo "    Disco /dev/$DiscoSATA"
+           #echo ""
+           hddtemp /dev/$DiscoSATA
+         done
 
-# Comprobar si el paquete hddtemp está instalado. Si no lo está, instalarlo.
-   if [[ $(dpkg-query -s hddtemp 2>/dev/null | grep installed) == "" ]]; then
-     echo ""
-     echo "  hddtemp no está instalado. Iniciando su instalación..."     echo ""
-     apt-get -y update
-     apt-get -y install hddtemp
-     echo ""
-   fi
+  elif [ $cVerSO == "10" ]; then
 
-# Mostrar la temperatura de cada disco detectado
-   for DiscoSATA in "${ArrayDiscosSATA[@]}"
-     do
-       #echo ""
-       #echo "    Disco /dev/$DiscoSATA"
-       #echo ""
-       hddtemp /dev/$DiscoSATA
-     done
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script que muestra la temperatura de los discos duros SATA en Debian 10 (Buster)...${cFinColor}"
+    echo ""
 
-echo ""
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
 
+  elif [ $cVerSO == "9" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script que muestra la temperatura de los discos duros SATA en Debian (Stretch)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "8" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script que muestra la temperatura de los discos duros SATA en Debian 8 (Jessie)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "7" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script que muestra la temperatura de los discos duros SATA en Debian 7 (Wheezy)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  fi
