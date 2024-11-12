7c2c75ebce9007d32967734d668a2c4fc226f704
NiPeGun <nipegun@gmail.com>
2024-11-12 18:54:17 +0100

    Update XMLCopyEditor-Instalar.sh

--- SoftInst/ParaGUI/XMLCopyEditor-Instalar.sh
+++ SoftInst/ParaGUI/XMLCopyEditor-Instalar.sh
@@ -8,9 +8,36 @@
 # ----------
 # Script de NiPeGun para instalar y configurar XMLCopyEditor en Debian
 #
-# Ejecución remota:
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/XMLCopyEditor-Instalar.sh | bash
+# Ejecución remota con sudo:
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaGUI/XMLCopyEditor-Instalar.sh | sudo bash
 # ----------
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Comprobar si el script está corriendo como root
+  #if [ $(id -u) -ne 0 ]; then     # Sólo comprueba si es root
+  if [[ $EUID -ne 0 ]]; then       # Comprueba si es root o sudo
+    echo ""
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse con privilegios de administrador (como root o con sudo).${cFinColor}"
+    echo ""
+    exit
+  fi
+
+# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    apt-get -y update && apt-get -y install curl
+    echo ""
+  fi
 
 # Determinar la versión de Debian
   if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
@@ -32,45 +59,29 @@
     cVerSO=$(uname -r)
   fi
 
-if [ $cVerSO == "7" ]; then
+if [ $cVerSO == "13" ]; then
 
   echo ""
-  echo "  Iniciando el script de instalación de XMLCopyEditor para Debian 7 (Wheezy)..."
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "8" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de jDownloader para Debian 8 (Jessie)..."  
+  echo "  Iniciando el script de instalación de XMLCopyEditor para Debian 13 (x)..."
   echo ""
 
   echo ""
-  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo "  Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
   echo ""
 
-elif [ $cVerSO == "9" ]; then
+elif [ $cVerSO == "12" ]; then
 
   echo ""
-  echo "  Iniciando el script de instalación de XMLCopyEditor para Debian 9 (Stretch)..."
+  echo -e "${cColorAzulClaro}  Iniciando el script de instalación de XMLCopyEditor para Debian 12 (Bookworm)...${cFinColor}"
   echo ""
 
-  echo ""
-  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
+  # Descargar paquete
+    vURLArchivo=$(curl -sL https://xml-copy-editor.sourceforge.io/ | grep .deb | grep href | cut -d'"' -f2 | head -n1)
+    mkdir-p /root/SoftInst/XMLCopyEditor/ 2> /dev/null
+    curl -sL -L $vURLArchivo --output /root/SoftInst/XMLCopyEditor/XMLCopyEditor.deb
 
-elif [ $cVerSO == "10" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de XMLCopyEditor para Debian 10 (Buster)..."
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
+  # Instalar paquete
+    apt -y install -i /root/SoftInst/XMLCopyEditor/XMLCopyEditor.deb
 
 elif [ $cVerSO == "11" ]; then
 
@@ -95,6 +106,46 @@ elif [ $cVerSO == "11" ]; then
     mkdir-p /root/SoftInst/XMLCopyEditor/ 2> /dev/null
     curl -sL -L $URLArchivo --output /root/SoftInst/XMLCopyEditor/XMLCopyEditor.deb
     dpkg -i /root/SoftInst/XMLCopyEditor/XMLCopyEditor.deb
+
+elif [ $cVerSO == "10" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de XMLCopyEditor para Debian 10 (Buster)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "9" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de XMLCopyEditor para Debian 9 (Stretch)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "8" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de jDownloader para Debian 8 (Jessie)..."  
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "7" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de XMLCopyEditor para Debian 7 (Wheezy)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
+  echo ""
 
 fi
 
