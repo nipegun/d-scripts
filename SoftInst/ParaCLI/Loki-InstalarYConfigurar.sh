0e865c46f419e7b18cb1a4284f280b082542a0d0
NiPeGun <nipegun@gmail.com>
2024-12-10 17:30:35 +0100

    Update Loki-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Loki-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Loki-InstalarYConfigurar.sh
@@ -66,9 +66,107 @@ elif [ $cVerSO == "12" ]; then
   echo "  Iniciando el script de instalación de Loki para Debian 12 (Bullseye)..."  
   echo ""
 
-  apt-get -y update
-  apt-get -y install loki
-  apt-get -y install promtail
+  # Actualizar la lista de paquetes disponibles en los repositorios
+    apt-get -y update
+
+  # Determinar la última versión de loki
+    echo ""
+    echo "    Determinando la última versión de Loki disponible en github"
+    echo ""
+    # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo -e "${cColorRojo}      El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+        echo ""
+        apt-get -y update
+        apt-get -y install curl
+        echo ""
+      fi
+    vUltVersLokiGitHub=$(curl -sL https://github.com/grafana/loki/releases/latest/ | grep -oP 'href="/grafana/loki/releases/tag/\K[^"]+' | head -n1 | cut -d'v' -f2)
+    echo "  La última versión es la $vUltVersLokiGitHub"
+    echo ""
+
+  # Descargar el paquete con la última versión
+    echo ""
+    echo "    Descargando el paquete con la última versión..."
+    echo ""
+    mkdir -p /root/SoftInst/Loki/
+    curl -L https://github.com/grafana/loki/releases/download/v$vUltVersLokiGitHub/loki-linux-amd64.zip -o /root/SoftInst/Loki/loki.zip
+
+  # Descomprimir el paquete
+    echo ""
+    echo "    Descomprimiendo el paquete..."
+    echo ""
+    cd /root/SoftInst/Loki/
+    # Comprobar si el paquete unzip está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s unzip 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo -e "${cColorRojo}      El paquete unzip no está instalado. Iniciando su instalación...${cFinColor}"
+        echo ""
+        apt-get -y update
+        apt-get -y install unzip
+        echo ""
+      fi
+    unzip loki.zip
+
+  # Copiar el binario a /bin/
+    echo ""
+    echo "  Copiando el binario a /usr/bin"
+    echo ""
+    cp -fv /root/SoftInst/Loki/loki-linux-amd64 /usr/bin/
+
+  # Crear el usuario sin privilegios para utilizar loki
+    useradd --no-create-home --shell /bin/false loki
+    mkdir -p /etc/loki
+    chown -R loki:loki /etc/loki
+    mkdir -p /var/lib/loki
+    chown -R loki:loki /var/lib/loki
+
+  # Crear el archivo de configuración
+    echo ""
+    echo "    Creando el archivo de configuración..."
+    echo ""
+    curl -L https://raw.githubusercontent.com/grafana/loki/refs/heads/main/cmd/loki/loki-local-config.yaml -o /etc/loki/loki-config.yaml
+    cp /etc/loki/loki-config.yaml /etc/loki/loki-config.yaml.bak.ori
+    # Reemplazar la IP de localhost por la IP privada del debian
+      vIPLocal=$(hostname -I | cut -d' ' -f1 | tr -d '[:space:]')
+      sed -i -e "s|127.0.0.1|$vIPLocal|g" /etc/loki/loki-config.yaml
+    # Reparar permisos
+      chown -R loki:loki /etc/loki
+
+  # Crear el servicio de systemd
+    echo "[Unit]"                                                                        > /etc/systemd/system/loki.service
+    echo "Description=Loki Service"                                                     >> /etc/systemd/system/loki.service
+    echo "After=network.target"                                                         >> /etc/systemd/system/loki.service
+    echo ""                                                                             >> /etc/systemd/system/loki.service
+    echo "[Service]"                                                                    >> /etc/systemd/system/loki.service
+    echo "Type=simple"                                                                  >> /etc/systemd/system/loki.service
+    echo "ExecStart=/usr/bin/loki-linux-amd64 --config.file=/etc/loki/loki-config.yaml" >> /etc/systemd/system/loki.service
+    echo "Restart=always"                                                               >> /etc/systemd/system/loki.service
+    echo "RestartSec=5"                                                                 >> /etc/systemd/system/loki.service
+    echo "User=loki"                                                                    >> /etc/systemd/system/loki.service
+    echo "Group=loki"                                                                   >> /etc/systemd/system/loki.service
+    echo ""                                                                             >> /etc/systemd/system/loki.service
+    echo "[Install]"                                                                    >> /etc/systemd/system/loki.service
+    echo "WantedBy=multi-user.target"                                                   >> /etc/systemd/system/loki.service
+
+  # Recargar los daemons
+    systemctl daemon-reload
+
+  # Habilitar e iniciar loki
+    systemctl enable loki --now
+
+  # Comprobar estado
+    systemctl status loki.service --no-pager
+
+  # Notificar fin de ejecución del script
+    echo ""
+    echo "    La ejecución de Loki ha finalizado."
+    echo ""
+    echo "    Recuerda instalar el forwarder promtail en cada máquina que enviará logs al servidor loki"
+    echo "    Puedes instalarlo con"
+    echo ""
+    echo ""
 
 elif [ $cVerSO == "11" ]; then
 
