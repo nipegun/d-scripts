55eb0ff8e74cbb6ea10c2781c7a93e6dcf2ae77e
NiPeGun <nipegun@gmail.com>
2024-12-10 09:23:16 +0100

    Update Grafana-Instalar.sh

--- SoftInst/ParaCLI/Grafana-Instalar.sh
+++ SoftInst/ParaCLI/Grafana-Instalar.sh
@@ -12,9 +12,33 @@
 #   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/Grafana-Instalar.sh | bash
 # ----------
 
-cColorRojo='\033[1;31m'
-cColorVerde='\033[1;32m'
-cFinColor='\033[0m'
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Comprobar si el script está corriendo como root
+  #if [ $(id -u) -ne 0 ]; then     # Sólo comprueba si es root
+  if [[ $EUID -ne 0 ]]; then       # Comprueba si es root o sudo
+    echo ""
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse con privilegios de administrador (como root o con sudo).${cFinColor}"
+    echo ""
+    exit
+  fi
+
+# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    apt-get -y update
+    apt-get -y install curl
+    echo ""
+  fi
 
 # Determinar la versión de Debian
   if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
@@ -36,51 +60,17 @@ cFinColor='\033[0m'
     cVerSO=$(uname -r)
   fi
 
-if [ $cVerSO == "7" ]; then
+if [ $cVerSO == "13" ]; then
 
   echo ""
-  echo "---------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación de Grafana para Debian 7 (Wheezy)..."  echo "---------------------------------------------------------------------------"
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "8" ]; then
-
-  echo ""
-  echo "---------------------------------------------------------------------------"
-  echo "  Iniciando el script de instalación de Grafana para Debian 8 (Jessie)..."  echo "---------------------------------------------------------------------------"
+  echo "  Iniciando el script de instalación de Grafana para Debian 13 (x)..."
   echo ""
 
   echo ""
-  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo "  Comandos para Debian 13 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
   echo ""
 
-elif [ $cVerSO == "9" ]; then
-
-  echo ""
-  
-  echo "  Iniciando el script de instalación de Grafana para Debian 9 (Stretch)..."  
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "10" ]; then
-
-  echo ""
-  
-  echo "  Iniciando el script de instalación de Grafana para Debian 10 (Buster)..."  
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "11" ]; then
+elif [ $cVerSO == "12" ]; then
 
   echo ""
   echo "  Iniciando el script de instalación de Grafana para Debian 11 (Bullseye)..."  
@@ -88,7 +78,7 @@ elif [ $cVerSO == "11" ]; then
 
   echo ""
   echo "  Instalando dependencias o paquetes necesarios para el script..." 
-echo ""
+  echo ""
   apt-get update
   apt-get -y insall wget
   apt-get -y install gnupg
@@ -98,21 +88,26 @@ echo ""
   echo ""
   echo "  Importando clave del repositorio.."
   echo ""
-  # Para cuando apt-key quede obsoleto:
-     # mkdir -p /root/aptkeys/
-     # wget -q -O- https://packages.grafana.com/gpg.key > /root/aptkeys/grafana.key
-     # gpg --dearmor /root/aptkeys/grafana.key
-     # cp /root/aptkeys/grafana.key.gpg /usr/share/keyrings/grafana.gpg
+  mkdir -p /root/aptkeys/
+  wget -q -O- https://packages.grafana.com/gpg.key > /root/aptkeys/grafana.key
+  gpg --dearmor /root/aptkeys/grafana.key
+  cp /root/aptkeys/grafana.key.gpg /usr/share/keyrings/grafana.gpg
+
+     
    wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
+
+wget -q -O /usr/share/keyrings/grafana-archive-keyring.gpg https://packages.grafana.com/gpg.key
+echo "deb [signed-by=/usr/share/keyrings/grafana-archive-keyring.gpg] https://packages.grafana.com/oss/deb stable main" | tee /etc/apt/sources.list.d/grafana.list
+apt update
 
   echo ""
   echo "  Agregando repositorio..." 
-echo ""
+  echo ""
   echo "deb https://packages.grafana.com/enterprise/deb stable main" > /etc/apt/sources.list.d/grafana.list
 
   echo ""
   echo "  Instalando paquetes..." 
-echo ""
+  echo ""
   apt-get update
   apt-get -y install grafana-enterprise
 
@@ -134,18 +129,127 @@ echo ""
 
   echo ""
   echo "  Activando el servicio..." 
-echo ""
+  echo ""
   systemctl enable grafana-server --now
 
   echo ""
   echo "  Instalando el plugin grafana-image-renderer..." 
-echo ""
+  echo ""
   grafana-cli plugins install grafana-image-renderer
 
   echo ""
   echo "  Reiniciando el servicio..." 
-echo ""
+  echo ""
   systemctl restart grafana-server
+
+elif [ $cVerSO == "11" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de Grafana para Debian 11 (Bullseye)..."  
+  echo ""
+
+  echo ""
+  echo "  Instalando dependencias o paquetes necesarios para el script..." 
+  echo ""
+  apt-get update
+  apt-get -y insall wget
+  apt-get -y install gnupg
+  #apt-get -y install apt-transport-https
+  #apt-get -y install software-properties-common
+
+  echo ""
+  echo "  Importando clave del repositorio.."
+  echo ""
+  # Para cuando apt-key quede obsoleto:
+     # mkdir -p /root/aptkeys/
+     # wget -q -O- https://packages.grafana.com/gpg.key > /root/aptkeys/grafana.key
+     # gpg --dearmor /root/aptkeys/grafana.key
+     # cp /root/aptkeys/grafana.key.gpg /usr/share/keyrings/grafana.gpg
+   wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
+
+  echo ""
+  echo "  Agregando repositorio..." 
+  echo ""
+  echo "deb https://packages.grafana.com/enterprise/deb stable main" > /etc/apt/sources.list.d/grafana.list
+
+  echo ""
+  echo "  Instalando paquetes..." 
+  echo ""
+  apt-get update
+  apt-get -y install grafana-enterprise
+
+  # Preparar para https
+     #cd /etc/grafana
+     #openssl genrsa -out grafana.key 2048
+     #openssl req -new -key grafana.key -out grafana.csr
+     #openssl x509 -req -days 365 -in grafana.csr -signkey grafana.key -out grafana.crt
+     #chown grafana:grafana grafana.crt
+     #chown grafana:grafana grafana.key
+     #chmod 400 grafana.key grafana.crt
+     #sed -i -e 's|;protocol = http|protocol = https|g'                 /etc/grafana/grafana.ini
+     #sed -i -e 's|;cert_file =|cert_key = /etc/grafana/grafana.key|g'  /etc/grafana/grafana.ini
+     #sed -i -e 's|;key_file =|cert_file = /etc/grafana/grafana.crt|g'  /etc/grafana/grafana.ini
+     
+  # Permitir embedded
+     sed -i -e 's|;allow_embedding = false|allow_embedding = true|g'   /etc/grafana/grafana.ini
+     sed -i -e 's|;cookie_samesite = lax|cookie_samesite = disabled|g' /etc/grafana/grafana.ini
+
+  echo ""
+  echo "  Activando el servicio..." 
+  echo ""
+  systemctl enable grafana-server --now
+
+  echo ""
+  echo "  Instalando el plugin grafana-image-renderer..." 
+  echo ""
+  grafana-cli plugins install grafana-image-renderer
+
+  echo ""
+  echo "  Reiniciando el servicio..." 
+  echo ""
+  systemctl restart grafana-server
+
+elif [ $cVerSO == "10" ]; then
+
+  echo ""
+  
+  echo "  Iniciando el script de instalación de Grafana para Debian 10 (Buster)..."  
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "9" ]; then
+
+  echo ""
+  
+  echo "  Iniciando el script de instalación de Grafana para Debian 9 (Stretch)..."  
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "8" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de Grafana para Debian 8 (Jessie)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "7" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de Grafana para Debian 7 (Wheezy)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
 
 fi
 
