5caac65af2f179773e7047e3985b8ec6a0ebe96d
NiPeGun <nipegun@gmail.com>
2024-12-11 17:35:18 +0100

    Update WinDir.sh

--- WinDir.sh
+++ WinDir.sh
@@ -12,14 +12,35 @@
 #   --all:                     No oculta las entradas que comienzan por .
 #   --almost-all:              No muestra las entradas . y .. implícitas
 #   --author:                  Usando en conjunto con -l imprime el autor de cada fichero
-#   --color:                   Colorea la salida. Puede ser 'always' (por defecto si se omite), 'auto', o 'never'.
-#   --group-directories-first: Agrupa directorios antes que los ficheros; se puede añadir una opción --sort, pero cualquier uso de --sort=none (-U) desactiva la agrupación
+#   --classify:                Añade un indicador (uno de */=@|) a las entradas
+#   --color=auto:              Colorea la salida. Puede ser 'always' (por defecto si se omite), 'auto', o 'never'.
+#   --context:                 Imprime el contexto de seguridad de cada archivo
+#   --format:                  Admite across (-x), commas (-m), horizontal (-x), long (-l), single-column (-1), verbose (-l), vertical (-C)
+#   --full-time:               Como -l --time-style=full-iso
+#   --group-directories-first: Agrupa directorios antes que los ficheros
+#   --human-readable:          Imprime el tamaño de  los archivos en KB, MB y GB
+#   --hyperlink=auto:          Crea enlaces a los archivos. Acepta 'always' (por defecto si se omite), 'auto' y 'never'
+#   --inode:                   Imprime el número de índice de cada archivo
+#   --sort=extension           Admite none (-U), size (-S), time (-t), version (-v), extension (-X)
 #   -l:                        Utiliza el formato de listado largo
 #-1: Muestra un archivo por línea
 # ----------
 
-vParam=" -lha1FXis --author --group-directories-first --color=always --time-style=long-iso"
-vParam=" --all --almost-all --author --color=always --group-directories-first --format=long -F --time-style=long-iso"
+#vParam=" --all --almost-all --author --classify --color=auto --context --format=verbose --full-time --group-directories-first --human-readable --hyperlink=auto --inode --sort=extension "
+
+vParam=" --all \
+         --almost-all \
+         --author \
+         --classify \
+         --color=auto \
+         --context \
+         --format=verbose \
+         --full-time \
+         --group-directories-first \
+         --human-readable \
+         --hyperlink=auto \
+         --inode \
+         --sort=extension "
 
 if [ $# -eq 1 ]
   then
