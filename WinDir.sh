f48c7e7106b04d0b17491e90555987f41e3091f7
NiPeGun <nipegun@gmail.com>
2024-12-11 16:38:39 +0100

    Update WinDir.sh

--- WinDir.sh
+++ WinDir.sh
@@ -9,6 +9,7 @@
 # Script de NiPeGun para mostrar directorios y archivos de forma similar a como lo hace el dir de Windows
 # ----------
 vParam=" -lha1FXis --author --group-directories-first --color=always --time-style=long-iso"
+vParam=" -lha1FXisZ --author --group-directories-first --color=always --time-style=long-iso"
 
 if [ $# -eq 1 ]
   then
