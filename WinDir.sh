03cac803cd46b9b8176b25b9016da3842d1f2578
NiPeGun <nipegun@gmail.com>
2024-12-11 16:33:06 +0100

    Update WinDir.sh

--- WinDir.sh
+++ WinDir.sh
@@ -8,15 +8,14 @@
 # ----------
 # Script de NiPeGun para mostrar directorios y archivos de forma similar a como lo hace el dir de Windows
 # ----------
+vParam=" -lha1FX --author --group-directories-first --color=auto --time-style=long-iso"
 
 if [ $# -eq 1 ]
   then
-    vParam=" -lha1FXC --group-directories-first --color=auto --time-style=long-iso"
     echo ""
     ls $vParam | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*(2^(8-i)));if(k)printf("%0o ",k); print}' "$1"
     echo ""
   else
-    vParam=" -lha1FX --group-directories-first --color=auto --time-style=long-iso"
     echo ""
     ls $vParam | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*(2^(8-i)));if(k)printf("%0o ",k); print}'
     echo ""
