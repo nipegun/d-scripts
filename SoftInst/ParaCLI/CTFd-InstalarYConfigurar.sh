44de3b1ab18aade60bbd12221cfeb151d8915570
NiPeGun <nipegun@gmail.com>
2024-12-11 10:43:02 +0100

    Create CTFd-InstalarYConfigurar.sh

---
+++ SoftInst/ParaCLI/CTFd-InstalarYConfigurar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para instalar CTFd en Debian
+#
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/DockerCE-Contenedor-Instalar-CTFd.sh | bash
+#
+# Enlace: https://github.com/CTFd/CTFd
+# ----------
