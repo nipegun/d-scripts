1fc18dd832c4c6a79deb4302477fabd1d91f47b8
NiPeGun <nipegun@gmail.com>
2024-12-11 17:40:06 +0100

    Update WinDir.sh

--- WinDir.sh
+++ WinDir.sh
@@ -13,21 +13,20 @@
 #   --almost-all:              No muestra las entradas . y .. implícitas
 #   --author:                  Usando en conjunto con -l imprime el autor de cada fichero
 #   --classify:                Añade un indicador (uno de */=@|) a las entradas
-#   --color=auto:              Colorea la salida. Puede ser 'always' (por defecto si se omite), 'auto', o 'never'.
+#   --color=always:            Colorea la salida. Puede ser 'always' (por defecto si se omite), 'auto', o 'never'.
 #   --context:                 Imprime el contexto de seguridad de cada archivo
-#   --format:                  Admite across (-x), commas (-m), horizontal (-x), long (-l), single-column (-1), verbose (-l), vertical (-C)
+#   --format=verbose:          Admite across (-x), commas (-m), horizontal (-x), long (-l), single-column (-1), verbose (-l), vertical (-C)
 #   --full-time:               Como -l --time-style=full-iso
 #   --group-directories-first: Agrupa directorios antes que los ficheros
 #   --human-readable:          Imprime el tamaño de  los archivos en KB, MB y GB
 #   --hyperlink=auto:          Crea enlaces a los archivos. Acepta 'always' (por defecto si se omite), 'auto' y 'never'
 #   --inode:                   Imprime el número de índice de cada archivo
 #   --sort=extension           Admite none (-U), size (-S), time (-t), version (-v), extension (-X)
-#   -l:                        Utiliza el formato de listado largo
 # ----------
 
 #vParam=" --all --almost-all --author --classify --color=auto --context --format=verbose --full-time --group-directories-first --human-readable --hyperlink=auto --inode --sort=extension "
 
-vParam=" --all \
+vParam=" --all \                     #  No oculta las entradas que comienzan por .
          --almost-all \
          --author \
          --classify \
