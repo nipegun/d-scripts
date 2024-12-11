efc7de983cfe2c858b0433e2ef50e67fad6d68f6
NiPeGun <nipegun@gmail.com>
2024-12-11 17:52:12 +0100

    Update WinDir.sh

--- WinDir.sh
+++ WinDir.sh
@@ -47,6 +47,18 @@ if [ $# -eq 1 ]
   else
     echo ""
     ls $vParam | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*(2^(8-i)));if(k)printf("%0o ",k); print}'
+    ls $vParam | awk '{
+    # Extraer los permisos en texto desde la segunda columna
+    perms = $2;
+
+    # Calcular los permisos octales
+    k = 0;
+    for (i = 0; i <= 8; i++) 
+        k += ((substr(perms, i + 2, 1) ~ /[rwx]/) * (2 ^ (8 - i)));
+
+    # Reconstruir la línea con los permisos octales después de la columna de permisos
+    printf("%s %s %0o %s\n", $1, perms, k, substr($0, index($0, $3)));
+}'
     #ls $vParam
     echo ""
 fi
