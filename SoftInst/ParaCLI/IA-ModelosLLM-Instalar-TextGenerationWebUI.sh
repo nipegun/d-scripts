9614abbd31d1752ad3b68e97d3fb6a43f81fdc0e
NiPeGun <nipegun@gmail.com>
2024-12-29 02:07:03 +0100

    Delete SoftInst/ParaCLI/IA-ModelosLLM-Instalar-TextGenerationWebUI.sh

--- SoftInst/ParaCLI/IA-ModelosLLM-Instalar-TextGenerationWebUI.sh
+++
@@ -1,8 +0,0 @@
-vCarpeta="/opt/text-generation-webui/models/"
-
-
-            #Use the download-model.py script to automatically download a model from Hugging Face. /opt/text-generation-webui/download-model.py
-
-chmod +x /opt/text-generation-webui/download-model.py
-cd /opt/text-generation-webui/
-./download-model.py
