b5e27912430b113accacf547fb1a06c963948997
NiPeGun <nipegun@gmail.com>
2025-01-11 00:35:31 +0100

    Update Cockpit-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/Cockpit-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/Cockpit-InstalarYConfigurar.sh
@@ -1,37 +1,193 @@
-apt-get -y update
-apt-get -y install cockpit
+#!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-# Cockpit user interface for 389 Directory Server
-  apt-get -y install cockpit-389-ds
-# Cockpit bridge server-side component
-  apt-get -y install cockpit-bridge
-# Cockpit deployment and developer guide
-  apt-get -y install cockpit-doc
-# Cockpit user interface for virtual machines
-  apt-get -y install cockpit-machines
-# Cockpit user interface for networking
-  apt-get -y install cockpit-networkmanager
-# Cockpit user interface for packages
-  apt-get -y install cockpit-packagekit
-# Cockpit PCP integration (para métricas)
-  apt-get -y install cockpit-pcp
-# Cockpit component for Podman containers
-  apt-get -y install cockpit-podman
-# Cockpit user interface for diagnostic reports
-  apt-get -y install cockpit-sosreport
-# Cockpit user interface for storage
-  apt-get -y install cockpit-storaged
-# Cockpit admin interface for a system
-  apt-get -y install cockpit-system
-# Tests for Cockpit
-  apt-get -y install cockpit-tests
-# Cockpit Web Service
-  apt-get -y install cockpit-ws
+# ----------
+# Script de NiPeGun para instalar y configurar Cockpit en Debian
+#
+# Ejecución remota con sudo:
+#   curl -sL x | sudo bash
+#
+# Ejecución remota como root:
+#   curl -sL x | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
 
-# Instalar soporte NFS
-  apt-get -y install nfs-common
+# Definir constantes de color
+  cColorAzul='\033[0;34m'
+  cColorAzulClaro='\033[1;34m'
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
 
-# Permitir el acceso root
-  echo "auth sufficient pam_rootok.so" >> /etc/pam.d/cockpit
+# Comprobar si el script está corriendo como root
+  #if [ $(id -u) -ne 0 ]; then     # Sólo comprueba si es root
+  if [[ $EUID -ne 0 ]]; then       # Comprueba si es root o sudo
+    echo ""
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse con privilegios de administrador (como root o con sudo).${cFinColor}"
+    echo ""
+    exit
+  fi
+
+# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    apt-get -y update
+    apt-get -y install curl
+    echo ""
+  fi
+
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
+    . /etc/os-release
+    cNomSO=$NAME
+    cVerSO=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # Para linuxbase.org.
+    cNomSO=$(lsb_release -si)
+    cVerSO=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release.
+    . /etc/lsb-release
+    cNomSO=$DISTRIB_ID
+    cVerSO=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    cNomSO=Debian
+    cVerSO=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD).
+    cNomSO=$(uname -s)
+    cVerSO=$(uname -r)
+  fi
+
+# Ejecutar comandos dependiendo de la versión de Debian detectada
+
+  if [ $cVerSO == "13" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de Cockpit para Debian 13 (x)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "12" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de Cockpit para Debian 12 (Bookworm)...${cFinColor}"
+    echo ""
+
+    # Actualizar la lista de paquetes disponibles en los repositorios
+      echo ""
+      echo "    Actualizando la lista de paquetes disponibles en los repositorios..."
+      echo ""
+      apt-get -y update
+
+    # Instalar el paquete principal
+      echo ""
+      echo "    Instalando el paquete principal..."
+      echo ""
+      apt-get -y install cockpit
+
+    # Instalar extenxiones
+      echo ""
+      echo "    Instalando extensiones..."
+      echo ""
+      # Cockpit user interface for 389 Directory Server
+        apt-get -y install cockpit-389-ds
+      # Cockpit bridge server-side component
+        apt-get -y install cockpit-bridge
+      # Cockpit deployment and developer guide
+        apt-get -y install cockpit-doc
+      # Cockpit user interface for virtual machines
+        apt-get -y install cockpit-machines
+      # Cockpit user interface for networking
+        apt-get -y install cockpit-networkmanager
+      # Cockpit user interface for packages
+        apt-get -y install cockpit-packagekit
+      # Cockpit PCP integration (para métricas)
+        apt-get -y install cockpit-pcp
+      # Cockpit component for Podman containers
+        apt-get -y install cockpit-podman
+      # Cockpit user interface for diagnostic reports
+        apt-get -y install cockpit-sosreport
+      # Cockpit user interface for storage
+        apt-get -y install cockpit-storaged
+      # Cockpit admin interface for a system
+        apt-get -y install cockpit-system
+      # Tests for Cockpit
+        apt-get -y install cockpit-tests
+      # Cockpit Web Service
+        apt-get -y install cockpit-ws
+
+    # Instalar soporte NFS
+      apt-get -y install nfs-common
+
+    # Permitir el acceso root
+      echo "auth sufficient pam_rootok.so" >> /etc/pam.d/cockpit
+
+  elif [ $cVerSO == "11" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de Cockpit para Debian 11 (Bullseye)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 11 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de Cockpit para Debian 10 (Buster)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "9" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de Cockpit para Debian 9 (Stretch)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "8" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de Cockpit para Debian 8 (Jessie)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "7" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de instalación de Cockpit para Debian 7 (Wheezy)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  fi
 
