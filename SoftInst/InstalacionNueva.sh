6bef0499e3d607d060def9e7ab656ffbf7db103b
NiPeGun <nipegun@gmail.com>
2025-01-11 00:39:55 +0100

    Update InstalacionNueva.sh

--- SoftInst/InstalacionNueva.sh
+++ SoftInst/InstalacionNueva.sh
@@ -8,11 +8,11 @@
 # ----------
 # Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
 #
-# Ejecución remota con sudo:
-#   curl -sL x | sudo bash
-#
-# Ejecución remota como root:
+# Ejecución remota (puede requerir permisos sudo):
 #   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
 #
 # Ejecución remota sin caché:
 #   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
