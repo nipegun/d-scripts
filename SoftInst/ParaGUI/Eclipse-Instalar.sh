b397b16a4e407a8e262e21fd620117079d9c2fe3
NiPeGun <nipegun@gmail.com>
2025-01-14 12:28:23 +0100

    Update and rename Eclipse-Instalar.sh to Eclipse-Instalar.sh

--- SoftInst/ParaGUI/Eclipse-Instalar.sh
+++ SoftInst/ParaGUI/Eclipse-Instalar.sh
@@ -9,13 +9,13 @@
 # Script de NiPeGun para instalar y configurar Eclipse en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Eclipse-Instalar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaGUI/Eclipse-Instalar.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Eclipse-Instalar.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaGUI/Eclipse-Instalar.sh | sed 's-sudo--g' | bash
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Eclipse-Instalar.sh | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaGUI/Eclipse-Instalar.sh | nano -
 # ----------
 
 # Definir constantes de color
