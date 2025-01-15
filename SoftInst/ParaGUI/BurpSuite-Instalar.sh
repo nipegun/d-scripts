6ac42cfea2eb02951a5482cf8cc7eae857507aa0
NiPeGun <nipegun@gmail.com>
2025-01-15 12:23:57 +0100

    Update BurpSuite-Instalar.sh

--- SoftInst/ParaGUI/BurpSuite-Instalar.sh
+++ SoftInst/ParaGUI/BurpSuite-Instalar.sh
@@ -1,4 +1,10 @@
+# Community
 
-curl -L 'https://portswigger-cdn.net/burp/releases/download?product=pro&version=2024.11.2&type=Linux' -o ~/BurpSuite.sh
-chmod +x ~/BurpSuite.sh
-~/BurpSuite.sh
+curl -L 'https://portswigger-cdn.net/burp/releases/download?product=community&version=2024.11.2&type=Linux' -o ~/BurpSuiteCommunity-Install.sh
+chmod +x ~/BurpSuiteCommunity-Install.sh
+~/BurpSuiteCommunity-Install.sh
+
+# Pro
+curl -L 'https://portswigger-cdn.net/burp/releases/download?product=pro&version=2024.11.2&type=Linux' -o ~/BurpSuitePro-Install.sh
+chmod +x ~/BurpSuitePro-Install.sh
+~/BurpSuitePro-Install.sh
