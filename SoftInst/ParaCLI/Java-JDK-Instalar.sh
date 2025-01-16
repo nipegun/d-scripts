3911160abbefeba9fce5773340001663c1c0065e
NiPeGun <nipegun@gmail.com>
2025-01-16 05:24:10 +0100

    Update and rename Java-Instalar.sh to Java-JDK-Instalar.sh

--- SoftInst/ParaCLI/Java-JDK-Instalar.sh
+++ SoftInst/ParaCLI/Java-JDK-Instalar.sh
@@ -6,22 +6,16 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para instalar y configurar Java en Debian
+# Script de NiPeGun para instalar y configurar Java JDK en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL x | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Java-JDK-Instalar.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL x | sed 's-sudo--g' | bash
-#
-# Ejecución remota sin caché:
-#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
-#
-# Ejecución remota con parámetros:
-#   curl -sL x | bash -s Parámetro1 Parámetro2
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Java-JDK-Instalar.sh | sed 's-sudo--g' | bash
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL x | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Java-JDK-Instalar.sh | nano -
 # ----------
 
 # Definir constantes de color
@@ -32,25 +26,6 @@
   # Para el color rojo también:
     #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
   cFinColor='\033[0m'
-
-# Comprobar si el script está corriendo como root
-  #if [ $(id -u) -ne 0 ]; then     # Sólo comprueba si es root
-  if [[ $EUID -ne 0 ]]; then       # Comprueba si es root o sudo
-    echo ""
-    echo -e "${cColorRojo}  Este script está preparado para ejecutarse con privilegios de administrador (como root o con sudo).${cFinColor}"
-    echo ""
-    exit
-  fi
-
-# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
-  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
-    echo ""
-    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
-    echo ""
-    apt-get -y update
-    apt-get -y install curl
-    echo ""
-  fi
 
 # Determinar la versión de Debian
   if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
@@ -90,9 +65,104 @@
     echo -e "${cColorAzulClaro}  Iniciando el script de instalación de Java para Debian 12 (Bookworm)...${cFinColor}"
     echo ""
 
-    echo ""
-    echo -e "${cColorRojo}    Comandos para Debian 12 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
-    echo ""
+    # Crear el menú
+      # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
+        if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
+          echo ""
+          echo -e "${cColorRojo}  El paquete dialog no está instalado. Iniciando su instalación...${cFinColor}"
+          echo ""
+          apt-get -y update
+          apt-get -y install dialog
+          echo ""
+        fi
+      menu=(dialog --checklist "Elige que versión de Java quieres instalar:" 22 96 16)
+        opciones=(
+          1 "Versión disponible en los repositorios de Debian 12" on
+          2 "Última versión disponible en la web oficial" off
+          3 "Opción 3" off
+          4 "Opción 4" off
+          5 "Opción 5" off
+        )
+      choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
+
+      for choice in $choices
+        do
+          case $choice in
+
+            1)
+
+              echo ""
+              echo "  Instalando la versión de Java JDK disponible en los repositorios de Debian 12..."
+              echo ""
+              sudo apt-get -y update
+              sudo apt-get -y install openjdk-17-jdk
+
+            ;;
+
+            2)
+
+              echo ""
+              echo "  Instalando la última versión de Java JDK disponible en la web oficial..."
+              echo ""
+
+              # Determinar el número de la última versión disponible
+                echo ""
+                echo "    Determinando cual es la última versión de Java JDK disponible en la web..."
+                echo ""
+                # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+                  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+                    echo ""
+                    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+                    echo ""
+                    apt-get -y update
+                    apt-get -y install curl
+                    echo ""
+                  fi
+                vUltVersDispJDK=$(curl -sL download.oracle.com/java/23/latest/)
+                echo "      La última versión disponible es la $vUltVersDispJDK"
+                echo ""
+
+              # Descargar el paquete .deb
+                echo ""
+                echo "    Descargando el paquete.deb..."
+                echo ""
+                curl -L https://download.oracle.com/java/23/latest/jdk-23_linux-x64_bin.deb -o /tmp/jdk.deb
+
+              # Instalar el paquete .deb
+                echo ""
+                echo "    Instalando el paquete .deb..."
+                echo ""
+                apt -y install /tmp/jdk.deb
+
+            ;;
+
+            3)
+
+              echo ""
+              echo "  Opción 3..."
+              echo ""
+
+            ;;
+
+            4)
+
+              echo ""
+              echo "  Opción 4..."
+              echo ""
+
+            ;;
+
+            5)
+
+              echo ""
+              echo "  Opción 5..."
+              echo ""
+
+            ;;
+
+        esac
+
+    done
 
   elif [ $cVerSO == "11" ]; then
 
