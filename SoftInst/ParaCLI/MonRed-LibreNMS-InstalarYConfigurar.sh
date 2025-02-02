c50d54f7564ea0c8872832e458d35b11153a62b6
NiPeGun <nipegun@gmail.com>
2025-02-02 22:41:46 +0100

    Update and rename Red-LibreNMS-InstalarYConfigurar.sh to MonRed-LibreNMS-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/MonRed-LibreNMS-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/MonRed-LibreNMS-InstalarYConfigurar.sh
@@ -9,13 +9,13 @@
 # Script de NiPeGun para instalar y configurar LibreNMS en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Red-LibreNMS-InstalarYConfigurar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/MonRed-LibreNMS-InstalarYConfigurar.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Red-LibreNMS-InstalarYConfigurar.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/MonRed-LibreNMS-InstalarYConfigurar.sh | sed 's-sudo--g' | bash
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Red-LibreNMS-InstalarYConfigurar.sh | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/MonRed-LibreNMS-InstalarYConfigurar.sh | nano -
 # ----------
 
 # Definir constantes de color
