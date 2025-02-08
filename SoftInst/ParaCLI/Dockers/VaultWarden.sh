6bf907a579cd487f2f66022c09ba3476e8c50d78
NiPeGun <nipegun@gmail.com>
2025-02-08 12:57:55 +0100

    Update VaultWarden.sh

--- SoftInst/ParaCLI/Dockers/VaultWarden.sh
+++ SoftInst/ParaCLI/Dockers/VaultWarden.sh
@@ -8,100 +8,42 @@
 # ----------
 # Script de NiPeGun para instalar VaultWarden en el DockerCE de Debian
 #
-# Ejecución remota:
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/DockerCE-Contenedor-Instalar-VaultWarden.sh | bash
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Dockers/VaultWarden.sh | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Dockers/VaultWarden.sh | sed 's-sudo--g' | bash
 # ----------
 
-cColorRojo='\033[1;31m'
-cColorVerde='\033[1;32m'
-cFinColor='\033[0m'
+# Definir constantes de color
+  cColorAzul='\033[0;34m'
+  cColorAzulClaro='\033[1;34m'
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
 
-# Determinar la versión de Debian
-  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org
-    . /etc/os-release
-    cNomSO=$NAME
-    cVerSO=$VERSION_ID
-  elif type lsb_release >/dev/null 2>&1; then # linuxbase.org
-    cNomSO=$(lsb_release -si)
-    cVerSO=$(lsb_release -sr)
-  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release
-    . /etc/lsb-release
-    cNomSO=$DISTRIB_ID
-    cVerSO=$DISTRIB_RELEASE
-  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
-    cNomSO=Debian
-    cVerSO=$(cat /etc/debian_version)
-  else                                        # Para el viejo uname (También funciona para BSD)
-    cNomSO=$(uname -s)
-    cVerSO=$(uname -r)
-  fi
-
-if [ $cVerSO == "7" ]; then
-
+# Notificar inicio de ejecución del script
   echo ""
-  echo "  Iniciando el script de instalación de VaultWarden en el DockerCE de Debian 7 (Wheezy)..." 
+  echo -e "${cColorAzulClaro}  Iniciando el script de instalación de VaultWarden en el DockerCE de Debian...${cFinColor}"
   echo ""
 
-  echo ""
-  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "8" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de VaultWarden en el DockerCE de Debian 8 (Jessie)..." 
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "9" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de VaultWarden en el DockerCE de Debian 9 (Stretch)..." 
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "10" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de VaultWarden en el DockerCE de Debian 10 (Buster)..." 
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
- 
-elif [ $cVerSO == "11" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de VaultWarden en el DockerCE de Debian 11 (Bullseye)..." 
-  echo ""
-
-elif [ $cVerSO == "12" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de VaultWarden en el DockerCE de Debian 12 (Bookworm)..." 
-  echo ""
-
+# Crear el menú
   # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
     if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
       echo ""
-      echo "  dialog no está instalado. Iniciando su instalación..."
+      echo -e "${cColorRojo}    El paquete dialog no está instalado. Iniciando su instalación...${cFinColor}"
+      echo "   "
       echo ""
-      apt-get -y update && apt-get -y install dialog
+      sudo apt-get -y update
+      sudo apt-get -y install dialog
       echo ""
     fi
   menu=(dialog --checklist "¿Donde quieres instalar VaultWarden?:" 22 76 16)
     opciones=(
       1 "En un ordenador o máquina virtual" off
-      2 "En un contenedor LXC de Proxmox" off
-      3 "..." off
-      4 "..." off
+      2 "En un contenedor LXC de Proxmox"   off
     )
   choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
 
@@ -114,7 +56,7 @@ elif [ $cVerSO == "12" ]; then
           echo ""
           echo -e "${cColorVerde}  Instalando VaultWarden en un ordenador o máquina virtual...${cFinColor}"
           echo ""
-          mkdir -p /Contenedores/VaultWarden/data 2> /dev/null
+          sudo mkdir -p /Contenedores/VaultWarden/data 2> /dev/null
 
           echo ""
           echo "  Creando el comando para iniciar el contenedor docker..."
@@ -275,4 +217,3 @@ https://gist.github.com/heinoldenhuis/f8164f73e5bff048e76fb4fff2e824e1
 
     done
 
-fi
