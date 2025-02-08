b0dbd7c533cab8d160e12e56bb9af514c3a467e0
NiPeGun <nipegun@gmail.com>
2025-02-08 12:57:04 +0100

    Update TheHive.sh

--- SoftInst/ParaCLI/Dockers/TheHive.sh
+++ SoftInst/ParaCLI/Dockers/TheHive.sh
@@ -8,8 +8,11 @@
 # ----------
 # Script de NiPeGun para instalar TheHive en el DockerCE de Debian
 #
-# Ejecución remota:
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/DockerCE-Contenedor-Instalar-TheHive.sh | bash
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Dockers/TheHive.sh | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Dockers/TheHive.sh | sed 's-sudo--g' | bash
 # ----------
 
 # Definir constantes de color
@@ -21,35 +24,26 @@
     #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
   cFinColor='\033[0m'
 
-# Notificar el inicio de ejecución del script
+# Notificar inicio de ejecución del script
   echo ""
-  echo "  Iniciando el script de instalación de TheHive en DockerCE..." 
+  echo -e "${cColorAzulClaro}  Iniciando el script de instalación de TheHive en el DockerCE de Debian...${cFinColor}"
   echo ""
-
-# Comprobar si el script está corriendo como root
-  #if [ $(id -u) -ne 0 ]; then     # Sólo comprueba si es root
-  if [[ $EUID -ne 0 ]]; then       # Comprueba si es root o sudo
-    echo ""
-    echo -e "${cColorRojo}  Este script está preparado para ejecutarse con privilegios de administrador (como root o con sudo).${cFinColor}"
-    echo ""
-    exit
-  fi
 
 # Crear el menú
   # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
     if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
-     echo ""
-     echo "  El paquete dialog no está instalado. Iniciando su instalación..."
-     echo ""
-     apt-get -y update && apt-get -y install dialog
-     echo ""
-   fi
-  menu=(dialog --timeout 5 --checklist "¿Donde quieres instalar TheHive?:" 22 76 16)
+      echo ""
+      echo -e "${cColorRojo}    El paquete dialog no está instalado. Iniciando su instalación...${cFinColor}"
+      echo "   "
+      echo ""
+      sudo apt-get -y update
+      sudo apt-get -y install dialog
+      echo ""
+    fi
+  menu=(dialog --checklist "¿Donde quieres instalar TheHive?:" 22 76 16)
     opciones=(
-      1 "En un ordenador o máquina virtual" on
-      2 "En un contenedor LXC de Proxmox" off
-      3 "..." off
-      4 "..." off
+      1 "En un ordenador o máquina virtual" off
+      2 "En un contenedor LXC de Proxmox"   off
     )
   choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
 
@@ -57,94 +51,101 @@
     do
       case $choice in
 
-        1)
+      1)
 
-          echo ""
-          echo -e "${cColorVerde}  Instalando TheHive en un ordenador o máquina virtual...${cFinColor}"
-          echo ""
-          mkdir -p /Contenedores/TheHive/data 2> /dev/null
+        echo ""
+        echo -e "${cColorVerde}  Instalando TheHive en un ordenador o máquina virtual...${cFinColor}"
+        echo ""
 
-          echo ""
-          echo "  Creando el comando para iniciar el contenedor docker..."
-          echo ""
-          echo '#!/bin/bash'                                        > /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo ""                                                  >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "docker run -d --restart=always                 \\" >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "  --name TheHive                               \\" >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "  -v /Contenedores/TheHive/data:/data          \\" >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "  -v /var/run/docker.sock:/var/run/docker.sock \\" >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "  -p 9000:9000                                 \\" >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "  strangebee/thehive:5.4"                          >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          chmod +x                                                    /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+        # Crear carpetas
+          sudo mkdir -p /Contenedores/TheHive/data/   2> /dev/null
+          sudo mkdir -p /root/scripts/ParaEsteDebian/ 2> /dev/null
 
+        # Crear el script iniciador
           echo ""
-          echo "  Creando el comando post arranque..."
+          echo "    Creando el script iniciador..."
           echo ""
-          echo "/root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh" >> /root/scripts/ParaEsteDebian/ComandosPostArranque.sh
+          echo '#!/bin/bash'                                       | sudo tee    /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo ""                                                  | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "docker run -d --restart=always                 \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "  --name TheHive                               \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "  -p 9000:9000                                 \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "  -v /var/run/docker.sock:/var/run/docker.sock \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "  -v /Contenedores/TheHive/data:/data          \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "  strangebee/thehive:5.4"                          | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          sudo chmod +x                                                          /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
 
+        # Insertar el script iniciador en los comandos post arranque
           echo ""
-          echo "  Iniciando el container por primera vez..."
+          echo "    Insertando el script iniciador en los ComandosPostArranque..."
           echo ""
-          /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "/root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh" | sudo tee -a /root/scripts/ParaEsteDebian/ComandosPostArranque.sh
 
-        ;;
+        # Iniciar el docker por primera vez
+          echo ""
+          echo "    Iniciando el container por primera vez..."
+          echo ""
+          sudo /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
 
-        2)
+       # Notificar fin de ejecución del script
+         echo ""
+         echo "  Script de instalación del Docker de TheHive, finalizado."
+         echo ""
+         echo "  Datos de acceso por defecto:"
+         echo "    Usuario: admin@thehive.local"
+         echo "    Contraseña: secret"
+         echo ""
 
-          echo ""
-          echo -e "${cColorVerde}  Instalando TheHive en un contenedor LXC...${cFinColor}"
-          echo ""
-          mkdir -p /Host/TheHive/data 2> /dev/null
+      ;;
 
-          echo ""
-          echo "  Creando el comando para iniciar el contenedor docker..."
-          echo ""
-          echo '#!/bin/bash'                                        > /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo ""                                                  >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "docker run -d --restart=always                 \\" >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "  --name TheHive                               \\" >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "  -v /Host/TheHive/data:/data                  \\" >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "  -v /var/run/docker.sock:/var/run/docker.sock \\" >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "  -p 9000:9000                                 \\" >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          echo "  strangebee/thehive:5.4"                          >> /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
-          chmod +x                                                    /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+      2)
 
-          echo ""
-          echo "  Creando el comando post arranque..."
-          echo ""
-          echo "/root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh" >> /root/scripts/ParaEsteDebian/ComandosPostArranque.sh
+        echo ""
+        echo -e "${cColorVerde}  Instalando Flowise en un contenedor LXC...${cFinColor}"
+        echo ""
 
-          echo ""
-          echo "  Iniciando el container por primera vez..."
-          echo ""
-          /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+        # Crear carpetas
+          sudo mkdir -p /Host/TheHive/data 2> /dev/null
+          sudo mkdir -p /root/scripts/ParaEsteDebian/ 2> /dev/null
 
+        # Crear el script iniciador
+          echo ""
+          echo "  Creando el script iniciador..."
+          echo ""
+          echo '#!/bin/bash'                                       | sudo tee    /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo ""                                                  | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "docker run -d --restart=always                 \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "  --name TheHive                               \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "  -p 9000:9000                                 \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "  -v /var/run/docker.sock:/var/run/docker.sock \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "  -v /Host/TheHive/data:/data                  \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          echo "  strangebee/thehive:5.4"                          | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+          sudo chmod +x                                                          /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+
+        # Insertar el script iniciador en los comandos post arranque
+          echo ""
+          echo "    Insertando el script iniciador en los ComandosPostArranque..."
+          echo ""
+          echo "/root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh" | sudo tee -a /root/scripts/ParaEsteDebian/ComandosPostArranque.sh
+
+        # Iniciar el docker por primera vez
+          echo ""
+          echo "    Iniciando el container por primera vez..."
+          echo ""
+          sudo /root/scripts/ParaEsteDebian/DockerCE-Cont-TheHive-Iniciar.sh
+
+        # Notificar fin de ejecución del script
+          echo ""
+          echo "  Script de instalación del Docker de TheHive, finalizado."
           echo ""
           echo "  Datos de acceso por defecto:"
           echo "    Usuario: admin@thehive.local"
           echo "    Contraseña: secret"
           echo ""
 
-        ;;
-
-        3)
-
-          echo ""
-          echo -e "${cColorVerde}  ...${cFinColor}"
-          echo ""
-
-        ;;
-
-        4)
-
-          echo ""
-          echo -e "${cColorVerde}  ...${cFinColor}"
-          echo ""
-
-        ;;
+      ;;
         
-      esac
+    esac
 
-    done
+  done
 
-fi
