93aac1bd8e745edb3cefc0cb461651db5296e8c8
NiPeGun <nipegun@gmail.com>
2025-02-08 14:09:52 +0100

    Update VaultWarden.sh

--- SoftInst/ParaCLI/Dockers/VaultWarden.sh
+++ SoftInst/ParaCLI/Dockers/VaultWarden.sh
@@ -13,6 +13,8 @@
 #
 # Ejecución remota como root (para sistemas sin sudo):
 #   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Dockers/VaultWarden.sh | sed 's-sudo--g' | bash
+#
+# Comando: docker run -d --restart=always --name VaultWarden -p 8080:80 vaultwarden:latest
 # ----------
 
 # Definir constantes de color
