821baf75ddf633e499fc33503c32a09aa8ee4a01
NiPeGun <nipegun@gmail.com>
2025-02-08 12:56:02 +0100

    Update Flowise.sh

--- SoftInst/ParaCLI/Dockers/Flowise.sh
+++ SoftInst/ParaCLI/Dockers/Flowise.sh
@@ -24,178 +24,119 @@
     #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
   cFinColor='\033[0m'
 
-# Determinar la versión de Debian
-  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org
-    . /etc/os-release
-    cNomSO=$NAME
-    cVerSO=$VERSION_ID
-  elif type lsb_release >/dev/null 2>&1; then # linuxbase.org
-    cNomSO=$(lsb_release -si)
-    cVerSO=$(lsb_release -sr)
-  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release
-    . /etc/lsb-release
-    cNomSO=$DISTRIB_ID
-    cVerSO=$DISTRIB_RELEASE
-  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
-    cNomSO=Debian
-    cVerSO=$(cat /etc/debian_version)
-  else                                        # Para el viejo uname (También funciona para BSD)
-    cNomSO=$(uname -s)
-    cVerSO=$(uname -r)
-  fi
-
-if [ $cVerSO == "13" ]; then
-
+# Notificar inicio de ejecución del script
   echo ""
-  echo "  Iniciando el script de instalación de Flowise en el DockerCE de Debian 13 (x)..." 
+  echo -e "${cColorAzulClaro}  Iniciando el script de instalación de Flowise en el DockerCE de Debian...${cFinColor}"
   echo ""
 
-  echo ""
-  echo "  Comandos para Debian 13 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
+# Crear el menú
+  # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${cColorRojo}    El paquete dialog no está instalado. Iniciando su instalación...${cFinColor}"
+      echo "   "
+      echo ""
+      sudo apt-get -y update
+      sudo apt-get -y install dialog
+      echo ""
+    fi
+  menu=(dialog --checklist "¿Donde quieres instalar Flowise?:" 22 76 16)
+    opciones=(
+      1 "En un ordenador o máquina virtual" off
+      2 "En un contenedor LXC de Proxmox"   off
+    )
+  choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
 
-elif [ $cVerSO == "12" ]; then
+  for choice in $choices
+    do
+      case $choice in
 
-  echo ""
-  echo "  Iniciando el script de instalación de Flowise en el DockerCE de Debian 12 (Bookworm)..." 
-  echo ""
+      1)
 
-  # Crear el menú
-    # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
-      if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
         echo ""
-        echo "    El paquete dialog no está instalado. Iniciando su instalación..."
+        echo -e "${cColorVerde}  Instalando Flowise en un ordenador o máquina virtual...${cFinColor}"
         echo ""
-        sudo apt-get -y update
-        sudo apt-get -y install dialog
+
+        # Crear carpetas
+          sudo mkdir -p /Contenedores/Flowise/data/   2> /dev/null
+          sudo mkdir -p /root/scripts/ParaEsteDebian/ 2> /dev/null
+
+        # Crear el script iniciador
+          echo ""
+          echo "    Creando el script iniciador..."
+          echo ""
+          echo '#!/bin/bash'                                       | sudo tee    /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo ""                                                  | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "docker run -d --restart=always                 \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "  --name Flowise                               \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "  -p 3000:3000                                 \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "  -v /var/run/docker.sock:/var/run/docker.sock \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "  -v /Contenedores/Flowise/data:/data          \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "  flowiseai/flowise:latest"                        | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          sudo chmod +x                                                          /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+
+        # Insertar el script iniciador en los comandos post arranque
+          echo ""
+          echo "    Insertando el script iniciador en los ComandosPostArranque..."
+          echo ""
+          echo "/root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh" | sudo tee -a /root/scripts/ParaEsteDebian/ComandosPostArranque.sh
+
+        # Iniciar el docker por primera vez
+          echo ""
+          echo "    Iniciando el container por primera vez..."
+          echo ""
+          sudo /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+
+        # Notificar fin de ejecución del script
+          echo ""
+          echo "  Script de instalación del Docker de TteHive, finalizado."
+          echo ""
+
+      ;;
+
+      2)
+
         echo ""
-      fi
-    menu=(dialog --checklist "¿Donde quieres instalar MediaWiki?:" 22 76 16)
-      opciones=(
-        1 "En un ordenador o máquina virtual" off
-        2 "En un contenedor LXC de Proxmox"   off
-      )
-    choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
+        echo -e "${cColorVerde}  Instalando Flowise en un contenedor LXC...${cFinColor}"
+        echo ""
 
-    for choice in $choices
-      do
-        case $choice in
-
-        1)
-
-          echo ""
-          echo -e "${cColorVerde}  Instalando Flowise en un ordenador o máquina virtual...${cFinColor}"
-          echo ""
-          sudo mkdir -p /Contenedores/Flowise/data 2> /dev/null
-
-          echo ""
-          echo "  Creando el comando para iniciar el contenedor docker..."
-          echo ""
-          echo '#!/bin/bash'                                       | sudo tee    /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo ""                                                  | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "docker run -d --restart=always                 \\" | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "  --name Flowise                               \\" | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "  -p 3000:3000                                 \\" | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "  -v /var/run/docker.sock:/var/run/docker.sock \\" | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "  -v /Contenedores/Flowise/data:/data          \\" | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "  flowiseai/flowise:latest"                        | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          sudo chmod +x                                                          /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-              
-          echo ""
-          echo "  Creando el comando post arranque..."
-          echo ""
-          echo "/root/scripts/DockerCE-Cont-Iniciar-Flowise.sh" | sudo tee -a /root/scripts/ComandosPostArranque.sh
-          echo ""
-          echo "  Iniciando el container por primera vez..."
-          echo ""
-          sudo /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-
-        ;;
-
-        2)
-
-          echo ""
-          echo -e "${cColorVerde}  Instalando Flowise en un contenedor LXC...${cFinColor}"
-          echo ""
+        # Crear carpetas
           sudo mkdir -p /Host/Flowise/data 2> /dev/null
+          sudo mkdir -p /root/scripts/ParaEsteDebian/ 2> /dev/null
 
+        # Crear el script iniciador
           echo ""
-          echo "  Creando el comando para iniciar el contenedor docker..."
+          echo "  Creando el script iniciador..."
           echo ""
-          echo '#!/bin/bash'                                       | sudo tee    /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo ""                                                  | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "docker run -d --restart=always                 \\" | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "  --name Flowise                               \\" | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "  -p 3000:3000                                 \\" | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "  -v /var/run/docker.sock:/var/run/docker.sock \\" | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "  -v /Host/Flowise/data:/data                  \\" | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          echo "  flowiseai/flowise:latest"                        | sudo tee -a /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
-          sudo chmod +x                                                          /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
+          echo '#!/bin/bash'                                       | sudo tee    /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo ""                                                  | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "docker run -d --restart=always                 \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "  --name Flowise                               \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "  -p 3000:3000                                 \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "  -v /var/run/docker.sock:/var/run/docker.sock \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "  -v /Host/Flowise/data:/data                  \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          echo "  flowiseai/flowise:latest"                        | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
+          sudo chmod +x                                                          /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
 
+        # Insertar el script iniciador en los comandos post arranque
           echo ""
-          echo "  Creando el comando post arranque..."
+          echo "    Insertando el script iniciador en los ComandosPostArranque..."
           echo ""
-          echo "/root/scripts/DockerCE-Cont-Iniciar-Flowise.sh" | sudo tee -a /root/scripts/ComandosPostArranque.sh
+          echo "/root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh" | sudo tee -a /root/scripts/ParaEsteDebian/ComandosPostArranque.sh
 
+        # Iniciar el docker por primera vez
           echo ""
-          echo "  Iniciando el container por primera vez..."
+          echo "    Iniciando el container por primera vez..."
           echo ""
-          sudo /root/scripts/DockerCE-Cont-Iniciar-Flowise.sh
+          sudo /root/scripts/ParaEsteDebian/DockerCE-Cont-Flowise-Iniciar.sh
 
-        ;;
+        # Notificar fin de ejecución del script
+          echo ""
+          echo "  Script de instalación del Docker de TteHive, finalizado."
+          echo ""
+
+      ;;
         
-      esac
+    esac
 
-    done
-
-elif [ $cVerSO == "11" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de Flowise en el DockerCE de Debian 11 (Bullseye)..." 
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 11 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "10" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de Flowise en el DockerCE de Debian 10 (Buster)..." 
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "9" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de Flowise en el DockerCE de Debian 9 (Stretch)..." 
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "8" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de Flowise en el DockerCE de Debian 8 (Jessie)..." 
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-elif [ $cVerSO == "7" ]; then
-
-  echo ""
-  echo "  Iniciando el script de instalación de Flowise en el DockerCE de Debian 7 (Wheezy)..." 
-  echo ""
-
-  echo ""
-  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
-  echo ""
-
-fi
+  done
