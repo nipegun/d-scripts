2201d461a609d6276dc88c437a42cc7c8790b837
NiPeGun <nipegun@gmail.com>
2025-02-08 13:10:19 +0100

    Update VaultWarden.sh

--- SoftInst/ParaCLI/Dockers/VaultWarden.sh
+++ SoftInst/ParaCLI/Dockers/VaultWarden.sh
@@ -51,169 +51,93 @@
     do
       case $choice in
 
-        1)
+      1)
 
-          echo ""
-          echo -e "${cColorVerde}  Instalando VaultWarden en un ordenador o máquina virtual...${cFinColor}"
-          echo ""
-          sudo mkdir -p /Contenedores/VaultWarden/data 2> /dev/null
+        echo ""
+        echo -e "${cColorVerde}  Instalando VaultWarden en un ordenador o máquina virtual...${cFinColor}"
+        echo ""
 
-          echo ""
-          echo "  Creando el comando para iniciar el contenedor docker..."
-          echo ""
-          echo '#!/bin/bash'                                         > /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo ""                                                   >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "docker run -d --restart=always                  \\" >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "  --name VaultWarden                            \\" >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "  -p 8080:80                                    \\" >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "  -v /var/run/docker.sock:/var/run/docker.sock  \\" >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "  -v /Contenedores/VaultWarden/data:/data       \\" >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "  vaultwarden:latest"                               >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          chmod +x                                                     /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-              
-          echo ""
-          echo "  Creando el comando post arranque..."
-          echo ""
-          echo "/root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh" >> /root/scripts/ComandosPostArranque.sh
-          echo ""
-          echo "  Iniciando el container por primera vez..."
-          echo ""
-          /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
+        # Crear carpetas
+          sudo mkdir -p /Contenedores/VaultWarden/data/   2> /dev/null
+          sudo mkdir -p /root/scripts/ParaEsteDebian/ 2> /dev/null
 
-        ;;
+        # Crear el script iniciador
+          echo ""
+          echo "    Creando el script iniciador..."
+          echo ""
+          echo '#!/bin/bash'                                       | sudo tee    /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo ""                                                  | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "docker run -d --restart=always                 \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "  --name VaultWarden                           \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "  -p 8080:80                                   \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "  -v /var/run/docker.sock:/var/run/docker.sock \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "  -v /Contenedores/VaultWarden/data:/data      \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "  vaultwarden:latest"                              | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          sudo chmod +x                                                          /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
 
-        2)
+        # Insertar el script iniciador en los comandos post arranque
+          echo ""
+          echo "    Insertando el script iniciador en los ComandosPostArranque..."
+          echo ""
+          echo "/root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh" | sudo tee -a /root/scripts/ParaEsteDebian/ComandosPostArranque.sh
 
+        # Iniciar el docker por primera vez
           echo ""
-          echo -e "${cColorVerde}  Instalando VaultWarden en un contenedor LXC...${cFinColor}"
+          echo "    Iniciando el container por primera vez..."
           echo ""
-          mkdir -p /Host/VaultWarden/data 2> /dev/null
+          sudo /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
 
+        # Notificar fin de ejecución del script
           echo ""
-          echo "  Creando el comando para iniciar el contenedor docker..."
-          echo ""
-          echo '#!/bin/bash'                                        > /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo ""                                                  >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "docker run -d --restart=always                 \\" >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "  --name VaultWarden                           \\" >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "  -p 8080:80                                   \\" >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "  -v /var/run/docker.sock:/var/run/docker.sock \\" >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "  -v /Host/VaultWarden/data:/data              \\" >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          echo "  vaultwarden:latest"                              >> /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-          chmod +x                                                    /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-
-          echo ""
-          echo "  Creando el comando post arranque..."
-          echo ""
-          echo "/root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh" >> /root/scripts/ComandosPostArranque.sh
-
-          echo ""
-          echo "  Iniciando el container por primera vez..."
-          echo ""
-          /root/scripts/DockerCE-Cont-Iniciar-VaultWarden.sh
-
-        ;;
-
-        3)
-
-          echo ""
-          echo -e "${cColorVerde}  ...${cFinColor}"
+          echo "  Script de instalación del Docker de VaultWarden, finalizado."
           echo ""
 
-        ;;
+      ;;
 
-        4)
+      2)
 
+        echo ""
+        echo -e "${cColorVerde}  Instalando VaultWarden en un contenedor LXC...${cFinColor}"
+        echo ""
+
+        # Crear carpetas
+          sudo mkdir -p /Host/VaultWarden/data 2> /dev/null
+          sudo mkdir -p /root/scripts/ParaEsteDebian/ 2> /dev/null
+
+        # Crear el script iniciador
           echo ""
-          echo -e "${cColorVerde}  ...${cFinColor}"
+          echo "  Creando el script iniciador..."
+          echo ""
+          echo '#!/bin/bash'                                       | sudo tee    /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo ""                                                  | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "docker run -d --restart=always                 \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "  --name VaultWarden                           \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "  -p 8080:80                                   \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "  -v /var/run/docker.sock:/var/run/docker.sock \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "  -v /Host/VaultWarden/data:/data              \\" | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          echo "  vaultwarden:latest"                              | sudo tee -a /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+          sudo chmod +x                                                          /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+
+        # Insertar el script iniciador en los comandos post arranque
+          echo ""
+          echo "    Insertando el script iniciador en los ComandosPostArranque..."
+          echo ""
+          echo "/root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh" | sudo tee -a /root/scripts/ParaEsteDebian/ComandosPostArranque.sh
+
+        # Iniciar el docker por primera vez
+          echo ""
+          echo "    Iniciando el container por primera vez..."
+          echo ""
+          sudo /root/scripts/ParaEsteDebian/DockerCE-Cont-VaultWarden-Iniciar.sh
+
+        # Notificar fin de ejecución del script
+          echo ""
+          echo "  Script de instalación del Docker de VaultWarden, finalizado."
           echo ""
 
-https://gist.github.com/heinoldenhuis/f8164f73e5bff048e76fb4fff2e824e1
-
-
-
-  # Desinstalar posibles versiones previas
-    echo ""
-    echo "    Desinstalando posibles versiones previas..."
-    echo ""
-    apt-get -y remove docker
-    apt-get -y remove docker-engine
-    apt-get -y remove docker.io
-    apt-get -y remove containerd
-    apt-get -y remove runc
-    rm -rf /var/lib/docker
-    rm -rf /var/lib/containerd
- 
-  # Agregar el repositorio de Docker
-    echo ""
-    echo "    Agregando repositorio de docker..."
-    echo ""
-    apt-get -y update
-    apt-get -y install ca-certificates
-    apt-get -y install curl
-    apt-get -y install gnupg
-    apt-get -y install lsb-release
-    mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get -y update
-    chmod a+r /etc/apt/keyrings/docker.gpg
-    apt-get -y update
-
-  # Instalar Docker Engine
-    echo ""
-    echo "    Instalando docker engine..."
-    echo ""
-    apt-get -y install docker-ce
-    apt-get -y install docker-ce-cli
-    apt-get -y install containerd.io
-    apt-get -y install docker-compose-plugin
-    # Comprobar que docker-engine se instaló
-      # docker run hello-world
-
-  # Crear usuario y grupo
-    echo ""
-    echo "    Creando el usuario vaultwarden..."
-    echo ""
-    adduser vaultwarden
-    #echo ""
-    #echo "    Asignando contraseña al usuario bitwarden..."    #echo ""
-    #passwd vaultwarden
-    echo ""
-    echo "    Creando el grupo docker..."
-    echo ""
-    groupadd docker
-    echo ""
-    echo "    Agregando el usuario vaultwarden al grupo docker..."
-    echo ""
-    usermod -aG docker vaultwarden
-    echo ""
-    echo "    Creando la carpeta de instalación para vaultwarden..."
-    echo ""
-    mkdir /opt/vaultwarden
-    chmod -R 700 /opt/vaultwarden
-    chown -R vaultwarden:vaultwarden /opt/vaultwarden
-
-  # Instalar bitwarden desde el script oficial
-    echo ""
-    echo "    Instalando el docker de BitWarden..."
-    echo ""
-    docker pull vaultwarden/server:latest
-    docker run -d --name vaultwarden -v /vw-data/:/data/ -p 80:80 vaultwarden/server:latest
-
-  # Agregando bitwarden a los ComandosPostArranque
-    echo ""                                    >> /root/scripts/ComandosPostArranque.sh
-    echo "# Iniciar BitWarden"                 >> /root/scripts/ComandosPostArranque.sh
-    echo "  /opt/bitwarden/bitwarden.sh start" >> /root/scripts/ComandosPostArranque.sh
-    echo ""                                    >> /root/scripts/ComandosPostArranque.sh
-
-
-
-
-
-        ;;
+      ;;
         
-      esac
+    esac
 
-    done
+  done
 
