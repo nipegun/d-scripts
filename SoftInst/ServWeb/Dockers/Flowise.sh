6c2663b431a3536913eb5bcc9acd86fa09d11808
NiPeGun <nipegun@gmail.com>
2025-02-27 16:59:36 +0100

    Update and rename Flowise.sh to Flowise.sh

--- SoftInst/ServWeb/Dockers/Flowise.sh
+++ SoftInst/ServWeb/Dockers/Flowise.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para instalar Flowise en el DockerCE de Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Dockers/Flowise.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ServWeb/Dockers/Flowise.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/Dockers/Flowise.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ServWeb/Dockers/Flowise.sh | sed 's-sudo--g' | bash
 #
 # Comando: docker run -d --restart=always --name Flowise -p 3000:3000 flowiseai/flowise:latest
 # ----------
