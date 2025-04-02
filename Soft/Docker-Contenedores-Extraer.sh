9607f1fc90a2bc0439ab17adabb9f1905b882da6
NiPeGun <nipegun@gmail.com>
2025-04-02 21:51:03 +0200

    Create Docker-Contenedores-Extraer.sh

---
+++ Soft/Docker-Contenedores-Extraer.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# Definir fecha de ejecución del script
+  cFechaDeEjec=$(date +a%Ym%md%d@%T)
+
+# Crear el menú
+  # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${cColorRojo}  El paquete dialog no está instalado. Iniciando su instalación...${cFinColor}"
+      echo ""
+      sudo apt-get -y update
+      sudo apt-get -y install dialog
+      echo ""
+    fi
+  menu=(dialog --checklist "Marca las opciones que quieras instalar:" 22 96 16)
+    opciones=(
+      1 "Clonado sin detener" on
+      2 "Opción 2" off
+      3 "Opción 3" off
+      4 "Opción 4" off
+      5 "Opción 5" off
+    )
+  choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
+    for choice in $choices
+      do
+        case $choice in
+
+          1)
+
+            echo ""
+            echo "  Opción 1..."
+            echo ""
+
+          ;;
+
+          2)
+
+            echo ""
+            echo "  Opción 2..."
+            echo ""
+
+          ;;
+
+          3)
+
+            echo ""
+            echo "  Opción 3..."
+            echo ""
+
+          ;;
+
+          4)
+
+            echo ""
+            echo "  Opción 4..."
+            echo ""
+
+          ;;
+
+          5)
+
+            echo ""
+            echo "  Opción 5..."
+            echo ""
+
+          ;;
+
+      esac
+
+  done
+
