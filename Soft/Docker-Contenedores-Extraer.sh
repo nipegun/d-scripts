fc50245d95a90c3c58cfac00ac003826dd0731ed
NiPeGun <nipegun@gmail.com>
2025-04-02 21:51:31 +0200

    Update Docker-Contenedores-Extraer.sh

--- Soft/Docker-Contenedores-Extraer.sh
+++ Soft/Docker-Contenedores-Extraer.sh
@@ -5,6 +5,33 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
+# ----------
+# Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
+# Definir constantes de color
+  cColorAzul='\033[0;34m'
+  cColorAzulClaro='\033[1;34m'
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
 # Definir fecha de ejecución del script
   cFechaDeEjec=$(date +a%Ym%md%d@%T)
 
