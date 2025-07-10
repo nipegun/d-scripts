2b0806df82e4cb8506396c1be0d1044088f87ff4
NiPeGun <nipegun@gmail.com>
2025-07-11 01:40:39 +0200

    Update NodeVersionManager-Instalar.sh

--- SoftInst/ParaCLI/NodeVersionManager-Instalar.sh
+++ SoftInst/ParaCLI/NodeVersionManager-Instalar.sh
@@ -1,1 +1,20 @@
-.
+#!/bin/bash
+
+
+            # Instalar paquetes necesarios para la correcta ejecución del script
+              sudo apt-get -y update
+              sudo apt-get -y install curl 2> /dev/null
+              sudo apt-get -y install jq   2> /dev/null
+
+              echo ""
+              echo "      Determinando la última versión de nvm..."
+              echo ""
+              vUltVersNVM=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r '.tag_name')
+              echo "        La última versión de nvm es $vUltVersNVM"
+              echo ""
+              echo ""
+              echo "      Instalando..."
+              echo ""
+              cd /tmp
+              curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/"$vUltVersNVM"/install.sh | bash
+
