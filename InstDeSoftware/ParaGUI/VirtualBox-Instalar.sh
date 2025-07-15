5e83593d062dccf2bff69549e5b374748994830d
NiPeGun <nipegun@gmail.com>
2025-07-16 01:00:58 +0200

    Update VirtualBox-Instalar.sh

--- InstDeSoftware/ParaGUI/VirtualBox-Instalar.sh
+++ InstDeSoftware/ParaGUI/VirtualBox-Instalar.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para instalar y configurar VirtualBox en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaGUI/VirtualBox-Instalar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/InstDeSoftware/ParaGUI/VirtualBox-Instalar.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaGUI/VirtualBox-Instalar.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/InstDeSoftware/ParaGUI/VirtualBox-Instalar.sh | sed 's-sudo--g' | bash
 # ----------
 
 # Definir constantes de color
