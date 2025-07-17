bede4304a4ade85160b129ce3ec497c3a78f3c1c
NiPeGun <nipegun@gmail.com>
2025-07-17 12:00:46 +0200

    Update scrcpy-Instalar.sh

--- InstDeSoftware/ParaCLI/scrcpy-Instalar.sh
+++ InstDeSoftware/ParaCLI/scrcpy-Instalar.sh
@@ -9,19 +9,13 @@
 # Script de NiPeGun para instalar y configurar scrcpy en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL x | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/InstDeSoftware/ParaCLI/scrcpy-Instalar.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL x | sed 's-sudo--g' | bash
-#
-# Ejecución remota sin caché:
-#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
-#
-# Ejecución remota con parámetros:
-#   curl -sL x | bash -s Parámetro1 Parámetro2
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/InstDeSoftware/ParaCLI/scrcpy-Instalar.sh | sed 's-sudo--g' | bash
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL x | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/InstDeSoftware/ParaCLI/scrcpy-Instalar.sh | nano -
 # ----------
 
 # Definir constantes de color
