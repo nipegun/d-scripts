62b8ae115df6ef7264e7a072fdc6422e192170d9
NiPeGun <nipegun@gmail.com>
2025-07-23 05:42:07 +0200

    Update Repositorio-Unstable-Agregar.sh

--- PostInstDebian/CLI/Repositorio-Unstable-Agregar.sh
+++ PostInstDebian/CLI/Repositorio-Unstable-Agregar.sh
@@ -9,7 +9,7 @@
 # Script de NiPeGun para agregar el repositorio unstable a Debian
 #
 # Ejecución remota:
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/PostInst/CLI/Repositorio-Unstable-Agregar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/PostInstDebian/CLI/Repositorio-Unstable-Agregar.sh | bash
 #
 # Para instalar un paquete desde backports:
 #   apt-get -y install -t unstable NombreDelPaquete
