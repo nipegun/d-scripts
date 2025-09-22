99ee353eb035c6246268bf7e5d35f4b71e5d7eca
NiPeGun <nipegun@gmail.com>
2025-09-22 03:27:23 +0200

    Update VirtualBox-Instalar.sh

--- InstDeSoftware/ParaGUI/VirtualBox-Instalar.sh
+++ InstDeSoftware/ParaGUI/VirtualBox-Instalar.sh
@@ -48,9 +48,152 @@ if [ $cVerSO == "13" ]; then
   echo "  Iniciando el script de instalación de VirtualBox para Debian 13 (x)..."  
   echo ""
 
-  echo ""
-  echo "  Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian."
-  echo ""
+  # Comprobar si el paquete dialog está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s dialog 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${cColorRojo}    El paquete dialog no está instalado. Iniciando su instalación...${cFinColor}"
+      echo ""
+      sudo apt-get -y update
+      sudo apt-get -y install dialog
+      echo ""
+    fi
+
+  # Crear el menú
+    menu=(dialog --checklist "Marca las opciones que quieras instalar:" 22 96 16)
+      opciones=(
+        1 "Instalar la versión disponible en el repositorio de Debian" off
+        2 "Instalar la última versión desde la web"                    off
+      )
+    choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
+
+    for choice in $choices
+      do
+        case $choice in
+
+          1)
+
+            echo ""
+            echo "  Instalando la última versión de VirtualBox disponible en los repositorios de Debian..."
+            echo ""
+
+            # Instalar paquetes necesarios
+               echo ""
+               echo "    Instalando paquetes necesarios..."
+               echo ""
+               sudo apt-get -y update
+               sudo apt-get -y install linux-headers-$(uname -r)
+               sudo apt-get -y install dkms
+
+             # Agregar repositorio
+               echo ""
+               echo "    Agregando repositorio de VirtualBox..."
+               echo ""
+               sudo apt-get -y install gnupg2
+               # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
+                 if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
+                   echo ""
+                   echo "      El paquete wget no está instalado. Iniciando su instalación..."
+                   echo ""
+                   sudo apt-get -y update
+                   sudo apt-get -y install wget
+                   echo ""
+                 fi
+               wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+               wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+               sudo echo "deb [arch=$(dpkg --print-architecture)] http://download.virtualbox.org/virtualbox/debian rixie contrib" > /etc/apt/sources.list.d/virtualbox.list
+               sudo apt-get -y update
+
+             # Instalar virtualbox
+               echo ""
+               echo "    Instalando el paquete virtualbox..."
+               echo ""
+               PaqueteAInstalar=$(apt-cache search virtualbox | grep "virtualbox-" | tail -n1 | cut -d' ' -f1)
+               sudo apt-get -y install $PaqueteAInstalar
+
+             # Instalar el pack de extensiones
+               echo ""
+               echo "    Instalando el pack de extensiones..."
+               echo ""
+               sudo mkdir -p /root/SoftInst/VirtualBox/
+               VersDeVBoxInstalada=$(virtualbox -h | grep "VirtualBox Manager" | cut -d'v' -f2)
+               sudo wget http://download.virtualbox.org/virtualbox/$VersDeVBoxInstalada/Oracle_VirtualBox_Extension_Pack-$VersDeVBoxInstalada.vbox-extpack -O /root/SoftInst/VirtualBox/Oracle_VirtualBox_Extension_Pack-$VersDeVBoxInstalada.vbox-extpack
+               echo y | sudo vboxmanage extpack install --replace /root/SoftInst/VirtualBox/Oracle_VirtualBox_Extension_Pack-$VersDeVBoxInstalada.vbox-extpack
+
+             # Agregar el usuario 1000 al grupo virtualbox
+               echo ""
+               echo "    Agregando el usuario 1000 en el grupo virtualbox..."
+               echo ""
+               Usuario1000=$(id 1000 | cut -d'(' -f2 | cut -d')' -f1)
+               sudo usermod -a -G vboxusers $Usuario1000
+
+          ;;
+
+          2)
+
+            echo ""
+            echo "  Instalando la última versión disponible en la web..."
+            echo ""
+
+            # Determinar la última versión
+              echo ""
+              echo "  Determinando la última versión..."
+              echo ""
+              # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+                if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+                  echo ""
+                  echo "    El paquete curl no está instalado. Iniciando su instalación..."
+                  echo ""
+                  sudo apt-get -y update
+                  sudo apt-get -y install curl
+                  echo ""
+                fi
+              vUltVersEnWeb=$(curl -sL http://download.virtualbox.org/virtualbox/LATEST.TXT)
+              echo "    La última versión disponible en la web es la $vUltVersEnWeb"
+              echo ""
+
+            # Determinar el enlace de descarga del archivo .deb
+              echo ""
+              echo "  Determinando el enlace de descarga del archivo .deb..."
+              echo ""
+              vEnlaceArchivoDeb=$(curl -sL https://download.virtualbox.org/virtualbox/$vUltVersEnWeb/ | grep rixie | cut -d'"' -f2)
+              echo "    En enlace de descarga es: https://download.virtualbox.org/virtualbox/$vUltVersEnWeb/$vEnlaceArchivoDeb"
+              echo ""
+
+            # Descargar archivo .deb
+              echo ""
+              echo "  Descargando archivo .deb..."
+              echo ""
+              cd /tmp/
+              # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
+                if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
+                  echo ""
+                  echo "    El paquete wget no está instalado. Iniciando su instalación..."
+                  echo ""
+                  sudo apt-get -y update
+                  sudo apt-get -y install wget
+                  echo ""
+                fi
+              wget https://download.virtualbox.org/virtualbox/$vUltVersEnWeb/$vEnlaceArchivoDeb
+
+            # Instalar el paquete
+              echo ""
+              echo "  Instalando el paquete .deb..."
+              echo ""
+              sudo apt -y install /tmp/$vEnlaceArchivoDeb
+
+            # Instalar el pack de extensiones
+              echo ""
+              echo "  Instalando el pack de extensiones..."
+              echo ""
+              cd /tmp/
+              wget http://download.virtualbox.org/virtualbox/$vUltVersEnWeb/Oracle_VirtualBox_Extension_Pack-$vUltVersEnWeb.vbox-extpack
+              echo y | sudo vboxmanage extpack install --replace /tmp/Oracle_VirtualBox_Extension_Pack-$vUltVersEnWeb.vbox-extpack
+
+          ;;
+
+      esac
+
+  done
 
 elif [ $cVerSO == "12" ]; then
 
