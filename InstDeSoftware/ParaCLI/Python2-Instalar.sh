36c026dd075311e61cedf25038b95a0a4649bf99
NiPeGun <nipegun@gmail.com>
2025-09-23 02:49:15 +0200

    Update Python2-Instalar.sh

--- InstDeSoftware/ParaCLI/Python2-Instalar.sh
+++ InstDeSoftware/ParaCLI/Python2-Instalar.sh
@@ -55,9 +55,68 @@
     echo -e "${cColorAzulClaro}  Iniciando el script de instalación de python2 para Debian 13 (x)...${cFinColor}"
     echo ""
 
-    echo ""
-    echo -e "${cColorRojo}    Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
-    echo ""
+    # Instalar paquetes necesarios para compilar
+      sudo apt-get -y update
+      sudo apt-get -y install build-essential
+      sudo apt-get -y install zlib1g-dev
+      sudo apt-get -y install libssl-dev
+      sudo apt-get -y install libncurses5-dev
+      sudo apt-get -y install libffi-dev
+      sudo apt-get -y install libsqlite3-dev
+      sudo apt-get -y install libncursesw5-dev
+      sudo apt-get -y install libreadline-dev
+      sudo apt-get -y install libsqlite3-dev
+      sudo apt-get -y install libgdbm-dev
+      sudo apt-get -y install libdb5.3-dev
+      sudo apt-get -y install libbz2-dev
+      sudo apt-get -y install libexpat1-dev
+      sudo apt-get -y install liblzma-dev
+      sudo apt-get -y install zlib1g-dev
+      sudo apt-get -y install tk-dev
+      sudo apt-get -y install tcl-dev
+    # Descargar el código fuente
+      # Determinar la última versión
+        echo ""
+        echo "    Determinando la última versión de Python 2..."
+        echo ""
+        # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+          if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+            echo ""
+            echo -e "${cColorRojo}      El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+            echo ""
+            sudo apt-get -y update
+            sudo apt-get -y install curl
+            echo ""
+          fi
+        vUltVersPython2=$(curl -sL https://www.python.org/ftp/python/ | grep href | cut -d'"' -f2 | cut -d'/' -f1 | grep ^[0-9] | sort -n | grep ^2 | tail -n1)
+        echo ""
+        echo "      La última versión es la $vUltVersPython2"
+        echo ""
+    # Descargando la última versión
+      sudo rm -rf /tmp/SoftInst/Python-$vUltVersPython2/
+      sudo mkdir -p /tmp/SoftInst/
+      sudo rm -f /tmp/python2.tgz
+      curl -L https://www.python.org/ftp/python/$vUltVersPython2/Python-$vUltVersPython2.tgz -o /tmp/python2.tgz
+      sudo tar -xzf /tmp/python2.tgz -C /tmp/SoftInst/
+      cd /tmp/SoftInst/Python-$vUltVersPython2/
+      # sudo ./configure --prefix=/usr/local --with-ssl=/path/to/openssl
+      # sudo ./configure --prefix=/opt/python2 --with-ensurepip=install --enable-optimizations   # Instala también /opt/python2/bin/pip2.7
+      sudo ./configure --prefix=/opt/python2 --enable-optimizations
+      sudo make -j $(nproc)
+      sudo make altinstall       # No se usa install para no sobreescribir la instalación de Python3
+      # Instalar pip2
+        vVersBinario=$(ls /opt/python2/bin/ | grep python | grep -v config | sed 's-python--g')
+        curl -L https://bootstrap.pypa.io/pip/$vVersBinario/get-pip.py -o /tmp/get-pip.py
+        sudo /opt/python2/bin/python$vVersBinario /tmp/get-pip.py
+      # Instalar virtualenv
+        sudo /opt/python2/bin/pip2 install virtualenv
+
+    # Notificar fin de ejecución del script
+      echo ""
+      echo "    Python $vUltVersPython2 se ha instalado en /opt/python2/"
+      echo ""
+      echo "      El binario está en /opt/python2/bin/python$vVersBinario"
+      echo ""
 
   elif [ $cVerSO == "12" ]; then
 
