1f4a8234363d626bbb19fc093ccdf9cf0f397e6d
NiPeGun <nipegun@gmail.com>
2025-10-04 13:50:09 +0200

    Update SIEM-Wazuh-Agente-Instalar.sh

--- InstDeSoftware/ParaCLI/SIEM-Wazuh-Agente-Instalar.sh
+++ InstDeSoftware/ParaCLI/SIEM-Wazuh-Agente-Instalar.sh
@@ -46,16 +46,6 @@ vWazuhServerIP='192.168.1.147'
     exit
   fi
 
-# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
-  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
-    echo ""
-    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
-    echo ""
-    sudo apt-get -y update
-    sudo apt-get -y install curl
-    echo ""
-  fi
-
 # Determinar la versión de Debian
   if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
     . /etc/os-release
