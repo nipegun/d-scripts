9487a3383443db20502fdde9ce1416d42dc1e914
NiPeGun <nipegun@gmail.com>
2025-10-04 07:28:41 +0200

    Update and rename sudo-Desinstalar.sh to sudo-DesinstalarYPurgar.sh

--- PostInstDebian/CLI/sudo-Desinstalar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-su -
-apt-get -y purge sudo
-apt-get -y autoremove --purge
-apt-get -y clean
-
---
+++ PostInstDebian/CLI/sudo-DesinstalarYPurgar.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para desinstalar y purgar sudo de un sistema Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
+su -
+apt-get -y purge sudo
+apt-get -y autoremove --purge
+apt-get -y clean
+
