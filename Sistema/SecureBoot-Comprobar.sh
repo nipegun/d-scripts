7773ee7055a707f3ab2178186ac2a5c489e6b8a5
NiPeGun <nipegun@gmail.com>
2025-10-04 06:59:14 +0200

    Update SecureBoot-Comprobar.sh

--- Sistema/SecureBoot-Comprobar.sh
+++ Sistema/SecureBoot-Comprobar.sh
@@ -6,13 +6,10 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para instalar nueva llave para firmar repositorios en Debian
+# Script de NiPeGun para comprobar si secure boot está habilitado
 #
 # Ejecución remota:
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/APTKey-BajarEInstalar.sh | bash -s URL Servicio
-#
-#  Ejemplo:
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/APTKey-BajarEInstalar.sh | bash -s https://nightly.odoo.com/odoo.key Odoo
+#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/Sistema/SecureBoot-Comprobar.sh | bash
 # ----------
 
 mokutil --sb-state
