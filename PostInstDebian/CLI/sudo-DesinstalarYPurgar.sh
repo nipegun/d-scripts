b8bc30b3bdbac9356573ab1630676b363a39dded
NiPeGun <nipegun@gmail.com>
2025-10-04 07:30:37 +0200

    Update sudo-DesinstalarYPurgar.sh

--- PostInstDebian/CLI/sudo-DesinstalarYPurgar.sh
+++ PostInstDebian/CLI/sudo-DesinstalarYPurgar.sh
@@ -8,23 +8,22 @@
 # ----------
 # Script de NiPeGun para desinstalar y purgar sudo de un sistema Debian
 #
-# Ejecución remota (puede requerir permisos sudo):
-#   curl -sL x | bash
-#
-# Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL x | sed 's-sudo--g' | bash
-#
-# Ejecución remota sin caché:
-#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
-#
-# Ejecución remota con parámetros:
-#   curl -sL x | bash -s Parámetro1 Parámetro2
+# Ejecución remota como root:
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/PostInstDebian/CLI/sudo-DesinstalarYPurgar.sh | bash
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL x | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/PostInstDebian/CLI/sudo-DesinstalarYPurgar.sh | nano -
+#
+# NOTAS:
+#   Loguearse como root para ejecutar este script
 # ----------
 
-su -
+# Comprobar si el script está corriendo como root
+  if [ $(id -u) -ne 0 ]; then
+    echo -e "${cColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${cFinColor}"
+    exit
+  fi
+
 apt-get -y purge sudo
 apt-get -y autoremove --purge
 apt-get -y clean
