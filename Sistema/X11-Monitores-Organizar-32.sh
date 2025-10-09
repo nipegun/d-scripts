41ef86d79342b81bf71355bc4dc331930b310e51
NiPeGun <nipegun@gmail.com>
2025-10-09 07:21:40 +0200

    Update and rename X11-Monitores-Organizar-31.sh to X11-Monitores-Organizar-32.sh

--- Sistema/X11-Monitores-Organizar-32.sh
+++ Sistema/X11-Monitores-Organizar-32.sh
@@ -49,7 +49,7 @@ echo "Hecho."
 echo ""
 
 # Detectar salidas conectadas
-aSalidasConectadas=($(xrandr | grep " connected" | grep -E "^(DP|HDMI|eDP|DisplayPort)" | sed -E 's/.* ([0-9]+)x[0-9]+\+([0-9]+)\+.*/\2 \0/' | sort -n | awk '{print $3}' ))
+aSalidasConectadas=($(xrandr | grep " connected" | grep -E "^(DP|HDMI|eDP|DisplayPort)" | sed -E 's/.* ([0-9]+)x[0-9]+\+([0-9]+)\+.*/\2 \0/' | sort -n | awk '{print $2}' | cut -d' ' -f1))
 
 if [ ${#aSalidasConectadas[@]} -eq 0 ]; then
   echo "  No se detectaron monitores conectados."
