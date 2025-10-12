df48c7921c80d4a08fe3897e58bfae92513a8b7f
NiPeGun <nipegun@gmail.com>
2025-10-12 14:52:35 +0200

    Update SIEM-Wazuh-Agente-Instalar.sh

--- InstDeSoftware/ParaCLI/SIEM-Wazuh-Agente-Instalar.sh
+++ InstDeSoftware/ParaCLI/SIEM-Wazuh-Agente-Instalar.sh
@@ -133,6 +133,74 @@ vWazuhServerIP="$1"
           echo "  Instalando wazuh-agent en un contendor LXC de Proxmox..."
           echo ""
 
+          # Instalar paquetes necesarios para el correcto funcionamiento del agente
+            sudo apt-get -y update
+            sudo apt-get -y install net-tools
+
+          # Descargar el script de instalación
+            cd /tmp/
+            # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
+              if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
+                echo ""
+                echo -e "${cColorRojo}  El paquete wget no está instalado. Iniciando su instalación...${cFinColor}"
+                echo ""
+                sudo apt-get -y update
+                sudo apt-get -y install wget
+                echo ""
+              fi
+            wget https://packages.wazuh.com/"$vVersWazuh"/apt/pool/main/w/wazuh-agent/"$vArchivoDeb"
+
+          # Lanzar el script de instalación
+            sudo WAZUH_MANAGER="$vWazuhServerIP" apt -y install ./"$vArchivoDeb"
+
+          # Iniciar el servicio
+            sudo systemctl daemon-reload
+            sudo systemctl enable wazuh-agent
+            sudo systemctl start wazuh-agent
+            sleep 3
+            sudo systemctl status wazuh-agent --no-pager
+
+          # Conectar con auditd
+            sudo mkdir -p /var/ossec/etc/rules/ 2> /dev/null
+            echo '<group name="auditd,">'                                          | sudo tee    /var/ossec/etc/rules/local_rules.xml
+            echo '  <rule id="100501" level="10">'                                 | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <if_sid>80700</if_sid>'                                      | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <field name="audit.key">lectura_passwd</field>'              | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <description>Lectura del archivo /etc/passwd</description>'  | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '  </rule>'                                                       | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo ''                                                                | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '  <rule id="100502" level="12">'                                 | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <if_sid>80700</if_sid>'                                      | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <field name="audit.key">lectura_shadow</field>'              | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <description>Lectura del archivo /etc/shadow</description>'  | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '  </rule>'                                                       | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo ''                                                                | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '  <rule id="100503" level="8">'                                  | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <if_sid>80700</if_sid>'                                      | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <field name="audit.key">listado_home</field>'                | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <description>Intento de listado de /home (ls)</description>' | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '  </rule>'                                                       | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo ''                                                                | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '  <rule id="100504" level="5">'                                  | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <if_sid>80700</if_sid>'                                      | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <field name="audit.key">ejecucion_pwd</field>'               | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <description>Ejecución del comando pwd</description>'        | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '  </rule>'                                                       | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo ''                                                                | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '  <rule id="100505" level="5">'                                  | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <if_sid>80700</if_sid>'                                      | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <field name="audit.key">ejecucion_whoami</field>'            | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '    <description>Ejecución del comando whoami</description>'     | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '  </rule>'                                                       | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+            echo '</group>'                                                        | sudo tee -a /var/ossec/etc/rules/local_rules.xml
+
+          # Agregar el archivo de log de audit
+            sudo sed -i -e s'|</ossec_config>|  <localfile>\n    <log_format>audit</log_format>\n    <location>/var/log/audit/audit.log</location>\n  </localfile>\n\n</ossec_config>|' /var/ossec/etc/ossec.conf
+
+          # Reinciar wazuh-manager
+            sudo systemctl restart wazuh-agent
+
+
         ;;
 
     esac
