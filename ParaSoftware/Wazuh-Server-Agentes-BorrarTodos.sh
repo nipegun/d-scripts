0b1c52a2fa623a65b6d9d6ed36ca75cefeacfd14
NiPeGun <nipegun@gmail.com>
2025-10-12 16:32:07 +0200

    Create Wazuh-Server-Agentes-BorrarTodos.sh

---
+++ ParaSoftware/Wazuh-Server-Agentes-BorrarTodos.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para borrar todos los agentes de un servidor Wazuh
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/SIEM-Wazuh-InstalarYConfigurar.sh | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/SIEM-Wazuh-InstalarYConfigurar.sh | sed 's-sudo--g' | bash
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/SIEM-Wazuh-InstalarYConfigurar.sh | nano -
+# ----------
+
+# Definir constantes de color
+  cColorAzul='\033[0;34m'
+  cColorAzulClaro='\033[1;34m'
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Notificar inicio de ejecución del script
+  echo ""
+  echo -e "${cColorAzulClaro}  Iniciando el script para borrar todos los agentes agregados al servidor Wazuh...${cFinColor}"
+  echo ""
+
+# Parar el servicio
+  sudo systemctl stop wazuh-manager
+
+# Borrar los archivos que almacenan los agentes:
+  sudo rm -fv /var/ossec/etc/client.keys
+  sudo rm -rfv /var/ossec/queue/agent-info/*
+  sudo rm -rfv /var/ossec/queue/rids/*
+
+# Iniciar el servicio nuevamente:
+  sudo systemctl start wazuh-manager
+
