25d4e5695419df0c6933d6f6d30da8ae3f5e6a89
NiPeGun <nipegun@gmail.com>
2025-10-12 13:09:31 +0200

    Update and rename SIEM-Splunk-InstalarYConfigurar.sh to SIEM-Splunk-Instalar.sh

--- InstDeSoftware/ParaCLI/SIEM-Splunk-Instalar.sh
+++ InstDeSoftware/ParaCLI/SIEM-Splunk-Instalar.sh
@@ -9,13 +9,13 @@
 # Script de NiPeGun para instalar y configurar splunk en Debian
 #
 # Ejecución remota (puede requierir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/SIEM-Splunk-InstalarYConfigurar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/SIEM-Splunk-Instalar.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/SIEM-Splunk-InstalarYConfigurar.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/SIEM-Splunk-Instalar.sh | sed 's-sudo--g' | bash
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/SIEM-Splunk-InstalarYConfigurar.sh | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/SoftInst/ParaCLI/SIEM-Splunk-Instalar.sh | nano -
 # ----------
 
 vSplunk="9.4.1"
