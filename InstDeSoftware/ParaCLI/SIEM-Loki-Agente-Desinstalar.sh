123d83c40de0f0e48a5df5ef50ff05aaa5fdd4fb
NiPeGun <nipegun@gmail.com>
2025-10-12 13:26:03 +0200

    Create SIEM-Loki-Agente-Desinstalar.sh

---
+++ InstDeSoftware/ParaCLI/SIEM-Loki-Agente-Desinstalar.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para desinstalar el agente Promtail de Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/SIEM-Loki-Agente-Desinstalar.sh | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/SIEM-Loki-Agente-Desinstalar.sh | sed 's-sudo--g' | bash
+# ----------
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
+    . /etc/os-release
+    cNomSO=$NAME
+    cVerSO=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # Para linuxbase.org.
+    cNomSO=$(lsb_release -si)
+    cVerSO=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release.
+    . /etc/lsb-release
+    cNomSO=$DISTRIB_ID
+    cVerSO=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    cNomSO=Debian
+    cVerSO=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD).
+    cNomSO=$(uname -s)
+    cVerSO=$(uname -r)
+  fi
+
+if [ $cVerSO == "13" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de Promtail para Debian 13 (x)..."
+  echo ""
+
+  # Parar y desactivar el servicio
+    sudo systemctl stop promtail.service
+    sudo systemctl disable promtail.service --now
+
+  # Borrar el archivo del servicio
+    sudo rm -vf /etc/systemd/system/promtail.service
+
+  # Recargar los daemons
+    sudo systemctl daemon-reload
+
+  # Quitar el usuario del grupo
+    sudo gpasswd -d prommtail adm
+
+  # Borrar el usuario
+    sudo userdel -r promtail
+
+  # Borrar carpetas
+    sudo rm -rvf /etc/promtail/
+    sudo rm -rvf /var/lib/promtail/
+    sudo rm -rvf /var/log/promtail/
+
+elif [ $cVerSO == "12" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de Promtail para Debian 12 (Bullseye)..."  
+  echo ""
+
+  # Parar y desactivar el servicio
+    sudo systemctl stop promtail.service
+    sudo systemctl disable promtail.service --now
+
+  # Borrar el archivo del servicio
+    sudo rm -vf /etc/systemd/system/promtail.service
+
+  # Recargar los daemons
+    sudo systemctl daemon-reload
+
+  # Quitar el usuario del grupo
+    sudo gpasswd -d prommtail adm
+
+  # Borrar el usuario
+    sudo userdel -r promtail
+
+  # Borrar carpetas
+    sudo rm -rvf /etc/promtail/
+    sudo rm -rvf /var/lib/promtail/
+    sudo rm -rvf /var/log/promtail/
+
+elif [ $cVerSO == "11" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de Promtail para Debian 11 (Bullseye)..."  
+  echo ""
+
+  # Parar y desactivar el servicio
+    sudo systemctl stop promtail.service
+    sudo systemctl disable promtail.service --now
+
+  # Borrar el archivo del servicio
+    sudo rm -vf /etc/systemd/system/promtail.service
+
+  # Recargar los daemons
+    sudo systemctl daemon-reload
+
+  # Quitar el usuario del grupo
+    sudo gpasswd -d prommtail adm
+
+  # Borrar el usuario
+    sudo userdel -r promtail
+
+  # Borrar carpetas
+    sudo rm -rvf /etc/promtail/
+    sudo rm -rvf /var/lib/promtail/
+    sudo rm -rvf /var/log/promtail/
+
+elif [ $cVerSO == "10" ]; then
+
+  echo ""
+  
+  echo "  Iniciando el script de instalación de Promtail para Debian 10 (Buster)..."  
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "9" ]; then
+
+  echo ""
+  
+  echo "  Iniciando el script de instalación de Promtail para Debian 9 (Stretch)..."  
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "8" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de Promtail para Debian 8 (Jessie)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $cVerSO == "7" ]; then
+
+  echo ""
+  echo "  Iniciando el script de instalación de Promtail para Debian 7 (Wheezy)..."
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+fi
