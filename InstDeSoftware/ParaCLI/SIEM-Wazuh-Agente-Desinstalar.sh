4e8afc0f1cfe2f6b5f7692e7db7f95b38bce13ca
NiPeGun <nipegun@gmail.com>
2025-10-12 15:31:10 +0200

    Create SIEM-Wazuh-Agente-Desinstalar.sh

---
+++ InstDeSoftware/ParaCLI/SIEM-Wazuh-Agente-Desinstalar.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para desinstalar wazuh-agent de Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/InstDeSoftware/ParaCLI/SIEM-Wazuh-Agente-Desinstalar.sh | bash -s '[IPDelWazuhServer]'
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/InstDeSoftware/ParaCLI/SIEM-Wazuh-Agente-Desinstalar.sh | sed 's-sudo--g' | bash -s '[IPDelWazuhServer]'
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/InstDeSoftware/ParaCLI/SIEM-Wazuh-Agente-Desinstalar.sh | nano -
+# ----------
+
+# Definir constantes de color
+  cColorAzul='\033[0;34m'
+  cColorAzulClaro='\033[1;34m'
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Reinciar wazuh-manager
+  sudo systemctl stop wazuh-agent
+  sudo systemctl disable wazuh-agent --now
+# Borrar paquete
+  sudo apt-get -y autoremove --purge wazuh-agent
+# Borrar carpetas
+  sudo rm -rf /var/ossec/
