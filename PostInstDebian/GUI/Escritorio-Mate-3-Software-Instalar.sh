33c4c8e1da4f4b2eeab80a50e54e290e7250c165
NiPeGun <nipegun@gmail.com>
2025-10-22 00:34:56 +0200

    Update and rename Escritorio-Mate-Software-Instalar.sh to Escritorio-Mate-3-Software-Instalar.sh

--- PostInstDebian/GUI/Escritorio-Mate-3-Software-Instalar.sh
+++ PostInstDebian/GUI/Escritorio-Mate-3-Software-Instalar.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para instalar software en el escritorio Mate de Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/PostInstDebian/GUI/Escritorio-Mate-Software-Instalar.sh | bash
+#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/PostInstDebian/GUI/Escritorio-Mate-Software-3-Instalar.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/PostInstDebian/GUI/Escritorio-Mate-Software-Instalar.sh | sed 's-sudo--g' | bash
+#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/PostInstDebian/GUI/Escritorio-Mate-Software-3-Instalar.sh | sed 's-sudo--g' | bash
 # ----------
 
 # Definir constantes de color
