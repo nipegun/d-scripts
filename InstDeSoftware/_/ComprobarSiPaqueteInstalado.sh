06fc577e72b2728b998371c1b0fcfe0171d8e434
NiPeGun <nipegun@gmail.com>
2025-10-22 16:29:39 +0200

    Create ComprobarSiPaqueteInstalado.sh

---
+++ InstDeSoftware/_/ComprobarSiPaqueteInstalado.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    sudo apt-get -y update
+    sudo apt-get -y install curl
+    echo ""
+  fi
