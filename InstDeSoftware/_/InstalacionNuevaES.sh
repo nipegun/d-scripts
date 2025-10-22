8416f5b1f8ae7ab9aab139bb0f193a666bf1f0af
NiPeGun <nipegun@gmail.com>
2025-10-22 16:28:54 +0200

    Update InstalacionNuevaES.sh

--- InstDeSoftware/_/InstalacionNuevaES.sh
+++ InstDeSoftware/_/InstalacionNuevaES.sh
@@ -42,21 +42,6 @@
     exit
   fi
 
-# Detectar si el script se está ejecutando dentro de un contendor LXC
-  if [ "$(systemd-detect-virt)" = "lxc" ]; then
-    echo "Contenedor LXC detectado"
-  fi
-
-# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
-  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
-    echo ""
-    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
-    echo ""
-    sudo apt-get -y update
-    sudo apt-get -y install curl
-    echo ""
-  fi
-
 # Determinar la versión de Debian
   if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
     . /etc/os-release
