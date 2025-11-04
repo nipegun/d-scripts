bfc2752f07a1e3ebebcf07ab9a3fe4ced596450b
NiPeGun <nipegun@gmail.com>
2025-11-04 18:38:34 +0100

    Update and rename Modem-MBIM-Query-All.sh to Modem-MBIM-Query-All0.sh

--- Sistema/Modem-MBIM-Query-All0.sh
+++ Sistema/Modem-MBIM-Query-All0.sh
@@ -8,29 +8,27 @@
 # ----------
 # Script de NiPeGun para obtener información del modem 4G mediante el protocolo MBIM
 #
-# Ejecución remota:
+# Ejecución remota (puede requerir permisos sudo):
 #   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/Sistema/Modem-MBIM-Query-All.sh | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/Sistema/Modem-MBIM-Query-All.sh | sed 's-sudo--g' | bash
 # ----------
 
-cColorAzul="\033[0;34m"
-cColorAzulClaro="\033[1;34m"
-cColorVerde='\033[1;32m'
-cColorRojo='\033[1;31m'
-cFinColor='\033[0m'
-
-# Comprobar si el script está corriendo como root
-  if [ $(id -u) -ne 0 ]; then
-    echo -e "${cColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${cFinColor}"
-    exit
-  fi
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  cFinColor='\033[0m'
 
 # Comprobar si el paquete libmbim-utils está instalado. Si no lo está, instalarlo.
   if [[ $(dpkg-query -s libmbim-utils 2>/dev/null | grep installed) == "" ]]; then
     echo ""
     echo -e "${cColorRojo}  libmbim-utils no está instalado. Iniciando su instalación...${cFinColor}"
     echo ""
-    apt-get -y update
-    apt-get -y install libmbim-utils
+    sudo apt-get -y update
+    sudo apt-get -y install libmbim-utils
     echo ""
   fi
 
