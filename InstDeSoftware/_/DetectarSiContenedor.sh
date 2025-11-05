6c26df79b872036ded77f10aee8de60459682b83
NiPeGun <nipegun@gmail.com>
2025-11-05 14:40:33 +0000

    Update DetectarSiContenedor.sh

--- InstDeSoftware/_/DetectarSiContenedor.sh
+++ InstDeSoftware/_/DetectarSiContenedor.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para detectar si el Debian es un contenedor
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
 # Detectar si el script se está ejecutando dentro de un contendor LXC
   if [ "$(systemd-detect-virt)" = "lxc" ]; then
     echo "Contenedor lxc detectado"
