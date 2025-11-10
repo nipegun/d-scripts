07a0d6355ad29b9950f1d30a7848ac09be02efba
NiPeGun <nipegun@gmail.com>
2025-11-10 09:31:08 +0100

    Update WiFi-Adaptador-Reiniciar.sh

--- Sistema/WiFi-Adaptador-Reiniciar.sh
+++ Sistema/WiFi-Adaptador-Reiniciar.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# ----------
+# Script de NiPeGun para reiniciar el adaptador WiFi
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/Sistema/WiFi-Adaptador-Reiniciar.sh | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/Sistema/WiFi-Adaptador-Reiniciar.sh | bash
+# ----------
+
 vNombreDelDisp='wlan0'
 vModKernelDelAdapt=$(lspci -kknn -d ::0280 | grep module | cut -d':' -f2 | sed 's- --g')
 
