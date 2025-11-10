2def25081f2f88f5194a772b4d1abb2021b8259a
NiPeGun <nipegun@gmail.com>
2025-11-11 00:38:39 +0100

    Update Servidor-VPN-Headscale-InstalarYConfigurar.sh

--- InstDeSoftware/ParaCLI/Servidor-VPN-Headscale-InstalarYConfigurar.sh
+++ InstDeSoftware/ParaCLI/Servidor-VPN-Headscale-InstalarYConfigurar.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 # Pongo a disposición pública este script bajo el término de "software de dominio público".
 # Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
