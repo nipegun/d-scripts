2fb2e70f3c5e5622f83d95643b74544da16dcab1
NiPeGun <nipegun@gmail.com>
2025-11-10 19:48:18 +0100

    Update Kismet-Instalar.sh

--- InstDeSoftware/ParaGUI/Kismet-Instalar.sh
+++ InstDeSoftware/ParaGUI/Kismet-Instalar.sh
@@ -59,6 +59,19 @@
     echo -e "${cColorRojo}    Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
     echo ""
 
+    # Borrar paques viejos
+      sudo rm -rfv /usr/local/bin/kismet* /usr/local/share/kismet* /usr/local/etc/kismet*
+
+    # Agregar el repo
+      wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null
+      echo 'deb [signed-by=/usr/share/keyrings/kismet-archive-keyring.gpg] https://www.kismetwireless.net/repos/apt/release/trixie trixie main' | sudo tee /etc/apt/sources.list.d/kismet.list >/dev/null
+
+    # Actualizar lista de paquetes disponibles en el repo
+      sudo apt-get -y update
+
+    # instalar
+      sudo apt-get -y install kismet
+
   elif [ $cVerSO == "12" ]; then
 
     echo ""
