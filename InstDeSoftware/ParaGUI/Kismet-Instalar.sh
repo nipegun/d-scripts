099df880ae68a31d08538609bafd110c8a715d00
NiPeGun <nipegun@gmail.com>
2025-11-10 19:39:43 +0100

    Create Kismet-Instalar.sh

---
+++ InstDeSoftware/ParaGUI/Kismet-Instalar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Borrar paques viejos
+  sudo rm -rfv /usr/local/bin/kismet* /usr/local/share/kismet* /usr/local/etc/kismet*
+
+# Agregar el repo
+  wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null
+  echo 'deb [signed-by=/usr/share/keyrings/kismet-archive-keyring.gpg] https://www.kismetwireless.net/repos/apt/release/bookworm bookworm main' | sudo tee /etc/apt/sources.list.d/kismet.list >/dev/null
+
+# Actualizar lista de paquetes disponibles en el repo
+  sudo apt-get -y update
+
+# instalar
+  sudo apt-get -y install kismet
+
