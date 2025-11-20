2d579edbcd0553556302a4849ae49b63ffd543f0
NiPeGun <nipegun@gmail.com>
2025-11-20 18:53:25 +0100

    Enhance script with remote execution instructions
    Added instructions for remote execution and editing.

--- ParaSoftware/WireGuard-ArchivoConf-AgregarAGnome.sh
+++ ParaSoftware/WireGuard-ArchivoConf-AgregarAGnome.sh
@@ -5,6 +5,25 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
+# ----------
+# Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
 # Definir constantes de color
   cColorAzul="\033[0;34m"
   cColorAzulClaro="\033[1;34m"
