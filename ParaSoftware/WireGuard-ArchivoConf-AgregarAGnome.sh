43f22c00e426ff36682209cfa999ecb3fd46f449
NiPeGun <nipegun@gmail.com>
2025-11-20 18:52:18 +0100

    Create WireGuard-ArchivoConf-AgregarAGnome.sh

---
+++ ParaSoftware/WireGuard-ArchivoConf-AgregarAGnome.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Definir la cantidad de argumentos esperados
+  cCantArgsEsperados=1
+
+# Comprobar que se hayan pasado la cantidad de argumentos esperados. Abortar el script si no.
+  if [ $# -ne $cCantArgsEsperados ]
+    then
+      echo ""
+      echo -e "${cColorRojo}  Mal uso del script. El uso correcto sería: ${cFinColor}"
+      echo ""
+      if [[ "$0" == "bash" ]]; then
+        vNombreDelScript="script.sh"
+      else
+        vNombreDelScript="$0"
+      fi
+      echo "    $vNombreDelScript [RutaAlArchivoConf]"
+      echo ""
+      echo "  Ejemplo:"
+      echo ""
+      echo "    $vNombreDelScript '/home/usuariox/Descargas/ConecWireg.conf'"
+      echo ""
+      exit
+  fi
+
+#Definir variables
+  vArchivoConf="$1"
+  vNomConex=$(basename -s .txt "$vArchivoConf")
+
+
+# Importar y activar la conexión
+  nmcli connection import type wireguard file "$vArchivoConf" || echo "  Algo anda mal con el archivo .conf. Puede ser su contenido o su nombre, dado que el nombre del archivo se utilizará como nombre de la interfaz."
+
+# Desactivar el auto-inicio de la conexión
+  nmcli connection modify "$vNomConex" connection.autoconnect no
+
