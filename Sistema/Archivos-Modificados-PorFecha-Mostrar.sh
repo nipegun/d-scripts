e3fc10993e8b1089c1580fe096d907b4ef62d0a5
NiPeGun <nipegun@gmail.com>
2025-12-01 22:53:34 +0100

    Update Archivos-Modificados-PorFecha-Mostrar.sh

--- Sistema/Archivos-Modificados-PorFecha-Mostrar.sh
+++ Sistema/Archivos-Modificados-PorFecha-Mostrar.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-vRuta='/home'
+# curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/Sistema/Archivos-Modificados-PorFecha-Mostrar.sh | sed 's-xxx-/home-g' | bash
+
+vRuta='xxx'
 
 find "$vRuta" -type f -printf "%T@ %p\n" | sort -nr
