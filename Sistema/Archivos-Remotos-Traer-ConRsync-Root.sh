d584221aab845de87631a089ef9187384fbe3e54
NiPeGun <nipegun@gmail.com>
2025-12-03 17:21:32 +0100

    Create Archivos-Remotos-Traer-ConRsync-Root.sh

---
+++ Sistema/Archivos-Remotos-Traer-ConRsync-Root.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para traer la carpeta /root de un sistema remoto mediante rsync en Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
+vIPRemota="$1"
+vUsuario="$2"
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Definir la cantidad de argumentos esperados
+  cCantArgsEsperados=2
+
+# Comprobar que se hayan pasado la cantidad de argumentos esperados. Abortar el script si no.
+  if [ $# -ne $cCantArgsEsperados ]
+    then
+      echo ""
+      echo -e "${cColorRojo}  Mal uso del script. El uso correcto sería: ${cFinColor}"
+      echo ""
+      if [[ "$0" == "bash" ]]; then
+        vNombreDelScript="script.sh"
+      else
+        vNombreDelScript="$0"
+      fi
+      echo "    $vNombreDelScript [Argumento1] [Argumentos2"
+      echo ""
+      echo "  Ejemplo:"
+      echo ""
+      echo "    $vNombreDelScript '10.10.76.111' 'arlina'"
+      echo ""
+      exit
+  fi
+
+# Traer todas las carpetas de dentro de /root
+  # rsync -avz --progress --exclude=".cache" "$vUsuario"@"$vIPRemota":/root/ .
+
+# Traer la carpeta /root entera
+  rsync -avz --progress --exclude=".cache" "$vUsuario"@"$vIPRemota":/root .
+
