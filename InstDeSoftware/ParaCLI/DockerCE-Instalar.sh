5969da0560f2290b6d5fa42a218341fac2d52b7e
NiPeGun <nipegun@gmail.com>
2025-12-31 20:06:23 +0100

    Update DockerCE-Instalar.sh

--- InstDeSoftware/ParaCLI/DockerCE-Instalar.sh
+++ InstDeSoftware/ParaCLI/DockerCE-Instalar.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para instalar Docker y Portainer en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/DockerCE-Instalar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/InstDeSoftware/ParaCLI/DockerCE-Instalar.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/SoftInst/ParaCLI/DockerCE-Instalar.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/InstDeSoftware/ParaCLI/DockerCE-Instalar.sh | sed 's-sudo--g' | bash
 # ----------
 
 # Definir constantes de color
