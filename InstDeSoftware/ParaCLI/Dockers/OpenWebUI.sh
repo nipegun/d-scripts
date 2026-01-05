e4adea99900c38a5698c9c2ef2a4996e43599db0
NiPeGun <nipegun@gmail.com>
2026-01-05 13:29:15 +0100

    Update OpenWebUI.sh

--- InstDeSoftware/ParaCLI/Dockers/OpenWebUI.sh
+++ InstDeSoftware/ParaCLI/Dockers/OpenWebUI.sh
@@ -45,7 +45,7 @@
       sudo apt-get -y install dialog
       echo ""
     fi
-  menu=(dialog --checklist "¿Donde quieres instalar OpenWebUI?:" 22 76 16)
+  menu=(dialog --radiolist "¿Donde quieres instalar OpenWebUI?:" 22 76 16)
     opciones=(
       1 "En un ordenador o máquina virtual" off
       2 "En un contenedor LXC de Proxmox"   off
