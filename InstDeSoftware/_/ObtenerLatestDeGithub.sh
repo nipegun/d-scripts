1f5db3b96edc594737d705f5e3ef829dc23053c2
NiPeGun <nipegun@gmail.com>
2026-01-06 19:31:27 +0100

    Update ObtenerLatestDeGithub.sh

--- InstDeSoftware/_/ObtenerLatestDeGithub.sh
+++ InstDeSoftware/_/ObtenerLatestDeGithub.sh
@@ -22,4 +22,10 @@
           sudo apt-get -y install jq
           echo ""
         fi
-      curl -s https://api.github.com/repos/Nombre/Repo/releases/latest | jq -r '.tag_name'
+      curl -s https://api.github.com/repos/USUARIO/NOMBREDELREPO/releases/latest | jq -r '.tag_name'
+
+    # Obtener assets
+      curl -sL https://api.github.com/repos/USUARIO/NOMBREDELREPO/releases/tags/25.12.1 | jq -r '.assets[].browser_download_url' | grep '\.zip$'
+      # Descargar todos los assets que acaban en .zip
+        cd /tmp/
+        curl -sL https://api.github.com/repos/USUARIO/NOMBREDELREPO/releases/tags/25.12.1 | jq -r '.assets[] | select(.name | endswith(".zip")) | .browser_download_url' | xargs -n1 wget -c
