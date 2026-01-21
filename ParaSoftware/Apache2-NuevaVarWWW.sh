eabbec4ddf78669973f40a31726aaa0834be9ee1
NiPeGun <nipegun@gmail.com>
2026-01-21 03:36:12 +0100

    Update and rename Apache-NuevaVarWWW.sh to Apache2-NuevaVarWWW.sh

--- ParaSoftware/Apache2-NuevaVarWWW.sh
+++ ParaSoftware/Apache2-NuevaVarWWW.sh
@@ -15,13 +15,47 @@
 #   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/ParaSoftware/Apache-NuevaVarWWW.sh | sed 's-sudo--g' | bash -s Extension Dominio Password
 # ----------
 
-cPassBD=""
-cDominio=""
-cExt=""
 
-cColorRojo='\033[1;31m'
-cColorVerde='\033[1;32m'
-cFinColor='\033[0m'
+
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
+# Definir constantes
+  cPassBD="$3"
+  cDominio="$2"
+  cExt="$1"
+
+# Definir constantes de color
+  cColorAzul='\033[0;34m'
+  cColorAzulClaro='\033[1;34m'
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
 
 # Definir la cantidad de argumentos esperados
   cCantArgsEsperados=3
@@ -47,6 +81,140 @@ cFinColor='\033[0m'
       echo ""
       exit
   fi
+
+# Determinar la versión de Debian
+  if [ -f /etc/os-release ]; then             # Para systemd y freedesktop.org.
+    . /etc/os-release
+    cNomSO=$NAME
+    cVerSO=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then # Para linuxbase.org.
+    cNomSO=$(lsb_release -si)
+    cVerSO=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then          # Para algunas versiones de Debian sin el comando lsb_release.
+    . /etc/lsb-release
+    cNomSO=$DISTRIB_ID
+    cVerSO=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then       # Para versiones viejas de Debian.
+    cNomSO=Debian
+    cVerSO=$(cat /etc/debian_version)
+  else                                        # Para el viejo uname (También funciona para BSD).
+    cNomSO=$(uname -s)
+    cVerSO=$(uname -r)
+  fi
+
+# Ejecutar comandos dependiendo de la versión de Debian detectada
+
+  if [ $cVerSO == "13" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de creación de nueva web de apache2 para Debian 13 (trixie)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "12" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de creación de nueva web de apache2 para Debian 12 (Bookworm)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 12 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "11" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de creación de nueva web de apache2 para Debian 11 (Bullseye)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 11 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "10" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de creación de nueva web de apache2 para Debian 10 (Buster)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 10 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "9" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de creación de nueva web de apache2 para Debian 9 (Stretch)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 9 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "8" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de creación de nueva web de apache2 para Debian 8 (Jessie)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 8 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  elif [ $cVerSO == "7" ]; then
+
+    echo ""
+    echo -e "${cColorAzulClaro}  Iniciando el script de creación de nueva web de apache2 para Debian 7 (Wheezy)...${cFinColor}"
+    echo ""
+
+    echo ""
+    echo -e "${cColorRojo}    Comandos para Debian 7 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
+    echo ""
+
+  fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Comprobar si el nombre de usuario MySQL deseado tiene mas de 16 caracteres
   nombre_mysql_deseado="$cDominio"
@@ -210,38 +378,39 @@ cFinColor='\033[0m'
       echo ""
       echo "    Creando la base de datos con su usuario..."
       echo ""
-      /root/scripts/d-scripts/MySQL-BaseDeDatos-Crear.sh "$cDominio" "$cDominio" "$cPassBD"
+      sudo /root/scripts/d-scripts/ParaSoftware/MySQL-BaseDeDatos-Crear.sh "$cDominio" "$cDominio" "$cPassBD"
 
     # Crear las carpetas de la web
-      mkdir -p /var/www/"$cDominio""$cExt"/_/logs/
-      echo "WEB OK" > /var/www/"$cDominio""$cExt"/index.html
+      sudo mkdir -p /var/www/"$cDominio""$cExt"/
+      sudo mkdir -p /var/www/"$cDominio""$cExt"-logs/
+      echo "WEB OK" | sudo tee /var/www/"$cDominio""$cExt"/index.html
 
     # Crear la web en Apache
-      cp /etc/apache2/sites-available/nuevawebvar.conf /etc/apache2/sites-available/"$cDominio""$cExt".conf
-      sed -i -e "s/nuevawebvar.com/"$cDominio""$cExt"/g" /etc/apache2/sites-available/"$cDominio""$cExt".conf
+      sudo cp /etc/apache2/sites-available/nuevawebvar.conf /etc/apache2/sites-available/"$cDominio""$cExt".conf
+      sudo sed -i -e "s/nuevawebvar.com/"$cDominio""$cExt"/g" /etc/apache2/sites-available/"$cDominio""$cExt".conf
 
     # Activar la configuración de la nueva Web en Apache
       echo ""
       echo "    Activando la web en apache..."
       echo ""
-      a2ensite "$cDominio""$cExt"
+      sudo a2ensite "$cDominio""$cExt"
 
     # Crear el certificado SSL, deteninendo Apache
       echo ""
       echo "    Creando el certificado SSL..."
       echo ""
       iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-      service apache2 stop
-      apt-get update
-      apt-get -y install certbot
-      apt-get -y install python3-certbot-apache
+      sudo systemctl apache2 stop
+      sudo apt-get -y update
+      sudo apt-get -y install certbot
+      sudo apt-get -y install python3-certbot-apache
       certbot --apache -d "$cDominio""$cExt" -d www."$cDominio""$cExt"
 
     # Volver a arrancar Apache
       echo ""
       echo "    Re-arrancando Apache..."
       echo ""
-      service apache2 start
+      sudo systemctl start apache2
 
     # Crear el archivo .htaccess con algunas opciones
       echo "# BEGIN Medidas de seguridad"                                              > /var/www/"$cDominio""$cExt"/.htaccess
