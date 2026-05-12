fb1592e95350653e9dff208e8d1b7dffd2ee6c8b
NiPeGun <nipegun@gmail.com>
2026-05-12 13:44:21 +0200

    Create Caches-Borrar.sh

---
+++ ParaSoftware/Caches-Borrar.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para borrar todos los caches de Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
+# /apt
+  sudo apt clean
+  sudo apt autoclean
+
+# /var/cache
+  sudo rm -rf /var/cache/apt/archives/*
+  sudo rm -rf /var/cache/apt/archives/partial/*
+  sudo rm -rf /var/cache/man/*
+  sudo rm -rf /var/cache/fontconfig/*
+  sudo rm -rf /var/cache/ldconfig/*
+  sudo rm -rf /var/cache/app-info/*
+  sudo rm -rf /var/cache/swcatalog/*
+  sudo rm -rf /var/cache/PackageKit/*
+  sudo rm -rf /var/cache/fwupd/*
+
+# /home/
+  sudo find /home -path '*/.cache/*' -mindepth 1 -delete
+
+# /root
+  sudo find /root/.cache -mindepth 1 -delete 2>/dev/null
+
