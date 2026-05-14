48006a262357a497d049639bed24c53f5c4d97ac
NiPeGun <nipegun@gmail.com>
2026-05-14 02:05:27 +0200

    Create RSync-ClonarLinux.sh

---
+++ ParaSoftware/RSync-ClonarLinux.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para clonar un linux con RSync en Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
+
+vCarpetaLinuxOrigen='/mnt/ibpve1PartLocal/'     # La / final es mandatoria
+vCarpetaLinuxDestino='/mnt/ibpve1PartLocalExt/' # La / final es mandatoria
+
+rsync -aAXHv --numeric-ids --info=progress2 --bwlimit=10M \
+  --exclude='/dev/*'                        \
+  --exclude='/proc/*'                       \
+  --exclude='/sys/*'                        \
+  --exclude='/run/*'                        \
+  --exclude='/tmp/*'                        \
+  --exclude='/mnt/*'                        \
+  --exclude='/media/*'                      \
+  --exclude='/lost+found'                   \
+  "$vCarpetaLinuxOrigen" "$vCarpetaLinuxDestino"
