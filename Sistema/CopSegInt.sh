d70a4a7f4144d98ad0f00afb6332cb0fb5829469
NiPeGun <nipegun@gmail.com>
2026-05-19 20:12:36 +0200

    Update CopSegInt.sh

--- Sistema/CopSegInt.sh
+++ Sistema/CopSegInt.sh
@@ -8,76 +8,41 @@
 # ----------
 # Script de NiPeGun para hacer copia de seguridad interna de Debian
 #
-# Ejecución remota:
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/CopSegInt.sh | bash
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/Sistema/CopSegInt.sh | bash
 #
-# Ejecución remota sin caché:
-#  curl -sL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/nipegun/d-scripts/master/CopSegInt-Servicios.sh | bash
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/Sistema/CopSegInt.sh | sed 's-sudo--g' | bash
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/Sistema/CopSegInt.sh | nano -
 # ----------
 
-ColorRojo='\033[1;31m'
-ColorVerde='\033[1;32m'
-FinColor='\033[0m'
+# Definir el archivo que contiene los datos a copiar
+  cArchivoConDatosACopiar='/root/DataToBackup.txt'
 
-vNombreDelServidor=$(cat /etc/hostname)
-vPuntoMontPartCopSeg="/Particiones/CopSeg/"
+# Definir constantes de color
+  cColorAzul='\033[0;34m'
+  cColorAzulClaro='\033[1;34m'
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
 
-# Determinar la fecha de ejecución del script
-  vFechaDeEjec=$(date +A%YM%mD%d@%T)
+# Definir el momento de ejecución del script
+  cFechaDeEjec=$(date +a%Ym%md%dh%Hm%Ms%S)
 
 # Notificar inicio de ejecución del script
   echo ""
-  echo -e "${ColorVerde}  Iniciando la copia de seguridad del los archivos de ProxmoxVE...${FinColor}"
+  echo -e "${cColorAzulClaro}  Iniciando la copia de seguridad interna el $cFechaDeEjec...${FinColor}"
   echo ""
 
-# Encender la luz del Blink1
-  #chmod +x /root/git/blink1-tool/blink1-tool
-  #/root/git/blink1-tool/blink1-tool --red > /dev/null
-
-# Montar disco de copias de seguridad
-  #echo ""
-  #echo -e "${ColorVerde}  Montando el disco de copias de seguridad...${FinColor}"
-  #echo ""
-  #umount /dev/disk/by-parlabel/PartCopSeg 2> /dev/null
-  #mount -t auto -v /dev/disk/by-partlabel/CopSeg "$vPuntoMontPartCopSeg"
-
-# Compartición Samba
-  mkdir -p               "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/samba/
-  cp /etc/samba/smb.conf "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/samba/
-
-# Transmission
-  mkdir -p                                  "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/transmission-daemon/
-  cp /etc/transmission-daemon/settings.json "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/transmission-daemon/
-
-# HostAPD
-  cp /etc/default/hostapd      "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/default/
-  mkdir -p                     "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/hostapd/
-  cp /etc/hostapd/hostapd.conf "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/hostapd/
-
-# HAProxy
-  mkdir -p                    "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/haproxy/
-  cp /etc/haproxy/haproxy.cfg "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/haproxy/
-
-# DDClient
-  cp /etc/default/ddclient "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/default/
-  cp /etc/ddclient.conf    "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/
-
-# DHCP
-  cp /etc/default/isc-dhcp-server "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/default/
-  mkdir -p                        "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/dhcp/
-  cp /etc/dhcp/dhcpd.conf         "$vPuntoMontPartCopSeg"ProxmoxVE/SO/"$vNombreDelServidor"/$vFechaDeEjec/etc/dhcp/
-
-# Desmontar el disco de copias de seguridad
-  echo ""
-  echo -e "${ColorVerde}  Desmontando el disco de copias de seguridad...${FinColor}"
-  echo ""
-  umount /dev/disk/by-partlabel/PartCopSeg
-
-# Apagar la luz del Blink1
-  #/root/git/blink1-tool/blink1-tool --off > /dev/null
+# Crear la carpeta de copia de seguridad
+  sudo mkdir -p /CopSegInt/"$cFechaDeEjec"/
 
 # Loguear tarea
-  echo "$vFechaDeEjec - Terminada la copia de seguridad del SO ProxmoxVE." >> /var/log/CopiasDeSeguridad.log
+  echo "$cFechaDeEjec - Terminada la copia de seguridad interna." >> /var/log/CopiasDeSeguridad.log
 
 # Notificar fin de ejecución del script
   echo ""
