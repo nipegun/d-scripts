8fb0f90adb410a570f1a9f04e9d6974e80a6a94a
NiPeGun <nipegun@gmail.com>
2026-06-16 13:44:24 +0200

    Create Hardware-Discos-SATA-Formatear-ABajoNivel.sh

---
+++ Sistema/Hardware-Discos-SATA-Formatear-ABajoNivel.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Definir la cantidad de argumentos esperados
+  cCantArgsEsperados=1
+
+# Comprobar que se hayan pasado la cantidad de argumentos esperados. Abortar el script si no.
+  if [ $# -ne $cCantArgsEsperados ]
+    then
+      echo ""
+      echo -e "${cColorRojo}  Mal uso del script. El uso correcto sería: ${cFinColor}"
+      echo ""
+      if [[ "$0" == "bash" ]]; then
+        vNombreDelScript="script.sh"
+      else
+        vNombreDelScript="$0"
+      fi
+      echo "    $vNombreDelScript [DeviceId]"
+      echo ""
+      echo "  Ejemplo:"
+      echo ""
+      echo "    $vNombreDelScript '/dev/sdh'"
+      echo ""
+      exit
+  fi
+
