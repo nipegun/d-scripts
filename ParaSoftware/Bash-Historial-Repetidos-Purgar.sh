5989380790475770909223c84c62e2a49e051903
NiPeGun <nipegun@gmail.com>
2026-07-26 06:45:11 +0200

    Create Bash-Historial-Repetidos-Purgar.sh

---
+++ ParaSoftware/Bash-Historial-Repetidos-Purgar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para eliminar líneas repetidas de .bash_history manteniendo el orden de ejecución
+#
+# Ejecución remota:
+#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/master/Web-Apache-CopSeg-Una.sh | bash
+# ----------
