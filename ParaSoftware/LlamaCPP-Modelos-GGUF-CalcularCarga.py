7a6e4aff5ab2f8af5edf5c9c6001912d85e5b382
NiPeGun <nipegun@gmail.com>
2026-07-26 07:36:01 +0200

    Delete ParaSoftware/LlamaCPP-Modelos-GGUF-CalcularCarga.py

--- ParaSoftware/LlamaCPP-Modelos-GGUF-CalcularCarga.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-# Ejecución remota
-#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/ParaSoftware/LlamaCPP-Modelos-GGUF-CalcularCarga.py | python3 - [ArchivoGGUF]
-
