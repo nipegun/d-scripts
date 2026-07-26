c58bb71d2c4738df7f961c20d7207b01ba66e823
NiPeGun <nipegun@gmail.com>
2026-07-26 07:35:29 +0200

    Update and rename LlamaCPP-Modelos-GGUF-Cargar1.py to LlamaCPP-Modelos-GGUF-CalcularCarga.py

---
+++ ParaSoftware/LlamaCPP-Modelos-GGUF-CalcularCarga.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# Ejecución remota
+#  curl -sL https://raw.githubusercontent.com/nipegun/d-scripts/refs/heads/master/ParaSoftware/LlamaCPP-Modelos-GGUF-CalcularCarga.py | python3 - [ArchivoGGUF]
+
