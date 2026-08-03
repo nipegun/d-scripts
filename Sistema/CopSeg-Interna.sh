5b4a8086dcd015624ca7d61693759b068cd3af2d
NiPeGun <nipegun@gmail.com>
2026-08-03 13:53:40 +0200

    Update CopSeg-Interna.sh

--- Sistema/CopSeg-Interna.sh
+++ Sistema/CopSeg-Interna.sh
@@ -108,7 +108,8 @@
   done
 
 # Corregir permisos de la carpeta
-  sudo chown root:root /CopSegInt -R
+  sudo chown root:root /CopSegInt
+  sudo chmod 700 /CopSegInt
 
 # Loguear tarea
   echo "$cFechaDeEjec - Terminada la copia de seguridad interna." | sudo tee -a /var/log/CopiasDeSeguridad.log > /dev/null
