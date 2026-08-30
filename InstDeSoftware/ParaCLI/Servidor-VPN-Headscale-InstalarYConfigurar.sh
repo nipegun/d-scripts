be50a217872a7408a0ef54d59bc7d536be4fb2ba
NiPeGun <nipegun@gmail.com>
2026-08-30 17:29:35 +0200

    Update Servidor-VPN-Headscale-InstalarYConfigurar.sh

--- InstDeSoftware/ParaCLI/Servidor-VPN-Headscale-InstalarYConfigurar.sh
+++ InstDeSoftware/ParaCLI/Servidor-VPN-Headscale-InstalarYConfigurar.sh
@@ -55,18 +55,50 @@
     echo -e "${cColorAzulClaro}  Iniciando el script de instalación de headscale para Debian 13 (x)...${cFinColor}"
     echo ""
 
-    vVersHeadscale="" # See above URL for latest version, e.g. "X.Y.Z" (NOTE: do not add the "v" prefix!)
-    vArquitectura="amd64" # Your system architecture, e.g. "amd64"
-    wget --output-document=/tmp/Headscale.deb "https://github.com/juanfont/headscale/releases/download/v${vVersHeadscale}/headscale_${vVersHeadscale}_linux_${vArquitectura}.deb"
+    # Obtener el tag de la última release del repo de Github
+      echo ""
+      echo "    Obteniendo el tag de la última release del repo de Github..."
+      echo ""
+      vUsuario='juanfont'
+      vNombreDelRepo='headscale'
+      vArquitectura="amd64"
+      # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+        if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+          echo ""
+          echo -e "${cColorRojo}      El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+          echo ""
+          sudo apt-get -y update
+          sudo apt-get -y install curl
+          echo ""
+        fi
+      # Comprobar si el paquete jq está instalado. Si no lo está, instalarlo.
+        if [[ $(dpkg-query -s jq 2>/dev/null | grep installed) == "" ]]; then
+          echo ""
+          echo -e "${cColorRojo}      El paquete jq no está instalado. Iniciando su instalación...${cFinColor}"
+          echo ""
+          sudo apt-get -y update
+          sudo apt-get -y install jq
+          echo ""
+        fi
+      vTagName=$(curl -s https://api.github.com/repos/"$vUsuario"/"$vNombreDelRepo"/releases/latest | jq -r '.tag_name')
+
+    # Obtener assets
+      vURLDelAsset=$(curl -sL https://api.github.com/repos/"$vUsuario"/"$vNombreDelRepo"/releases/tags/"$vTagName" | jq -r '.assets[].browser_download_url' | grep '\.deb$' | grep "$vArquitectura")
+      # Descargar todos los assets que acaban en .zip
+        cd /tmp/
+        curl -L "$vURLDelAsset" -o /tmp/headscale.deb
 
     # Instalar
-      sudo apt -y install /tmp/Headscale.deb
+      sudo apt -y install /tmp/headscale.deb
 
     # Editar el archivo principal
-      sudo nano /etc/headscale/config.yaml
+      sudo sed -i -e 's|127.0.0.1:8080|127.0.0.1:11080|g'          /etc/headscale/config.yaml
+      sudo sed -i -e 's|trusted_proxies: []|trusted_proxies: []|g' /etc/headscale/config.yaml
 
     # Activar e inniciar el servicio
       sudo systemctl enable --now headscale
+      sudo systemctl stop  headscale
+      sudo systemctl start headscale
 
     # Verificar que el servicio esté corriendo
       sudo systemctl status headscale
